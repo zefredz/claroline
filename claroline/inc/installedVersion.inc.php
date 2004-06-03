8c176d0e36e720a06b9f2ba79f38852c00bcc750
moosh <moosh@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2004-06-03 15:17:25 +0000

    - it's now RC1 and not beta

--- claroline/inc/installedVersion.inc.php
+++ claroline/inc/installedVersion.inc.php
@@ -1,11 +1,11 @@
 <?php // $Id$
 
 $stable = false;
-$clarolinePhase = "beta";
-$is_upgrade_available = false;
+$clarolinePhase = "RC1";
+$is_upgrade_available = true;
 
-$clarolineVersion 	= "1.5.0.beta";
-$versionDb 			= "1.5.0.beta";
+$clarolineVersion 	= "1.5.0.RC1";
+$versionDb 			= "1.5.0.RC1";
 
 
 if (!$stable)
