2107daab70deac25b62ccc314a38f689304b5b78
mathieu <mathieu@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2004-06-04 12:40:24 +0000

    Change condition stable by is_upgrade_available because if stable is false, upgrade is not possible.

--- claroline/inc/installedVersion.inc.php
+++ claroline/inc/installedVersion.inc.php
@@ -7,7 +7,7 @@ $is_upgrade_available = true;
 $version_file_cvs = "1.5.0.RC1";
 $version_db_cvs   = "1.5.0.RC1";
 
-if (!$stable)
+if (!$is_upgrade_available)
 {
 	$version_file_cvs = $version_file_cvs .".[unstable:".date("yzBs")."]";
 	$version_db_cvs	  = $version_db_cvs .".[unstable:".date("yzBs")."]";
@@ -16,6 +16,7 @@ if (!$stable)
 // to keep compatibility the two next value are set
 // but  it's same name than values in main conf.
 // code would be parse to be able to remove these two lines /
+
 $clarolineVersion = $version_file_cvs;
 $versionDb = $version_db_cvs;
 
