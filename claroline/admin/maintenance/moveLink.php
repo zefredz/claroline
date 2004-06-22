05d86409d085f3d5312d82d8fb067ff58f980325
mathieu <mathieu@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2004-06-22 08:52:13 +0000

    rewrite moveLink

--- claroline/admin/maintenance/moveLink.php
+++ claroline/admin/maintenance/moveLink.php
@@ -4,36 +4,49 @@
 	the table "link" is  moved to document in  file .url.
 */
 
-/****** MOVE FILES **********/
+/**
+ * include lib to move files 
+*/
+
 include_once($includePath."/lib/fileUpload.lib.php");
 include_once($includePath."/lib/fileManage.lib.php");
 
-$sql_get_links="
-SELECT id, url, titre, description
-FROM `".$tbl_links."`
-";
+$tbl_links = $currentCourseDbNameGlu."liens" ;
+$tbl_old_tool_list = $currentCourseDbNameGlu."accueil" ;
+$dbTable = $currentCourseDbNameGlu."document" ;
+$docRepository = $currentcoursePathSys."document/";
 
-$res = claro_sql_query($sql_get_links);
+/**
+ * select link
+*/ 
 
-if(mysql_errno()==0 && mysql_num_rows($res)>0)
+$sql_get_links=" SELECT id, url, titre, description
+                 FROM `".$tbl_links."` ";
+
+$result_links = claro_sql_query($sql_get_links);
+
+if(mysql_errno()==0 && mysql_num_rows($result_links)>0)
 {
-	$linkRepository = "links";
+	$linkRepository = $docRepository . "links";
 	
 	// create_unexisting_directory from fileUpload.lib.php
 	$linkRepository = create_unexisting_directory($linkRepository);
 	
-	if (!$linkRepository) echo "** creation of $linkRepository error";
-	$sql_get_visibility_of_tool_link = "
-	SELECT visible FROM `".$tbl_old_tool_list."` 
-	WHERE lien ='../claroline/link/link.php';";
+     	if (!$linkRepository) echo "** creation of $linkRepository error";
+        
+        $sql_get_visibility_of_tool_link = " SELECT visible FROM `".$tbl_old_tool_list."`" . 
+                                           " WHERE lien ='../claroline/link/link.php';";
+                                            
 	$result_linkVisibility = mysql_query($sql_get_visibility_of_tool_link);
 	$linkVisibility = ((mysql_fetch_array($result_linkVisibility) == 1) ?'v':'i');
+        
 	update_db_info('update', $linkRepository, array( 'visible' => $linkVisibility ) );
-
-	while ($linkToMove=myqsl_fetch_array($res))
+                
+	while ( $linkToMove = mysql_fetch_array($result_links, MYSQL_ASSOC))
 	{
 		$fileName = replace_dangerous_char($linkToMove['titre']);
 		$url = trim($linkToMove['url']);
+                
 		// check for "http://", if the user forgot "http://" or "ftp://" or ...
 		// the link will not be correct
 		if( !ereg( "://",$url ) )
@@ -45,39 +58,28 @@ if(mysql_errno()==0 && mysql_num_rows($res)>0)
 		if ( ! empty($fileName) && ! empty($url) )
 		{
 		    $linkFileExt = ".url";
-		    create_link_file( $baseWorkDir.$linkRepository.'/'.$fileName.$linkFileExt, 
+		    create_link_file( $linkRepository.'/'.$fileName.$linkFileExt, 
 		                      $url);
-							          $directoryName = dirname($file);
-	        
-	        if ( $directoryName == '/' || $directoryName == '\\' )
-	        {
-	            // When the dir is root, PHP dirname leaves a '\' for windows or a '/' for Unix
-	            $directoryName = '';
-	        }
-	        $newComment = trim($linkToMove['description']); // remove spaces
-			update_db_info('update', $file, array( 'comment' => $newComment ) );
-			$sql_get_links="
-			DELETE 
-			FROM `".$tbl_links."`
-			WHERE id = ".$linkToMove['id']."";
-			$res=mysql_query($sql_get_links);
+                    $newComment = trim($linkToMove['description']); // remove spaces
+                    update_db_info('update', $file, array( 'comment' => $newComment ) );
+                    $sql_get_links="DELETE 
+                                    FROM `".$tbl_links."`
+                                    WHERE id = ".$linkToMove['id']."";
+                    $res=mysql_query($sql_get_links);
 		}
 		else
 		{
-			$badUrl[]=$linkToMove;
+                    $badUrl[]=$linkToMove;
 		}
-		
-		
-		
 		
 	};
 	
 	if (is_array($badUrl))
 	{
 	
-	//	créer un fichier texte avec le contenu.
+                // create a text file with bad url
 		
-		$fileBadUrl = fopen($baseWorkDir.$linkRepository.'/url.txt');
+		$fileBadUrl = fopen($linkRepository.'/url.txt');
 		foreach($badurl as  $linkInfo)
 		{
 			fwrite("== ".$linkInfo['url']." ==",$fileBadUrl);
