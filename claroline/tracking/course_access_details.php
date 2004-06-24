b6b2d9aa7d21fcb44e2ccf7b3a8fd1bf6017c473
seb <seb@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2004-06-24 10:18:20 +0000

    clean header use claro_disp_tool_title

--- claroline/tracking/course_access_details.php
+++ claroline/tracking/course_access_details.php
@@ -1,32 +1,13 @@
 <?php 
 /*
       +----------------------------------------------------------------------+
-      | CLAROLINE version 1.3.0 $Revision$                            |
+      | CLAROLINE version 1.5.*			                            |
       +----------------------------------------------------------------------+
       | Copyright (c) 2001, 2002 Universite catholique de Louvain (UCL)      |
       +----------------------------------------------------------------------+
       |   $Id$         |
       +----------------------------------------------------------------------+
-      |   This program is free software; you can redistribute it and/or      |
-      |   modify it under the terms of the GNU General Public License        |
-      |   as published by the Free Software Foundation; either version 2     |
-      |   of the License, or (at your option) any later version.             |
-      |                                                                      |
-      |   This program is distributed in the hope that it will be useful,    |
-      |   but WITHOUT ANY WARRANTY; without even the implied warranty of     |
-      |   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the      |
-      |   GNU General Public License for more details.                       |
-      |                                                                      |
-      |   You should have received a copy of the GNU General Public License  |
-      |   along with this program; if not, write to the Free Software        |
-      |   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA          |
-      |   02111-1307, USA. The GNU GPL license is also available through     |
-      |   the world-wide-web at http://www.gnu.org/copyleft/gpl.html         |
-      +----------------------------------------------------------------------+
-      | Authors: Thomas Depraetere <depraetere@ipm.ucl.ac.be>                |
-      |          Hugues Peeters    <peeters@ipm.ucl.ac.be>                   |
-      |          Christophe Gesché <gesche@ipm.ucl.ac.be>                    |
-      |          Sebastien Piraux  <piraux_seb@hotmail.com>
+      |   Authors : see CREDITS.txt					|
       +----------------------------------------------------------------------+
  */
  
@@ -57,10 +38,9 @@ $TABLETRACK_ACCESS = $_course['dbNameGlu']."track_e_access";
 
 $is_allowedToTrack = $is_platformAdmin || $is_courseAdmin;
 
+claro_disp_tool_title($nameTools);
+
 ?>
-<h3>
-    <?php echo $nameTools ?>
-</h3>
 <table width="100%" cellpadding="2" cellspacing="3" border="0">
 <?php
     if( $is_allowedToTrack && $is_trackingEnabled)
