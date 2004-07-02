e5ce0413698b1344498d97275eb481cb356db05e
guillaume <guillaume@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2004-07-02 08:09:55 +0000

    orthographe

--- claroline/lang/french/trad4all.inc.php
+++ claroline/lang/french/trad4all.inc.php
@@ -122,7 +122,7 @@ $lang_if_you_wish_to_enroll_to_this_course = "Si vous souhaitez vous inscrire à 
 
 
 // TOOLS NAMES
-$langCourseHome = "Acceuil";
+$langCourseHome = "Accueil";
 
 $langAgenda="Agenda";
 $langLink="Liens";
