2efa9f778138a14d7829eb7a6bbf9e4c9a13de00
seb <seb@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2004-07-14 14:02:31 +0000

    use $_REQUEST for a var (needed for the $_REQUEST modif in exercice_admin.php)

--- claroline/exercice/admin.php
+++ claroline/exercice/admin.php
@@ -298,7 +298,7 @@ if(!$newQuestion && !$modifyQuestion && !$editQuestion && !$modifyAnswers)
 	// exercise management
 	include('exercise_admin.inc.php');
 
-	if(!$modifyExercise)
+	if(!$_REQUEST['modifyExercise'])
 	{
 		// question list management
 		include('question_list_admin.inc.php');
