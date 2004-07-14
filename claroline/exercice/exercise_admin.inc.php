ec56f25d67f169369be1f599d2798a1b48e26658
seb <seb@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2004-07-14 14:04:53 +0000

    use $_REQUEST add change forms element to modify new exercise properties (max time, max tries, shon to anonymous, show answers)

--- claroline/exercice/exercise_admin.inc.php
+++ claroline/exercice/exercise_admin.inc.php
@@ -29,41 +29,56 @@ if(!defined('ALLOWED_TO_INCLUDE'))
 }
 
 // the exercise form has been submitted
-if($submitExercise)
+if($_REQUEST['submitExercise'])
 {
-	$exerciseTitle=trim($exerciseTitle);
-	$exerciseDescription=trim($exerciseDescription);
-	$randomQuestions=$randomQuestions?$questionDrawn:0;
+	$exerciseTitle=trim($_REQUEST['exerciseTitle']);
+	$exerciseDescription=trim($_REQUEST['exerciseDescription']);
+	$randomQuestions=$_REQUEST['randomQuestions']?$_REQUEST['questionDrawn']:0;
 
 	// no title given
-	if(empty($exerciseTitle))
+	if(empty($_REQUEST['exerciseTitle']))
 	{
 		$msgErr=$langGiveExerciseName;
 	}
 	else
 	{
-		$objExercise->updateTitle($exerciseTitle);
-		$objExercise->updateDescription($exerciseDescription);
-		$objExercise->updateType($exerciseType);
+		$objExercise->updateTitle($_REQUEST['exerciseTitle']);
+		$objExercise->updateDescription($_REQUEST['exerciseDescription']);
+		$objExercise->updateType($_REQUEST['exerciseType']);
+		$objExercise->set_max_time($_REQUEST['exerciseMaxTime']);
+		$objExercise->set_max_tries($_REQUEST['exerciseMaxTries']);
+		if($_REQUEST['exerciseShowAnon'] == "show") 
+		{
+			$objExercise->set_show_anon();	
+		}
+		else
+		{
+			$objExercise->set_hide_anon();
+		}
+		$objExercise->set_show_answer($_REQUEST['exerciseShowAnswer']);
 		$objExercise->setRandom($randomQuestions);
 		$objExercise->save();
 
 		// reads the exercise ID (only usefull for a new exercise)
 		$exerciseId=$objExercise->selectId();
 
-		unset($modifyExercise);
+		unset($_REQUEST['modifyExercise']);
 	}
 }
 else
 {
-	$exerciseTitle=$objExercise->selectTitle();
-	$exerciseDescription=$objExercise->selectDescription();
-	$exerciseType=$objExercise->selectType();
-	$randomQuestions=$objExercise->isRandom();
+	$exerciseTitle		= $objExercise->selectTitle();
+	$exerciseDescription= $objExercise->selectDescription();
+	$exerciseType		= $objExercise->selectType();
+	$randomQuestions	= $objExercise->isRandom();
+	$maxTime			= $objExercise->get_max_time();
+	$maxTries			= $objExercise->get_max_tries();
+	$showAnon			= $objExercise->get_show_anon();
+	$showAnswer			= $objExercise->get_show_answer();
 }
 
 // shows the form to modify the exercise
-if($modifyExercise)
+if($_REQUEST['modifyExercise'])
 {
 ?>
 
@@ -109,6 +124,42 @@ if($modifyExercise)
       <input type="radio" name="exerciseType" id="exerciseType2" value="2" <?php if($exerciseType >= 2) echo 'checked="checked"'; ?>> <label for="exerciseType2"><?php echo $langSequentialExercise; ?></td></label>
 </tr>
 
+<tr>
+  <td><label for="exerciseMaxTime"><?php echo $langAllowedTime; ?> :</label></td>
+  <td>
+	<input type="text" name="exerciseMaxTime" id="exerciseMaxTime" size="4" maxlength="4" value="<?php echo $maxTime; ?>">
+	( '0' pour 'aucune limite' )
+  </td>
+</tr>
+
+<tr>
+  <td><label for="exerciseMaxTries"><?php echo $langAllowedAttempts; ?> :</label></td>
+  <td>
+	<input type="text" name="exerciseMaxTries" id="exerciseMaxTries" size="2" maxlength="2" value="<?php echo $maxTries; ?>">
+	( '0' pour 'nombre de tentatives illimité' )
+  </td>
+</tr>
+
+<tr>
+  <td valign="top"><?php echo $langAnonymousVisibility; ?> : </td>
+  <td>	<input type="radio" name="exerciseShowAnon" id="showAnon" value="show" <?php if($showAnon) echo 'checked="checked"'; ?>>
+		<label for="showAnon"><?php echo $langShow; ?></label>
+		<br />
+  		<input type="radio" name="exerciseShowAnon" id="hideAnon" value="hide" <?php if(!$showAnon) echo 'checked="checked"'; ?>>
+		<label for="hideAnon"><?php echo $langHide; ?></label>
+  </td>
+</tr>
+
+<tr>
+  <td><label for="exerciseShowAnswer"><?php echo $langShowAnswers; ?> : </label></td>
+  <td><select name="exerciseShowAnswer" id="exerciseShowAnswer">
+		<option value="ALWAYS" <?php if($showAnswer == 'ALWAYS') echo 'selected="selected"';?>><?php echo $langAlways; ?></option>
+		<option value="NEVER" <?php if($showAnswer == 'NEVER') echo 'selected="selected"'; ?>><?php echo $langNever; ?></option>
+		<option value="LASTTRY" <?php if($showAnswer == 'LASTTRY') echo 'selected="selected"'; ?>><?php echo $langAfterLastTry; ?></option>
+		<option value="ENDDATE" <?php if($showAnswer == 'ENDDATE') echo 'selected="selected"'; ?>><?php echo $langAfterEndDate; ?></option>
+		</select>
+  </td>
+</tr>
 <?php
 	if($exerciseId && $nbrQuestions)
 	{
@@ -140,8 +191,8 @@ if($modifyExercise)
 
 <tr>
   <td colspan="2" align="center">
-	<input type="submit" name="cancelExercise" value="<?php echo $langCancel; ?>">
-	&nbsp;&nbsp;<input type="submit" name="submitExercise" value="<?php echo $langOk; ?>">
+	<input type="submit" name="submitExercise" value="<?php echo $langOk; ?>">
+	&nbsp;&nbsp;<input type="submit" name="cancelExercise" value="<?php echo $langCancel; ?>">
   </td>
 </tr>
 </table>
