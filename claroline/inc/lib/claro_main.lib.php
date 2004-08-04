5543b55ef013c3574adbcfea7793729026ea1622
seb <seb@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2004-08-04 13:16:36 +0000

    Add new function : claro_disp_localised_date, use the time formats defined in trad4all to correctly display the date, with day and month name in the good language

--- claroline/inc/lib/claro_main.lib.php
+++ claroline/inc/lib/claro_main.lib.php
@@ -917,6 +917,33 @@ function claro_disp_progress_bar ($progress, $factor)
 	return $progressBar;
 }
 
+/**
+
+ * @author Christophe Gesché <gesche@ipm.ucl.ac.be>
+ * @param formatOfDate
+         see http://www.php.net/manual/en/function.strftime.php
+         for syntax to use for this string
+         I suggest to use the format you can find in trad4all.inc.php files
+ * @param timestamp timestamp of date to format
+ * @desc        display a date at localized format
+ */
+function claro_disp_localised_date($formatOfDate,$timestamp = -1) //PMAInspiration :)
+{
+	$langMonthNames			= $GLOBALS["langMonthNames"];
+	$langDay_of_weekNames	= $GLOBALS["langDay_of_weekNames"];
+	if ($timestamp == -1)
+	{
+		$timestamp = time();
+	}
+	// avec un ereg on fait nous même le replace des jours et des mois
+	// with the ereg  we  replace %aAbB of date format
+	//(they can be done by the system when  locale date aren't aivailable
+	$date = ereg_replace('%[A]', $langDay_of_weekNames["long"][(int)strftime('%w', $timestamp)], $formatOfDate);
+	$date = ereg_replace('%[a]', $langDay_of_weekNames["short"][(int)strftime('%w', $timestamp)], $date);
+	$date = ereg_replace('%[B]', $langMonthNames["long"][(int)strftime('%m', $timestamp)-1], $date);
+	$date = ereg_replace('%[b]', $langMonthNames["short"][(int)strftime('%m', $timestamp)-1], $date);
+	return strftime($date, $timestamp);
+}
 
 /**
  * Insert a	sort of	HTML Wysiwyg textarea inside a FORM
