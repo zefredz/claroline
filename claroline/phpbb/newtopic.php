ac5f7135b02a006cbd3d8e1292aac0e006990e94
peeters <peeters@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2004-08-13 19:10:31 +0000

    Refactoirng

--- claroline/phpbb/newtopic.php
+++ claroline/phpbb/newtopic.php
@@ -70,198 +70,200 @@ $forum_id 			= $forum;
 // or -- if it is attached --, check the user 
 // is allowed to see the current group forum.
 
-if (     is_null($myrow['idGroup']) 
-    || ( $myrow['idGroup'] == $_gid && $is_groupAllowed) )
+/* 
+ * Check if the topic isn't attached to a group,  or -- if it is attached --, 
+ * check the user is allowed to see the current group forum.
+ */
+
+if (   ! is_null($forumSettingList['idGroup']) 
+    && ( $forumSettingList['idGroup'] != $_gid || ! $is_groupAllowed) )
 {
-	if($submit)
-	{
-		$subject = strip_tags($subject);
-		
-		if(trim( strip_tags($message)) == '' || trim($subject) == '')
-		{
-			error_die($l_emptymsg);
-		}
-		
-		// set as anonymous phpBB user. we try to do it better soon
-		// Claroline team
-			
-		$userdata = array("user_id" => -1);
-		
-		// Commented by the Claroline team
-		//
-		// Either valid user/pass, or valid session. continue with post.. but first:
-		// Check that, if this is a private forum, the current user can post here.
-		//
-		// if ($forum_type == 1)
-		// {
-		//		if (!check_priv_forum_auth($userdata[user_id], $forum, TRUE, $db))
-		// 		{
-		//			error_die("$l_privateforum $l_nopost");
-		//		}
-		// }
+    // NOTE : $forumSettingList['idGroup'] != $_gid is necessary to prevent any hacking 
+    // attempt like rewriting the request without $cidReq. If we are in group 
+    // forum and the group of the concerned forum isn't the same as the session 
+    // one, something weird is happening, indeed ...
 
-		$is_html_disabled = false;
-	
-		if($allow_html == 0 || isset($html))
-		{
-			$message          = htmlspecialchars($message);
-			$is_html_disabled = true;
-		}
-
-		if($allow_bbcode == 1 && !($HTTP_POST_VARS[bbcode]))
-		{
-			$message = bbencode($message, $is_html_disabled);
-		}
-
-		// MUST do make_clickable() and smile() before changing \n into <br>.
-		
-		$message = make_clickable($message);
-		
-		if(!$smile)
-		{
-			$message = smile($message);
-		}
-
-		$message   = str_replace("\n", "<BR>", $message);
-		$message   = str_replace("<w>", "<s><font color=red>", $message);
-	    $message   = str_replace("</w>", "</font color></s>", $message);
-	    $message   = str_replace("<r>", "<font color=#0000FF>", $message);
-	    $message   = str_replace("</r>", "</font color>", $message);
-	
-		$message   = censor_string($message, $db);
-		$message   = addslashes($message);
-		$subject   = strip_tags($subject);
-		$subject   = censor_string($subject, $db);
-		$subject   = addslashes($subject);
-		$poster_ip = $REMOTE_ADDR;
-		$time      = date('Y-m-d H:i');
+    die ('<center>not allowed</center>');
+}
 
 
-		// ADDED BY Thomas 20.2.2002
 
-		$userLastName    = addslashes($userLastName);
-		$userFirstName = addslashes($userFirstName);
+if($submit)
+{
+    $subject = strip_tags($subject);
+    
+    if(trim( strip_tags($message)) == '' || trim($subject) == '')
+    {
+        error_die($l_emptymsg);
+    }
+    
+    // set as anonymous phpBB user. we try to do it better soon
+    // Claroline team
+        
+    $userdata = array('user_id' => -1);
+    
+    // Commented by the Claroline team
+    //
+    // Either valid user/pass, or valid session. continue with post.. but first:
+    // Check that, if this is a private forum, the current user can post here.
+    //
+    // if ($forum_type == 1)
+    // {
+    //		if (!check_priv_forum_auth($userdata[user_id], $forum, TRUE, $db))
+    // 		{
+    //			error_die("$l_privateforum $l_nopost");
+    //		}
+    // }
 
-		// END ADDED BY THOMAS
+    $is_html_disabled = false;
 
-		// to prevent [addsig] from getting in the way, 
-		// let's put the sig insert down here.
-		
-		if($sig && $userdata["user_id"] != -1)
-		{
-			$message .= "\n[addsig]";
-		}
-	
-		$sql = "INSERT INTO `".$tbl_topics."` 
-		        SET topic_title  = '".$subject."', 
-				    topic_poster = '".$userdata['user_id']."', 
-				    forum_id     = '".$forum."', 
-				    topic_time   = '".$time."', 
-				    topic_notify = 1,
-			 	    nom          = '".$userLastName."', 
-				    prenom       = '".$userFirstName."'";
+    if($allow_html == 0 || isset($html))
+    {
+        $message          = htmlspecialchars($message);
+        $is_html_disabled = true;
+    }
 
-	$result = mysql_query($sql, $db) 
-			  or error_die("Couldn't enter topic in database.");
-	
-	$topic_id = mysql_insert_id();
-	
-	$sql = "INSERT INTO `".$tbl_posts."`
-			SET topic_id  = '".$topic_id."', 
-			    forum_id  = '".$forum."', 
-			    poster_id = '".$userdata[user_id]."', 
-			    post_time = '".$time."', 
-			    poster_ip = '".$poster_ip."', 
-			    nom       = '".$userLastName."', 
-			    prenom    = '".$userFirstName."'";
-	
-	$result = mysql_query($sql) 
-			  or error_die("Couldn't enter post in database.");
+    if($allow_bbcode == 1 && !($HTTP_POST_VARS[bbcode]))
+    {
+        $message = bbencode($message, $is_html_disabled);
+    }
 
-	$post_id = mysql_insert_id();
-	
-	if($post_id)
-	{
-		$sql = "INSERT INTO `".$tbl_posts_text."` 
-		        SET post_id   = '".$post_id."', 
-		            post_text = '".$message."'";
-		
-		$result = mysql_query($sql) 
-		          or error_die("Could not enter post text!");
+    // MUST do make_clickable() and smile() before changing \n into <br>.
+    
+    $message = make_clickable($message);
+    
+    if( ! $smile) $message = smile($message);
 
-		$sql = "UPDATE `".$tbl_topics."` 
-		        SET   topic_last_post_id = '".$post_id."' 
-				WHERE topic_id = '".$topic_id."'";
+    $message   = str_replace("\n", "<BR>", $message);
 
-		$result = mysql_query($sql) 
-				  or error_die("Could not update topics table!");
-	}
+    $message   = censor_string($message, $db);
+    $message   = addslashes($message);
+    $subject   = strip_tags($subject);
+    $subject   = censor_string($subject, $db);
+    $subject   = addslashes($subject);
+    $poster_ip = $REMOTE_ADDR;
+    $time      = date('Y-m-d H:i');
 
 
-	if($userdata[user_id] != -1)
-	{
-		$sql = "UPDATE `$tbl_users` 
-		        SET user_posts=user_posts+1 
-		        WHERE user_id = '".$userdata['user_id']."'";
-		
-		$result = mysql_query($sql) 
-				  or error_die("Couldn't update users post count.");
-	}
-	
-	$sql = "UPDATE `".$tbl_forums."` 
-            SET   forum_posts        = forum_posts+1, 
-                  forum_topics       = forum_topics+1, 
-                  forum_last_post_id = '".$post_id."' 
-            WHERE forum_id           = '".$forum."'";
+    // ADDED BY Thomas 20.2.2002
 
-	$result = mysql_query($sql, $db) 
-			  or error_die("Couldn't update forums post count.");
-			  
-	$topic       = $topic_id;
-	$total_topic = get_total_posts($topic, $db, "topic")-1;
-	
-	// Subtract 1 because we want the nr of replies, not the nr of posts.
-	$forward = 1;
-	
-	include('page_header.php');
-	
-	echo	"<br>",
-	
-			"<table border=\"0\" cellpadding=\"1\" cellspacing=\"0\" ",
-			"align=\"center\" valign=\"top\" width=\"$tablewidth\">",
-			
-			"<tr align=\"left\">",
-			"<td>",
-			"<center>",
-			$l_stored,
-			"<p>",$l_click,
-			" <a href=\"viewtopic.$phpEx?topic=$topic_id&forum=$forum\">",
-			$l_here,
-			"</a> ",
-			$l_viewmsg,"<p>",
-			$l_click,
-			" <a href=\"viewforum.$phpEx?forum=$forum_id\">",
-			$l_here,
-			"</a> ", 
-			$l_returntopic,
-			"</center>",
-			"</td>",
-			"</tr>",
-			
-			"</table>";
-			
-	} // end if submit
-	else
-	{
-		include('page_header.php');
-	
-		// ADDED BY CLAROLINE: exclude non identified visitors
-		if (!$_uid AND !$fakeUid)
-		{
-			die("<center><br><br><font face=\"arial, helvetica\" size=2>$langLoginBeforePost1<br>
-				$langLoginBeforePost2<a href=../../index.php>$langLoginBeforePost3.</a></center>");
-		}
-		// END ADDED BY CLAROLINE exclude visitors unidentified
+    $userLastName    = addslashes($userLastName);
+    $userFirstName   = addslashes($userFirstName);
+
+    // END ADDED BY THOMAS
+
+    // to prevent [addsig] from getting in the way, 
+    // let's put the sig insert down here.
+    
+    if($sig && $userdata['user_id'] != -1)
+    {
+        $message .= "\n[addsig]";
+    }
+
+    $sql = "INSERT INTO `".$tbl_topics."` 
+            SET topic_title  = '".$subject."', 
+                topic_poster = '".$userdata['user_id']."', 
+                forum_id     = '".$forum."', 
+                topic_time   = '".$time."', 
+                topic_notify = 1,
+                nom          = '".$userLastName."', 
+                prenom       = '".$userFirstName."'";
+
+    $result = claro_sql_query($sql);
+
+    $topic_id = mysql_insert_id();
+
+    $sql = "INSERT INTO `".$tbl_posts."`
+            SET topic_id  = '".$topic_id."', 
+                forum_id  = '".$forum."', 
+                poster_id = '".$userdata['user_id']."', 
+                post_time = '".$time."', 
+                poster_ip = '".$poster_ip."', 
+                nom       = '".$userLastName."', 
+                prenom    = '".$userFirstName."'";
+
+    $post_id = claro_sql_query_insert_id($sql);
+
+    if($post_id)
+    {
+        $sql = "INSERT INTO `".$tbl_posts_text."` 
+                SET post_id   = '".$post_id."', 
+                    post_text = '".$message."'";
+        
+        $result = claro_sql_query($sql);
+
+        $sql = "UPDATE `".$tbl_topics."` 
+                SET   topic_last_post_id = '".$post_id."' 
+                WHERE topic_id = '".$topic_id."'";
+
+        $result = claro_sql_query($sql);
+    }
+
+
+if($userdata['user_id'] != -1)
+{
+    $sql = "UPDATE `".$tbl_users."` 
+            SET user_posts=user_posts+1 
+            WHERE user_id = '".$userdata['user_id']."'";
+    
+    $result = claro_sql_query($sql);
+}
+
+$sql = "UPDATE `".$tbl_forums."` 
+        SET   forum_posts        = forum_posts+1, 
+              forum_topics       = forum_topics+1, 
+              forum_last_post_id = '".$post_id."' 
+        WHERE forum_id           = '".$forum."'";
+
+$result = claro_sql_query($sql);
+          
+$topic       = $topic_id;
+$total_topic = get_total_posts($topic, $db, 'topic')-1;
+
+// Subtract 1 because we want the nr of replies, not the nr of posts.
+$forward = 1;
+
+include('page_header.php');
+
+    echo "<br>"
+
+        ."<table border=\"0\" cellpadding=\"1\" cellspacing=\"0\" "
+        ."align=\"center\" valign=\"top\" width=\"".$tablewidth."\">"
+        
+        ."<tr align=\"left\">"
+        ."<td>"
+        ."<center>"
+        .$l_stored
+        ."<p>"
+        .$l_click
+        ." <a href=\"viewtopic.$phpEx?topic=".$topic_id."&forum=".$forum."\">"
+        .$l_here
+        ."</a> "
+        .$l_viewmsg
+        ."<p>"
+        .$l_click
+        ." <a href=\"viewforum.$phpEx?forum=$forum_id\">"
+        .$l_here
+        ."</a> " 
+        .$l_returntopic
+        ."</center>"
+        ."</td>"
+        ."</tr>"
+        
+        ."</table>";
+
+} // end if submit
+else
+{
+    include('page_header.php');
+
+    // ADDED BY CLAROLINE: exclude non identified visitors
+    if (!$_uid && ! $fakeUid)
+    {
+        die("<center><br><br><font face=\"arial, helvetica\" size=2>$langLoginBeforePost1<br>
+            $langLoginBeforePost2<a href=../../index.php>".$langLoginBeforePost3.".</a></center>");
+    }
+    // END ADDED BY CLAROLINE exclude visitors unidentified
 ?>
 
 <form action="<?php echo $php_self?>" method="post">
@@ -305,12 +307,7 @@ if (     is_null($myrow['idGroup'])
 </form>
 
 <?php
-	}
 }
-else
-{
-	header("Location: index.php");
-	exit();
-}
+
 require('page_tail.php');
 ?>
