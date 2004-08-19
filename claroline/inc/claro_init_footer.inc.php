00f6f985c6ac70b45df1e056a2b6a53249168508
peeters <peeters@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2004-08-19 08:43:32 +0000

    remove small tags from the footer. This is now manage by the css sheet.

--- claroline/inc/claro_init_footer.inc.php
+++ claroline/inc/claro_init_footer.inc.php
@@ -21,10 +21,9 @@ if(isset($_cid))
 {
 ?>
 <td width="30%">
-<small>
 <?php echo $lang_footer_CourseManager ?> : <a href="<?php echo (empty($_course['email'])?$clarolineRepositoryWeb."user/user.php":"mailto:".$_course['email']."?body=".$_course['officialCode']."&subject=[".rawurlencode($siteName)."]") ?>">
 <?php echo $_course['titular'] ?></a>
-</small>
+
 </td>
 <td align="center" width="*">
 <?php
@@ -37,15 +36,15 @@ else
 }
 ?>
 
-<small>
+
 <?php echo $langManager." ".$siteName; ?> : <a href="mailto:<?php echo $administrator["email"]."?body=".$_course['officialCode']."&subject=[".rawurlencode($siteName)."]" ?>">
 <?php echo $administrator["name"] ?></a>
-</small>
+
 </td>
 <td align="right" width="30%">
-<small>
+
 <?php echo $langPlatform ?> <a href="http://www.claroline.net" target="_blank">Claroline</a> &copy; 2001 - 2004
-</small>
+
 </td>
 </tr>
 </table>
