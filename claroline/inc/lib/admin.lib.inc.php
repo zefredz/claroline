f99158c3fb1cb9d4255916ba50f38eef05a16404
guillaume <guillaume@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2004-08-31 07:45:08 +0000

    CSV import script, functions needed

--- claroline/inc/lib/admin.lib.inc.php
+++ claroline/inc/lib/admin.lib.inc.php
@@ -32,21 +32,454 @@
  */
 
 $tbl_category           = $mainDbName.'`.`faculte';
-
 $tbl_course             = $mainDbName.'`.`cours';
 $tbl_courses            = $mainDbName.'`.`cours';
-
 $tbl_courseUser         = $mainDbName.'`.`cours_user';
 $tbl_user               = $mainDbName.'`.`user';
 $tbl_courses_nodes      = $mainDbName.'`.`faculte';
 $tbl_admin              = $mainDbName.'`.`admin';
-$tbl_track_default    = $statsDbName."`.`track_e_default";
-$tbl_track_login    = $statsDbName."`.`track_e_login";
-
+$tbl_track_default      = $statsDbName."`.`track_e_default";
+$tbl_track_login        = $statsDbName."`.`track_e_login";
 
 include_once($includePath."/lib/fileManage.lib.php");
 
+/**
+ * @author Hugues Peeters <peeters@ipm.ucl.ac.be>
+ * INVERT A MATRIX function :
+ * 
+ * this function allows to invert cols and rows of a 2D array
+ */
+
+function array_swap_cols_and_rows( $OrigMatrix, $presumedColKeyList)
+{
+    $RevertedMatrix = array();
+
+    foreach($OrigMatrix as $thisRow)
+    {
+        $ActualColKeyList = array();
+
+        foreach($thisRow as $thisColKey => $thisColValue)
+        {
+            $RevertedMatrix[$thisColKey][] = $thisColValue;
+
+            $actualColKeyList[] = $thisColKey;
+        }
+
+        // IN case of missing columns, fill them with NULL
+
+        $missingColKeyList = array_diff($presumedColKeyList, $actualColKeyList);
+
+        if (count($missingColKeyList) > 0)
+        {
+            foreach($missingColKeyList as $thisColKey)
+            {
+                $RevertedMatrix[$thisColKey][] = NULL;
+            }
+           
+        }
+    }
+    return $RevertedMatrix;
+} 
+
+
+/**
+ * Check EMAIL SYNTHAX : if a new users in Claroline with the specified parameters contains synthax error
+ * in mail given.
+ *
+ * @author Guillaume Lederer <lederer@cerdecam.be>
+ *
+ * @param  $userlist must be a 2D array with the list of potential new users :
+ *         $userlist['email'][$i] for the email  
+ *
+ *
+ * @return $errors : an array of boolean where $errors[$i] is TRUE if there is an error with entry $i in the given array.
+ *
+ * 
+ */
+
+function check_email_synthax_userlist($userlist)
+{
+    global $tbl_user;        
+    $errors = array();
+  
+    //CHECK: check email validity
+    
+    $regexp = "^[0-9a-z_\.-]+@(([0-9]{1,3}\.){3}[0-9]{1,3}|([0-9a-z][0-9a-z-]*[0-9a-z]\.)+[a-z]{2,4})$";
+    
+    for ($i=0, $size=sizeof($userlist['email']); $i<$size; $i++)
+    {
+        if ((!empty($userlist['email'][$i])) && !eregi( $regexp, $userlist['email'][$i] )) 
+        {
+	    $errors[$i] = TRUE;
+        }
+    }
+    
+    return $errors;
+}
+
  /**
+ * Check USERNAME NOT TAKEN YET : check if usernames are not already token by someone else
+ *
+ * @author Guillaume Lederer <lederer@cerdecam.be>
+ *
+ * @param  $userlist must be a 2D array with the list of potential new users :
+ *         $userlist['username'][$i]   for the username
+ *
+ * @return $errors : an array of boolean where $errors[$i] is TRUE if there is an error with entry $i in the given array.
+ *
+ * 
+ */
+ 
+function check_username_used_userlist($userlist)
+{   
+    global $tbl_user;        
+    $errors = array();
+    
+    //CHECK : check if usernames are not already token by someone else
+    
+    $sql = "SELECT * FROM `".$tbl_user."` WHERE 1=0 ";
+    
+    for ($i=0, $size=sizeof($userlist['username']); $i<$size; $i++)
+    {
+        if (!empty($userlist['username'][$i]) && ($userlist['username'][$i]!=""))
+	{
+	    $sql .= " OR username=\"".$userlist['username'][$i]."\"";
+	}  
+    }  
+    
+    //echo $sql."<br>\n";
+    
+    //for each user found, report the potential problem in an error array returned
+    
+    $foundUser = claro_sql_query($sql);
+    
+    while ($list = mysql_fetch_array($foundUser))
+    {
+	
+	$found = array_search($list['username'],$userlist['username']);
+		
+	if (!($found===FALSE)) 
+	{
+	    $errors[$found] = TRUE; 
+	}
+    }
+    return $errors;
+}
+ 
+ /**
+ * Check OFFICIAL CODE NOT TAKEN YET : check if admincode (officialCode) is not already taken by someone else
+ *
+ * @author Guillaume Lederer <lederer@cerdecam.be>
+ *
+ * @param  $userlist must be a 2D array with the list of potential new users :
+ *         
+ *         $userlist['officialCode'][$i]  for the officialCode
+ *
+ * @return $errors : an array of boolean where $errors[$i] is TRUE if there is an error with entry $i in the given array.
+ *
+ * 
+ */
+    
+function check_officialcode_used_userlist($userlist)
+{
+    global $tbl_user;        
+    $errors = array();
+    
+    //CHECK : check if admincode (officialCode) is not already taken by someone else
+    
+    $sql = "SELECT * FROM `".$tbl_user."` WHERE 1=0 ";
+    
+    for ($i=0, $size=sizeof($userlist['officialCode']); $i<$size; $i++) 
+    {
+        if (!empty($userlist['officialCode'][$i]) && ($userlist['officialCode'][$i]!=""))
+	{
+	    $sql .= " OR officialCode=\"".$userlist['officialCode'][$i]."\"";
+	}  
+    }
+    
+    //for each user found, report the potential problem
+    
+    $foundUser = claro_sql_query($sql);
+    
+    //echo $sql."<br>\n";
+    
+    while ($list = mysql_fetch_array($foundUser))
+    {
+	$found = array_search($list['officialCode'],$userlist['officialCode']);
+		
+	if (!($found===FALSE)) 
+	{
+	    $errors[$found] = TRUE; 
+	}
+    }   
+    return $errors;
+}    
+     
+/**
+ * Check PASSWORD ACCEPTABLE  : check if password is sufficently complex for this user
+ *
+ * @author Guillaume Lederer <lederer@cerdecam.be>
+ *
+ * @param  $userlist must be a 2D array with the list of potential new users :
+ *
+ *         $userlist['username'][$i]   for the username
+ *         $userlist['password'][$i]   for the password
+ *
+ * @return $errors : an array of boolean where $errors[$i] is TRUE if there is an error with entry $i in the given array.
+ *
+ * 
+ */ 
+ 
+ function check_password_userlist($userlist)
+ {
+     $errors = array();
+     
+     for ($i=0, $size=sizeof($userlist['password']); $i<$size; $i++) 
+     {
+        if ($userlist['password'][$i]==$userlist['username'][$i])
+	{
+	    $errors[$i] = TRUE; 
+	}  
+     }
+          
+     return $errors;
+ }   
+       
+ /**
+ * Check EMAIL NOT TAKEN YET : check if the e-mails are not already taken by someone in the plateform
+ *
+ * @author Guillaume Lederer <lederer@cerdecam.be>
+ *
+ * @param  $userlist must be a 2D array with the list of potential new users :
+ *
+ *         $userlist['email'][$i] for the email
+ *
+ * @return $errors : an array of boolean where $errors[$i] is TRUE if there is an error with entry $i in the given array.
+ *
+ * 
+ */    
+    
+function check_mail_used_userlist($userlist)
+{
+    global $tbl_user;        
+    $errors = array();
+    
+    $sql = "SELECT * FROM `".$tbl_user."` WHERE 1=0 ";
+    
+    for ($i=0, $size=sizeof($userlist['email']); $i<$size; $i++) 
+    {
+        if (!empty($userlist['email'][$i]) && ($userlist['email'][$i]!=""))
+	{
+	    $sql .= " OR email=\"".$userlist['email'][$i]."\"";
+	}  
+    }
+    
+        //for each user found, report the potential problem for email
+    
+    $foundUser = claro_sql_query($sql);
+    
+    while ($list = mysql_fetch_array($foundUser))
+    {
+        
+	$found = array_search($list['email'],$userlist['email']);
+		
+	if (!($found===FALSE)) 
+	{
+	    $errors[$found] = TRUE; 
+	}
+    }
+    
+    //echo $sql."<br>\n";
+     
+    return $errors; 
+}
+
+/**
+ * Check DUPLICATE EMAIL OF ADDABLE USERS : take the 2D array in param and check if  email 
+ * are all different.
+ *
+ * @author Guillaume Lederer <lederer@cerdecam.be>
+ *
+ * @param  $userlist must be a 2D array with the list of potential new users :
+ *         $userlist['email'][$i] for the email  
+ *         $userlist['username'][$i] for the username
+ *         $userlist['officialCode'][$i]  for the officialCod
+ *
+ * @return $errors : an array of boolean where $errors[$i] is TRUE if there is an error with entry $i in the given 2D array.
+ *
+ * 
+ */
+
+function check_duplicate_mail_userlist($userlist)
+{
+    global $tbl_user;        
+    $errors = array();
+        
+    for ($i=0, $size=sizeof($userlist['name']); $i<$size; $i++)
+    {       
+        //check email duplicata in the array
+    
+	$found = array_search($userlist['email'][$i],$userlist['email']);
+	
+	if (!($found===FALSE) && ($i!=$found))
+        {
+	    $errors[$i] = TRUE;
+        }
+    }
+       
+    return $errors;
+}
+
+/**
+ * Check DUPLICATE USERNAMES OF ADDABLE USERS : take the 2D array in param and check if username are all different.
+ *
+ * @author Guillaume Lederer <lederer@cerdecam.be>
+ *
+ * @param  $userlist must be a 2D array with the list of potential new users :
+ *         $userlist['email'][$i] for the email  
+ *         $userlist['username'][$i] for the username
+ *         $userlist['officialCode'][$i]  for the officialCod
+ *
+ * @return $errors : an array of boolean where $errors[$i] is TRUE if there is an error with entry $i in the given 2D array.
+ *
+ * 
+ */
+
+function check_duplicate_username_userlist($userlist)
+{
+    global $tbl_user;        
+    $errors = array();
+        
+    for ($i=0, $size=sizeof($userlist['username']); $i<$size; $i++)
+    {       
+        //check username duplicata in the array
+    
+	$found = array_search($userlist['username'][$i],$userlist['username']);
+	
+	if (!($found===FALSE) && ($i!=$found))
+        {
+	    $errors[$i] = TRUE;
+        }
+    }
+       
+    return $errors;
+}
+
+/**
+ * Check DUPLICATE OFFICIAL CODE OF ADDABLE USERS : take the 2D array in param and check if official codes are all different.
+ *
+ * @author Guillaume Lederer <lederer@cerdecam.be>
+ *
+ * @param  $userlist must be a 2D array with the list of potential new users :
+ *         $userlist['email'][$i] for the email  
+ *         $userlist['username'][$i] for the username
+ *         $userlist['officialCode'][$i]  for the officialCod
+ *
+ * @return $errors : an array of boolean where $errors[$i] is TRUE if there is an error with entry $i in the given 2D array.
+ *
+ * 
+ */
+
+function check_duplicate_officialcode_userlist($userlist)
+{
+    global $tbl_user;        
+    $errors = array();
+        
+    for ($i=0, $size=sizeof($userlist['username']); $i<$size; $i++)
+    {       
+        //check username duplicata in the array
+    
+	$found = array_search($userlist['username'][$i],$userlist['username']);
+	
+	if (!($found===FALSE) && ($i!=$found))
+        {
+	    $errors[$i] = TRUE;
+        }
+    }
+       
+    return $errors;
+}
+
+ /**
+ * Create a new user in Claroline with the specified informations
+ *
+ * @author Guillaume Lederer <lederer@cerdecam.be>
+ *
+ * @param  string  $name name of the new user                 
+ * @param  string  $surname surname of the new user              
+ * @param  string  $email email of the new user                 
+ * @param  string  $admincode official code of the new user           
+ * @param  string  $name of the new user               
+ * @param  string  $password password of the new user               
+ * @param  boolean $teacher :                                     
+ *                      TRUE  if new user must be a teacher,
+ *                      FALSE otherwise,              
+ * 
+ * @return TRUE if creation succeeded, FALSE otherwise
+ * 
+ */
+
+function add_user($name,$surname,$email,$phone,$admincode,$username,$password,$teacher)
+{
+    // see if password must be stored crypted or not
+    global $tbl_user;
+    global $userPasswordCrypted;
+    if (!isset($userPasswordCrypted))  $userPasswordCrypted	 = false;
+    
+    // set the status DB code with the value of $teacher
+    
+    if ($teacher==TRUE) 
+    { 
+        $status = 1;
+    }
+    else
+    {
+        $status = 5;
+    }
+    
+    $sql = "INSERT INTO `".$tbl_user."`
+            SET `nom`          = \"".$name."\",
+                `prenom`       = \"".$surname."\",
+                `username`     = \"".$username."\",
+                `password`     = \"".($userPasswordCrypted?md5($password):$password)."\",
+                `email`        = \"".$email."\",
+                `statut`       = \"".$status."\",
+                `officialCode` = \"".$admincode."\",
+                `phoneNumber`  = \"".$phone."\"";
+
+    $_uid = claro_sql_query_insert_id($sql);
+    return true;
+}
+
+/**
+ * Create users in Claroline from a list in a CSV file with the specified format
+ *
+ * @author Guillaume Lederer <lederer@cerdecam.be>
+ *
+ * @param  $userlist a 2D array with the list of new users :
+ *         $userlist[$i]['name']       for the name
+ *         $userlist[$i]['surname']    for the surname
+ *         $userlist[$i]['username']   for the username
+ *         $userlist[$i]['password']   for the password
+ *         $userlist[$i]['officialcode']  for the official Code
+ *         $userlist[$i]['phone']      for the phone
+ *         $userlist[$i]['email']      for the email
+ * 
+ *
+ * NOTE : COULD BE OPTIMIZED : NOT USING THE ADD_USER function so that only query creates all the users
+ *
+ */
+ 
+function add_userlist($userlist)
+{
+    
+    foreach ($userlist as $user)
+    {
+       add_user($user['name'],$user['surname'],$user['email'],$user['phone'],$user['officialCode'],$user['username'], $user['password'],TRUE); 
+    }
+}
+
+/**
  * subscribe a specific user to a specific course
  *
  * @author Hugues Peeters <peeters@ipm.ucl.ac.be>
