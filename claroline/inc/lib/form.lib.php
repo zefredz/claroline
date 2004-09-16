73a99acf2f2e7bbac9a9575880de4c4306514460
seb <seb@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2004-09-16 12:22:07 +0000

    New lib dedicated to function that create some element of html forms (for an exemple : build the 3 listbox needed to choose a date [day][month][year] )

---
+++ claroline/inc/lib/form.lib.php
@@ -0,0 +1,102 @@
+<?php
+
+function claro_disp_date_form($dayFieldName, $monthFieldName, $yearFieldName, $selectedDate = 0 )
+{
+    global $langMonthNames;
+    
+    if(!$selectedDate)
+    {
+        $selectedDate = date("Y-m-d");
+    }
+    // split selectedDate
+    list($selYear, $selMonth, $selDay) = split("-", $selectedDate);
+    
+    // day field
+    $dayField = "<select name=\"".$dayFieldName."\" id=\"".$dayFieldName."\">\n";
+    for ($i=1;$i <=31; $i++)
+    {
+        $dayField .= "<option value=\"".$i."\"";
+        if($i == $selDay)
+        {
+            $dayField .= " selected=\"true\"";
+        }
+        $dayField .= ">".$i."</option>\n";
+    }
+    $dayField .="</select>\n";
+    
+    // month field
+    $monthField = "<select name=\"".$monthFieldName."\" id=\"".$monthFieldName."\">\n";
+    for ($i=1;$i <=12; $i++)
+    {
+        $monthField .= "<option value=\"".$i."\"";
+        if($i == $selMonth)
+        {
+            $monthField .= " selected=\"true\"";
+        }
+        $monthField .= ">".$langMonthNames['long'][$i-1]."</option>\n";
+    }
+    $monthField .="</select>\n";
+    
+    // year field
+    $yearField = "<select name=\"".$yearFieldName."\" id=\"".$yearFieldName."\">\n";
+    for ($i= $selYear-2;$i <=$selYear+5; $i++)
+    {
+        $yearField .= "<option value=\"".$i."\"";
+        if($i == $selYear)
+        {
+            $yearField .= " selected=\"true\"";
+        }
+        $yearField .= ">".$i."</option>\n";
+    }
+    $yearField .='</select>';
+    
+    return $dayField.'&nbsp;'.$monthField.'&nbsp;'.$yearField;
+}
+
+
+function claro_disp_time_form($hourFieldName, $minuteFieldName, $selectedTime = 0)
+{
+    if(!$selectedTime)
+    {
+        $selectedTime = date("H:i");
+    }
+    
+    //split selectedTime 
+    list($selHour, $selMinute) = split(":",$selectedTime);
+    
+    if ($hourFieldName != "")
+    {
+        $hourField = "<select name=\"".$hourFieldName."\" id=\"".$hourFieldName."\">\n";
+        for($i=0;$i < 24; $i++)
+        {
+            $hourField .= "<option value=\"".$i."\"";
+            if($i == $selHour)
+            {
+                $hourField .= " selected=\"true\"";
+            }
+            $hourField .= ">".$i."</option>\n";
+        }
+        $hourField .= "</select>";
+    }
+    
+    if($minuteFieldName != "")
+    {
+        $minuteField = "<select name=\"".$minuteFieldName."\" id=\"".$minuteFieldName."\">\n";
+        $i = 0;
+        while($i < 60)
+        {
+            $minuteField .= "<option value=\"".$i."\"";
+            if($i == $selMinute)
+            {
+                $minuteField .= " selected=\"true\"";
+            }
+            $minuteField .= ">".$i."</option>\n";
+            $i += 5;
+        }
+        $minuteField .= "</select>";
+    }
+    
+    return "&nbsp;".$hourField."&nbsp;".$minuteField;
+}
+
+?>
