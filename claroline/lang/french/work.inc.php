31ddfad4fb8ca4132830457bccba7fe9bb21543e
seb <seb@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2004-09-28 13:07:23 +0000

    French translation for the new assignments tool

--- claroline/lang/french/work.inc.php
+++ claroline/lang/french/work.inc.php
@@ -1,40 +1,106 @@
 <?php # $Id$
 /*
 //+----------------------------------------------------------------------+
-//| CLAROLINE version 1.5.*
+//| CLAROLINE version 1.5.* 
 //+----------------------------------------------------------------------+
 //| Copyright (c) 2001, 2004 Universite catholique de Louvain (UCL)
 //+----------------------------------------------------------------------+
 */
-// GENERIC
-
-$langIntroWork="Cette page est un espace de publication. Elle permet à chaque étudiant ou groupe d'étudiants d'envoyer un document (Word, Excel, HTML... ) vers le site du cours afin de le rendre accessible aux autres étudiants ainsi qu'au professeur.
-<BR><BR>Si vous passez par votre espace de groupe pour publier le document (option publier), l'outil de travaux fera un simple lien vers le document là où il se trouve dans votre répertoire de groupe sans le déplacer.
-<BR><BR>Si vous désirez donner des consignes aux étudiants, cliquez sur ce bouton"; // JCC 
-
-
 // WORKS
-$langDownloadFile="Envoyer le document";
-$langMaxFileSize = "Taille maximale autorisée : ";
-$langTooBig="Vous n'avez pas choisi de fichier à envoyer ou bien le fichier est trop gros.";
-$langListDeleted="La liste a été complètement effacée";
-$langDocModif="Le document a été modifié";
-$langDocAdd="Le document a été ajouté";
-$langDocDel="Le travail a été effacé";
-$langTitleWork="Titre du travail en toutes lettres";
-$langAuthors="Auteurs";
-$langDescription="Description éventuelle";
-$langDelList="Effacer complètement la liste";
+$langWorks = "Travaux";
 
+// generic
+$langMakeVisible = "Rendre Visible";
+$langMakeInvisible = "Rendre Invisible";
 
-$langWorks="Travaux d'étudiants";
+// -- Session list
+$langCreateSession = "Créer une nouvelle session de travail";
+$langVisibility = "Visibilité";
 
-$langDocument = "Document";
+// -- Work list
+$langWrkSession = "Session";
+$langSubmitWork = "Soumettre un travail";
+$langEditWork = "Modifier un travail";
+$langWorkDetails = "Détails du travail";
+$langGradeWork = "Corriger";
+$langShowGrade = "Voir correction";
 
-$langWorkConfirmDelete="Confirmez-vous cette suppression ?";
-$langAllFiles="Tous les fichiers";
+// -- Forms
 
-$lang_make_invisible = "Rendre invisible";
-$lang_make_visible = "Rendre visible";
-$langNotPossible = "Impossible";
+// session
+$langSessionTitle = "Titre de la session";
+$langSessionDescription = "Description";
+
+$langSubmissionType = "Type de soumission";
+$langTextOnly = "Texte seul";
+$langFileOnly = "Fichier seul";
+$langTextFile = "Texte avec un fichier attaché (le texte est requis, pas le fichier)";
+
+$langStartDate = "Date de début";
+$langEndDate = "Date de fin";
+
+$langDefSubVisibility = "Visibilité par défaut";
+$langVisible = "Visible";
+$langInvisible = "Invisible";
+
+$langSessionType = "Type de session";
+$langIndividual = "Individuelle";
+$langGroup = "Groupe";
+
+$langAllowAnonymous = "Autoriser les utilisateurs anonymes";
+$langAnonAllowed = "Oui, les utilisateurs anonymes peuvent soumettre leur travail"; 
+$langAnonNotAllowed = "Non, les utilisateurs anonymes ne peuvent pas soumettre leur travail";
+
+$langAllowLateUploadShort = "Soumissions tardives";
+$langAllowLateUpload = "Oui, autoriser les utilisateurs à soumettre leur travail après la date de fin";
+$langPreventLateUpload = "Non, ne pas autoriser les utilisateurs à soumettre leur travail après la date de fin";
+
+// work
+$langWrkTitle = "Titre du travail";
+$langWrkAuthors = "Auteur(s)";
+$langUploadDoc = "Envoyer un fichier";
+$langAttachDoc = "Attacher un fichier";
+$langCurrentAttachedDoc = "Fichier attaché actuel";
+$langCurrentDoc = "Fichier actuel";
+$langExplainModifyAttachedfile = "Cochez cette case pour effacer le fichier attaché, envoyez un nouveau fichier pour le remplacer.";
+$langFileDesc = "Description du fichier";
+$langAnswer = "Réponse";
+$langNoFile = "- pas de fichier -";
+
+// -- Form errors and confirmations
+//confirmations
+$langSessionAdded = "Session ajoutée";
+$langAreYouSureToDelete = "Etes vous certain de vouloir effacer";
+$langDeleteCaution = "! Cela effacera aussi tous les travaux soumis dans cette session !";
+$langSessionDeleted = "Session effacée";
+$langSessionEdited = "Session modifiée";
+$langWrkAdded = "Travail ajouté";
+$langWrkEdited = "Travail modifié";
+$langGradeAdded = "Correction ajoutée";
+
+// errors
+$langTooBig = "Fichier trop grand";
+$langSessionTitleRequired = "Titre de la session requis";
+$langTitleAlreadyExists = "Le titre de la session existe déjà";
+$langCannotCopyFile = "Impossible de copier le fichier";
+$langWrkTitleAlreadyExists = "Le titre du travail existe déjà";
+$langWrkTitleRequired = "Titre du travail requis";
+$langAnswerRequired = "Réponse requise";
+$langWrkAuthorsRequired = "Auteur(s) requis";
+$langFileRequired = "Fichier requis";
+$langIncorrectDate = "La date de début doit être antérieure à la date de fin";
+
+// work details
+$langUploadedFile = "Fichier envoyé";
+$langAttachedFile = "Fichier attaché";
+$langSubmissionDate = "Date de soumission";
+$langLastSubmissionDate = "Date dernière soumission";
+$langLastEditDate = "Date dernière édition";
+$langSubmittedBy = "Soumis par";
+$langAnonymousUser = "Utilisateur anonyme";
+$langAfterEndDate = "Après la date de fin";
+
+$langRequired = "Requis";
+$langChooseDateHelper = "(d/m/y hh:mm)";
+
 ?>
