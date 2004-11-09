296e816c3fbaa38d9558b552763cf3df2ed37dae
mathieu <mathieu@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2004-11-09 15:11:15 +0000

    remove include and add missing lang var

--- claroline/lang/english/dev.adduser.inc.php
+++ claroline/lang/english/dev.adduser.inc.php
@@ -1,26 +1,28 @@
 <?php # $Id$
-include dirname(__FILE__)."/../french/dev.adduser.inc.php";
 
 $langAddNewHeading = "Add new heading";
+$langAdd_users = "Add users";
 $langAdmin = "Admin";
-$langComment = "Comment";
-$langCourseManager = "Course manager";
-$langMoveUp = "Move up";
-$langMoveDown = "Move down";
-$langRemove = "Remove";
 $langBackToUsersList = "Back to users list";
+$langComment = "Comment";
 $langCourseAdministratorOnly = "Course administrator only";
+$langCourseManager = "Course manager";
 $langDefineHeadings = "Define Headings";
-$langEdit = "Editer";
+$langDevTools = "Devel Tools";
+$langEdit = "Edit";
 $langLine = "line";
 $langLineNumber =  "Lines Number";
 $langLineOrLines = "line(s)";
-$langUserName = "User name";
-$langUsers = "Users";
-$langEdit = "Edit";
 $langLines =  "lines";
+$langMoveDown = "Move down";
+$langMoveUp = "Move up";
+$langRemove = "Remove";
+$langStudents="Students";
+$langTeachers="Teachers";
 $langTitle =  "Title";
 $langTracking = "Tracking";
 $langUserName = "User name";
+$langUsers = "Users";
+$lang_you_had_request ="You ask" ;
 
 ?>
