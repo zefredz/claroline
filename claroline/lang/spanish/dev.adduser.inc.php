a42f6d37bd1feb51cda98a2cffdc57e27985598a
mathieu <mathieu@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2004-11-10 10:33:32 +0000

    harmonize lang file: remove include in dev.adduser.inc.php

--- claroline/lang/spanish/dev.adduser.inc.php
+++ claroline/lang/spanish/dev.adduser.inc.php
@@ -16,9 +16,30 @@
 
 */
 
-
-include dirname(__FILE__)."/../french/dev.adduser.inc.php";
-
+$langAddNewHeading = "Add new heading";
+$langAdd_users = "Add users";
+$langAdmin = "Admin";
+$langBackToUsersList = "Back to users list";
+$langComment = "Comment";
+$langCourseAdministratorOnly = "Course administrator only";
+$langCourseManager = "Course manager";
+$langDefineHeadings = "Define Headings";
+$langDevTools = "Devel Tools";
+$langEdit = "Edit";
+$langLine = "line";
+$langLineNumber =  "Lines Number";
+$langLineOrLines = "line(s)";
+$langLines =  "lines";
+$langMoveDown = "Move down";
+$langMoveUp = "Move up";
+$langRemove = "Remove";
+$langStudents="Students";
+$langTeachers="Teachers";
+$langTitle =  "Title";
+$langTracking = "Tracking";
+$langUserName = "User name";
+$langUsers = "Users";
+$lang_you_had_request ="You ask" ;
 
 $langAddNewHeading = "Agregar una nueva cabecera ";
 $langAdmin = "Admin";
@@ -41,4 +62,5 @@ $langLines =  "lineas";
 $langTitle =  "Titulo";
 $langTracking = "Seguimiento";
 $langUserName = "Nombre de usuario";
+
 ?>
