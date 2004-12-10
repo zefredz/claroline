1ae0817bc92feadd4ed3506e8b5d6ad5edf00460
mathieu <mathieu@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2004-12-10 13:14:24 +0000

    add new language file FRENCH

---
+++ claroline/lang/french/claroline_course_description.lang.php
@@ -0,0 +1,108 @@
+<?php //$Id$
+$langAdd = "Ajouter";
+$langAddCat = "Ajouter une catégorie";
+$langAddIntro = "Ajouter un texte d'introduction";
+$langAgenda = "Agenda";
+$langAnnouncement = "Annonces";
+$langAreYouSureToDelete = "Etes-vous sûr de vouloir supprimer ";
+$langBackAndForget = "Abandonner";
+$langChat = "Discussion";
+$langConfirmYourChoice = "Veuillez confirmer votre choix";
+$langContenuPlan = "";
+$langCourseContent = "Contenu du cours";
+$langCourseContentComment1 = "Quelle est l'importance des différents contenus à traiter dans le cadre du cours&nbsp;?
+Quel est le niveau de difficulté de ces contenus&nbsp;? 
+Comment structurer l'ensemble de la matière&nbsp;?  
+Quelle sera la séquence des contenus&nbsp;? 
+Quelle sera la progression dans les contenus&nbsp;?";
+$langCourseContentComment2 = "Présentation de la table des matières du cours, de la structuration du 
+contenu, de la progression et du calendrier";
+$langCourseHome = "Accueil";
+$langCourseManagerView = "Gestionnaire de cours";
+$langCourseProgram = "Description du cours";
+$langDay_of_weekNames = "Array";
+$langDelete = "Effacer";
+$langDeleted = "Supprimés";
+$langDescription = "Description";
+$langDescriptionComment1 = "Quelle est la place et la spécificité du cours dans le programme&nbsp;?
+Existe-t-il des cours pré-requis&nbsp;?
+Quels sont les liens avec les autres cours&nbsp;?";
+$langDescriptionComment2 = "
+Information permettant d'identifier le cours 
+(sigle, titre, nombre d'heure de cours, de TP, ...) 
+et l'enseignant (nom, prénom, bureau, tél, e-mail, disponibilités éventuelles).
+<br>
+Présentation générale du cours dans le programme.";
+$langDescriptionCours = "Description du cours";
+$langDocument = "Documents et liens";
+$langEditCourseProgram = "Créer et éditer à l'aide des formulaires une description de cours";
+$langExercises = "Exercices";
+$langForums = "Forums";
+$langGroups = "Groupes";
+$langHelp = "Aide";
+$langHumanAndPhysicalResourcesComment1 = "
+Quelles sont les ressources humaines et physiques disponibles&nbsp;?
+Quelle sera la nature de l'encadrement&nbsp;? 
+Que peuvent attendre les étudiants de l'équipe d'encadrement ou de l'encadrement de l'enseignant&nbsp;?";
+$langHumanAndPhysicalResourcesComment2 = "Présentation des autres 
+enseignants qui vont encadrer le cours (assistants, chercheurs, 
+étudiants-moniteurs,...), des disponibilités des personnes, des locaux et des 
+équipements ou matériel informatique disponibles.";
+$langHumanAndPhysicalRessources = "Ressources humaines et physiques";
+$langInfo2Say = "Informations à communiquer <BR>à l'étudiant";
+$langLearningPath = "Parcours pédagogique";
+$langLogin = "login";
+$langLogout = "Quitter";
+$langManager = "Gestionnaire";
+$langMethodsOfEvaluation = "Modalités d'évaluation";
+$langMethodsOfEvaluationComment1 = "Précisions quant aux moyens d'évaluation (examens écrits, oraux, projets, 
+travaux à remettre, ...), quant au(x) moment(s) d'évaluation formative prévu(s), 
+échéances pour la remise des travaux, aux critères d'évaluation, éventuellement 
+la pondération des critères ou des catégories de critères.";
+$langModify = "Modifier";
+$langModifyProfile = "Modifier mon profil";
+$langMonthNames = "Array";
+$langMyAgenda = "Mon agenda";
+$langMyCourses = "Liste de mes cours";
+$langNewBloc = "Autre";
+$langOk = "Valider";
+$langOtherCourses = "Liste des cours";
+$langPassword = "mot de passe";
+$langPlatformAdministration = "Administration de la plate-forme";
+$langPoweredBy = "Utilise la plate-forme";
+$langQualificationsAndGoals = "Compétences et Objectifs";
+$langQualificationsAndGoalsComment1 = "Présentation des objectifs généraux et spécifiques du cours, des compétences auxquelles la maîtrise de tels objectifs pourrait conduire.";
+$langQualificationsAndGoalsComment2 = "Quels sont les apprentissages visés par l'enseignement&nbsp;?
+<br>
+Au terme du cours, quelles sont les compétences, les capacités et les connaissances que les étudiants seront en mesure de maîtriser, de mobiliser&nbsp;?";
+$langQuestionPlan = "Question de planification <BR>pour le gestionnaire de cours";
+$langReg = "Inscription";
+$langStudentView = "Etudiant";
+$langSupports = "Supports";
+$langSupportsComment1 = "Existe-t-il un support de cours ? Quel type de support vais-je privilégier ? 
+Ouvert ? Fermé ?";
+$langSupportsComment2 = "Présentation du ou des supports de 
+cours. Présentation de la bibliographie, du portefeuille de documents ou 
+d'une bibliographie complémentaire.";
+$langTeachingTrainingActivities = "Activités d'enseignement-apprentissage";
+$langTeachingTrainingActivitiesComment1 = "Quelles méthodes et quelles activités vont-elles favoriser l'atteinte des 
+objectifs définis pour le cours&nbsp;?
+Quel est le calendrier des activités&nbsp;?";
+$langTeachingTrainingActivitiesComment2 = "Présentation des activités prévues 
+(exposés magistraux, participation attendue des étudiants, travaux pratiques, 
+séances de laboratoire, visites, recueil d'informations sur le terrain, 
+...).";
+$langThisCourseDescriptionIsEmpty = "La description de ce cours est vide";
+$langTitle = "Titre";
+$langUserName = "Nom d'utilisateur";
+$langUsers = "Utilisateurs";
+$langValid = "Enregistrer";
+$langWork = "Travaux";
+$lang_click_here = "cliquez ici";
+$lang_enter_your_user_name_and_password = "Tapez votre nom d'utilisateur et votre mot de passe";
+$lang_footer_CourseManager = "Gestionnaire(s) du cours";
+$lang_if_you_dont_have_a_user_account_profile_on = "Si vous n'avez pas de compte sur";
+$lang_if_you_wish_to_enroll_to_this_course = "Si vous souhaitez vous inscrire à ce cours,";
+$lang_this_course_is_protected = "Ce cours est protégé";
+$lang_your_user_profile_doesnt_seem_to_be_enrolled_to_this_course = "Votre profil utilisateur ne semble pas être inscrit à ce cours";
+?>
---
+++ claroline/lang/french/claroline_phpbb.lang.php
@@ -0,0 +1,184 @@
+<?php //$Id$
+$l_aim = "AIM";
+$l_author = "Auteur";
+$l_banned = "Vous avez été banni de ce forum. Contactez l'administrateur système si vous avez des questions.";
+$l_body = "Message";
+$l_by = "par";
+$l_cancelpost = "Annuler ce message";
+$l_click = "Cliquez";
+$l_delete = "Effacer ce message";
+$l_deleted = "Votre Message a été effacé.";
+$l_deletesucces = "Effacement réussi.";
+$l_emailaddress = "Adresse e-mail";
+$l_emptymsg = "Vous devez taper un Message à poster. Vous ne pouvez poster un Message vide.";
+$l_enterpassword = "Vous devez taper votre mot de passe.";
+$l_error = "Erreur";
+$l_go = "Aller à";
+$l_guest = "Invité";
+$l_here = "ici";
+$l_icq = "ICQ";
+$l_icqadd = "Ajouter";
+$l_icqpager = "Pager";
+$l_indextitle = "Index du forum";
+$l_infoupdated = "Vos caractéristiques sont mises à jour";
+$l_interests = "Loisirs";
+$l_itemsreq = "Les champs marqués par un * sont obligatoires";
+$l_joined = "Inscrit";
+$l_lastpost = "Dernier Message";
+$l_location = "De";
+$l_messenger = "MSN Messenger";
+$l_mismatch = "Les mots de passe ne correspondent pas.";
+$l_msn = "MSN";
+$l_new = "Nouveaux";
+$l_nomatches = "Aucun enregistrement ne correspond à votre demande. Affinez votre recherche.";
+$l_nopmsgs = "Vous n'avez pas de Messages privés.";
+$l_nopostlock = "Vous ne pouvez répondre sur ce sujet, il a été fermé.";
+$l_noposts = "Pas de Messages";
+$l_norecipient = "Vous devez taper le nom d'utilisateur à qui vous désirez envoyer ce Message.";
+$l_notify = "Notifier par e-mail quand des réponses sont postées";
+$l_notifybody = "Vous recevez cet e-mail parce qu\'un message
+que vous avez posté sur nos forums a reçu une réponse, et que
+vous avez choisi d\'en être informé.
+
+Vous pouvez voir le sujet à:
+
+%s1
+
+Ou voir l\'index du forum  à
+
+%s2
+
+Merci d\'utiliser les forums .
+
+Bonne journée.";
+$l_notifysubj = "Une réponse à votre sujet a été posté.";
+$l_notopics = "Il n'y a pas de sujet sur ce forum. Vous pouvez en poster un.";
+$l_nouser = "Ce Nom d'utilisateur n'existe pas.";
+$l_nousers = "Aucun membre n'est actuellement sur ces forums";
+$l_number = "Nombre";
+$l_occupation = "Emploi";
+$l_oftotal = "du total";
+$l_onlyreq = "Demandé seulement si changé";
+$l_options = "Options";
+$l_perday = "Messages par jour";
+$l_pmposted = "Réponse postée, vous pouvez cliquer <a href=\"viewpmsg.php\">ici</a> pour voir vos Messages privés";
+$l_posted = "Posté le";
+$l_poster = "Initiateur";
+$l_posts = "Messages";
+$l_privnotify = "<br>Vous avez %s1 <a href=\"%s2\">nouveau(x) message(s) privée(s)</a>.";
+$l_profile = "Profil";
+$l_profileof = "Voir le profil de";
+$l_publicmail = "Autoriser les autres membres à voir votre Adresse e-mail";
+$l_quotemsg = "[quote]
+Le %s1, %s2 a écrit:
+%s3
+[/quote]";
+$l_recptname = "Nom d'utilisateur du destinataire";
+$l_replies = "Réponses";
+$l_reply = "Réponse";
+$l_replyquote = "Répondre en citant";
+$l_returnindex = "pour retourner à l'index du forum";
+$l_returntopic = "pour retourner à la liste des sujets du forum.";
+$l_searchall = "Rechercher TOUS les mots";
+$l_searchallfrm = "Rechercher dans tous les forums";
+$l_searchany = "Rechercher CHACUN de ces mots (par défaut)";
+$l_searchin = "Rechercher dans";
+$l_searchterms = "Mots clés";
+$l_selectforum = "Sélectionnez un Forum";
+$l_sendothermsg = "Envoyer un autre message privé";
+$l_sendpmsg = "Envoyer un message privé";
+$l_separator = " > ";
+$l_sigexplain = "C'est un texte qui peut être ajouté aux messages que vous postez.<BR>255 caractères maximum !";
+$l_signature = "Signature";
+$l_sortby = "Trier par";
+$l_stored = "Votre Message a été enregistré.";
+$l_subject = "Sujet";
+$l_topic = "Sujet";
+$l_topicreview = "Revue du sujet";
+$l_topics = "Sujets";
+$l_topictitle = "Voir Sujet";
+$l_toreturn = "pour retourner";
+$l_tryagain = "Revenez en arrière et réessayez.";
+$l_url = "URL";
+$l_userdisallowed = "Le Nom d'utilisateur choisi n'a pas été autorisé par l'administrateur. Revenez en arrière et réessayez.";
+$l_username = "Nom d'utilisateur";
+$l_userpass = "Veuillez taper votre nom d'utilisateur et votre mot de passe.";
+$l_userremoved = "Ce membre a été retiré de la base de données.";
+$l_usertaken = "Le Nom d'utilisateur que vous avez choisi existe déjà.";
+$l_viewforum = "Voir le forum";
+$l_viewmsg = "pour voir votre Message.";
+$l_viewpostuser = "Voir les messages de ce membre";
+$l_website = "Adresse du Site Web";
+$l_whosonline = "Qui est en ligne ?";
+$l_wrongpass = "Vous avez tapé un mauvais mot de passe.";
+$l_yahoo = "Yahoo Messenger";
+$l_yim = "YIM";
+$l_yourname = "Votre Nom d'utilisateur";
+$langAddIntro = "Ajouter un texte d'introduction";
+$langAdm = "Administrer";
+$langAgenda = "Agenda";
+$langAnnouncement = "Annonces";
+$langBack = "Retour";
+$langBackTo = "Retourner dans : ";
+$langChat = "Discussion";
+$langConfirm = "Confirmer";
+$langConfirmYourChoice = "Veuillez confirmer votre choix";
+$langCourseHome = "Accueil";
+$langCourseManagerView = "Gestionnaire de cours";
+$langDate = "Date";
+$langDay_of_weekNames = "Array";
+$langDear = "Cher(ère)";
+$langDelete = "Effacer";
+$langDescriptionCours = "Description du cours";
+$langDisable = "Désactiver";
+$langDocument = "Documents et liens";
+$langEditDel = "Editer/Effacer";
+$langEmail = "E-mail";
+$langExercises = "Exercices";
+$langForum = "Forum";
+$langForums = "Forums";
+$langFrom = "Du";
+$langGroupDocumentsLink = "Documents du groupe";
+$langGroupSpaceLink = "Espace du groupe";
+$langGroups = "Groupes";
+$langHelp = "Aide";
+$langLastMsg = "Dernier message";
+$langLearningPath = "Parcours pédagogique";
+$langLogin = "login";
+$langLoginBeforePost1 = "Pour publier des messages dans le forum, ";
+$langLoginBeforePost2 = "vous devez d'abord ";
+$langLoginBeforePost3 = "vous identifier sur le Campus";
+$langLogout = "Quitter";
+$langManager = "Gestionnaire";
+$langModify = "Modifier";
+$langModifyProfile = "Modifier mon profil";
+$langMonthNames = "Array";
+$langMyAgenda = "Mon agenda";
+$langMyCourses = "Liste de mes cours";
+$langMyGroup = "mon groupe";
+$langNewTopic = "Lancer un nouveau sujet";
+$langNextPage = "Page suivante";
+$langNoPost = "Pas de message";
+$langOk = "Valider";
+$langOneMyGroups = "sous ma supervision";
+$langOtherCourses = "Liste des cours";
+$langPassword = "mot de passe";
+$langPlatformAdministration = "Administration de la plate-forme";
+$langPoweredBy = "Utilise la plate-forme";
+$langReg = "Inscription";
+$langReply = "Répondre";
+$langSearch = "Rechercher";
+$langSeen = "Vu";
+$langStudentView = "Etudiant";
+$langSubmit = "Envoyer";
+$langUserName = "Nom d'utilisateur";
+$langUsers = "Utilisateurs";
+$langWork = "Travaux";
+$lang_click_here = "cliquez ici";
+$lang_enter_your_user_name_and_password = "Tapez votre nom d'utilisateur et votre mot de passe";
+$lang_footer_CourseManager = "Gestionnaire(s) du cours";
+$lang_if_you_dont_have_a_user_account_profile_on = "Si vous n'avez pas de compte sur";
+$lang_if_you_wish_to_enroll_to_this_course = "Si vous souhaitez vous inscrire à ce cours,";
+$lang_this_course_is_protected = "Ce cours est protégé";
+$lang_your_user_profile_doesnt_seem_to_be_enrolled_to_this_course = "Votre profil utilisateur ne semble pas être inscrit à ce cours";
+?>
