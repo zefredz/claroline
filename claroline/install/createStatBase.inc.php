f6739735651c7d669ce4f43b90d93d95567e09f0
seb <seb@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2004-12-15 07:15:04 +0000

    Remove creation instructions of track_c_* tables

--- claroline/install/createStatBase.inc.php
+++ claroline/install/createStatBase.inc.php
@@ -12,53 +12,10 @@
 // Authors: see 'credits' file
 //----------------------------------------------------------------------
         /**************Statistics Tables****************/
-        //claro_sql_query("DROP TABLE IF EXISTS track_c_browsers");
-//         claro_sql_query("DROP TABLE IF EXISTS track_c_countries");
-//         claro_sql_query("DROP TABLE IF EXISTS track_c_os");
-//         claro_sql_query("DROP TABLE IF EXISTS track_c_providers");
-//         claro_sql_query("DROP TABLE IF EXISTS track_c_referers");
- 
 //         claro_sql_query("DROP TABLE IF EXISTS track_e_default");
 //         claro_sql_query("DROP TABLE IF EXISTS track_e_login");
 //         claro_sql_query("DROP TABLE IF EXISTS track_e_open");
 //         claro_sql_query("DROP TABLE IF EXISTS track_e_subscriptions");
-
-        $sql = "CREATE TABLE `track_c_browsers` (
-                  `id` int(11) NOT NULL auto_increment,
-                  `browser` varchar(255) NOT NULL default '',
-                  `counter` int(11) NOT NULL default '0',
-                  PRIMARY KEY  (`id`)
-                ) TYPE=MyISAM COMMENT='record browsers occurences'";
-        claro_sql_query($sql);
-        $sql = "CREATE TABLE `track_c_countries` (
-                  `id` int(11) NOT NULL auto_increment,
-                  `code` varchar(40) NOT NULL default '',
-                  `country` varchar(50) NOT NULL default '',
-                  `counter` int(11) NOT NULL default '0',
-                  PRIMARY KEY  (`id`)
-                ) TYPE=MyISAM";
-        claro_sql_query($sql);
-        $sql = "CREATE TABLE `track_c_os` (
-                  `id` int(11) NOT NULL auto_increment,
-                  `os` varchar(255) NOT NULL default '',
-                  `counter` int(11) NOT NULL default '0',
-                  PRIMARY KEY  (`id`)
-                ) TYPE=MyISAM COMMENT='record OS occurences'";
-        claro_sql_query($sql);
-        $sql = "CREATE TABLE `track_c_providers` (
-                  `id` int(11) NOT NULL auto_increment,
-                  `provider` varchar(255) NOT NULL default '',
-                  `counter` int(11) NOT NULL default '0',
-                  PRIMARY KEY  (`id`)
-                ) TYPE=MyISAM COMMENT='list of providers used by users and number of occurences'";
-        claro_sql_query($sql);
-        $sql = "CREATE TABLE `track_c_referers` (
-                  `id` int(11) NOT NULL auto_increment,
-                  `referer` varchar(255) NOT NULL default '',
-                  `counter` int(11) NOT NULL default '0',
-                  PRIMARY KEY  (`id`)
-                ) TYPE=MyISAM COMMENT='record refering url occurences'";
-        claro_sql_query($sql);
 
         $sql = "CREATE TABLE `track_e_default` (
                   `default_id` int(11) NOT NULL auto_increment,
@@ -71,6 +28,8 @@
                   PRIMARY KEY  (`default_id`)
                 ) TYPE=MyISAM COMMENT='Use for other develloppers users'";
         claro_sql_query($sql);
+		
+		
         $sql = "CREATE TABLE `track_e_login` (
                   `login_id` int(11) NOT NULL auto_increment,
                   `login_user_id` int(11)  NOT NULL default '0',
@@ -79,6 +38,8 @@
                   PRIMARY KEY  (`login_id`)
                 ) TYPE=MyISAM COMMENT='Record informations about logins'";
         claro_sql_query($sql);
+		
+		
         $sql = "CREATE TABLE `track_e_open` (
                   `open_id` int(11) NOT NULL auto_increment,
                   `open_date` datetime NOT NULL default '0000-00-00 00:00:00',
