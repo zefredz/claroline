b1ec153dc65493c8d7534b75197b14a42a632d05
peeters <peeters@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-01-13 17:00:39 +0000

    PHP nuke driver

---
+++ claroline/auth/extauth/drivers/phpnuke.inc.php
@@ -0,0 +1,59 @@
+<?php # -$Id$
+
+//----------------------------------------------------------------------
+// CLAROLINE
+//----------------------------------------------------------------------
+// Copyright (c) 2001-2003 Universite catholique de Louvain (UCL)
+//----------------------------------------------------------------------
+// This program is under the terms of the GENERAL PUBLIC LICENSE (GPL)
+// as published by the FREE SOFTWARE FOUNDATION. The GPL is available
+// through the world-wide-web at http://www.gnu.org/copyleft/gpl.html
+//----------------------------------------------------------------------
+// Authors: see 'credits' file
+//----------------------------------------------------------------------
+
+$authSourceName = 'phpnuke';
+$authSourceType = 'DB';
+
+// Define the Auth driver options
+
+$extAuthOptionList = array(
+
+    // PUT HERE THE CORRECT DSN FOR YOUR DB SYSTEM
+    'dsn'         => 'mysql://dbuser:dbpassword@domain/phpnuke',
+
+    'table'       => 'nuke_users', // warning ! table prefix can change from one system to another 
+    'usernamecol' => 'username',
+    'passwordcol' => 'user_password',
+    'db_fields'   => array('name', 'user_email'),
+    'cryptType'   => 'md5'
+);
+
+
+// Link additionnal external authentication attributes to the Claroline 
+// user attribute.
+//
+// array KEYS   are the Claroline attributes and
+// array VALUES are the authentication external attributes.
+
+$extAuthAttribNameList = array (
+    'lastname'     => 'name',
+    'email'        => 'user_email',
+);
+
+// Array setting optionnal preliminary treatment to the data retrieved from the 
+// exernal authentication source. Array KEYS are the concernend claroline 
+// user table fields, and Array VALUES are either the name of a function which 
+// makes the treatment or simply a default value to insert
+// Note. Treatments doesn't necessary previously require data from the external 
+// authentication system. They're able to be trigged from NULL value ...
+
+$extAuthAttribTreatmentList = array ('status' => 5);
+
+
+// PROCESS AUTHENTICATION
+
+require $clarolineRepositorySys.'/auth/extauth/extAuthProcess.inc.php';
+
+
+?>
