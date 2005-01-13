26b14234e2752b7d7960288a6dddf58afd72c860
peeters <peeters@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-01-13 17:03:42 +0000

    dokeos driver

---
+++ claroline/auth/extauth/drivers/dokeos.inc.php
@@ -0,0 +1,63 @@
+<?php # -$Id$
+
+//----------------------------------------------------------------------
+// CLAROLINE
+//----------------------------------------------------------------------
+// Copyright (c) 2001-2003 Universite catholique de Louvain (UCL)
+//----------------------------------------------------------------------
+// This program is under the terms of the GENERAL PUBLIC LICENSE (GPL)
+// as published by the FREE SOFTWARE FOUNDATION. The GPL is available
+// through the world-wide-web at http://www.gnu.org/copyleft/gpl.html
+//----------------------------------------------------------------------
+// Authors: see 'credits' file
+//----------------------------------------------------------------------
+
+$authSourceName = 'dokeos';
+$authSourceType = 'DB';
+
+// Define the Auth driver options
+
+$extAuthOptionList = array(
+
+    // PUT HERE THE CORRECT DSN FOR YOUR DB SYSTEM
+    'dsn'         => 'mysql://dbuser:dbpassword@domaine/dokeos',
+
+    'table'       => 'user', // warning ! table prefix can change from one system to another 
+    'usernamecol' => 'username',
+    'passwordcol' => 'password',
+    'db_fields'   => array('prenom', 'nom', 'email', 'phoneNumber', 'statut', 'officialCode', 'pictureUri'),
+    'cryptType'   => 'none'
+);
+
+
+// Link additionnal external authentication attributes to the Claroline 
+// user attribute.
+//
+// array KEYS   are the Claroline attributes and
+// array VALUES are the authentication external attributes.
+
+$extAuthAttribNameList = array (
+    'firstname'    => 'prenom',
+    'lastname'     => 'nom',
+    'email'        => 'email',
+    'phoneNumber'  => 'phoneNumber',
+    'statut'       => 'statut',
+    'officialCode' => 'officialCode',
+    'pictureUri'   => 'pictureUri',
+    'status'      => 'statut'
+);
+
+// Array setting optionnal preliminary treatment to the data retrieved from the 
+// exernal authentication source. Array KEYS are the concernend claroline 
+// user table fields, and Array VALUES are either the name of a function which 
+// makes the treatment or simply a default value to insert
+// Note. Treatments doesn't necessary previously require data from the external 
+// authentication system. They're able to be trigged from NULL value ...
+
+$extAuthAttribTreatmentList = array ();
+
+// PROCESS AUTHENTICATION
+
+require $clarolineRepositorySys.'/auth/extauth/extAuthProcess.inc.php';
+
+?>
