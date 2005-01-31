b0e34e29b6c1b8905e9af2f4b380e887d69a7459
peeters <peeters@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-01-31 11:36:42 +0000

    last fixes to make "document.php" register_globals_off compatible

--- claroline/document/document.php
+++ claroline/document/document.php
@@ -1,12 +1,12 @@
 <?php // $Id$
-/*
-+----------------------------------------------------------------------+
-| CLAROLINE 1.6
-+----------------------------------------------------------------------+
-| Copyright (c) 2001, 2004 Universite catholique de Louvain (UCL)      |
-+----------------------------------------------------------------------+
+
+//----------------------------------------------------------------------
+// CLAROLINE
+//----------------------------------------------------------------------
+// Copyright (c) 2001-2003 Universite catholique de Louvain (UCL)
+//----------------------------------------------------------------------
 // This program is under the terms of the GENERAL PUBLIC LICENSE (GPL)
-// as published by the FREE SOFTWARE FOUNDATION. The GPL is available 
+// as published by the FREE SOFTWARE FOUNDATION. The GPL is available
 // through the world-wide-web at http://www.gnu.org/copyleft/gpl.html
 //----------------------------------------------------------------------
 // Authors: see 'credits' file
@@ -45,7 +45,7 @@ require '../inc/claro_init_global.inc.php';
  * Library for the file display
  */
 
-include("../inc/lib/fileDisplay.lib.php");
+require '../inc/lib/fileDisplay.lib.php';
 
 /*
  * Lib for event log, stats & tracking
