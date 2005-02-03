91a1042423f5cd610bc1d0be81263e797155e01c
moosh <moosh@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-02-03 09:02:12 +0000

    refine  comments

--- claroline/inc/conf/def/CLWRK.def.conf.inc.php
+++ claroline/inc/conf/def/CLWRK.def.conf.inc.php
@@ -1,16 +1,16 @@
 <?php //$Id$
 // TOOL
-$conf_def['config_code']='CLWRK';
+$conf_def['config_code'] = 'CLWRK';
 $conf_def['config_file'] = 'work.conf.inc.php';
-$conf_def['config_name']='Quota';
+$conf_def['config_name'] = 'Quota';
 $conf_def['description'] = 'Assignment tool. this is a course tool';
-$conf_def['section']['storage']['label']='Quota';
+$conf_def['section']['storage']['label']      = 'Quota';
 $conf_def['section']['storage']['properties'] = 
 array ( 'CONFVAL_MAX_FILE_SIZE_PER_WORKS'
       );
       
 //PROPERTIES
-$conf_def_property_list['CONFVAL_MAX_FILE_SIZE_PER_WORKS']['label']     = 'max size for an assignment';
+$conf_def_property_list['CONFVAL_MAX_FILE_SIZE_PER_WORKS']['label']     = 'Max size for an assignment';
 $conf_def_property_list['CONFVAL_MAX_FILE_SIZE_PER_WORKS']['default']   = '200000000';
 $conf_def_property_list['CONFVAL_MAX_FILE_SIZE_PER_WORKS']['unit' ]     = 'bytes';
 $conf_def_property_list['CONFVAL_MAX_FILE_SIZE_PER_WORKS']['type' ]     = 'integer';
