4c492bb64476cc7c2f799b1d802d967d075b6ffd
seb <seb@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-02-08 09:58:34 +0000

    Add english translation for missing vars to avoid a display error

--- claroline/inc/htmlarea/lang/pt.js
+++ claroline/inc/htmlarea/lang/pt.js
@@ -3,9 +3,7 @@
 
 HTMLArea.I18N = {
 
-	// the following should be the filename without .js extension
-	// it will be used for automatically load plugin language.
-	lang: "pt_br",
+	lang: "pt",
 
 	tooltips: {
 		bold:           "Negrito",
@@ -33,5 +31,38 @@ HTMLArea.I18N = {
 		about:          "Sobre",
 		help:           "Ajuda",
 		textindicator:  "Estilo Atual"
+	},
+
+	buttons: {
+		"ok":           "OK",
+		"cancel":       "Cancel"
+	},
+
+	msg: {
+		"Path":         "Path",
+		"TEXT_MODE":    "You are in TEXT MODE.  Use the [<>] button to switch back to WYSIWYG.",
+
+		"IE-sucks-full-screen" :
+		// translate here
+		"The full screen mode is known to cause problems with Internet Explorer, " +
+		"due to browser bugs that we weren't able to workaround.  You might experience garbage " +
+		"display, lack of editor functions and/or random browser crashes.  If your system is Windows 9x " +
+		"it's very likely that you'll get a 'General Protection Fault' and need to reboot.\n\n" +
+		"You have been warned.  Please press OK if you still want to try the full screen editor."
+	},
+
+	dialogs: {
+		"Cancel"                                            : "Cancel",
+		"Insert/Modify Link"                                : "Insert/Modify Link",
+		"New window (_blank)"                               : "New window (_blank)",
+		"None (use implicit)"                               : "None (use implicit)",
+		"OK"                                                : "OK",
+		"Other"                                             : "Other",
+		"Same frame (_self)"                                : "Same frame (_self)",
+		"Target:"                                           : "Target:",
+		"Title (tooltip):"                                  : "Title (tooltip):",
+		"Top frame (_top)"                                  : "Top frame (_top)",
+		"URL:"                                              : "URL:",
+		"You must enter the URL where this link points to"  : "You must enter the URL where this link points to"
 	}
 };
