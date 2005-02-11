1c58523933f328586a6c2b4c5654d91a2b80d211
moosh <moosh@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-02-11 21:12:17 +0000

    complete doc

--- claroline/inc/conf/def/CLDOC.def.conf.inc.php
+++ claroline/inc/conf/def/CLDOC.def.conf.inc.php
@@ -1,8 +1,19 @@
 <?php //$Id$
-// TOOL
+/**
+ * This file describe the parameter for CLDOC config file
+ * @author Hugues Peeters <peeters@ipm.ucl.ac.be>
+ * @version CLAROLINE 1.6
+ * @copyright &copy; 2001-2005 Universite catholique de Louvain (UCL)
+ * @license This program is under the terms of the GENERAL PUBLIC LICENSE (GPL)
+ * as published by the FREE SOFTWARE FOUNDATION. The GPL is available 
+ * through the world-wide-web at http://www.gnu.org/copyleft/gpl.html
+ * @see http://www.claroline.net/wiki/config_def/
+ * @package CLDOC
+ */
+
 $conf_def['config_code']='CLDOC';
 $conf_def['config_file']='CLDOC___.conf.php';
-$conf_def['config_name']='general setting for document tool';
+$conf_def['config_name']='General setting for document tool';
 // $conf_def['config_repository']=''; Disabled = includePath.'/conf'
 $conf_def['description'] = 'Document tool. This is a course tool';
 $conf_def['section']['quota']['label']='quota';
@@ -18,6 +29,7 @@ $conf_def_property_list['maxFilledSpace_for_course']
         ,'unit'      => 'bytes'
         ,'type'      => 'integer'
         ,'container' => 'VAR'
+        ,'acceptedValue' => array('min' => '1024')
         );
 
 $conf_def_property_list['maxFilledSpace_for_groups']
