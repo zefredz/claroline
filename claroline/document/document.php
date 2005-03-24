66ee85f1802cb852e73d190a4a475e583cfe3235
mathieu <mathieu@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-03-24 13:43:21 +0000

    replace langDoc by langDocument

--- claroline/document/document.php
+++ claroline/document/document.php
@@ -1027,7 +1027,7 @@ function confirmation (name)
 }
 </script>";
 
-$nameTools = $langDoc;
+$nameTools = $langDocument;
 
 $QUERY_STRING=''; // used for the breadcrumb 
                   // when one need to add a parameter after the filename
@@ -1040,7 +1040,7 @@ $cmdParentDir  = rawurlencode($parentDir);
 
 //display toot title and subtitle
 
-$titleElement['mainTitle'] = $langDoc;
+$titleElement['mainTitle'] = $langDocument;
 if ( $_gid && $is_groupAllowed) $titleElement['subTitle'] = $_group['name'];
 
 claro_disp_tool_title($titleElement, 
@@ -1138,7 +1138,7 @@ claro_disp_tool_title($titleElement,
 				. "<tr>\n"
 				. "<th class=\"superHeader\" colspan=\"". $colspan . "\" align=\"left\">\n"
 				. "<img src=\"".$clarolineRepositoryWeb."img/opendir.gif\" align=\"absbottom\" vspace=\"2\" hspace=\"5\" alt=\"\">\n"
-				. $langDoc 
+				. $langDocument
 				. "<small>&nbsp;&nbsp;[&nbsp;<a href=\""
 				. $_SERVER['PHP_SELF']."\">" . $langBackToDir . "</a>&nbsp;]\n"
                 . "&nbsp;&nbsp;[&nbsp;<a href=\"" .  $_SERVER['PHP_SELF']
@@ -1289,7 +1289,7 @@ claro_disp_tool_title($titleElement,
 				. "\" align=\"left\">\n"
 				. "<img src=\"".$clarolineRepositoryWeb
 				. "img/opendir.gif\" align=\"absbottom\" vspace=\"2\" hspace=\"5\" alt=\"\">\n"
-            	. $langDoc."<small>&nbsp;&nbsp;[&nbsp;<a href=\"". $_SERVER['PHP_SELF']
+            	. $langDocument."<small>&nbsp;&nbsp;[&nbsp;<a href=\"". $_SERVER['PHP_SELF']
 				. "\">" . $langBackToDir . "</a>&nbsp;]</small>\n"
 				. "</th>\n"
 				. "</tr>\n"
