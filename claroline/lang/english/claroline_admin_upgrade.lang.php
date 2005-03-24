fe1b1c34490b58c6fdb02490fb566d26553d06a7
mathieu <mathieu@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-03-24 13:03:43 +0000

    add new language file

---
+++ claroline/lang/english/claroline_admin_upgrade.lang.php
@@ -0,0 +1,143 @@
+<?php
+$langAddIntro = "Add introduction text";
+$langAdministration = "Administration";
+$langAgenda = "Agenda";
+$langAnnouncement = "Announcements";
+$langCancel = "Cancel";
+$langChat = "Chat";
+$langCodeAppearAlready = "This official code already appears in a previous line of the CSV file.";
+$langCodeUsed = "This official code is already used by another user.";
+$langConfirm = "Confirm";
+$langConfirmYourChoice = "Please confirm your choice";
+$langCourseHome = "Course Home";
+$langCourseManager = "Course manager";
+$langDay_of_weekNames = "Array";
+$langDelete = "Delete";
+$langDescriptionCours = "Course description";
+$langDocument = "Documents and Links";
+$langDone = "Steps done";
+$langEmpty = "empty";
+$langExercises = "Exercises";
+$langFailed = "<span style=\"color: red\">Failed</span>";
+$langFirstDefOfThisValue = "!!! First Definition of this value !!!";
+$langForums = "Forums";
+$langGroups = "Groups";
+$langHelp = "Help";
+$langIntroStep1 = "<p>The <em>Claroline Upgrade Tool</em> is going to proceed to the main setting upgrade.
+
+                These settings were stored into claroline/include/config.inc.php in your previous platform version.</p>";
+$langIntroStep2 = "<p>Now, the <em>Claroline Upgrade Tool</em> is going to upgrade the data stored into the main Claroline tables
+
+                    (users, course categories, tools list, ...) and set it to be compatible with the new Claroline version.</p>
+
+                   <p class=\"help\">Note: According to the speed of your server or the amount of data stored on your platform, this
+
+                   operation may take some time.</p>";
+$langIntroStep3 = "<p>Now the <em>Claroline Upgrade Tool</em> is going to update course data (directories and database tables) one by one.
+
+                   <p class=\"help\">Note: According to the speed of your server or the amount of data stored on your platform,
+
+                   this operation may take some time.</p>";
+$langIntroStep3Run = "<p>The <em>Claroline Upgrade Tool</em> proceeds to the courses data upgrade</p>";
+$langLaunchRestoreCourseRepository = "Launch restore of the course repository";
+$langLaunchStep1 = "<p><button onclick=\"document.location='%s';\">Launch platform main settings upgrade</button></p>";
+$langLaunchStep2 = "<p><button onclick=\"document.location='%s';\">Launch main platform tables upgrade</button></p>";
+$langLaunchStep3 = "<p><button onclick=\"document.location='%s';\">Launch course data upgrade</button></p>";
+$langLearningPath = "Learning Path";
+$langLogin = "Login";
+$langLogout = "Logout";
+$langMailAppearAlready = "This mail appears already in a previous line of the CSV file.";
+$langMailSynthaxError = "Mail synthax error.";
+$langMailUsed = "Mail is already used by another user.";
+$langMakeABackupBefore = "<p>The <em>Claroline Upgrade Tool</em> will retrieve the data of your previous Claroline
+
+installation and set them to be compatible with the new Claroline version. This upgrade proceeds in three steps:</p>
+
+
+
+<ol>
+
+
+
+<li>It will get your previous platform main settings and put them in a new configuration files</li>
+
+
+
+<li>It will set the main Claroline tables (user, course categories, course list, ...) to be compatible with the new data structure.</li>
+
+
+
+<li>It will update one by one each course data (directories, database tables, ...)</li>
+
+
+
+</ol>
+
+
+
+<p>Before proceeding to this upgrade:</p>
+
+
+
+<table>
+
+<tbody>
+
+<tr valign=\"top\"><td>-</td><td>Make a complete backup of all your platform data (files and databases)</td><td>%s</td></tr>
+
+
+
+</tbody>
+
+</table>
+
+<p>You won't be allowed to start the upgrade process before this point is marked as 'done'.</p>
+
+";
+$langManager = "Manager";
+$langModify = "Modify";
+$langModifyProfile = "Modify my profile";
+$langMonthNames = "Array";
+$langMyAgenda = "My calendar";
+$langMyCourses = "My course list";
+$langNbCoursesUpgraded = "<p style=\"text-align: center\"><strong>%s courses on %s already upgraded</strong><br /></p>";
+$langNext = "Next";
+$langNextStep = "<p><button onclick=\"document.location='%s';\">Next ></button></p>";
+$langNo = "No";
+$langNotAllowed = "Not allowed";
+$langOk = "Ok";
+$langOtherCourses = "Course list";
+$langPassword = "Password";
+$langPasswordSimple = "Password given is too simple or too close to the username.";
+$langPlatformAdministration = "Platform Administration";
+$langPoweredBy = "Powered by";
+$langReg = "Registration";
+$langRestoreCourseRepository = "Restore course repository";
+$langStudent = "Student";
+$langSucceed = "succeed";
+$langSwitchEditorToTextConfirm = "This command is going to remove the current text layout. Do you want to continue ?";
+$langTextEditorDisable = "Disable text editor";
+$langTextEditorEnable = "Enable text editor";
+$langTitleUpgrade = "<h2>Claroline Upgrade Tool<br />from %s to %s </h2>";
+$langTodo = "Steps todo";
+$langUpgrade = "Upgrade";
+$langUpgradeStep0 = "Backup confirm";
+$langUpgradeStep1 = "Step 1 of 3: platform main settings";
+$langUpgradeStep2 = "Step 2 of 3: main platform tables upgrade";
+$langUpgradeStep3 = "Step 3 of 3: courses upgrade";
+$langUserName = "User name";
+$langUsernameAppearAlready = "This username appears already in a previous line of the CSV file.";
+$langUsernameUsed = "This username is already used by another user.";
+$langUsers = "Users";
+$langViewMode = "View mode";
+$langWork = "Assignments";
+$langYes = "Yes";
+$lang_click_here = "click here";
+$lang_enter_your_user_name_and_password = "Enter your user name and password";
+$lang_footer_p_CourseManager = "Manager(s) for %s";
+$lang_if_you_dont_have_a_user_account_profile_on = "If you don't have a user account on";
+$lang_if_you_wish_to_enroll_to_this_course = "If you wish to enrol on this course";
+$lang_p_platformManager = "Administrator for %s";
+$lang_this_course_is_protected = "This course is protected";
+$lang_your_user_profile_doesnt_seem_to_be_enrolled_to_this_course = "Your user profile doesn't seem to be enrolled on this course";
+?>
