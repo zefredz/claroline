422cd49c407ad4a282517f4343ba668dd09d2ef2
mathieu <mathieu@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-03-31 13:12:33 +0000

    add create table for sso and config_file

--- claroline/admin/upgrade/sql_statement_main_db.php
+++ claroline/admin/upgrade/sql_statement_main_db.php
@@ -1,8 +1,8 @@
 <?php // $Id$
 
 /*
-    Sql query to update main database
-*/
+ *  Sql query to update main database
+ */
 
 $lenForDbNameOfACourse = 20 + 30; // (max for prefix + max  for code course);
 
@@ -38,5 +38,22 @@ $sqlForUpdate[] = "ALTER IGNORE TABLE `user` CHANGE `creatorId` `creatorId` int(
 
 // Update table cours
 $sqlForUpdate[] = " ALTER IGNORE TABLE `cours` CHANGE `dbName` `dbName` varchar(".$lenForDbNameOfACourse.") default NULL";
+
+// Create new table config_file
+$sqlForUpdate[] = "CREATE TABLE `config_file` (
+  `config_code` varchar(30) NOT NULL default '',
+  `config_hash` varchar(40) NOT NULL default '',
+  PRIMARY KEY  (`config_code` )
+) TYPE=MyISAM  AVG_ROW_LENGTH=48";
+
+
+// Create new table sso
+$sqlForUpdate[] = "CREATE TABLE `sso` (
+  `id` int(11) NOT NULL auto_increment,
+  `cookie` varchar(255) NOT NULL default '',
+  `rec_time` datetime NOT NULL default '0000-00-00 00:00:00',
+  `user_id` int(11) NOT NULL default '0',
+  PRIMARY KEY  (`id`)
+) TYPE=MyISAM";
 
 ?>
