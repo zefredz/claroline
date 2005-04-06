d8f364632cd0794b988910e8bf239f044c1da0f6
mathieu <mathieu@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-04-06 19:51:02 +0000

    secure access on page --> if ! is_allowedToUseSDK display claro_disp_auth_form

--- claroline/admin/devTools/index.php
+++ claroline/admin/devTools/index.php
@@ -10,55 +10,44 @@
  * 
  * This is the index page of sdk tools
 */
-$langFillToolCourses = 'Fill tools of a course (lorem ipsum filler)';
+
 require '../../inc/claro_init_global.inc.php';
 
-$is_allowedToUseSDK 	= $is_platformAdmin;
-if ($is_allowedToUseSDK) claro_disp_auth_form(); 
+$langFillToolCourses = 'Fill tools of a course (lorem ipsum filler)';
+
+$is_allowedToUseSDK = $is_platformAdmin;
+
+if (! $is_allowedToUseSDK) claro_disp_auth_form(); 
 
 $nameTools = $langDevTools;
+
 $interbredcrump[]= array ('url'=>'../index.php', 'name'=> $langAdmin);
-@include('../checkIfHtAccessIsPresent.php');
+
 include($includePath.'/claro_init_header.inc.php');
+
 claro_disp_tool_title(
 	array(
 	'mainTitle'=>$nameTools,
 	'subTitle'=>$siteName.' - '.$clarolineVersion
 	)
 	);
-claro_disp_msg_arr($controlMsg);
-if ($is_allowedToUseSDK)
-{
-?>
-<H4>
-	<?php echo $langTranslations ?>
-</H4>
-<ul>
-	<LI>
-		<a href="../xtra/sdk/translation_index.php"><?php echo $langTranslations ?></a>
-	</LI>
-</uL>
-<H4><?php echo $langFilling ?></H4>
-<UL>
-	<LI>
-		<a href="./fillUser.php"><?php echo $langFillUsers ?></a>
-	</LI>
-	<LI>
-		<a href="./fillCourses.php"><?php echo $langFillCourses ?></a>(and  subscribe some existing students)
-	</LI>
-	<LI>
-		<a href="./fillTree.php"><?php echo $langFillTree ?></a>
-	</LI>
-	<LI>
-		<a href="./fillToolCourses.php"><?php echo $langFillToolCourses ?></a>
-	</LI>
-</UL>
-<?php
 
-}
-else
-{
-	echo $lang_no_access_here;
-}
+?>
+
+<h4><?php echo $langTranslations ?></h4>
+
+<ul>
+<li><a href="../xtra/sdk/translation_index.php"><?php echo $langTranslations ?></a></li>
+</ul>
+
+<h4><?php echo $langFilling ?></h4>
+<ul>
+ <li><a href="./fillUser.php"><?php echo $langFillUsers ?></a></li>
+ <li><a href="./fillCourses.php"><?php echo $langFillCourses ?></a>(and  subscribe some existing students)</li>
+ <li><a href="./fillTree.php"><?php echo $langFillTree ?></a></li>
+ <li><a href="./fillToolCourses.php"><?php echo $langFillToolCourses ?></a></li>
+</ul>
+
+<?php
 include($includePath."/claro_init_footer.inc.php");
 ?>
