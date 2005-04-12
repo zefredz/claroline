8bcdc7acdc9cac455e532aa7bbbf3cae130d4efd
moosh <moosh@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-04-12 18:52:25 +0000

    -phpDoc -change state of values

--- claroline/inc/installedVersion.inc.php
+++ claroline/inc/installedVersion.inc.php
@@ -1,25 +1,38 @@
 <?php // $Id$
-
-$stable = false;
+/**
+ * CLAROLINE 
+ *
+ * Set value to detect if script set version is same than upgrade state
+ * @var $version_file_cvs contain the version of script set 
+ *                        (kernel sctructure, document structure, config value, ...)
+ * @var $version_db_cvs   contain the version of script set 
+ *                        (different from _file_ because some time there is nothing to change in db)
+ *
+ *
+ * @version 1.6 $Revision$
+ *
+ * @copyright (c) 2001-2005 Universite catholique de Louvain (UCL)
+ *
+ * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE 
+ *
+ * @package UPGRADE
+ *
+ * @author Claro Team <cvs@claroline.net>
+ *
+ */
+$stable = TRUE;
 $clarolinePhase = 'RC';
-$is_upgrade_available = false;
+$is_upgrade_available = TRUE;
 
 // var version_db  max. 10 chars
 
-$version_file_cvs = '1.6.0.RC1';
-$version_db_cvs   = '1.6.0.RC1';
+$version_file_cvs = '1.6.0.ST';
+$version_db_cvs   = '1.6.0.ST';
 
 if (!$is_upgrade_available)
 {
 	$version_file_cvs = $version_file_cvs .".[unstable:".date("yzBs")."]";
 	$version_db_cvs	  = $version_db_cvs .".[unstable:".date("yzBs")."]";
 }
-
-// to keep compatibility the two next value are set
-// but  it's same name than values in main conf.
-// code would be parse to be able to remove these two lines /
-
-$clarolineVersion = $version_file_cvs;
-$versionDb = $version_db_cvs;
 
 ?>
