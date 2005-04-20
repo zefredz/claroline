9cec64ccf1dbf56aff190de067533f4b532ed370
mathieu <mathieu@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-04-20 10:07:57 +0000

    french rebuild complete and missing lang file

--- claroline/lang/french/missing.lang.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$langAuthentication = "Authentication";
-$langConfigurationFile = "Configuration file";
-$langCreateUserAccount = "Create user account";
-$langMultipleAnswers = "Multiple answers";
-$langStartAgain = "Start again";
-$langUniqueAnswer = "Unique answer";
-$langUpgradeSucceed = "The <em>Claroline Upgrade Tool</em> has completly upgraded your platform.";
-?>
