9c6df101de654e6ca5bf65170fbd07e2e9e71f7c
mathieu <mathieu@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-04-26 12:48:51 +0000

    don't display script generation and stats if no data extracted

--- claroline/admin/xtra/sdk/translation_index.php
+++ claroline/admin/xtra/sdk/translation_index.php
@@ -18,13 +18,29 @@ $langBuildEmptyLangFile = "Empty language file";
 
 require '../../../inc/claro_init_global.inc.php';
 include($includePath."/lib/debug.lib.inc.php");
+include ('lang/language.conf.php');
 
 $nameTools = $langTranslationTools;
 $urlSDK = $rootAdminWeb . 'xtra/sdk/'; 
+$table_exists = TRUE;
 
 // SECURITY CHECK
 
 if (!$is_platformAdmin) claro_disp_auth_form();
+
+// table
+
+$tbl_used_lang = '`' . $mainDbName . '`.`' . $mainTblPrefix . TABLE_USED_LANG_VAR . '`';
+$tbl_translation =  '`' . $mainDbName . '`.`' . $mainTblPrefix . TABLE_TRANSLATION . '`';
+
+$sql1 = " select count(*) from " . $tbl_used_lang;
+$sql2 = " select count(*) from " . $tbl_used_translation;
+
+mysql_query($sql1);
+if ( mysql_errno() == 1146 ) $table_exists = FALSE;
+
+mysql_query($sql2);
+if ( mysql_errno() == 1146 ) $table_exists = FALSE;
 
 // DISPLAY
 
@@ -45,6 +61,11 @@ claro_disp_tool_title('<img src="lang/language.png" style="vertical-align: middl
 <li><a href="lang/extract_var_from_script_file.php"><?php echo $langExtractFromScriptFile?></a></li>
 </ul>
 
+<?php
+if ( $table_exists == TRUE )
+{
+?>
+
 <h4><?php echo $langBuildLangFile?></h4>
 <ul>
 <li><a href="lang/build_devel_lang_file.php"><?php echo $langBuildCompleteLangFile?></a></li>
@@ -63,6 +84,9 @@ claro_disp_tool_title('<img src="lang/language.png" style="vertical-align: middl
 <ul>
 <li><a href="lang/progression_translation.php"><?php echo $langTranslationStatistics?></a></li>
 </ul>
+<?
+}
+?>
 
 <?php
 include($includePath."/claro_init_footer.inc.php");
