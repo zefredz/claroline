6bdbf0c48ebe3c707edb0384cfa2fbcacb8f0554
mathieu <mathieu@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-04-28 17:23:51 +0000

    sdk fill user : corrections + RG off & E_ALL

--- claroline/admin/devTools/fillUser.php
+++ claroline/admin/devTools/fillUser.php
@@ -20,67 +20,85 @@
  *
  */
 
-DEFINE('DISP_RESULT_INSERT'		,1);
-DEFINE('DISP_FORM_SET_OPTION'	,2);
-DEFINE('DISP_INSERT_COMPLETE'	,3);
-
-DEFINE('DEFAULT_MIN_QTY_STUDENT_REGISTRED_IN_COURSE',5);
-DEFINE('DEFAULT_SUFFIX_MAIL','@exemple.com');
-DEFINE('DEFAULT_QTY_TEACHER',5);
-DEFINE('DEFAULT_QTY_STUDENT',20);
-DEFINE('ADD_FIRSTNAMES_FROM_BASE',TRUE);
-DEFINE('ADD_NAMES_FROM_BASE ',TRUE);
-DEFINE('ADD_USERNAMES_FROM_BASE',TRUE);
-DEFINE('USE_FIRSTNAMES_AS_LASTNAMES',FALSE);
-
-unset($includePath);
-
 require '../../inc/claro_init_global.inc.php';
 
 // Security check
 if (!$is_platformAdmin) claro_disp_auth_form();
 
-//// Config tool
+DEFINE('DISP_RESULT_INSERT'		, __LINE__);
+DEFINE('DISP_FORM_SET_OPTION'	, __LINE__);
+DEFINE('DISP_INSERT_COMPLETE'	, __LINE__);
+
+// default_display
+$display = DISP_FORM_SET_OPTION;
+
+// constant
+DEFINE('DEFAULT_MIN_QTY_STUDENT_REGISTRED_IN_COURSE',5);
+DEFINE('DEFAULT_MAX_QTY_STUDENT_REGISTRED_IN_COURSE',40);
+DEFINE('DEFAULT_MIN_QTY_TEACHER_REGISTRED_IN_COURSE',1);
+DEFINE('DEFAULT_MAX_QTY_TEACHER_REGISTRED_IN_COURSE',5);
+DEFINE('DEFAULT_SUFFIX_MAIL','@example.com');
+DEFINE('DEFAULT_QTY_TEACHER',5);
+DEFINE('DEFAULT_QTY_STUDENT',20);
+DEFINE('ADD_FIRSTNAMES_FROM_BASE',TRUE);
+DEFINE('ADD_NAMES_FROM_BASE',TRUE);
+DEFINE('ADD_USERNAMES_FROM_BASE',TRUE);
+DEFINE('USE_FIRSTNAMES_AS_LASTNAMES',FALSE);
+DEFINE('CONFVAL_LIST_USER_ADDED',TRUE);
+
+// Config tool
 include($includePath.'/conf/course_main.conf.php');
 
-//// LIBS
+// LIBS
 include($includePath.'/lib/add_course.lib.inc.php');
 include($includePath.'/lib/debug.lib.inc.php');
 include($includePath.'/lib/fileManage.lib.php');
 include($includePath.'/conf/course_main.conf.php');
 
 $nameTools = $langAdd_users;
+
 $interbredcrump[]= array ('url'=>'../index.php', 'name'=> $langAdmin);
 $interbredcrump[]= array ('url'=>'index.php', 'name'=> $langDevTools);
 
 $tbl_mdb_names = claro_sql_get_main_tbl();
 $tbl_user      = $tbl_mdb_names['user'];
+
 $can_create_courses = (bool) ($is_allowedCreateCourse);
+$controlMsg = array();
 
 // fix setting
-$nc   = is_numeric($_REQUEST['nc'])?$_REQUEST['nc']:DEFAULT_MIN_QTY_STUDENT_REGISTRED_IN_COURSE;
-$smin = is_numeric($_REQUEST['smin'])?$_REQUEST['smin']:DEFAULT_MIN_QTY_STUDENT_REGISTRED_IN_COURSE;
-$smax = is_numeric($_REQUEST['smax'])?$_REQUEST['smax']:DEFAULT_MAX_QTY_STUDENT_REGISTRED_IN_COURSE;
-$pmin = is_numeric($_REQUEST['pmin'])?$_REQUEST['pmin']:DEFAULT_MIN_QTY_TEACHER_REGISTRED_IN_COURSE;
-$pmax = is_numeric($_REQUEST['pmax'])?$_REQUEST['pmax']:DEFAULT_MAX_QTY_TEACHER_REGISTRED_IN_COURSE;
+if ( isset($_REQUEST['create']) )
+{
+    $nbp    = (int) $_REQUEST['nbp'];
+    $nbs    = (int) $_REQUEST['nbs'];
+    $sfMail = strtoupper($_REQUEST['sfMail']); 
+    $nom    = $_REQUEST['nom'];
+    $prenom = $_REQUEST['prenom'];
+    $login  = $_REQUEST['login'];
+}
+else
+{
+    $nbp    = (int) DEFAULT_QTY_TEACHER;
+    $nbs    = (int) DEFAULT_QTY_STUDENT;
+    $sfMail = strtoupper(DEFAULT_SUFFIX_MAIL);
+    $nom    = '';
+    $prenom = '';
+    $login  = '';
+}
+    
+$nc     = (int) DEFAULT_MIN_QTY_STUDENT_REGISTRED_IN_COURSE;
+$smin   = (int) DEFAULT_MIN_QTY_STUDENT_REGISTRED_IN_COURSE;
+$smax   = (int) DEFAULT_MAX_QTY_STUDENT_REGISTRED_IN_COURSE;
+$pmin   = (int) DEFAULT_MIN_QTY_TEACHER_REGISTRED_IN_COURSE;
+$pmax   = (int) DEFAULT_MAX_QTY_TEACHER_REGISTRED_IN_COURSE;
 
-$sfMail = strtoupper($_REQUEST['sfMail']!=''?$_REQUEST['sfMail']:DEFAULT_SUFFIX_MAIL);
-$nbp = is_numeric($_REQUEST['nbp'])?$_REQUEST['nbp']:DEFAULT_QTY_TEACHER;
-$nbs = is_numeric($_REQUEST['nbs'])?$_REQUEST['nbs']:DEFAULT_QTY_STUDENT;
 $nbUsers = $nbp + $nbs;
 
-$display = DISP_FORM_SET_OPTION;
+$result = "";
 
-include($includePath.'/claro_init_header.inc.php');
-claro_disp_tool_title(
-	array(
-	'mainTitle'=>$nameTools
-	)
-	);
-claro_disp_msg_arr($controlMsg);
-if (isset($HTTP_POST_VARS['nbp']))
+if ( isset($_REQUEST['create']) && $nbUsers > 0 )
 {
-/* fillUSER */
+    /* fillUSER */
 
 	srand ((double) microtime() * 685435100);
 	$firstnames = array (
@@ -110,78 +128,99 @@ if (isset($HTTP_POST_VARS['nbp']))
 		$consonne	= array('','b','c','d','f','j','k','l','m','n','p','r','s','t','v','z');
 
 	$sqlUsers = "Select * from `".$tbl_user."`";
-	$resUsers = claro_sql_query($sqlUsers);
-	while ($users = mysql_fetch_array($resUsers,MYSQL_ASSOC))
-	{
-		if(ADD_FIRSTNAMES_FROM_BASE)	$firstnames[] 	= $users['prenom'];
-		if(ADD_NAMES_FROM_BASE)			$names[] 		= $users['nom'];
-		if(ADD_USERNAMES_FROM_BASE)		$usernames[] 	= $users['username'];
-	}
-	if(USE_FIRSTNAMES_AS_LASTNAMES)		$names 	= array_merge ( $names,$firstnames);
 
+	$resUsers = claro_sql_query($sqlUsers);
+
+	while ( $users = mysql_fetch_array($resUsers,MYSQL_ASSOC) )
+	{
+		if ( ADD_FIRSTNAMES_FROM_BASE )	$firstnames[] 	= $users['prenom'];
+		if ( ADD_NAMES_FROM_BASE )			$names[] 		= $users['nom'];
+		if ( ADD_USERNAMES_FROM_BASE )		$usernames[] 	= $users['username'];
+	}
+
+	if (USE_FIRSTNAMES_AS_LASTNAMES)	$names 	= array_merge ( $names,$firstnames);
 
 	unset($users);
 
-
-	for($noUser=0;$noUser<=($nbUsers*10); $noUser++)
+	for ( $noUser=0 ; $noUser<=($nbUsers*10) ; $noUser++ )
     {
-		$nom ='';
-		for($s=0;$s<rand(1,3); $s++)
+		$nom = '';
+		for( $s=0 ; $s<rand(1,3) ; $s++ )
 		{
 			$nom .= field_rand($consonne).field_rand($voyel).field_rand($consonne);
 		}
 		$names[] = $nom;
 	}
 
-	echo '<OL>';
-	for($noUser=0;$noUser<=$nbUsers; $noUser++)
+    $nbssAdded = 0;
+
+    $nbp_created = 0;
+
+	for( $noUser=0 ; $noUser<$nbUsers ; $noUser++ )
 	{
-		$statut = 5;
-		if ($nbp-- > 0) $statut = 1;
-		$nom = ucfirst(strToLower(field_rand($names)));
-		$prenom = ucfirst(strToLower(field_rand($firstnames)));
+		if ($nbp_created < $nbp) $statut = 1;
+        else                     $statut = 5;
+
+		$nom      = ucfirst(strToLower(field_rand($names)));
+		$prenom   = ucfirst(strToLower(field_rand($firstnames)));
 		$username = strToLower($nom);
 		$password = strToLower($nom.$prenom);
-		$email = strToLower($prenom.'.'.$noUser).$sfMail;
+		$email    = strToLower($prenom.'.'.$noUser).$sfMail;
+
 		$sqlInsertUser = "
-	INSERT INTO `".$tbl_user."`
-	(
-	`nom`, `prenom`,
-	`username`, `password`,
-	`email`, `statut`,
-	`creatorId`)
-	VALUES
-	('".$nom."', '".$prenom."',
-	'".$username."', '".$password."',
-	'".$email."', $statut,
-	'".$_uid."')
+        	INSERT INTO `".$tbl_user."`
+            	(
+        	    `nom`, `prenom`,
+            	`username`, `password`,
+            	`email`, `statut`,
+            	`creatorId`)
+        	VALUES
+        	('".$nom."', '".$prenom."',
+        	'".$username."', '".$password."',
+        	'".$email."', $statut,
+        	'".$_uid."')
 		";
 		claro_sql_query($sqlInsertUser);
+
 		$nbssAdded += mysql_affected_rows();
-		$users[]= $prenom." ".$nom.", L/P ".$username." / ".$password;
+		$users[] = $prenom." ".$nom.", L/P ".$username." / ".$password;
+
+        $nbp_created++;
 	}
-	$display=DISP_RESULT_INSERT;
+	$display = DISP_RESULT_INSERT;
 }
 
-//////////////// OUTPUT
+// OUTPUT
+
+include($includePath.'/claro_init_header.inc.php');
+
+claro_disp_tool_title( array('mainTitle'=>$nameTools));
+
+if ( count($controlMsg) )
+{
+    claro_disp_msg_arr($controlMsg);
+}
+
 switch ($display)
 {
 	case DISP_RESULT_INSERT :
         echo $lang_you_had_request; ?> :
 		<UL>
 			<LI>
-				<?php echo $_REQUEST["nbp"] ." " . $langTeachers; ?></LI>
+				<?php echo $nbp ." " . $langTeachers; ?></LI>
 			<LI>
-				<?php echo $_REQUEST["nbs"] ." " . $langStudents; ?>
+				<?php echo $nbs ." " . $langStudents; ?>
 			</LI>
 		</UL>
 <?php
 			echo $nbssAdded.' new users';
-			if (CONFVAL_LIST_USER_ADDED)
+			if ( CONFVAL_LIST_USER_ADDED )
 			{
-				echo '<OL><LI>';
-				echo implode('</LI><LI>',$users);
-				echo '</LI></OL>';
+				echo '<ol>' . "\n" 
+                    . '<li>' 
+				    . implode('</LI>'."\n".'<LI>',$users)
+				    . '</li>' . "\n" 
+                    .  '</ol>' . "\n";
 			}
 
 ?>
@@ -196,7 +235,7 @@ switch ($display)
 		<?php
 		break;
 	case DISP_FORM_SET_OPTION :
-		?><br><br>
+		?>
 <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST" enctype="multipart/form-data" target="_self">
 	<fieldset>
 		<legend >Comptes à créer</legend>
@@ -270,7 +309,7 @@ switch ($display)
 					</div>
 					<div>
 						<input type="radio" name="selLogin" value="fix">
-						<input type="text" id="selLoginFix" align="right" name="selLoginFix" value="" size="10" maxlength="25">
+						<input type="text" id="selLoginFix" align="right" name="login" value="" size="10" maxlength="25">
 						<Label for="selLoginFix" ><?php echo $langFree ?></Label>
 					</div>
 				</td>
@@ -294,6 +333,8 @@ switch ($display)
 	default : "hum erreur de display";
 
 }
+
+include($includePath.'/claro_init_footer.inc.php');
 
 function field_rand($arr)
 {
