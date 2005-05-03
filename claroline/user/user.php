02defb45c4a7b86a10bc130e01b0e58418b528ff
mathieu <mathieu@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-05-03 14:41:02 +0000

    refactoring user tool: * register_globals : off * E_ALL * use claro_pager

--- claroline/user/user.php
+++ claroline/user/user.php
@@ -17,9 +17,39 @@
  * @package CLUSR
  *
  */
+
+/*=====================================================================
+   Initialisation
+  =====================================================================*/
+
 $tlabelReq = 'CLUSR___';
+
 require '../inc/claro_init_global.inc.php';
-if (!($_cid)) claro_disp_select_course();
+
+if ( ! isset($_cid) ) claro_disp_select_course();
+
+if ( ! $is_courseAllowed ) claro_disp_auth_form();
+
+claro_set_display_mode_available(true);
+
+/*----------------------------------------------------------------------
+   Include Library
+  ----------------------------------------------------------------------*/
+
+include($includePath.'/lib/admin.lib.inc.php');
+include($includePath."/lib/pager.lib.php");
+include($includePath.'/lib/events.lib.inc.php');
+@include($includePath.'/lib/debug.lib.inc.php');
+
+/*----------------------------------------------------------------------
+  Stats
+  ----------------------------------------------------------------------*/
+
+event_access_tool($_tid, $_courseTool['label']);
+
+/*----------------------------------------------------------------------
+   JavaScript - Delete Confirmation
+  ----------------------------------------------------------------------*/
 
 $htmlHeadXtra[] =
 '
@@ -34,12 +64,11 @@ function confirmation (name)
 </script>
 ';
 
-include($includePath.'/lib/admin.lib.inc.php');
-@include($includePath.'/lib/debug.lib.inc.php');
+/*----------------------------------------------------------------------
+   Variables
+  ----------------------------------------------------------------------*/
 
-claro_set_display_mode_available(true);
-
-$step             = (isset($nbUsersPerPage)?$nbUsersPerPage:50);
+$userPerPage = isset($nbUsersPerPage)?$nbUsersPerPage:50;
 
 $is_allowedToEdit = claro_is_allowed_to_edit();
 
@@ -47,43 +76,51 @@ $can_add_user     = (bool) (   $is_courseAdmin
                      && isset($is_coursemanager_allowed_to_add_user)
                      && $is_coursemanager_allowed_to_add_user)
                      || $is_platformAdmin;
-$currentCourse    = $currentCourseID  = $_course['sysCode'];
 
-/**
- * DB tables definition
- */
+$currentCourse = $currentCourseID  = $_course['sysCode'];
+
+/*----------------------------------------------------------------------
+  DB tables definition
+  ----------------------------------------------------------------------*/
 
 $tbl_cdb_names = claro_sql_get_course_tbl();
 $tbl_mdb_names = claro_sql_get_main_tbl();
+
 $tbl_rel_course_user = $tbl_mdb_names['rel_course_user'  ];
 $tbl_users           = $tbl_mdb_names['user'             ];
 $tbl_courses_users   = $tbl_rel_course_user;
+
 $tbl_rel_users_groups= $tbl_cdb_names['group_rel_team_user'    ];
 $tbl_groups          = $tbl_cdb_names['group_team'             ];
 
-////////// WORKS /////////////
+/*=====================================================================
+  Main section
+  =====================================================================*/
 
-if ($is_allowedToEdit)
+if ( $is_allowedToEdit )
 {
     $disp_tool_link = TRUE;
+
     // Unregister user from course
     // (notice : it does not delete user from claroline main DB)
 
-    if($unregister)
+    if ( isset($_REQUEST['unregister']) && $_REQUEST['unregister'] )
     {
         // delete user from course user list
-
-        $done = remove_user_from_course($user_id, $_cid);
-    if ($done)
+        if ( remove_user_from_course($user_id, $_cid) )
         {
-           $dialogBox =$langUserUnsubscribedFromCourse;
+           $dialogBox = $langUserUnsubscribedFromCourse;
         }
         else
         {
-           $dialogBox =$langUserNotUnsubscribedFromCourse;
+           $dialogBox = $langUserNotUnsubscribedFromCourse;
         }
    }
 }    // end if allowed to edit
+
+/*----------------------------------------------------------------------
+   Get total user
+  ----------------------------------------------------------------------*/
 
 $sqlNbUser = 'SELECT count(user.user_id) `nb_users`
               FROM `'.$tbl_rel_course_user.'` `cours_user`,
@@ -92,33 +129,83 @@ $sqlNbUser = 'SELECT count(user.user_id) `nb_users`
               AND cours_user.user_id = `user`.user_id';
 
 $userTotalNb = claro_sql_query_fetch_all($sqlNbUser);
+
 $userTotalNb = $userTotalNb[0]['nb_users'];
+
+/*----------------------------------------------------------------------
+   Get User List
+  ----------------------------------------------------------------------*/
+
+$sqlGetUsers ='SELECT `user`.`user_id`, `user`.`nom`, `user`.`prenom`, 
+                      `user`.`email`, `cours_user`.`statut`, 
+                      `cours_user`.`tutor`, `cours_user`.`role`
+               FROM `'.$tbl_users.'` `user`, `'.$tbl_rel_course_user.'` `cours_user`
+               WHERE `user`.`user_id`=`cours_user`.`user_id`
+               AND `cours_user`.`code_cours`="'.$currentCourseID.'"
+               ORDER BY `cours_user`.`statut` ASC, `cours_user`.`tutor` DESC,
+                        UPPER(`user`.`nom`), UPPER(`user`.`prenom`) ';
+
+if ( !isset($_REQUEST['offset']) )
+{
+    $offset = "0";
+}
+else
+{
+    $offset = $_REQUEST['offset'];
+}
+
+$myPager = new claro_sql_pager($sqlGetUsers, $offset, $userPerPage);
+$userList = $myPager->get_result_list();
+
+/*----------------------------------------------------------------------
+  Get groups
+  ----------------------------------------------------------------------*/
+
+foreach ( $userList as $thisUser )
+{
+    $users[$thisUser['user_id']]    = $thisUser;
+    $usersId[]    = $thisUser['user_id'];
+}
+
+$sqlGroupOfUsers = "SELECT `ug`.`user` uid, `ug`.`team` team, 
+                    `sg`.`name` nameTeam
+                    FROM `".$tbl_rel_users_groups."` `ug`
+                    LEFT JOIN `".$tbl_groups."` `sg`
+                    ON `ug`.`team` = `sg`.`id`
+                    WHERE `ug`.`user` IN (".implode(",",$usersId).")";
+
+$resultUserGroup = claro_sql_query($sqlGroupOfUsers);
+
+$usersGroup = array();
+
+while ($thisAffiliation = mysql_fetch_array($resultUserGroup,MYSQL_ASSOC))
+{
+    $usersGroup[$thisAffiliation['uid']][$thisAffiliation['team']]['nameTeam'] = $thisAffiliation['nameTeam'];
+}
+
+/*=====================================================================
+  Display section
+  =====================================================================*/
 
 $nameTools = $langUsers;
 
-///////////// OUTPUTS ///////////
-
-if ( ! $is_courseAllowed) claro_disp_auth_form();
+// Display header
 
 include($includePath.'/claro_init_header.inc.php');
-
-//stats
-include($includePath.'/lib/events.lib.inc.php');
-event_access_tool($_tid, $_courseTool['label']);
 
 claro_disp_tool_title($nameTools.' ('.$langUserNumber.' : '.$userTotalNb.')',
             $is_allowedToEdit ? 'help_user.php' : FALSE);
 
 // Display Forms or dialog box(if needed)
 
-if($dialogBox)
+if ( !empty($dialogBox) )
 {
     claro_disp_message_box($dialogBox);
 }
 
-// display tool links
+// Display tool links
 
-if ($disp_tool_link)
+if ( $disp_tool_link )
 {
     echo "<p>";
     if ($can_add_user)
@@ -143,60 +230,15 @@ if ($disp_tool_link)
 <?php
 }
 
-/*==========================
-      DISPLAY USERS LIST
-  ==========================*/
+/*----------------------------------------------------------------------
+   Display pager
+  ----------------------------------------------------------------------*/
 
-// PAGER - don't show the pager link if there are less than 50 user
+$myPager->disp_pager_tool_bar($_SERVER['PHP_SELF']);
 
-if ($userTotalNb > $step)
-{
-    if(!isset($offset))
-    {
-        $offset = 0;
-    }
-
-    $next     = $offset + $step;
-    $previous = $offset - $step;
-
-    $navLink = '<table summary="'.$langSummaryNavBar.'" width="100%" border="0">'."\n"
-              .'<tr>'."\n"
-              .'<td align="left">'
-              ;
-
-    if ($previous >= 0)
-    {
-        $navLink .= '<small><a href="'.$_SERVER['PHP_SELF'].'?offset='.$previous.'" rel="next" >&lt;&lt; </a></small>';
-    }
-
-    $navLink .= '</td>'."\n"
-             .  '<td align="right">';
-
-    if ($next < $userTotalNb)
-    {
-        $navLink .= '<small><a href="'.$_SERVER['PHP_SELF'].'?offset='.$next.'">&gt;&gt;</a></small>';
-    }
-
-    $navLink .= '</td>'."\n"
-               .'</tr>'."\n"
-               .'</table>'."\n"
-               ;
-}
-else
-{
-    $offset = 0;
-}
-
-echo $navLink;
-
-// NOTE !! NEED ALSO 'SHOW ALL USERS'
-
-$i= $offset;
-
-/*----------------------------------------
-              COLUMN HEADERS
- --------------------------------------*/
-
+/*----------------------------------------------------------------------
+   Display table header
+  ----------------------------------------------------------------------*/
 
 echo '<table class="claroTable emphaseLine" '
    . 'width="100%" cellpadding="2" cellspacing="1" '
@@ -232,73 +274,16 @@ echo '</tr>'."\n"
    . '<tbody>'."\n"
    ;
 
-/*==========================
-      DB USERS SEARCH
-  ==========================*/
+/*----------------------------------------------------------------------
+   Display users
+  ----------------------------------------------------------------------*/
 
-$resultUsers = claro_sql_query("SELECT `user`.`user_id`, `user`.`nom`, `user`.`prenom`, `user`.`email`,
-                              `cours_user`.`statut`, `cours_user`.`tutor`, `cours_user`.`role`,
-                               `ug`.`team` ,
-                               `sg`.`name` nameTeam
+$i = $offset;
+$previousUser = -1;
 
-                        FROM `".$tbl_users."` `user`, `".$tbl_rel_course_user."` `cours_user`
-                        LEFT JOIN `".$tbl_rel_users_groups."` `ug`
-                        ON `user`.`user_id`=`ug`.`user`
-                        LEFT JOIN `".$tbl_groups."` `sg`
-                        ON `ug`.`team` = `sg`.`id`
-                        WHERE `user`.`user_id`=`cours_user`.`user_id`
-                        AND `cours_user`.`code_cours`='".$currentCourseID."'
+reset($userList);
 
-                        ORDER BY
-                            `cours_user`.`statut` ASC,
-                            `cours_user`.`tutor` DESC,
-                            UPPER(`user`.`nom`),
-                            UPPER(`user`.`prenom`),
-                            UPPER(`sg`.`name`)
-
-                        LIMIT $offset, $step"); // ORDER BY cours_user.statut, tutor DESC, nom, prenom
-
-$sqlGetUsers ='SELECT `user`.`user_id`, `user`.`nom`, `user`.`prenom`, 
-                      `user`.`email`, `cours_user`.`statut`, 
-                      `cours_user`.`tutor`, `cours_user`.`role`
-               FROM `'.$tbl_users.'` `user`, `'.$tbl_rel_course_user.'` `cours_user`
-               WHERE `user`.`user_id`=`cours_user`.`user_id`
-               AND `cours_user`.`code_cours`="'.$currentCourseID.'"
-               ORDER BY `cours_user`.`statut` ASC, `cours_user`.`tutor` DESC,
-                        UPPER(`user`.`nom`), UPPER(`user`.`prenom`)
-               LIMIT '.$offset.', '.$step;
-
-$resultUsers = claro_sql_query($sqlGetUsers);
-
-// ORDER BY cours_user.statut, tutor DESC, nom, prenom
-while ($thisUser = mysql_fetch_array($resultUsers,MYSQL_ASSOC))
-{
-    $users[$thisUser['user_id']]    = $thisUser;
-    $usersId[]    = $thisUser['user_id'];
-}
-
-$sqlGroupOfUsers = "SELECT `ug`.`user` uid, `ug`.`team` team, 
-                    `sg`.`name` nameTeam
-                    FROM `".$tbl_rel_users_groups."` `ug`
-                    LEFT JOIN `".$tbl_groups."` `sg`
-                    ON `ug`.`team` = `sg`.`id`
-                    WHERE `ug`.`user` IN (".implode(",",$usersId).")";
-
-$resultUserGroup = claro_sql_query($sqlGroupOfUsers);
-
-while ($thisAffiliation = mysql_fetch_array($resultUserGroup,MYSQL_ASSOC))
-{
-    $usersGroup[$thisAffiliation['uid']][$thisAffiliation['team']]['nameTeam'] = $thisAffiliation['nameTeam'];
-}
-
-/*==========================
-      USERS LIST DISPLAY
-  ==========================*/
-
-$previousUser = '';
-reset($users);
-
-while (list(,$thisUser) = each($users))
+foreach ( $userList as $thisUser )
 {
     // User name column
     $i++;
@@ -314,14 +299,15 @@ while (list(,$thisUser) = each($users))
        // User role column
        . '<td headers="role u'.$i.'" align="left">'.$thisUser['role'].'</td>'."\n"
        ;
+    
     // User group column
-    $userGroups = $usersGroup[$thisUser['user_id']];
-    if($userGroups == NULL)    // NULL and not '0' because team can be inexistent
+    if ( !isset ($usersGroup[$thisUser['user_id']]) )    // NULL and not '0' because team can be inexistent
     {
         echo '<td headers="team" > - </td>'."\n";
     }
     else
     {
+        $userGroups = $usersGroup[$thisUser['user_id']];
         echo '<td headers="team u'.$i.'">'."\n";
         reset($userGroups);
         while (list($thisGroupsNo,$thisGroupsName)=each($userGroups))
@@ -334,15 +320,13 @@ while (list(,$thisUser) = each($users))
         echo '</td>'."\n";
     }
 
-
     if ($previousUser == $thisUser['user_id'])
     {
         echo '<td headers="team u'.$i.'" >&nbsp;</td>'."\n";
     }
-    elseif($is_allowedToEdit)
+    elseif ( $is_allowedToEdit )
     {
         // Tutor column
-
         if($thisUser['tutor'] == '0')
         {
             echo '<td headers="tut u'.$i.'"> - </td>'."\n";
@@ -351,6 +335,7 @@ while (list(,$thisUser) = each($users))
         {
             echo '<td headers="tut u'.$i.'">'.$langGroupTutor.'</td>'."\n";
         }
+
         // course manager column
         if($thisUser['statut'] == '1')
         {
@@ -360,8 +345,8 @@ while (list(,$thisUser) = each($users))
         {
             echo '<td headers="CM u'.$i.'"> - </td>'."\n";
         }
-        // Edit user column
 
+        // Edit user column
         echo '<td headers="edit u'.$i.'">'
            . '<a href="userInfo.php?editMainUserInfo='.$thisUser['user_id'].'">'
            . '<img border="0" alt="'.$langEdit.'" src="'.$imgRepositoryWeb.'edit.gif">'
@@ -385,13 +370,22 @@ while (list(,$thisUser) = each($users))
     echo '</tr>'."\n";
 
     $previousUser = $thisUser['user_id'];
-} // END - while fetch_array
 
-echo '</tbody>'
-    .'</table>'
-    ;
+} // END - foreach users
 
-echo $navLink;
+/*----------------------------------------------------------------------
+   Display table footer
+  ----------------------------------------------------------------------*/
+
+echo '</tbody>' . "\n"
+    .'</table>' . "\n" ;
+
+/*----------------------------------------------------------------------
+   Display pager
+  ----------------------------------------------------------------------*/
+
+$myPager->disp_pager_tool_bar($_SERVER['PHP_SELF']);
+
 
 include($includePath.'/claro_init_footer.inc.php');
 ?>
