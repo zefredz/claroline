528cc90c77e3d8b23c6b3ce5819de2c8421bd3a9
seb <seb@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-05-04 14:05:54 +0000

    fix a var for display_error e_all and register_globals off;  remove label in url

--- claroline/tracking/toolaccess_details.php
+++ claroline/tracking/toolaccess_details.php
@@ -24,212 +24,181 @@ $interbredcrump[]= array ("url"=>"courseLog.php", "name"=> $langStatistics);
 
 $nameTools = $langDetails;
 
-include($includePath."/claro_init_header.inc.php");
-
-claro_disp_tool_title($nameTools);
-
 // main page
 include($includePath."/lib/statsUtils.lib.inc.php");
 
+
+$tbl_cdb_names = claro_sql_get_course_tbl();
+$TABLETRACK_ACCESS = $tbl_cdb_names['track_e_access'];
+
+if(isset($_cid)) //stats for the current course
+{
+    // to see stats of one course user must be courseAdmin of this course
+    $is_allowedToTrack = $is_courseAdmin;
+}
+else
+{
+    // cid has to be set here else it probably means that the user has directly access this page by url
+    $is_allowedToTrack = false;
+}
+
+if( $is_allowedToTrack && $is_trackingEnabled )
+{
+	// toolId is required, go to the tool list if it is missing
+    if( empty($_REQUEST['toolId']) ) header("Location: ./courseLog.php?view=0010000");
+    
+
+  	if( !isset($_REQUEST['reqdate']) || $_REQUEST['reqdate'] < 0 || $_REQUEST['reqdate'] > 2149372861 )
+    	$reqdate = time();  // default value
+	else
+	    $reqdate = $_REQUEST['reqdate'];
+
+    if( isset($_REQUEST['period']) )    $period = $_REQUEST['period'];
+    else                                $period = "day"; // default value
+
+
+    $sql = "SELECT `access_tlabel` as `label`
+            FROM `".$TABLETRACK_ACCESS."`
+            WHERE `access_tid` = ".$_REQUEST['toolId']."
+			GROUP BY `access_tid`" ;
+
+    $result = claro_sql_query_fetch_all($sql);
+    
+    include($includePath."/claro_init_header.inc.php");
+	$title['mainTitle'] = $nameTools;
+	
+	if( isset($result[0]['label']) )
+		if( isset($toolNameList[$result[0]['label']]) )
+			$title['subTitle'] = $toolNameList[$result[0]['label']];
+
+	claro_disp_tool_title( $title );
+
 ?>
 <table width="100%" cellpadding="2" cellspacing="0" border="0">
-<?php 
-    
-	$tbl_cdb_names = claro_sql_get_course_tbl();
-    $TABLETRACK_ACCESS = $tbl_cdb_names['track_e_access'];
-    
-    if(isset($_cid)) //stats for the current course
+<?php
+
+    /* ------ display ------ */
+    // displayed period
+    echo "<tr><td>";
+    switch($period)
     {
-        // to see stats of one course user must be courseAdmin of this course
-        $is_allowedToTrack = $is_courseAdmin;
+        case "month" : 
+            echo $langMonthNames['long'][date("n", $reqdate)-1].date(" Y", $reqdate);
+            break;
+        case "week" : 
+            $weeklowreqdate = ($reqdate-(86400*date("w" , $reqdate)));
+            $weekhighreqdate = ($reqdate+(86400*(6-date("w" , $reqdate)) ));
+            echo "<b>".$langFrom."</b> ".date("d " , $weeklowreqdate).$langMonthNames['long'][date("n", $weeklowreqdate)-1].date(" Y" , $weeklowreqdate);
+            echo " <b>".$langToDate."</b> ".date("d " , $weekhighreqdate ).$langMonthNames['long'][date("n", $weekhighreqdate)-1].date(" Y" , $weekhighreqdate);
+            break;
+        // default == day
+        default :
+            $period = "day";            
+        case "day" : 
+            echo $langDay_of_weekNames['long'][date("w" , $reqdate)].date(" d " , $reqdate).$langMonthNames['long'][date("n", $reqdate)-1].date(" Y" , $reqdate);
+            break;
+    }
+    echo "</tr></td>";
+    // periode choice
+    echo "<tr>
+            <td>
+            <small>
+            [<a href='".$_SERVER['PHP_SELF']."?toolId=".$_REQUEST['toolId']."&amp;period=day&amp;reqdate=".$reqdate."'>".$langPeriodDay."</a>]
+            [<a href='".$_SERVER['PHP_SELF']."?toolId=".$_REQUEST['toolId']."&amp;period=week&amp;reqdate=".$reqdate."'>".$langPeriodWeek."</a>]
+            [<a href='".$_SERVER['PHP_SELF']."?toolId=".$_REQUEST['toolId']."&amp;period=month&amp;reqdate=".$reqdate."'>".$langPeriodMonth."</a>]
+            &nbsp;&nbsp;&nbsp;||&nbsp;&nbsp;&nbsp;
+            
+            ";
+    switch($period)
+    {
+        case "month" :
+            // previous and next date must be evaluated
+            // 30 days should be a good approximation
+            $previousReqDate = mktime(1,1,1,date("m",$reqdate)-1,1,date("Y",$reqdate));
+            $nextReqDate = mktime(1,1,1,date("m",$reqdate)+1,1,date("Y",$reqdate));
+            echo   "
+                [<a href='".$_SERVER['PHP_SELF']."?toolId=".$_REQUEST['toolId']."&amp;period=month&amp;reqdate=".$previousReqDate."'>".$langPreviousMonth."</a>]
+                [<a href='".$_SERVER['PHP_SELF']."?toolId=".$_REQUEST['toolId']."&amp;period=month&amp;reqdate=".$nextReqDate."'>".$langNextMonth."</a>]
+            ";
+            break;
+        case "week" :
+            // previous and next date must be evaluated
+            $previousReqDate = $reqdate - 7*86400;
+            $nextReqDate = $reqdate + 7*86400;
+            echo   "
+                [<a href='".$_SERVER['PHP_SELF']."?toolId=".$_REQUEST['toolId']."&amp;period=week&amp;reqdate=".$previousReqDate."'>".$langPreviousWeek."</a>]
+                [<a href='".$_SERVER['PHP_SELF']."?toolId=".$_REQUEST['toolId']."&amp;period=week&amp;reqdate=".$nextReqDate."'>".$langNextWeek."</a>]
+            ";
+            break;
+        case "day" :
+            // previous and next date must be evaluated
+            $previousReqDate = $reqdate - 86400;
+            $nextReqDate = $reqdate + 86400;
+            echo   "
+                [<a href='".$_SERVER['PHP_SELF']."?toolId=".$_REQUEST['toolId']."&amp;period=day&amp;reqdate=".$previousReqDate."'>".$langPreviousDay."</a>]
+                [<a href='".$_SERVER['PHP_SELF']."?toolId=".$_REQUEST['toolId']."&amp;period=day&amp;reqdate=".$nextReqDate."'>".$langNextDay."</a>]
+            ";
+            break;
+    }
+    
+    echo"   &nbsp;&nbsp;&nbsp;||&nbsp;&nbsp;&nbsp;
+            [<a href='./courseLog.php?view=0010000'>".$langViewToolList."</a>]
+            </small>
+            </td>
+        </tr>
+    ";
+    // display information about this period
+    switch($period)
+    {
+        // all days
+        case "month" :
+            $sql = "SELECT UNIX_TIMESTAMP(`access_date`)
+                    FROM `$TABLETRACK_ACCESS`
+                    WHERE `access_tid` = '".$_REQUEST['toolId']."'
+                        AND MONTH(`access_date`) = MONTH(FROM_UNIXTIME($reqdate))
+                        AND YEAR(`access_date`) = YEAR(FROM_UNIXTIME($reqdate))
+                        ORDER BY `access_date` ASC";
+            
+            $days_array = daysTab($sql);
+            makeHitsTable($days_array,$langDay);
+            break;
+        // all days
+        case "week" :
+            $sql = "SELECT UNIX_TIMESTAMP(`access_date`)
+                    FROM `$TABLETRACK_ACCESS`
+                    WHERE `access_tid` = '".$_REQUEST['toolId']."'
+                        AND WEEK(`access_date`) = WEEK(FROM_UNIXTIME($reqdate))
+                        AND YEAR(`access_date`) = YEAR(FROM_UNIXTIME($reqdate))
+                        ORDER BY `access_date` ASC";
+
+            $days_array = daysTab($sql);
+            makeHitsTable($days_array,$langDay);
+            break;
+        // all hours
+        case "day"  :
+            $sql = "SELECT UNIX_TIMESTAMP(`access_date`)
+                        FROM `$TABLETRACK_ACCESS`
+                        WHERE `access_tid` = '".$_REQUEST['toolId']."'
+                            AND DAYOFYEAR(`access_date`) = DAYOFYEAR(FROM_UNIXTIME($reqdate))
+                            AND YEAR(`access_date`) = YEAR(FROM_UNIXTIME($reqdate))
+                        ORDER BY `access_date` ASC";
+            
+            $hours_array = hoursTab($sql,$reqdate);
+            makeHitsTable($hours_array,$langHour);
+            break;
+    }
+}
+else // not allowed to track
+{
+    if(!$is_trackingEnabled)
+    {
+        echo $langTrackingDisabled;
     }
     else
     {
-        // cid has to be set here else it probably means that the user has directly access this page by url
-        $is_allowedToTrack = false;
+        echo $langNotAllowed;
     }
-    if( $is_allowedToTrack && $is_trackingEnabled)
-    {
-        // list of all tools
-        if (!isset($_REQUEST['tool']))
-        {
-            $sql = "SELECT `access_tid`, count( access_tid ), `access_tlabel`
-                        FROM `$TABLETRACK_ACCESS`
-                        WHERE `access_tid` IS NOT NULL
-                        GROUP BY `access_tid`";
-            
-            echo "<tr><td>";  
-            echo "<tr>
-                    <td>
-                    <b>$langToolList</b>";
-            if(isset($_cid)) echo " for <b>$_cid</b>";
-            echo "       </td>
-                </tr>
-            ";
-    
-            $results = getManyResults3Col($sql);
-            echo "<table class='claroTable' cellpadding='0' cellspacing='0' border='0' align=center>";
-            echo "<tr class='headerX'>
-                    <th width='70%'>
-                    $langToolTitleToolnameColumn
-                    </th>
-                    <th width='30%'>
-                    $langToolTitleCountColumn
-                    </th>
-                </tr><tbody>";
-            if (is_array($results))
-            { 
-                for($j = 0 ; $j < count($results) ; $j++)
-                { 
-                        echo "<tr>"
-                              ."<td><a href='toolaccess_details.php?tool=".$results[$j][0]."&label=".$results[$j][2]."'>".$toolNameList[$results[$j][2]]."</a></td>"
-                              ."<td align='right'>".$results[$j][1]."</td>"
-                              ."</tr>";
-                }
-            
-            }
-            else
-            {
-                echo "<tr>"
-                      ."<td colspan='2'><center>".$langNoResult."</center></td>"
-                      ."</tr>";
-            }
-            echo "</tbody></table></td></tr>";
-        }
-        else
-        {
-            $tool = htmlspecialchars($_REQUEST['tool']);
-            if( !isset($reqdate) )
-                $reqdate = time();
-            echo "<tr>
-                    <td>
-                    <b>".$toolNameList[$_REQUEST['label']]."</b>
-                    </td>
-            </tr>";
-            
-            /* ------ display ------ */
-            // displayed period
-            echo "<tr><td>";
-            switch($period)
-            {
-                case "month" : 
-                    echo $langMonthNames['long'][date("n", $reqdate)-1].date(" Y", $reqdate);
-                    break;
-                case "week" : 
-                    $weeklowreqdate = ($reqdate-(86400*date("w" , $reqdate)));
-                    $weekhighreqdate = ($reqdate+(86400*(6-date("w" , $reqdate)) ));
-                    echo "<b>".$langFrom."</b> ".date("d " , $weeklowreqdate).$langMonthNames['long'][date("n", $weeklowreqdate)-1].date(" Y" , $weeklowreqdate);
-                    echo " <b>".$langToDate."</b> ".date("d " , $weekhighreqdate ).$langMonthNames['long'][date("n", $weekhighreqdate)-1].date(" Y" , $weekhighreqdate);
-                    break;
-                // default == day
-                default :
-                    $period = "day";            
-                case "day" : 
-                    echo $langDay_of_weekNames['long'][date("w" , $reqdate)].date(" d " , $reqdate).$langMonthNames['long'][date("n", $reqdate)-1].date(" Y" , $reqdate);
-                    break;
-            }
-            echo "</tr></td>";
-            // periode choice
-            echo "<tr>
-                    <td>
-                    <small>
-                    [<a href='".$_SERVER['PHP_SELF']."?tool=$tool&amp;period=day&amp;reqdate=$reqdate'>$langPeriodDay</a>] 
-                    [<a href='".$_SERVER['PHP_SELF']."?tool=$tool&amp;period=week&amp;reqdate=$reqdate'>$langPeriodWeek</a>]
-                    [<a href='".$_SERVER['PHP_SELF']."?tool=$tool&amp;period=month&amp;reqdate=$reqdate'>$langPeriodMonth</a>]
-                    &nbsp;&nbsp;&nbsp;||&nbsp;&nbsp;&nbsp;
-                    
-                    ";
-            switch($period)
-            {
-                case "month" :
-                    // previous and next date must be evaluated
-                    // 30 days should be a good approximation
-                    $previousReqDate = mktime(1,1,1,date("m",$reqdate)-1,1,date("Y",$reqdate));
-                    $nextReqDate = mktime(1,1,1,date("m",$reqdate)+1,1,date("Y",$reqdate));
-                    echo   "
-                        [<a href='".$_SERVER['PHP_SELF']."?tool=$tool&amp;period=month&amp;reqdate=$previousReqDate'>$langPreviousMonth</a>] 
-                        [<a href='".$_SERVER['PHP_SELF']."?tool=$tool&amp;period=month&amp;reqdate=$nextReqDate'>$langNextMonth</a>]
-                    ";
-                    break;
-                case "week" :
-                    // previous and next date must be evaluated
-                    $previousReqDate = $reqdate - 7*86400;
-                    $nextReqDate = $reqdate + 7*86400;
-                    echo   "
-                        [<a href='".$_SERVER['PHP_SELF']."?tool=$tool&amp;period=week&amp;reqdate=$previousReqDate'>$langPreviousWeek</a>] 
-                        [<a href='".$_SERVER['PHP_SELF']."?tool=$tool&amp;period=week&amp;reqdate=$nextReqDate'>$langNextWeek</a>]
-                    ";
-                    break;
-                case "day" :
-                    // previous and next date must be evaluated
-                    $previousReqDate = $reqdate - 86400;
-                    $nextReqDate = $reqdate + 86400;
-                    echo   "
-                        [<a href='".$_SERVER['PHP_SELF']."?tool=$tool&amp;period=day&amp;reqdate=$previousReqDate'>$langPreviousDay</a>] 
-                        [<a href='".$_SERVER['PHP_SELF']."?tool=$tool&amp;period=day&amp;reqdate=$nextReqDate'>$langNextDay</a>]
-                    ";
-                    break;
-            }
-            
-            echo"   &nbsp;&nbsp;&nbsp;||&nbsp;&nbsp;&nbsp;
-                    [<a href='./courseLog.php?view=0010000'>$langViewToolList</a>]
-                    </small>
-                    </td>
-                </tr>
-            ";
-            // display information about this period
-            switch($period)
-            {
-                // all days
-                case "month" :
-                    $sql = "SELECT UNIX_TIMESTAMP(`access_date`)
-                            FROM `$TABLETRACK_ACCESS`
-                            WHERE `access_tid` = '".$_REQUEST['tool']."' 
-                                AND MONTH(`access_date`) = MONTH(FROM_UNIXTIME($reqdate))
-                                AND YEAR(`access_date`) = YEAR(FROM_UNIXTIME($reqdate))
-                                ORDER BY `access_date` ASC";
-                    
-                    $days_array = daysTab($sql);
-                    makeHitsTable($days_array,$langDay);
-                    break;
-                // all days
-                case "week" :
-                    $sql = "SELECT UNIX_TIMESTAMP(`access_date`)
-                            FROM `$TABLETRACK_ACCESS`
-                            WHERE `access_tid` = '".$_REQUEST['tool']."' 
-                                AND WEEK(`access_date`) = WEEK(FROM_UNIXTIME($reqdate))
-                                AND YEAR(`access_date`) = YEAR(FROM_UNIXTIME($reqdate))
-                                ORDER BY `access_date` ASC";
-    
-                    $days_array = daysTab($sql);
-                    makeHitsTable($days_array,$langDay);
-                    break;
-                // all hours
-                case "day"  :
-                    $sql = "SELECT UNIX_TIMESTAMP(`access_date`)
-                                FROM `$TABLETRACK_ACCESS`
-                                WHERE `access_tid` = '".$_REQUEST['tool']."' 
-                                    AND DAYOFYEAR(`access_date`) = DAYOFYEAR(FROM_UNIXTIME($reqdate))
-                                    AND YEAR(`access_date`) = YEAR(FROM_UNIXTIME($reqdate))
-                                ORDER BY `access_date` ASC";
-                    
-                    $hours_array = hoursTab($sql,$reqdate);
-                    makeHitsTable($hours_array,$langHour);
-                    break;
-            }
-        }
-    }
-    else // not allowed to track
-    {
-        if(!$is_trackingEnabled)
-        {
-            echo $langTrackingDisabled;
-        }
-        else
-        {
-            echo $langNotAllowed;
-        }
-    }
+}
     
     
 ?>
