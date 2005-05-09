8ba402ea9db7f61981382e6e45fc915c9177cde0
guillaume <guillaume@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-05-09 07:45:03 +0000

    register global off refactoring

--- claroline/learnPath/insertMyDoc.php
+++ claroline/learnPath/insertMyDoc.php
@@ -40,13 +40,23 @@ require '../inc/claro_init_global.inc.php';
 
 
   // tables names
+  
+  $tbl_cdb_names = claro_sql_get_course_tbl();
+  
+  $tbl_lp_learnPath            = $tbl_cdb_names['lp_learnPath'           ];
+  $tbl_lp_rel_learnPath_module = $tbl_cdb_names['lp_rel_learnPath_module'];
+  $tbl_lp_user_module_progress = $tbl_cdb_names['lp_user_module_progress'];
+  $tbl_lp_module               = $tbl_cdb_names['lp_module'              ];
+  $tbl_lp_asset                = $tbl_cdb_names['lp_asset'               ];
 
-  $TABLELEARNPATH         = $_course['dbNameGlu']."lp_learnPath";
-  $TABLEMODULE            = $_course['dbNameGlu']."lp_module";
-  $TABLELEARNPATHMODULE   = $_course['dbNameGlu']."lp_rel_learnPath_module";
-  $TABLEASSET             = $_course['dbNameGlu']."lp_asset";
-  $TABLEUSERMODULEPROGRESS= $_course['dbNameGlu']."lp_user_module_progress";
-
+  $TABLELEARNPATH            = $tbl_lp_learnPath;
+  $TABLELEARNPATHMODULE      = $tbl_lp_rel_learnPath_module;
+  $TABLEUSERMODULEPROGRESS   = $tbl_lp_user_module_progress;
+  $TABLEMODULE               = $tbl_lp_module;
+  $TABLEASSET                = $tbl_lp_asset;
+    
+  $dbTable = $tbl_cdb_names['document'];
+  
   // document browser vars
   $TABLEDOCUMENT     = $_course['dbNameGlu']."document";
 
@@ -266,9 +276,9 @@ require '../inc/claro_init_global.inc.php';
              DEFINE CURRENT DIRECTORY
         ======================================*/
 
-      if (isset($openDir) ) // $newDirPath is from createDir command (step 2) and $uploadPath from upload command
+      if (isset($_REQUEST['openDir']) ) // $newDirPath is from createDir command (step 2) and $uploadPath from upload command
       {
-              $curDirPath = $openDir;
+              $curDirPath = $_REQUEST['openDir'];
               /*
                * NOTE: Actually, only one of these variables is set.
                * By concatenating them, we eschew a long list of "if" statements
@@ -423,7 +433,7 @@ require '../inc/claro_init_global.inc.php';
                       {
                               if (isset($dirNameList) && in_array(basename($chekinFile), $dirNameList))
                                       continue;
-                              elseif ($fileNameList && in_array(basename($chekinFile), $fileNameList))
+                              elseif (isset($fileNameList) && $fileNameList && in_array(basename($chekinFile), $fileNameList))
                                       continue;
                               else
                                       $recToDel[]= $chekinFile; // add chekinFile to the list of records to delete
@@ -431,7 +441,8 @@ require '../inc/claro_init_global.inc.php';
 
                       /* BUILD THE QUERY TO DELETE DEPRECATED DB RECORDS */
                       $nbrRecToDel = sizeof ($recToDel);
-
+                      $queryClause = "";
+		      
                       for ($i=0; $i < $nbrRecToDel ;$i++)
                       {
                               $queryClause .= "path LIKE \"".$recToDel[$i]."%\"";
