e5a846afb622b5e78a1deab62cf43ec9c4175f6b
moosh <moosh@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-05-17 20:13:50 +0000

    - use claro_sql_get_main_tbl for notifier function - phpdoc - protect foreach for empty array

--- claroline/inc/lib/event/notifier.php
+++ claroline/inc/lib/event/notifier.php
@@ -1,6 +1,12 @@
 <?php
 /**
- *  Class which update the main database with the date of the latest new item in each tool of each course.
+ * CLAROLINE 
+ *
+ * Class which update the main database with the date of the latest new item in each tool of each course.
+ *
+ * @version 1.7 $Revision$
+ * @author Guillaume Lederer guim@claroline.net
+ *
  */
 class Notifier extends EventDriven 
 {
@@ -11,38 +17,47 @@ class Notifier extends EventDriven
     {
         parent::EventDriven( $registry );
     }
-	
+    
     /**
     * implements the parent update function
+    * @param $event object 
+    * @return void
     */
-     
+
     function update ($event) 
-    { 
-    	// get needed info from event
-    	
-    	$event_args = $event->getArgs();
-    	
-    	$course     = $event_args['cid'];
-    	$tool       = $event_args['tid'];
-    	$ressource  = $event_args['rid'];
-    	$gid        = $event_args['gid'];
-    	$uid        = $event_args['uid'];
-    	$eventType  = $event->getEventType();
-    
-    	// call function to update db info
-    	
-    	if ($eventType != "DELETE")
-    	{    
-                $this->update_last_event($course, $tool, $ressource,$gid, $uid);
-    	}    
+    {
+        // get needed info from event
+
+        $event_args = $event->getArgs();
+
+        $course     = $event_args['cid'];
+        $tool       = $event_args['tid'];
+        $ressource  = $event_args['rid'];
+        $gid        = $event_args['gid'];
+        $uid        = $event_args['uid'];
+        $eventType  = $event->getEventType();
+
+        // call function to update db info
+
+        if ($eventType != "DELETE")
+        {
+             $this->update_last_event($course, $tool, $ressource,$gid, $uid);
+        }
     }
-    
+
     /**
      * Function used to tell the notifier that some new event happened in a tool :
      * For a specific tool in a specific course, 
      * the field 'date' of the latest new event, is set to the current date.
      * Event might concern a specific group (optionnal)
      * Event might concern a specific user (optionnal)
+     *
+     * @param $course_id 
+     * @param $tool_id
+     * @param $ressource_id
+     * @param $gid 
+     * @param $uid
+     * @param $dbnameGlu default NULL
      * 
      * @return void 
      */
@@ -52,101 +67,104 @@ class Notifier extends EventDriven
         $tbl_cdb_names = claro_sql_get_course_tbl($dbnameGlu);
         $tbl_notify    = $tbl_cdb_names['notify'];
     
-    	// 1- check if row already exists
-    	
-    	$sql = "SELECT * 
-    	          FROM `".$tbl_notify."` 
-    		 WHERE `course_code`='".$course_id."'
-    		   AND `tool_id`='".$tool_id."'
-    		   AND `ressource_id`='".$ressource_id."'
-    		   AND `group_id` = '".$gid."'
-    		   AND `user_id` = '".$uid."'
-    		   ";
-    	
-    	$notificationTable = claro_sql_query_fetch_all($sql);
-    	$notification = $notificationTable[0];
-    	
-    	if ($notification!=null)
-    	{
-    	  $do_update = true;
-    	}
-    	else
-    	{
-    	  $do_update = false;
-    	}
-    	
-    	claro_sql_query($sql);
-    	
-    	// 2- update or create for concerned row
-    	
-    	if ($do_update) 
+        // 1- check if row already exists
+        
+        $sql = "SELECT * 
+                  FROM `".$tbl_notify."` 
+             WHERE `course_code`='".$course_id."'
+               AND `tool_id`='".$tool_id."'
+               AND `ressource_id`='".$ressource_id."'
+               AND `group_id` = '".$gid."'
+               AND `user_id` = '".$uid."'
+               ";
+        
+        $notificationTable = claro_sql_query_fetch_all($sql);
+        $notification = $notificationTable[0];
+        
+        if ($notification!=null)
         {
-    	    $sql = "UPDATE `".$tbl_notify."` 
-    	               SET `date`= NOW() 
-    		     WHERE `course_code`='".$course_id."' 
-    		       AND `tool_id`='".$tool_id."'
-    		       AND `ressource_id`='".$ressource_id."'
-    		       AND `group_id` = '".$gid."'
-    		       AND `user_id` = '".$uid."'
-    		           ";
-    	    claro_sql_query($sql);
+          $do_update = true;
         }
-    	else
-    	{
-    	    
+        else
+        {
+          $do_update = false;
+        }
+        
+        claro_sql_query($sql);
+
+        // 2- update or create for concerned row
+
+        if ($do_update) 
+        {
+            $sql = "UPDATE `".$tbl_notify."` 
+                       SET `date`= NOW() 
+                 WHERE `course_code`='".$course_id."' 
+                   AND `tool_id`='".$tool_id."'
+                   AND `ressource_id`='".$ressource_id."'
+                   AND `group_id` = '".$gid."'
+                   AND `user_id` = '".$uid."'
+                       ";
+            claro_sql_query($sql);
+        }
+        else
+        {
+            
                 $sql = "INSERT INTO `".$tbl_notify."` 
                         SET   `course_code`   = '".$course_id."',
                               `tool_id` = '".$tool_id."',
-    			  `date` = NOW(),
-    			  `ressource_id` = '".$ressource_id."',
-    			  `group_id` = '".$gid."',
-    			  `user_id` = '".$uid."'
-    			  ";
-    	    claro_sql_query($sql);
-    	}
-    	
-    	//echo "updating in db for last event : ".$course_id." ".$tool_id." : <br>";
-    	
-    	// echo $sql."<br>"; debug	
-        
+                  `date` = NOW(),
+                  `ressource_id` = '".$ressource_id."',
+                  `group_id` = '".$gid."',
+                  `user_id` = '".$uid."'
+                  ";
+            claro_sql_query($sql);
+        }
+
+        //echo "updating in db for last event : ".$course_id." ".$tool_id." : <br>";
+
+        // echo $sql."<br>"; debug    
+
     }
      
     /**
-     *  Function to know which course of a user
-     *  $date must be given in unix_timestamp
-     * 
-     *  @return an array with the courses with recent unknown event until the date '$date' in the course list of the user
+     * Function to know which course of a user
+     *
+     * @param $user_id
+     * @param $date unix_timestamp
+     * @param $dbnameGlu=NULL
+     *
+     * @return an array with the courses with recent unknown event until the date '$date' in the course list of the user
      */
      
     function get_notified_courses($user_id,$date, $dbnameGlu=NULL)
     {
+        $tbl_mdb_names = claro_sql_get_main_tbl();
         $tbl_cdb_names = claro_sql_get_course_tbl($dbnameGlu);
+        $tbl_cours_user = $tbl_mdb_names['rel_course_user'];
         $tbl_notify    = $tbl_cdb_names['notify'];
-    
-    	$tbl_cours_user = "cl_cours_user";
-    	
-    	$courses = array();
-    	
-    	//1- find the list of the user's course and in this list, take only the course where recent events happened
-    	
-    	$sql="SELECT `code_cours` FROM `".$tbl_cours_user."` AS CU, `".$tbl_notify."` AS N 
-    	       WHERE CU.`code_cours` = N.`course_code`
-    	         AND CU.`user_id` = '".$user_id."'
-    		 AND UNIX_TIMESTAMP(N.`date`) > '".$date."'
-    	         ";
-    		
-    	$result = claro_sql_query_fetch_all($sql);
-    	
-    	foreach ($result as $course)
-    	{
-    	    $courses[] = $course["code_cours"];
-    	} 
-    	
-    	//2- return an array with the courses with recent unknow event until the date '$date' in the course list of the user
-    	
-    	return $courses;
+
+        $courses = array();
+
+        //1- find the list of the user's course and in this list, take only the course where recent events happened
+
+        $sql="SELECT `code_cours` FROM `".$tbl_cours_user."` AS CU, `".$tbl_notify."` AS N 
+               WHERE CU.`code_cours` = N.`course_code`
+                 AND CU.`user_id` = '".$user_id."'
+             AND UNIX_TIMESTAMP(N.`date`) > '".$date."'
+                 ";
+
+        $courseList = claro_sql_query_fetch_all($sql);
+        if (is_array($courseList))
+        foreach ($courseList as $course)
+        {
+            $courses[] = $course['code_cours'];
+        } 
+
+        //2- return an array with the courses with recent unknow event until the date '$date' in the course list of the user
+
+        return $courses;
     }
-    
+
     /**
      *  Function to know which course of a user
      * 
@@ -157,29 +175,28 @@ class Notifier extends EventDriven
     {
         $tbl_cdb_names = claro_sql_get_course_tbl($dbnameGlu);
         $tbl_notify    = $tbl_cdb_names['notify'];
-    
-    	
-    	$tools = array();
-    	
-    	// 1- find the tool list of the given course that contains some event newer than the date '$date'
-    	
-    	$sql = "SELECT `tool_id`, MAX(`date`)
-    	        FROM `".$tbl_notify."` AS N 
-    	        WHERE N.`course_code` = '".$course_id."'
-    		      AND UNIX_TIMESTAMP(N.`date`) > '".$date."'
-    	        GROUP BY `tool_id`
-    		 
-    	         ";
-    	$toolList = claro_sql_query_fetch_all($sql);
-    	if (is_array($toolList))
-    	foreach ($toolList as $tool)
-    	{
-    	    $tools[] = $tool["tool_id"];
-    	}
-    	
-    	// 2- return an array with the tools id with recent unknow event until the date '$date'
-    	
-    	return $tools;
+
+        $tools = array();
+
+        // 1- find the tool list of the given course that contains some event newer than the date '$date'
+        
+        $sql = "SELECT `tool_id`, MAX(`date`)
+                FROM `".$tbl_notify."` AS N 
+                WHERE N.`course_code` = '".$course_id."'
+                  AND UNIX_TIMESTAMP(N.`date`) > '".$date."'
+                GROUP BY `tool_id`
+
+                 ";
+        $toolList = claro_sql_query_fetch_all($sql);
+        if (is_array($toolList))
+        foreach ($toolList as $tool)
+        {
+            $tools[] = $tool['tool_id'];
+        }
+
+        // 2- return an array with the tools id with recent unknow event until the date '$date'
+
+        return $tools;
 
     }    
 } // CLASS Notifier extends EventDriven 
