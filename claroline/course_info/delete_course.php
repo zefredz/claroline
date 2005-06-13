ae79735e2154325d37b6fa87e4387a4fc70103ab
moosh <moosh@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-06-13 13:38:40 +0000

    split  work and output

--- claroline/course_info/delete_course.php
+++ claroline/course_info/delete_course.php
@@ -1,30 +1,41 @@
 <?php # $Id$
-/*
-+----------------------------------------------------------------------+
-| CLAROLINE 1.6
-+----------------------------------------------------------------------+
-| Copyright (c) 2001, 2004 Universite catholique de Louvain (UCL)      |
-+----------------------------------------------------------------------+
-*/
+/**
+ * CLAROLINE 
+ *
+ * @version 1.7 $Revision$
+ *
+ * @copyright (c) 2001-2005 Universite catholique de Louvain (UCL)
+ *
+ * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE 
+ *
+ * @package CLCRS
+ *
+ * @author Claro Team <cvs@claroline.net>
+ */
+$_tid='deletecourse';
 
-$_tid="deletecourse";
+
+define('DISP_CONFIRM_DELETE', __LINE__);
+define('DISP_DELETE_RESULT', __LINE__);
+define('DISP_NOT_ALLOWED', __LINE__);
 
 require '../inc/claro_init_global.inc.php';
-
 if ( ! $_cid) claro_disp_select_course();
 
-// in case of admin access (from admin tool) to the script, we must determine which course we are wroking with
+include($includePath . '/lib/fileManage.lib.php');
+include($includePath . '/lib/events.lib.inc.php');
+include($includePath . '/lib/admin.lib.inc.php');
 
+// in case of admin access (from admin tool) to the script, we must determine which course we are working with
 $addToURL = '';
-
 if ( isset($cidToEdit) && ($is_platformAdmin) )
 {
     $current_cid       = $cidToEdit;
     $currentCourseId   = $cidToEdit;
     $cidReq            = $cidToEdit;
     $isAllowedToDelete = true;
-    $addToURL          = "&amp;cidToEdit=".$cidToEdit;
-    $addToURL         .="&amp;cfrom=".$cfrom;
+    $addToURL          = '&amp;cidToEdit=' . $cidToEdit;
+    $addToURL         .='&amp;cfrom=' . $cfrom;
 }
 else
 {
@@ -32,108 +43,89 @@ else
 }
 
 //check user right
-
 $isAllowedToDelete = ($is_courseAdmin || $is_platformAdmin);
-
-/*
- * DB tables definition
- */
-
-//$tbl_cdb_names = claro_sql_get_course_tbl();
-$tbl_mdb_names     = claro_sql_get_main_tbl();
-$tbl_course        = $tbl_mdb_names['course'           ];
-$tbl_relCourseUser = $tbl_mdb_names['rel_course_user'  ];
 
 //find needed info in db
 
-$sql = "SELECT * 
-        FROM `".$tbl_course."` 
-        WHERE code = '".$current_cid."'";
-
-list($course_to_delete) = claro_sql_query_fetch_all($sql);
-
-$currentCourseCode 		= $course_to_delete['fake_code'];
-$currentCourseName 		= $course_to_delete['intitule'];
+$course_to_delete = claro_get_course_data($current_cid);
+$currentCourseCode = $course_to_delete['officialCode'];
+$currentCourseName = $course_to_delete['name'];
 
 $nameTools = $langDelCourse;
-
-$interbredcrump[]=array("url" => "infocours.php?".$addToURL,"name" => $langCourseSettings);
-
-include($includePath."/claro_init_header.inc.php");
-include($includePath."/lib/fileManage.lib.php");
-include($includePath."/lib/events.lib.inc.php");
-include($includePath."/lib/admin.lib.inc.php");
-
-// display tool title
-
-claro_disp_tool_title($nameTools);
+$interbredcrump[] = array('url' => 'infocours.php?' . $addToURL, 'name' => $langCourseSettings);
 
 if( $isAllowedToDelete )
 {
-	if ( isset($_REQUEST['delete']) && $_REQUEST['delete'] )
+    if ( isset($_REQUEST['delete']) && $_REQUEST['delete'] )
     {
         // DO DELETE
-
         delete_course($current_cid);
+        event_default( 'DELETION COURSE' , array ('courseName' => addslashes($currentCourseName), 'uid' => $_uid));
 
-        // DELETE CONFIRMATION MESSAGE
-
-        event_default("DELETION COURSE",array ("courseName"=>addslashes($currentCourseName), "uid"=> $_uid));
-        echo     '<p>'
-                .$langCourse.' &quot;'.$currentCourseName.'&quot; '
-                .'('.$currentCourseCode.') '
-                .$langHasDel.'</p>';
-
-        echo     '<p>'
-                .'<a href="../../index.php">'
-                .$langBackHomeOf.' '. $siteName
-                .'</a>'
-                ;
-
-        if ( isset($cidToEdit) ) //we can suppose that script is accessed from admin tool in this case
-        {
-            echo " | "
-                ."<a href=\"../admin/index.php\">"
-                .$langBackToAdmin." </a>"
-                ;
-        }
-        echo "</p>";
-	}					// end if $delete
-	else
-	{
-		// ASK DELETE CONFIRMATION TO THE USER
-
-		echo	 "<p>"
-
-				."<font color=\"#CC0000\">"
-				.$langByDel." &quot;".$currentCourseName,"&quot; "
-				."(".$currentCourseCode.") ?"
-				."</font>"
-				."</p>"
-
-				."<p>"
-				."<font color=\"#CC0000\">";
-
-		echo "<a href=\"".$_SERVER['PHP_SELF']."?delete=yes".$addToURL."\">"
-			.$langYes
-			."</a>";
-
-
-		echo "&nbsp;|&nbsp;";
-
-        echo "<a href=\"infocours.php?".$addToURL."\">"
-				.$langNo
-				."</a>";
-
-		echo "</font>"
-			."</p>";
-
-	}		// end else if $delete
-}			// end if $isAllowedToDelete
+        $display = DISP_DELETE_RESULT;
+    } // end if $delete
+    else
+    {
+        $display = DISP_CONFIRM_DELETE;
+    }        // end else if $delete
+}            // end if $isAllowedToDelete
 else
 {
-	echo $langNotAllowed;
+    $display = DISP_NOT_ALLOWED;
 }
 
-include($includePath."/claro_init_footer.inc.php");
+include($includePath . '/claro_init_header.inc.php');
+// display tool title
+claro_disp_tool_title($nameTools);
+
+switch ($display)
+{
+    case DISP_DELETE_RESULT :
+        echo '<p>'
+        .    $langCourse.' &quot;'.$currentCourseName.'&quot; '
+        .    '('.$currentCourseCode.') '
+        .    $langHasDel
+        .    '</p>'
+        .    '<p>'
+        .    '<a href="../../index.php">'
+        .    $langBackHomeOf.' '. $siteName
+        .    '</a>'
+        ;
+    
+        if ( isset($cidToEdit) ) //we can suppose that script is accessed from admin tool in this case
+        {
+            echo ' | '
+            .    '<a href="../admin/index.php">'
+            .    $langBackToAdmin . '</a>'
+            ;
+        }
+        echo '</p>';
+        break;
+    // ASK DELETE CONFIRMATION TO THE USER
+    case DISP_CONFIRM_DELETE :
+        echo '<p>'
+        .    '<font color="#CC0000">'
+        .    $langByDel.' &quot;' . $currentCourseName . '&quot; '
+        .    '(' . $currentCourseCode.') ?'
+        .    '</font>'
+        .    '</p>'
+        .    '<p>'
+        .    '<font color="#CC0000">'
+        .    '<a href="' . $_SERVER['PHP_SELF'] . '?delete=yes' . $addToURL . '">'
+        .    $langYes
+        .    '</a>'
+        .    '&nbsp;|&nbsp;'
+        .    '<a href="infocours.php?'.$addToURL.'">'
+        .    $langNo
+        .    '</a>'
+        .    '</font>'
+        .    '</p>'
+        ;
+        break;
+    case DISP_NOT_ALLOWED :
+      echo $langNotAllowed;
+       break;
+}
+
+include($includePath . '/claro_init_footer.inc.php');
 ?>
