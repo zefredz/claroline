3edd88e6cc0507b6a6e6ed942071a0ab956def2f
moosh <moosh@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-06-18 21:24:07 +0000

    - PHPDoc valid - space

--- claroline/inc/lib/auth.lib.inc.php
+++ claroline/inc/lib/auth.lib.inc.php
@@ -17,75 +17,72 @@
  * generates randomly a new password
  *
  * @author Damien Seguy
- * @param - void
  * @return string the new password
  */
- 
+
 function generate_passwd()
 {
-	if (func_num_args() == 1) $nb = func_get_arg(0);
-	else                      $nb = 8;
+    if (func_num_args() == 1) $nb = func_get_arg(0);
+    else                      $nb = 8;
 
-	// on utilise certains chiffres : 1 = i, 5 = S, 6=b, 3=E, 9=G, 0=O
+    // on utilise certains chiffres : 1 = i, 5 = S, 6=b, 3=E, 9=G, 0=O
 
-	$lettre = array();
+    $lettre = array();
 
-	$lettre[0] = array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 
-	                   'j', 'k', 'l', 'm', 'o', 'n', 'p', 'q', 'r', 
-	                   's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 
-	                   'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
-	                   'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'D', 
-	                   'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '9', 
-	                   '0', '6', '5', '1', '3');
+    $lettre[0] = array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+    'j', 'k', 'l', 'm', 'o', 'n', 'p', 'q', 'r',
+    's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A',
+    'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+    'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'D',
+    'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '9',
+    '0', '6', '5', '1', '3');
 
-	$lettre[1] =  array('a', 'e', 'i', 'o', 'u', 'y', 'A', 'E', 
-	                    'I', 'O', 'U', 'Y' , '1', '3', '0' );
+    $lettre[1] =  array('a', 'e', 'i', 'o', 'u', 'y', 'A', 'E',
+    'I', 'O', 'U', 'Y' , '1', '3', '0' );
 
-	$lettre[-1] = array('b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 
-	                    'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 
-	                    'v', 'w', 'x', 'z', 'B', 'C', 'D', 'F', 
-	                    'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 
-	                    'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Z', 
-	                    '5', '6', '9');
+    $lettre[-1] = array('b', 'c', 'd', 'f', 'g', 'h', 'j', 'k',
+    'l', 'm', 'n', 'p', 'q', 'r', 's', 't',
+    'v', 'w', 'x', 'z', 'B', 'C', 'D', 'F',
+    'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P',
+    'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Z',
+    '5', '6', '9');
 
-	$retour   = "";
-	$prec     = 1;
-	$precprec = -1;
+    $retour   = "";
+    $prec     = 1;
+    $precprec = -1;
 
-	srand((double)microtime()*20001107);
+    srand((double)microtime()*20001107);
 
-	while(strlen($retour) < $nb)
-	{
-		// To generate the password string we follow these rules : (1) If two 
-		// letters are consonnance (vowel), the following one have to be a vowel 
-		// (consonnace) - (2) If letters are from different type, we choose a 
-		// letter from the alphabet.
+    while(strlen($retour) < $nb)
+    {
+        // To generate the password string we follow these rules : (1) If two
+        // letters are consonnance (vowel), the following one have to be a vowel
+        // (consonnace) - (2) If letters are from different type, we choose a
+        // letter from the alphabet.
 
-		$type     = ($precprec + $prec)/2;
-		$r        = $lettre[$type][array_rand($lettre[$type], 1)];
-		$retour  .= $r;
-		$precprec = $prec;
-		$prec     = in_array($r, $lettre[-1]) - in_array($r, $lettre[1]);
+        $type     = ($precprec + $prec)/2;
+        $r        = $lettre[$type][array_rand($lettre[$type], 1)];
+        $retour  .= $r;
+        $precprec = $prec;
+        $prec     = in_array($r, $lettre[-1]) - in_array($r, $lettre[1]);
 
-	}
-	return $retour;
+    }
+    return $retour;
 }
 
 /**
  * Check an email
- * @author Christophe Gesche <gesche@ipm.ucl.ac.be>
  * @version 1.0
- * @param  integer	$email 			email to check
+ * @param  integer    $email email to check
  * @return boolean state of validity.
- * @desc return true  if email  is  acceptable for claroline usage.
- * @package claro.auth.lib
+ * @author Christophe Gesche <moosh@claroline.net>
  */
 
 function is_well_formed_email_address($address)
 {
     $regexp = '^[0-9a-z_\.-]+@(([0-9]{1,3}\.){3}[0-9]{1,3}|([0-9a-z][0-9a-z-]*[0-9a-z]\.)+[a-z]{2,4})$';
 
-//  $regexp = '^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$';
-	return eregi($regexp, $address);
+    //  $regexp = '^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$';
+    return eregi($regexp, $address);
 }
 ?>
