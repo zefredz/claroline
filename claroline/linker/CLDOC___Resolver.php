894cf7e31af53dcef86944c152f50f6047dd98cd
moosh <moosh@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-06-21 22:09:38 +0000

    - phpDoc

--- claroline/linker/CLDOC___Resolver.php
+++ claroline/linker/CLDOC___Resolver.php
@@ -1,15 +1,20 @@
 <?php // $Id$
-//----------------------------------------------------------------------
-// CLAROLINE
-//----------------------------------------------------------------------
-// Copyright (c) 2001-2005 Universite catholique de Louvain (UCL)
-//----------------------------------------------------------------------
-// This program is under the terms of the GENERAL PUBLIC LICENSE (GPL)
-// as published by the FREE SOFTWARE FOUNDATION. The GPL is available
-// through the world-wide-web at http://www.gnu.org/copyleft/gpl.html
-//----------------------------------------------------------------------
-// Authors: see 'credits' file
-//----------------------------------------------------------------------
+/**
+ * CLAROLINE 
+ *
+ * @version 1.7
+ *
+ * @copyright (c) 2001-2005 Universite catholique de Louvain (UCL)
+ *
+ * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE 
+ * 
+ * @author claroline Team <cvs@claroline.net>
+ * @author Renaud Fallier <captren@gmail.com>
+ * @author Frédéric Minne <minne@ipm.ucl.ac.be>
+ *
+ * @package CLLINKER
+ *
+ */
 
     require_once dirname(__FILE__) . '/resolver.lib.php';
 
