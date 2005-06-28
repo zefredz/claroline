426137812815120f049bdad75f59a690c4736898
moosh <moosh@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-06-28 21:56:46 +0000

    use $_POST

--- claroline/document/pdf_sample_generator.php
+++ claroline/document/pdf_sample_generator.php
@@ -1,19 +1,20 @@
 <?php
 //require '../inc/claro_init_global.inc.php';
-//define('FPDF_font_path',"../admin/mysql/libraries/fpdf/font/");
+//define('FPDF_font_path','../admin/mysql/libraries/fpdf/font/');
 
-GLOBAL $siteName,$administrator,$administrator_email, $currentCourseCode, $HTTP_POST_VARS;
+GLOBAL $siteName,$administrator,$administrator_email,
+ $currentCourseCode, $_POST;
 
 
-$screenCodeCourse 	= $currentCourseCode;
-$nameCourse 		= $HTTP_POST_VARS["intitule"];
-$titu				= $HTTP_POST_VARS["titulaires"];
-$fac				= $HTTP_POST_VARS["faculte"];
-$courseLang			= $HTTP_POST_VARS["languageCourse"];
+$screenCodeCourse   = $currentCourseCode;
+$nameCourse         = $_REQUEST['intitule'];
+$titu               = $_REQUEST['titulaires'];
+$fac                = $_REQUEST['faculte'];
+$courseLang         = $_REQUEST['languageCourse'];
 
-$FPDF_font_path = "../admin/mysql/libraries/fpdf/font/";
-//$FPDF_font_path = "font/";
-require("../admin/mysql/libraries/fpdf/fpdf.php");
+$FPDF_font_path = '../admin/mysql/libraries/fpdf/font/';
+//$FPDF_font_path = 'font/';
+require('../admin/mysql/libraries/fpdf/fpdf.php');
 $pdf=new FPDF();
 $pdf->Open();
 $pdf->AddPage();
@@ -22,17 +23,17 @@ $pdf->Cell(40,10,$siteName,'B',1);
 $pdf->SetFont('times','B',12);
 $pdf->Cell(40,10,'Hello World !',0,1);
 $pdf->SetFont('times','',12);
-$pdf->Cell(40,10,"Cours ".$screenCodeCourse." ",'',1);
+$pdf->Cell(40,10,'Cours '.$screenCodeCourse . ' ','',1);
 $pdf->SetFont('times','I',12);
-$pdf->Cell(40,10,$nameCourse,'',1);
-$pdf->SetFont('times','',11);
-$pdf->Cell(40,10,'Hello World !',0,1);
-$pdf->Cell(40,10,'Titulaire : 	'.$titu,0,1);
-$pdf->Cell(40,10,'Faculté : 	'.$fac,0,1);
-$pdf->Cell(40,10,'Langue : 	'.$courseLang,0,1);
-$pdf->SetFont('times','',10);
-$pdf->Cell(180,8,$administrator_name." ".$administrator_email,'T');
-$fichierTemp =  tempnam ( ".", "samplePDF");
+$pdf->Cell(40,10,$nameCourse, '', 1);
+$pdf->SetFont('times', '', 11);
+$pdf->Cell(40,10,'Hello World !', 0, 1);
+$pdf->Cell(40,10,'Titulaire :     ' . $titu, 0, 1);
+$pdf->Cell(40,10,'Faculté :     ' . $fac, 0, 1);
+$pdf->Cell(40,10,'Langue :     ' . $courseLang, 0, 1);
+$pdf->SetFont('times', '', 10);
+$pdf->Cell(180,8,$administrator_name . ' ' . $administrator_email, 'T');
+$fichierTemp =  tempnam ( '.', 'samplePDF');
 $pdf->Output($fichierTemp);
 
 ?>
