e2d97ad0d20c494a8cdde0d3fbdf528e347298ed
seb <seb@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-06-29 14:09:54 +0000

    Add tracking question by question for the exercise

--- claroline/tracking/exercises_details.php
+++ claroline/tracking/exercises_details.php
@@ -16,12 +16,17 @@ if( empty($_REQUEST['exo_id']) ) header("Location: ../exercice/exercice.php");
 /**
  * DB tables definition
  */
-$tbl_cdb_names = claro_sql_get_course_tbl();
 $tbl_mdb_names = claro_sql_get_main_tbl();
 $tbl_rel_course_user = $tbl_mdb_names['rel_course_user'  ];
 $tbl_user            = $tbl_mdb_names['user'             ];
-$tbl_quiz_test      = $tbl_cdb_names['quiz_test'              ];
-$tbl_track_e_exercices = $tbl_cdb_names['track_e_exercices'];
+
+$tbl_cdb_names = claro_sql_get_course_tbl();
+$tbl_quiz_test      	= $tbl_cdb_names['quiz_test'              ];
+$tbl_quiz_question      = $tbl_cdb_names['quiz_question'          ];
+$tbl_quiz_rel_test_question  = $tbl_cdb_names['quiz_rel_test_question' ];
+$tbl_track_e_exercices 	= $tbl_cdb_names['track_e_exercices'];
+$tbl_track_e_exe_details = $tbl_cdb_names['track_e_exe_details'];
+$tbl_track_e_exe_answers = $tbl_cdb_names['track_e_exe_answers'];
 
 include($includePath."/lib/statsUtils.lib.inc.php");
 
@@ -93,61 +98,109 @@ if($is_allowedToTrack && $is_trackingEnabled)
 
 
 <?php
-  // display details
-   $sql = "SELECT `U`.`nom`, `U`.`prenom`, `U`.`user_id`,
-            MIN(TEX.`exe_result`) AS `minimum`,
-            MAX(TEX.`exe_result`) AS `maximum`,
-            AVG(TEX.`exe_result`) AS `average`,
-            COUNT(TEX.`exe_result`) AS `attempts`,
-			AVG(TEX.`exe_time`) AS `avgTime`
-    FROM `".$tbl_user."` AS `U`, `".$tbl_rel_course_user."` AS `CU`, `".$tbl_quiz_test."` AS `QT`
-    LEFT JOIN `".$tbl_track_e_exercices."` AS `TEX`
-          ON `CU`.`user_id` = `TEX`.`exe_user_id` 
-          AND `QT`.`id` = `TEX`.`exe_exo_id`
-    WHERE `CU`.`user_id` = `U`.`user_id`
-      AND `CU`.`code_cours` = '".$_cid."'
-      AND (
-            `TEX`.`exe_exo_id` = ".$exo_details['id']." 
-            OR 
-            `TEX`.`exe_exo_id` IS NULL 
-          )
-    GROUP BY `U`.`user_id`
-    ORDER BY `U`.`nom` ASC, `U`.`prenom` ASC";
+	//-- display details : USERS VIEW
+	$sql = "SELECT `U`.`nom`, `U`.`prenom`, `U`.`user_id`,
+	        MIN(TE.`exe_result`) AS `minimum`,
+	        MAX(TE.`exe_result`) AS `maximum`,
+	        AVG(TE.`exe_result`) AS `average`,
+	        COUNT(TE.`exe_result`) AS `attempts`,
+			AVG(TE.`exe_time`) AS `avgTime`
+	FROM `".$tbl_user."` AS `U`, `".$tbl_rel_course_user."` AS `CU`, `".$tbl_quiz_test."` AS `QT`
+	LEFT JOIN `".$tbl_track_e_exercices."` AS `TE`
+	      ON `CU`.`user_id` = `TE`.`exe_user_id`
+	      AND `QT`.`id` = `TE`.`exe_exo_id`
+	WHERE `CU`.`user_id` = `U`.`user_id`
+	  AND `CU`.`code_cours` = '".$_cid."'
+	  AND (
+	        `TE`.`exe_exo_id` = ".$exo_details['id']."
+	        OR
+	        `TE`.`exe_exo_id` IS NULL
+	      )
+	GROUP BY `U`.`user_id`
+	ORDER BY `U`.`nom` ASC, `U`.`prenom` ASC";
     
     
-  $result = claro_sql_query($sql);
-  // display tab header
-  echo "<table class=\"claroTable\" width=\"100%\" border=\"0\" cellspacing=\"2\">\n
-      <tr class=\"headerX\" align=\"center\" valign=\"top\">\n
-        <th>".$langStudent."</th>\n
-        <th>".$langScoreMin."</th>\n
-        <th>".$langScoreMax."</th>\n
-        <th>".$langScoreAvg."</th>\n
-        <th>".$langAttempts."</th>\n
-        <th>".$langExeAvgTime."</th>\n
-      </tr>\n
-      <tbody>";
-  // display tab content
-  while ( $exo_users_details = mysql_fetch_array($result) )
-  {
-    if ( $exo_users_details['minimum'] == '' )
-    {
-      $exo_users_details['minimum'] = 0;
-      $exo_users_details['maximum'] = 0;
-    }
-    echo 	 "<tr>\n"
-      		."<td><a href=\"userLog.php?uInfo=".$exo_users_details['user_id']."&view=0100000&exoDet=".$exo_details['id']."\">"
-			.$exo_users_details['nom']." ".$exo_users_details['prenom']."</a></td>\n"
-      		."<td>".$exo_users_details['minimum']."</td>\n"
-      		."<td>".$exo_users_details['maximum']."</td>\n"
-      		."<td>".(round($exo_users_details['average']*100)/100)."</td>\n"
-      		."<td>".$exo_users_details['attempts']."</td>\n"
-      		."<td>".claro_disp_duration(floor($exo_users_details['avgTime']))."</td>\n"
-    		."</tr>";
-  }
-  // foot of table
-  echo "</tbody>\n</table>";
+	$exo_users_details = claro_sql_query_fetch_all($sql);
 
+	echo '<p><b>By users</b></p>'."\n";
+	// display tab header
+	echo '<table class="claroTable" width="100%" border="0" cellspacing="2">'."\n"
+		.'<tr class="headerX" align="center" valign="top">'."\n"
+	    .'<th>'.$langStudent.'</th>'."\n"
+	    .'<th>'.$langScoreMin.'</th>'."\n"
+	    .'<th>'.$langScoreMax.'</th>'."\n"
+	    .'<th>'.$langScoreAvg.'</th>'."\n"
+	    .'<th>'.$langAttempts.'</th>'."\n"
+	    .'<th>'.$langExeAvgTime.'</th>'."\n"
+	  	.'</tr>'."\n"
+	  	.'<tbody>'."\n\n";
+	  	
+	// display tab content
+	foreach( $exo_users_details as $exo_users_detail )
+	{
+		if ( $exo_users_detail['minimum'] == '' )
+		{
+			$exo_users_detail['minimum'] = 0;
+			$exo_users_detail['maximum'] = 0;
+		}
+		echo 	 '<tr>'."\n"
+		  		.'<td><a href="userLog.php?uInfo='.$exo_users_detail['user_id'].'&view=0100000&exoDet='.$exo_details['id'].'">'."\n"
+				.$exo_users_detail['nom'].' '.$exo_users_detail['prenom'].'</a></td>'."\n"
+		  		.'<td>'.$exo_users_detail['minimum'].'</td>'."\n"
+		  		.'<td>'.$exo_users_detail['maximum'].'</td>'."\n"
+		  		.'<td>'.(round($exo_users_detail['average']*100)/100).'</td>'."\n"
+		  		.'<td>'.$exo_users_detail['attempts'].'</td>'."\n"
+		  		.'<td>'.claro_disp_duration(floor($exo_users_detail['avgTime'])).'</td>'."\n"
+				.'</tr>'."\n";
+	}
+	// foot of table
+	echo '</tbody>'."\n".'</table>'."\n\n";
+
+	// display details : QUESTIONS VIEW
+	$sql = "SELECT `Q`.`question`, `Q`.`type`, `Q`.`ponderation`,
+          		MIN(TED.`result`) AS `minimum`,
+				MAX(TED.`result`) AS `maximum`,
+				AVG(TED.`result`) AS `average`
+		FROM `".$tbl_quiz_question."` AS `Q`, `".$tbl_quiz_rel_test_question."` AS `RTQ`
+		LEFT JOIN `".$tbl_track_e_exercices."` AS `TE`
+		    ON `TE`.`exe_exo_id` = `RTQ`.`exercice_id`
+		LEFT JOIN `".$tbl_track_e_exe_details."` AS `TED`
+      		ON `TED`.`exercise_track_id` = `TE`.`exe_id`
+			AND `TED`.`question_id` = `Q`.`id`
+		WHERE `Q`.`id` = `RTQ`.`question_id`
+			AND `RTQ`.`exercice_id` = ".$exo_details['id']."
+		GROUP BY `TED`.`question_id`
+		ORDER BY `Q`.`question` ASC";
+
+	$exo_questions_details = claro_sql_query_fetch_all($sql);
+
+	echo '<p><b>By questions</b></p>'."\n";
+	// display tab header
+	echo '<table class="claroTable" width="100%" border="0" cellspacing="2">'."\n"
+		.'<tr class="headerX" align="center" valign="top">'."\n"
+	    .'<th>'.$langTitle.'</th>'."\n"
+	    .'<th>'.$langScoreMin.'</th>'."\n"
+	    .'<th>'.$langScoreMax.'</th>'."\n"
+	    .'<th>'.$langScoreAvg.'</th>'."\n"
+	  	.'</tr>'."\n"
+	  	.'<tbody>'."\n";
+	// display tab content
+	foreach ( $exo_questions_details as $exo_questions_detail )
+	{
+		if ( $exo_questions_detail['minimum'] == '' )
+		{
+			$exo_questions_detail['minimum'] = 0;
+			$exo_questions_detail['maximum'] = 0;
+		}
+		echo 	 '<tr>'."\n"
+		  		.'<td>'.$exo_questions_detail['question'].'</td>'."\n"
+		  		.'<td>'.$exo_questions_detail['minimum'].'/'.$exo_questions_detail['ponderation'].'</td>'."\n"
+		  		.'<td>'.$exo_questions_detail['maximum'].'/'.$exo_questions_detail['ponderation'].'</td>'."\n"
+		  		.'<td>'.(round($exo_questions_detail['average']*100)/100).'/'.$exo_questions_detail['ponderation'].'</td>'."\n"
+				.'</tr>'."\n";
+	}
+	// foot of table
+	echo '</tbody>'."\n".'</table>'."\n\n";
 }
 // not allowed
 else
