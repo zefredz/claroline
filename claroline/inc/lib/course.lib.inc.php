5709f11c84c881028b6c5e38dbb1fcff4c2a6959
moosh <moosh@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-06-29 07:48:43 +0000

    - add function claro_get_cat_list - add function claro_get_cat_flat_list

--- claroline/inc/lib/course.lib.inc.php
+++ claroline/inc/lib/course.lib.inc.php
@@ -553,6 +553,62 @@ function build_editable_cat_table($selectedCat = null, $separator = "&gt;")
     return $tableToDisplay;
 }
 
+
+/**
+ * build the <option> element with categories where we can create/have courses
+ *
+ * @param the code of the preselected categorie
+ * @param the separator used between a cat and its paretn cat to display in the <select> 
+ * @return echo all the <option> elements needed for a <select>. 
+ * 
+ */
+
+
+function claro_get_cat_list()
+{
+    $tbl_mdb_names = claro_sql_get_main_tbl();
+    $tbl_category  = $tbl_mdb_names['category'];
+
+    $sql = " SELECT code, code_P, name, canHaveCoursesChild
+               FROM `" . $tbl_category . "` 
+               ORDER BY `name`";
+    return claro_sql_query_fetch_all($sql);
+    
+}
+
+
+function claro_get_cat_flat_list($separator = ' > ')
+{
+    
+    $fac_list = claro_get_cat_list();
+    
+    if(is_array($fac_list))
+    foreach ($fac_list as $myfac)
+    {
+        $categories[$myfac['code']]['code']   = $myfac['code'];
+        $categories[$myfac['code']]['parent'] = $myfac['code_P'];
+        $categories[$myfac['code']]['name']   = $myfac['name'];
+        $categories[$myfac['code']]['childs'] = $myfac['canHaveCoursesChild'];
+    }
+
+    // then we build the table we need : full path of editable cats in an array
+    
+    $tableToDisplay = array();
+    foreach ($categories as $cat)
+    {
+        if ( $cat['childs'] == 'TRUE' ) 
+        { 
+            $fac_array[$cat['code']] = '(' 
+            .                          get_full_path($categories, $cat['code'], $separator)
+            .                          ') ' 
+            .                          htmlspecialchars($cat['name'])
+            ;
+        }
+    }
+    
+    return $fac_array;
+}
+
 /**
  * Recursive function to get the full categories path of a specified categorie
  *
@@ -564,7 +620,7 @@ function build_editable_cat_table($selectedCat = null, $separator = "&gt;")
   */
 
  
-function get_full_path($categories, $catcode = NULL, $separator = ' &gt; ')
+function get_full_path($categories, $catcode = NULL, $separator = ' > ')
 {
     //Find parent code
     
