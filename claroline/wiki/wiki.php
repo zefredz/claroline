e419e462de01849dc05659c9087f884d5f295f54
Frédéric Minne <zefredz@users.noreply.github.com>
2005-07-04 14:07:11 +0000

    add ucl copyright

--- claroline/wiki/wiki.php
+++ claroline/wiki/wiki.php
@@ -3,16 +3,19 @@
     // vim: expandtab sw=4 ts=4 sts=4:
 
     /**
-      * Wiki Administration Index
-      *
-      * @module Wiki
-      * @access public
-      * @author Frederic Minne <zefredz@gmail.com>
-      * @copyright  (c) 2005 Frederic Minne under the terms of the GNU General
-      *     Public License as published by the Free Software Foundation; either
-      *     version 2 of the License, or (at your option) any later version.
-      * @version 0.1
-      */
+     * @version CLAROLINE 1.7
+     *
+     * @copyright 2001-2005 Universite catholique de Louvain (UCL)
+     *
+     * @license GENERAL PUBLIC LICENSE (GPL)
+     * This program is under the terms of the GENERAL PUBLIC LICENSE (GPL)
+     * as published by the FREE SOFTWARE FOUNDATION. The GPL is available
+     * through the world-wide-web at http://www.gnu.org/copyleft/gpl.html
+     *
+     * @author Frederic Minne <zefredz@gmail.com>
+     *
+     * @package Wiki
+     */
       
     $tlabelReq = 'CLWIKI__';
 
