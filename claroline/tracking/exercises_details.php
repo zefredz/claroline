4674e30b14da75666cda48c70de3888f22bb8cf6
seb <seb@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-07-19 11:04:34 +0000

    Create a new group in display for csv export

--- claroline/tracking/exercises_details.php
+++ claroline/tracking/exercises_details.php
@@ -123,6 +123,9 @@ if($is_allowedToTrack && $is_trackingEnabled)
 	.'<ul>'."\n"
 	.'<li>'.$langExerciseUsersAttempts.' : '.$exo_scores_details['users'].'</li>'."\n"
 	.'<li>'.$langExerciseTotalAttempts.' : '.$exo_scores_details['tusers'].'</li>'."\n"
+	.'</ul>'."\n\n";
+	
+	echo '<ul>'."\n"
 	.'<li><a href="'.$_SERVER['PHP_SELF'].'?exportCsv=1&exo_id='.$_REQUEST['exo_id'].'">'.$langExportTrackingCsv.'</a></li>'."\n"
 	.'</ul>'."\n\n";
 
