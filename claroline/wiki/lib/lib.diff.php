53e237d37e3f24e32f8c896c0cd640657057c316
Frédéric Minne <zefredz@users.noreply.github.com>
2005-07-23 18:17:43 +0000

    CLWIKI, beautify code

--- claroline/wiki/lib/lib.diff.php
+++ claroline/wiki/lib/lib.diff.php
@@ -1,49 +1,49 @@
 <?php // $Id$
-
+     
     // vim: expandtab sw=4 ts=4 sts=4:
-
-    if( (bool) stristr( $_SERVER['PHP_SELF'], basename(__FILE__) ) )
+     
+    if ( (bool) stristr( $_SERVER['PHP_SELF'], basename(__FILE__) ) )
     {
-        die("This file cannot be accessed directly! Include it in your script instead!");
+        die( "This file cannot be accessed directly! Include it in your script instead!" );
     }
-    
+     
     define( "DIFF_EQUAL", "=" );
     define( "DIFF_ADDED", "+" );
     define( "DIFF_DELETED", "-" );
     define( "DIFF_MOVED", "M" );
-    
+     
     /**
-     * @version CLAROLINE 1.7
-     *
-     * @copyright 2001-2005 Universite catholique de Louvain (UCL)
-     *
-     * @license GENERAL PUBLIC LICENSE (GPL)
-     * This program is under the terms of the GENERAL PUBLIC LICENSE (GPL)
-     * as published by the FREE SOFTWARE FOUNDATION. The GPL is available
-     * through the world-wide-web at http://www.gnu.org/copyleft/gpl.html
-     *
-     * @author Frederic Minne <zefredz@gmail.com>
-     *
-     * @package Wiki
-     */
-    
+    * @version CLAROLINE 1.7
+    *
+    * @copyright 2001-2005 Universite catholique de Louvain (UCL)
+    *
+    * @license GENERAL PUBLIC LICENSE (GPL)
+    * This program is under the terms of the GENERAL PUBLIC LICENSE (GPL)
+    * as published by the FREE SOFTWARE FOUNDATION. The GPL is available
+    * through the world-wide-web at http://www.gnu.org/copyleft/gpl.html
+    *
+    * @author Frederic Minne <zefredz@gmail.com>
+    *
+    * @package Wiki
+    */
+     
     function diff( $old, $new, $show_equals = false )
     {
         $oldArr = str_split( $old );
         $newArr = str_split( $new );
-        
+         
         $oldCount = count ( $oldArr );
         $newCount = count ( $newArr );
-        
+         
         $max = max( $oldCount, $newCount );
-        
+         
         //get added and deleted lines
-        
+         
         $deleted = array_diff_assoc( $oldArr, $newArr );
         $added = array_diff_assoc( $newArr, $oldArr );
-
+         
         $moved = array();
-
+         
         foreach ( $added as $key => $candidate )
         {
             foreach ( $deleted as $index => $content )
@@ -51,32 +51,32 @@
                 if ( $candidate == $content )
                 {
                     $moved[$key] = $candidate;
-                    unset( $added[$key] );
-                    unset( $deleted[$index] );
+                    unset($added[$key] );
+                    unset($deleted[$index] );
                     break;
                 }
             }
         }
-        
+         
         $output = '';
-        
+         
         for ( $i = 0; $i < $max; $i++ )
         {
             // line changed
             if ( isset ( $deleted[$i] ) && isset( $added[$i] ) )
             {
-                $output .= format_line( $i, DIFF_DELETED, $deleted[$i] );
-                $output .= format_line( $i, DIFF_ADDED, $added[$i] );
+                $output .= format_line($i, DIFF_DELETED, $deleted[$i] );
+                $output .= format_line($i, DIFF_ADDED, $added[$i] );
             }
             // line deleted
             elseif ( isset ( $deleted[$i] ) && ! isset ( $added[$i] ) )
             {
-                $output .= format_line( $i, DIFF_DELETED, $deleted[$i] );
+                $output .= format_line($i, DIFF_DELETED, $deleted[$i] );
             }
             // line added
             elseif ( isset ( $added[$i] ) && ! isset ( $deleted[$i] ) )
             {
-                $output .= format_line( $i, DIFF_ADDED, $added[$i] );
+                $output .= format_line($i, DIFF_ADDED, $added[$i] );
             }
             // line moved
             elseif ( isset ( $moved[$i] ) )
@@ -86,24 +86,24 @@
             // line unchanged
             elseif ( $show_equals == true )
             {
-                $output .= format_line( $i, DIFF_EQUAL, $newArr[$i] );
+                $output .= format_line($i, DIFF_EQUAL, $newArr[$i] );
             }
             else
             {
                 // skip
             }
         }
-        
+         
         return $output;
     }
-    
-    function str_split( $str )
+     
+    function str_split($str )
     {
         $content = array();
-
+         
         if ( strpos( $str, "\r\n" ) != false )
         {
-            $content = explode( "\r\n", $str );
+            $content = explode("\r\n", $str );
         }
         elseif ( strpos( $str, "\n" ) != false )
         {
@@ -117,11 +117,11 @@
         {
             $content[] = $str;
         }
-
+         
         return $content;
     }
-    
-    function format_line( $line, $type, $value, $skip_empty = false )  #change to $value if problems
+     
+    function format_line( $line, $type, $value, $skip_empty = false )
     {
         if ( trim( $value ) == "" && $skip_empty )
         {
@@ -131,111 +131,90 @@
         {
             $value = '&nbsp;';
         }
-
+         
         switch ( $type )
         {
             case DIFF_EQUAL:
             {
-                return $line. ' : '
-                    . ' = <span class="diffEqual" >'
-                    . $value
-                    . '</span><br />' . "\n"
-                    ;
-
+                return $line. ' : ' . ' = <span class="diffEqual" >' . $value . '</span><br />' . "\n" ;
+                 
                 break;
             }
             case DIFF_MOVED:
             {
-                return $line. ' : '
-                    . ' M <span class="diffMoved" >'
-                    . $value
-                    . '</span><br />' . "\n"
-                    ;
-
+                return $line. ' : ' . ' M <span class="diffMoved" >' . $value . '</span><br />' . "\n" ;
+                 
                 break;
             }
             case DIFF_ADDED:
             {
-                return $line . ' : '
-                    . ' + <span class="diffAdded" >'
-                    . $value
-                    . '</span><br />' . "\n"
-                    ;
-
+                return $line . ' : ' . ' + <span class="diffAdded" >' . $value . '</span><br />' . "\n" ;
+                 
                 break;
             }
             case DIFF_DELETED:
             {
-                return $line . ' : '
-                    . ' - <span class="diffDeleted" >'
-                    . $value
-                    . '</span><br />' . "\n"
-                    ;
-
+                return $line . ' : ' . ' - <span class="diffDeleted" >' . $value . '</span><br />' . "\n" ;
+                 
                 break;
             }
         }
     }
-    
-if ( ! function_exists('array_diff_assoc') )
-{
-    /**
-     * Replace array_diff_assoc()
-     *
-     * @category    PHP
-     * @package     PHP_Compat
-     * @link        http://php.net/function.array_diff_assoc
-     * @author      Aidan Lister <aidan@php.net>
-     * @version     $Revision$
-     * @since       PHP 4.3.0
-     * @require     PHP 4.0.0 (user_error)
-     */
-    function array_diff_assoc()
+     
+    if (! function_exists('array_diff_assoc') )
     {
-        // Check we have enough arguments
-        $args = func_get_args();
-        $count = count( $args );
-        if ( count( $args ) < 2 )
+        /**
+        * Replace array_diff_assoc()
+        *
+        * @link        http://php.net/function.array_diff_assoc
+        * @author      Aidan Lister <aidan@php.net>
+        * @since       PHP 4.3.0
+        * @require     PHP 4.0.0 (user_error)
+        */
+        function array_diff_assoc()
         {
-            trigger_error( 'Wrong parameter count for array_diff_assoc()', E_USER_WARNING );
-            return;
-        }
-
-        // Check arrays
-        for ( $i = 0; $i < $count; $i++ )
-        {
-            if ( ! is_array( $args[$i] ) )
+            // Check we have enough arguments
+            $args = func_get_args();
+            $count = count($args );
+            if (count($args ) < 2 )
             {
-                trigger_error( 'array_diff_assoc() Argument #' .
-                    ($i + 1) . ' is not an array', E_USER_WARNING );
+                trigger_error('Wrong parameter count for array_diff_assoc()', E_USER_WARNING );
                 return;
             }
-        }
-
-        // Get the comparison array
-        $array_comp = array_shift( $args );
-        --$count;
-
-        // Traverse values of the first array
-        foreach ( $array_comp as $key => $value )
-        {
-            // Loop through the other arrays
-            for ( $i = 0; $i < $count; $i++ )
+             
+            // Check arrays
+            for ($i = 0; $i < $count; $i++ )
             {
-                // Loop through this arrays key/value pairs and compare
-                foreach ( $args[$i] as $comp_key => $comp_value )
+                if (! is_array($args[$i] ) )
                 {
-                    if ( (string) $key === (string)$comp_key &&
-                        (string) $value === (string) $comp_value )
+                    trigger_error('array_diff_assoc() Argument #' . ($i + 1) . ' is not an array', E_USER_WARNING );
+                    return;
+                }
+            }
+             
+            // Get the comparison array
+            $array_comp = array_shift($args );
+            --$count;
+             
+            // Traverse values of the first array
+            foreach ($array_comp as $key => $value )
+            {
+                // Loop through the other arrays
+                for ($i = 0; $i < $count; $i++ )
+                {
+                    // Loop through this arrays key/value pairs and compare
+                    foreach ($args[$i] as $comp_key => $comp_value )
                     {
-
-                        unset( $array_comp[$key] );
+                        if ((string) $key === (string)$comp_key && (string) $value === (string) $comp_value )
+                        {
+                             
+                            unset($array_comp[$key] );
+                        }
                     }
                 }
             }
+             
+            return $array_comp;
         }
-
-        return $array_comp;
     }
-}
 ?>
