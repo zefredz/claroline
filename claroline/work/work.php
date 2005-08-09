dfd56e0a39e04d29b404fc7d8cd38c06e12ea9d7
seb <seb@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-08-09 07:31:11 +0000

    rename assignement.lib.php to assignment.lib.php

--- claroline/work/work.php
+++ claroline/work/work.php
@@ -22,7 +22,7 @@ $tlabelReq = 'CLWRK___';
 require '../inc/claro_init_global.inc.php';
 
 include($includePath . '/lib/events.lib.inc.php');
-include($includePath . '/lib/assignement.lib.php');
+include($includePath . '/lib/assignment.lib.php');
 
 $tbl_cdb_names = claro_sql_get_course_tbl();
 $tbl_wrk_assignment = $tbl_cdb_names['wrk_assignment'];
