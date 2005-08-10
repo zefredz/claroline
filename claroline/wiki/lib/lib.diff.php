efa73c7055b01374bdbcad5094d2131a3684e54a
Frédéric Minne <zefredz@users.noreply.github.com>
2005-08-10 07:52:28 +0000

    move copyright block

--- claroline/wiki/lib/lib.diff.php
+++ claroline/wiki/lib/lib.diff.php
@@ -6,26 +6,26 @@
     {
         die( "This file cannot be accessed directly! Include it in your script instead!" );
     }
+    
+    /**
+     * @version CLAROLINE 1.7
+     *
+     * @copyright 2001-2005 Universite catholique de Louvain (UCL)
+     *
+     * @license GENERAL PUBLIC LICENSE (GPL)
+     * This program is under the terms of the GENERAL PUBLIC LICENSE (GPL)
+     * as published by the FREE SOFTWARE FOUNDATION. The GPL is available
+     * through the world-wide-web at http://www.gnu.org/copyleft/gpl.html
+     *
+     * @author Frederic Minne <zefredz@gmail.com>
+     *
+     * @package Wiki
+     */
      
     define( "DIFF_EQUAL", "=" );
     define( "DIFF_ADDED", "+" );
     define( "DIFF_DELETED", "-" );
     define( "DIFF_MOVED", "M" );
-     
-    /**
-    * @version CLAROLINE 1.7
-    *
-    * @copyright 2001-2005 Universite catholique de Louvain (UCL)
-    *
-    * @license GENERAL PUBLIC LICENSE (GPL)
-    * This program is under the terms of the GENERAL PUBLIC LICENSE (GPL)
-    * as published by the FREE SOFTWARE FOUNDATION. The GPL is available
-    * through the world-wide-web at http://www.gnu.org/copyleft/gpl.html
-    *
-    * @author Frederic Minne <zefredz@gmail.com>
-    *
-    * @package Wiki
-    */
 
     /**
      * Get difference between two strings
