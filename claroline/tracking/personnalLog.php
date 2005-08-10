017ca72024da075f308569dd18d4f50ee1b7cf32
seb <seb@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-08-10 07:20:45 +0000

    Use single quotes in echo

--- claroline/tracking/personnalLog.php
+++ claroline/tracking/personnalLog.php
@@ -30,39 +30,39 @@ echo claro_disp_tool_title($nameTools);
 
 if ( $is_trackingEnabled )
 {
-      // display list of course of the student with links to the corresponding userLog
-      $sql = "SELECT cours.code code, 
-                     cours.intitule name, 
-                     cours.titulaires prof
-                                             
-	          FROM    `".$tbl_courses."`       cours,
-	                  `".$tbl_link_user_courses."`   cours_user
-	                                  
-	          WHERE cours.code = cours_user.code_cours
-	            AND   cours_user.user_id = '". (int)$_uid."'";
+	// display list of course of the student with links to the corresponding userLog
+	$sql = "SELECT cours.code code,
+			cours.intitule name,
+			cours.titulaires prof
 
-      $resCourseListOfUser = claro_sql_query($sql);
+			FROM    `".$tbl_courses."`       cours,
+				`".$tbl_link_user_courses."`   cours_user
 
-      if(mysql_num_rows($resCourseListOfUser) > 0)
-      {
-          echo "<ul>\n";
-          while ( $courseOfUser = mysql_fetch_array($resCourseListOfUser) )
-          {
-              echo "<li>\n"
-                        ."<a href=\"userLog.php?uInfo=".$_uid."&cidReset=true&cidReq=".$courseOfUser['code']."\">".$courseOfUser['name']."</a><br>\n"
-                        ."<small>".$courseOfUser['code']." - ".$courseOfUser['prof']."</small>\n"
-                        ."</li>\n\n";
-          }
-          echo "</ul>\n";
-      }
-      else
-      {
-          echo $langNoRegisteredCourses;
-      }
+			WHERE cours.code = cours_user.code_cours
+			AND   cours_user.user_id = '". (int)$_uid."'";
+
+	$resCourseListOfUser = claro_sql_query($sql);
+
+	if(mysql_num_rows($resCourseListOfUser) > 0)
+	{
+		echo "\n\n".'<ul>'."\n\n";
+		while ( $courseOfUser = mysql_fetch_array($resCourseListOfUser) )
+		{
+			echo '<li>'."\n"
+				.'<a href="userLog.php?uInfo='.$_uid.'&cidReset=true&cidReq='.$courseOfUser['code'].'">'.$courseOfUser['name'].'</a><br>'."\n"
+				.'<small>'.$courseOfUser['code'].' - '.$courseOfUser['prof'].'</small>'."\n"
+				.'</li>'."\n";
+		}
+		echo "\n".'</ul>'."\n";
+	}
+	else
+	{
+		echo $langNoRegisteredCourses;
+	}
 }
 else
 {
-    echo $langTrackingDisabled;
+	echo $langTrackingDisabled;
 }
 
 include($includePath."/claro_init_footer.inc.php");
