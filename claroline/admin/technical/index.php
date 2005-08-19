7efa6008a6ac1bc1c8dbfd9a13e3cff98bf1a90e
seb <seb@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-08-19 10:10:05 +0000

    Add index for this directory

---
+++ claroline/admin/technical/index.php
@@ -0,0 +1,49 @@
+<?php // $Id$
+/**
+ * CLAROLINE
+ *
+ * This is the index page of sdk tools
+ *
+ * @version 1.6 $Revision$
+ *
+ * @copyright (c) 2001-2005 Universite catholique de Louvain (UCL)
+ *
+ * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
+ *
+ * @package SDK
+ *
+ * @author Claro Team <cvs@claroline.net>
+ * @author Christophe Gesché <moosh@claroline.net>
+ *
+ */
+
+require '../../inc/claro_init_global.inc.php';
+if(file_exists($includePath.'/currentVersion.inc.php')) include ($includePath.'/currentVersion.inc.php');
+$langFillToolCourses = 'Fill tools of a course (lorem ipsum filler)';
+
+$is_allowedToUseSDK = $is_platformAdmin;
+
+if (! $is_allowedToUseSDK) claro_disp_auth_form();
+
+$nameTools = $langTechnical;
+
+$interbredcrump[]= array ('url'=>'../index.php', 'name'=> $langAdmin);
+
+include($includePath.'/claro_init_header.inc.php');
+
+echo claro_disp_tool_title(
+	array(
+	'mainTitle'=>$nameTools
+	)
+	);
+
+?>
+<ul>
+ <li><a href="./diskUsage.php"><?php echo $langDiskUsage ?></a></li>
+ <li><a href="./phpInfo.php"><?php echo $lang_php_info ?></a></li>
+ <li><a href="./security.php">Security</a></li>
+</ul>
+
+<?php
+include($includePath."/claro_init_footer.inc.php");
+?>
