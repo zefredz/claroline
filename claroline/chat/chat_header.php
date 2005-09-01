5112e78989048cef3379eca67fd704ea2f520035
mathieu <mathieu@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-09-01 14:25:36 +0000

    fix bug 154: breadcrumb standardization

--- claroline/chat/chat_header.php
+++ claroline/chat/chat_header.php
@@ -22,7 +22,8 @@ require '../inc/claro_init_global.inc.php';
 
 if(isset($_gid))
 {
-    $interbredcrump[]= array ('url'=>'../group/group_space.php', 'name'=> $_group['name']);
+    $interbredcrump[]  = array ('url'=>'../group/group.php', 'name'=> $langGroups);
+    $interbredcrump[]= array ('url'=>'../group/group_space.php', 'name'=> $langGroupSpace);
 }
 
 $nameTools  = $langChat;
