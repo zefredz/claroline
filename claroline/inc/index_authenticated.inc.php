14f204b3cfb2722afd656e70fd732053d0e34585
moosh <moosh@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-09-01 15:01:47 +0000

    use $allowed_to_register value to hide command  if  not allowed

--- claroline/inc/index_authenticated.inc.php
+++ claroline/inc/index_authenticated.inc.php
@@ -1,18 +1,18 @@
 <?php // $Id$
 /**
- * CLAROLINE 
+ * CLAROLINE
  *
  * this  is  the  home page  of a campus  for an authenticated user
  * this  page  list of users subscribed courses
- * when the user is anonymous, index anonymous.inc.php 
+ * when the user is anonymous, index anonymous.inc.php
  * is load instead of this code.
  *
  * @version 1.7
  *
  * @copyright (c) 2001-2005 Universite catholique de Louvain (UCL)
  *
- * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE 
- * 
+ * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
+ *
  * @author claroline Team <cvs@claroline.net>
  *
  * @package CLINDEX
@@ -22,18 +22,18 @@
 if ((bool) stristr($_SERVER['PHP_SELF'], basename(__FILE__))) die('---');
 if ( ! isset($_uid) ) claro_disp_auth_form();
 
-$sql = "SELECT course.code           `sysCode`, 
-               course.directory      `directory`, 
-               course.fake_code      `officialCode`, 
+$sql = "SELECT course.code           `sysCode`,
+               course.directory      `directory`,
+               course.fake_code      `officialCode`,
                course.dbName         `db`,
-               course.intitule       `title`, 
+               course.intitule       `title`,
                course.titulaires     `titular`,
                course.languageCourse `language`,
                course_user.statut    `userSatus`
 
                FROM `" . $tbl_courses . "`           course,
                     `" . $tbl_link_user_courses . "` course_user
-                                 
+
                WHERE course.code         = course_user.code_cours
                  AND course_user.user_id = '" . (int) $_uid . "'
                ORDER BY UPPER(fake_code)";
@@ -44,10 +44,10 @@ $personnalCourseList = claro_sql_query_fetch_all($sql);
  * get a digest of announcement and calendar of each of these courses
  */
 
-$courseDigestList = array('courseSysCode' => array(), 
-                          'courseOfficialCode' => array(), 
-                          'toolLabel' => array(), 
-                          'date' => array(), 
+$courseDigestList = array('courseSysCode' => array(),
+                          'courseOfficialCode' => array(),
+                          'toolLabel' => array(),
+                          'date' => array(),
                           'content' => array());
 
 foreach($personnalCourseList as $thisCourse)
@@ -61,7 +61,7 @@ foreach($personnalCourseList as $thisCourse)
     $sql = "SELECT '" . addslashes($thisCourse['sysCode']) ."' AS `courseSysCode`,
                    '" . addslashes($thisCourse['officialCode']) ."' AS `courseOfficialCode`,
                    'CLANN___' AS `toolLabel`,
-                   CONCAT(`temps`, ' ', '00:00:00') AS `date`, 
+                   CONCAT(`temps`, ' ', '00:00:00') AS `date`,
                    CONCAT(`title`,' - ',`contenu`) AS `content`
             FROM `" . $tableAnn . "`
             WHERE    CONCAT(`title`, `contenu`) != ''
@@ -80,7 +80,7 @@ foreach($personnalCourseList as $thisCourse)
     /*
      * AGENDA : get the next agenda entries of this course from now
      */
-    
+
     $tableCal = $courseTablePrefix . $thisCourse['db'] . $dbGlu . 'calendar_event';
 
     $sql = "SELECT '". addslashes($thisCourse['sysCode']) ."' AS `courseSysCode`,
@@ -110,7 +110,7 @@ foreach($personnalCourseList as $thisCourse)
  * Sort all these digest by date
  */
 
-array_multisort( $courseDigestList['toolLabel'         ], 
+array_multisort( $courseDigestList['toolLabel'         ],
                  $courseDigestList['date'              ],
                  $courseDigestList['courseOfficialCode'],
                  $courseDigestList['courseSysCode'     ],
@@ -152,46 +152,49 @@ echo "<p>"
         .    '<img src="' . $imgRepositoryWeb . 'course.gif" alt="" /> '
         .    $langCourseCreate
         .    '</a>'
-        .    '&nbsp;|&nbsp;'
         ;
+        if ($allowed_to_register) echo '&nbsp;|&nbsp;';
     }
 
-    echo '<a href="claroline/auth/courses.php?cmd=rqReg&amp;category=">'
-    .    '<img src="'.$imgRepositoryWeb.'enroll.gif" alt="" /> '
-    .    $lang_enroll_to_a_new_course
-    .    '</a>'
-    .    '&nbsp;|&nbsp;'
+    if ($allowed_to_register)
+    {
+        echo '<a href="claroline/auth/courses.php?cmd=rqReg&amp;category=">'
+        .    '<img src="'.$imgRepositoryWeb.'enroll.gif" alt="" /> '
+        .    $lang_enroll_to_a_new_course
+        .    '</a>'
+        .    '&nbsp;|&nbsp;'
 
-    .    '<a href="claroline/auth/courses.php?cmd=rqUnreg">'
-    .    '<img src="'.$imgRepositoryWeb.'unenroll.gif" alt="" /> '
-    .    $lang_remove_course_enrollment
-    .    '</a>'
-    .    '</b>'
-    .    '</small>' . "\n"
-    .    '</p>' . "\n"
-    ;
+        .    '<a href="claroline/auth/courses.php?cmd=rqUnreg">'
+        .    '<img src="'.$imgRepositoryWeb.'unenroll.gif" alt="" /> '
+        .    $lang_remove_course_enrollment
+        .    '</a>'
+        .    '</b>'
+        .    '</small>' . "\n"
+        .    '</p>' . "\n"
+        ;
+    }
 
 /*
  * Course List
  */
 
 
-// get the list of personnal courses marked as contening new events 
+// get the list of personnal courses marked as contening new events
 
 $date = $claro_notifier->get_notification_date($_uid);
 
 $modified_course = $claro_notifier->get_notified_courses($date,$_uid);
 
 //display list
- 
+
 echo '<ul style="list-style-image:url(claroline/img/course.gif);list-style-position:inside">'."\n";
 
 foreach($personnalCourseList as $thisCourse)
 {
 
     // if the course contains new things to see since last login, its name will be displayed in bold text in the list
-    
-    if (in_array ($thisCourse['sysCode'], $modified_course)) 
+
+    if (in_array ($thisCourse['sysCode'], $modified_course))
     {
         $classItem = " hot";
     }
@@ -201,7 +204,7 @@ foreach($personnalCourseList as $thisCourse)
     }
 
     // show course language if not the same of the platform
-    if ( $platformLanguage!=$thisCourse['language'] ) 
+    if ( $platformLanguage!=$thisCourse['language'] )
     {
         if ( !empty($langNameOfLang[$thisCourse['language']]) )
         {
@@ -215,8 +218,8 @@ foreach($personnalCourseList as $thisCourse)
     else
     {
         $course_language_txt = '';
-    }    
-       
+    }
+
     echo '<li class="item' . $classItem . '">' ."\n"
     .    '<a href="' . $coursesRepositoryWeb . $thisCourse['directory'] . '/">'
     .    $thisCourse['officialCode'] . ' - '
@@ -247,22 +250,22 @@ echo '<td width="200" class="claroRightMenu"><!-- RIGHT COLUMN -->' . "\n";
     {
         switch ($courseDigestList['toolLabel'][$i])
         {
-            case 'CLANN___': 
+            case 'CLANN___':
                 $itemIcon = 'announcement.gif';
                 $url = 'claroline/announcements/announcements.php?cidReq='
-                     . $courseDigestList['courseSysCode'][$i]; 
+                     . $courseDigestList['courseSysCode'][$i];
                 $name = $langValvas;
                 break;
 
 
-            case 'CLCAL___': 
+            case 'CLCAL___':
                 $itemIcon = 'agenda.gif';
                 $url = 'claroline/calendar/agenda.php?cidReq='
                      . $courseDigestList['courseSysCode'][$i];
                 $name = $langAgendaNextEvents;
                 break;
         }
-        
+
         if ($title != $name)
         {
             $title = $name;
@@ -303,17 +306,17 @@ echo '<td width="200" class="claroRightMenu"><!-- RIGHT COLUMN -->' . "\n";
 </p>
 
 <?php
-	if ($is_platformAdmin) /* Admin Section links.
-	                        Only available for platform administrator */
-	{
+    if ($is_platformAdmin) /* Admin Section links.
+                            Only available for platform administrator */
+    {
 ?>
 <p><a href="claroline/admin/"><?php echo $langPlatformAdministration ?></a></p>
 
 <?php
-	} // end if is_platformAdmin
+    } // end if is_platformAdmin
 
 
-if (file_exists('./textzone_right.inc.html')) include './textzone_right.inc.html'; 
+if (file_exists('./textzone_right.inc.html')) include './textzone_right.inc.html';
 
 if ($is_platformAdmin)
 {
