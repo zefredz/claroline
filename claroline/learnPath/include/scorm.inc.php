4805513247ba9437b30c7dcff634d5efaf03fd26
seb <seb@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-09-01 09:16:35 +0000

    Fix echoes and queries

--- claroline/learnPath/include/scorm.inc.php
+++ claroline/learnPath/include/scorm.inc.php
@@ -12,49 +12,46 @@
  * @package CLLNP
  */
 
-  if(isset($cmd) && $cmd = "raw")
-  {
-              // change raw if value is a number between 0 and 100
-              if (isset($_POST['newRaw']) && is_num($_POST['newRaw']) && $_POST['newRaw'] <= 100 && $_POST['newRaw'] >= 0 )
-              {
-                        $sql = "UPDATE `".$TABLELEARNPATHMODULE."`
-                                   SET `raw_to_pass` = ". (int)$_POST['newRaw']."
-                                 WHERE `module_id` = ". (int)$_SESSION['module_id']."
-                                   AND `learnPath_id` = ". (int)$_SESSION['path_id'];
-                        claro_sql_query($sql);
+// change raw if value is a number between 0 and 100
+if( isset($_POST['newRaw']) && is_num($_POST['newRaw']) && $_POST['newRaw'] <= 100 && $_POST['newRaw'] >= 0 )
+{
+	$sql = "UPDATE `".$TABLELEARNPATHMODULE."`
+			SET `raw_to_pass` = ". (int)$_POST['newRaw']."
+			WHERE `module_id` = ". (int)$_SESSION['module_id']."
+			AND `learnPath_id` = ". (int)$_SESSION['path_id'];
+	claro_sql_query($sql);
 
-                        $dialogBox .= $langRawHasBeenChanged;
-              }
-  }
+	$dialogBox = $langRawHasBeenChanged;
+}
 
-   //####################################################################################\\
-   //############################### DIALOG BOX SECTION #################################\\
-   //####################################################################################\\
-   if (isset($dialogBox) && $dialogBox!="")
-   {
-      echo claro_disp_message_box($dialogBox);
-   }
 
-  // form to change raw needed to pass the exercise
-  $sql = "SELECT *
-            FROM `".$TABLELEARNPATHMODULE."` AS LPM
-           WHERE LPM.`module_id` = ". (int)$_SESSION['module_id']."
-             AND LPM.`learnPath_id` = ". (int)$_SESSION['path_id'];
+//####################################################################################\\
+//############################### DIALOG BOX SECTION #################################\\
+//####################################################################################\\
+if( !empty($dialogBox) )
+{
+	echo claro_disp_message_box($dialogBox);
+}
 
-  $query = claro_sql_query($sql);
-  $learningPath_module = mysql_fetch_array($query);
+// form to change raw needed to pass the exercise
+$sql = "SELECT `lock`, `raw_to_pass`
+        FROM `".$TABLELEARNPATHMODULE."` AS LPM
+       WHERE LPM.`module_id` = ". (int)$_SESSION['module_id']."
+         AND LPM.`learnPath_id` = ". (int)$_SESSION['path_id'];
 
-  if ($learningPath_module['lock'] == 'CLOSE') // this module blocks the user if he doesn't complete
-  {
-       echo "<hr noshade=\"noshade\" size=\"1\" />";
-       //echo "<p>".$langModuleHelpExercise."</p>";
-       echo "<form method=\"POST\" action=\"".$_SERVER['PHP_SELF']."\"><label for=\"newRaw\">";
-       echo $langChangeRaw;
-       echo "</label><input type=\"text\" value=\"".htmlspecialchars($learningPath_module['raw_to_pass'])."\" name=\"newRaw\" id=\"newRaw\" size=\"3\" maxlength=\"3\" /> % ";
-       echo "<input type=\"hidden\" name=\"cmd\" value=\"raw\" />";
-       echo "<input type=\"submit\" value=\"$langOk\" />";
-       echo "</form>";
+$learningPath_module = claro_sql_query_fetch_all($sql);
 
-  }
+if( isset($learningPath_module[0]['lock'])
+	&& $learningPath_module[0]['lock'] == 'CLOSE'
+	&& isset($learningPath_module[0]['raw_to_pass']) ) // this module blocks the user if he doesn't complete
+{
+	echo "\n\n".'<hr noshade="noshade" size="1" />'."\n"
+		.'<form method="POST" action="'.$_SERVER['PHP_SELF'].'">'."\n"
+		.'<label for="newRaw">'.$langChangeRaw.'</label>'."\n"
+		.'<input type="text" value="'.htmlspecialchars($learningPath_module[0]['raw_to_pass']).'" name="newRaw" id="newRaw" size="3" maxlength="3" /> % '."\n"
+		.'<input type="submit" value="'.$langOk.'" />'."\n"
+		.'</form>'."\n\n";
+
+}
 
 ?>
