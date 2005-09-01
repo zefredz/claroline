4242f36fc64d914e6908b44db7f1992c6158cb47
seb <seb@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-09-01 07:36:37 +0000

    Fix echoes and queries

--- claroline/learnPath/include/exercise.inc.php
+++ claroline/learnPath/include/exercise.inc.php
@@ -12,72 +12,75 @@
  * @package CLLNP
  */
 
-  if(isset($cmd) && $cmd = "raw")
-  {
-              // change raw if value is a number between 0 and 100
-              if (isset($_POST['newRaw']) && is_num($_POST['newRaw']) && $_POST['newRaw'] <= 100 && $_POST['newRaw'] >= 0 )
-              {
-                        $sql = "UPDATE `".$TABLELEARNPATHMODULE."`
-                                   SET `raw_to_pass` = ". (int)$_POST['newRaw']."
-                                 WHERE `module_id` = ". (int)$_SESSION['module_id']."
-                                   AND `learnPath_id` = ". (int)$_SESSION['path_id'];
-                        claro_sql_query($sql);
+if( isset($cmd) && $cmd = "raw" )
+{
+	// change raw if value is a number between 0 and 100
+	if (isset($_POST['newRaw']) && is_num($_POST['newRaw']) && $_POST['newRaw'] <= 100 && $_POST['newRaw'] >= 0 )
+	{
+		$sql = "UPDATE `".$TABLELEARNPATHMODULE."`
+				SET `raw_to_pass` = ". (int)$_POST['newRaw']."
+				WHERE `module_id` = ". (int)$_SESSION['module_id']."
+				AND `learnPath_id` = ". (int)$_SESSION['path_id'];
+		claro_sql_query($sql);
 
-                        $dialogBox .= $langRawHasBeenChanged;
-              }
-  }
+		$dialogBox = $langRawHasBeenChanged;
+	}
+}
 
 
-  echo "<hr noshade=\"noshade\" size=\"1\" />";
+echo '<hr noshade="noshade" size="1" />';
 
-   //####################################################################################\\
-   //############################### DIALOG BOX SECTION #################################\\
-   //####################################################################################\\
-   if (isset($dialogBox)&& $dialogBox!="")
-   {
-      echo claro_disp_message_box($dialogBox);
-   }
+//####################################################################################\\
+//############################### DIALOG BOX SECTION #################################\\
+//####################################################################################\\
+if( !empty($dialogBox) )
+{
+	echo claro_disp_message_box($dialogBox);
+}
 
-  // form to change raw needed to pass the exercise
-  $sql = "SELECT *
-            FROM `".$TABLELEARNPATHMODULE."` AS LPM
-           WHERE LPM.`module_id` = ". (int)$_SESSION['module_id']."
-             AND LPM.`learnPath_id` = ". (int)$_SESSION['path_id'];
+// form to change raw needed to pass the exercise
+$sql = "SELECT `lock`, `raw_to_pass`
+        FROM `".$TABLELEARNPATHMODULE."` AS LPM
+       WHERE LPM.`module_id` = ". (int)$_SESSION['module_id']."
+         AND LPM.`learnPath_id` = ". (int)$_SESSION['path_id'];
 
-  $query = claro_sql_query($sql);
-  $learningPath_module = mysql_fetch_array($query);
+$learningPath_module = claro_sql_query_fetch_all($sql);
 
-  if ($learningPath_module['lock'] == 'CLOSE') // this module blocks the user if he doesn't complete
-  {
-       //echo "<p>".$langModuleHelpExercise."</p>";
-       echo "<form method=\"POST\" action=\"".$_SERVER['PHP_SELF']."\"><label for=\"newRaw\">";
-       echo $langChangeRaw;
-       echo "</label><input type=\"text\" value=\"".htmlspecialchars($learningPath_module['raw_to_pass'])."\" name=\"newRaw\" id=\"newRaw\" size=\"3\" maxlength=\"3\" /> % ";
-       echo "<input type=\"hidden\" name=\"cmd\" value=\"raw\" />";
-       echo "<input type=\"submit\" value=\"$langOk\" />";
-       echo "</form>";
+// if this module blocks the user if he doesn't complete
+if( isset($learningPath_module[0]['lock'])
+	&& $learningPath_module[0]['lock'] == 'CLOSE'
+	&& isset($learningPath_module[0]['raw_to_pass']) )
+{
+	echo '<form method="POST" action="'.$_SERVER['PHP_SELF'].'">'."\n"
+		.'<label for="newRaw">'.$langChangeRaw.'</label>'."\n"
+		.'<input type="text" value="'.htmlspecialchars( $learningPath_module[0]['raw_to_pass'] ).'" name="newRaw" id="newRaw" size="3" maxlength="3" /> % '."\n"
+		.'<input type="hidden" name="cmd" value="raw" />'."\n"
+		.'<input type="submit" value="'.$langOk.'" />'."\n"
+		.'</form>'."\n\n";
+}
 
-  }
+// display current exercise info and change comment link
+$sql = "SELECT `E`.`id` AS `exerciseId`, `M`.`name`
+        FROM `".$TABLEMODULE."` AS `M`,
+             `".$TABLEASSET."`  AS `A`,
+             `".$TABLEQUIZTEST."` AS `E`
+       WHERE `A`.`module_id` = M.`module_id`
+         AND `M`.`module_id` = ". (int) $_SESSION['module_id']."
+         AND `E`.`id` = `A`.`path`";
 
-  // display list and form to change the exercise in module
-  //display_my_exercises($dialogBox);
-
-  // display current exercise info and change comment link
-  $sql = "SELECT *, M.`comment` AS Mcomment, A.`comment` AS Acomment
-            FROM `".$TABLEMODULE."` AS M,
-                 `".$TABLEASSET."`  AS A,
-                 `".$TABLEQUIZTEST."` AS E
-           WHERE A.`module_id` = M.`module_id`
-             AND M.`module_id` = ". (int)$_SESSION['module_id']."
-             AND E.`id` = A.`path`";
-   //echo $sql;
-   $query = claro_sql_query($sql);
-   $module = mysql_fetch_array($query);
-
-   echo "<h4>".$langExerciseInModule." :</h4><p>"
-          . htmlspecialchars($module['titre'])
-          ."<a href=\"../exercice/admin.php?exerciseId=".$module['id']."\"><img src=\"".$imgRepositoryWeb."edit.gif\" border=\"0\" alt=\"$langModify\" /></a></p>";
-
+$result = claro_sql_query_fetch_all($sql);
+if( is_array($result[0]) )
+{
+  	$module = $result[0];
+  	
+	echo "\n\n".'<h4>'.$langExerciseInModule.' :</h4>'."\n"
+		.'<p>'."\n"
+		.htmlspecialchars($module['name'])
+		.'<a href="../exercice/admin.php?exerciseId='.$module['exerciseId'].'">'
+		.'<img src="'.$imgRepositoryWeb.'edit.gif" border="0" alt="'.$langModify.'" />'
+		.'</a>'."\n"
+		.'</p>'."\n";
+} // else sql error, do nothing except in debug mode, where claro_sql_query_fetch_all will show the error
 
 
 ?>
