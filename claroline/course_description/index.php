496e626131a132fb3b3394b0d26e2ee8145abb6a
peeters <peeters@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-09-07 10:25:33 +0000

    replace the select_course() funtcion by disp_auth_form()

--- claroline/course_description/index.php
+++ claroline/course_description/index.php
@@ -24,8 +24,7 @@ $tlabelReq = 'CLDSC___';
 
 require '../inc/claro_init_global.inc.php';
 
-if ( ! $_cid)             claro_disp_select_course();
-if ( ! $is_courseAllowed) claro_disp_auth_form();
+if ( ! $_cid || ( ! $is_courseAllowed && !$_uid ) ) claro_disp_auth_form(true);
 
 claro_set_display_mode_available(TRUE);
 $nameTools = $langCourseProgram;
