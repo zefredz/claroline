87a75b188cbe80f09956a718132281e726c0b98a
moosh <moosh@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-09-12 10:27:41 +0000

    - correct month and week names

--- claroline/lang/arabic/locale_settings.php
+++ claroline/lang/arabic/locale_settings.php
@@ -20,40 +20,50 @@ $localLangName = " �?رنسية ";
 $iso639_1_code = "ar";
 $iso639_2_code = "ara";
 
-$langNameOfLang['arabic'        ] = "arabian";
-$langNameOfLang['brazilian'        ] = "brazilian";
-$langNameOfLang['croatian'        ] = "croatian";
-$langNameOfLang['dutch'            ] = "dutch";
-$langNameOfLang['english'        ] = "english";
-$langNameOfLang['finnish'        ] = "finnish";
-$langNameOfLang['french'        ] = "french";
-$langNameOfLang['german'        ] = "german";
-$langNameOfLang['greek'            ] = "greek";
-$langNameOfLang['italian'        ] = "italian";
-$langNameOfLang['japanese'        ] = "japanese";
-$langNameOfLang['polish'        ] = "polish";
-$langNameOfLang['simpl_chinese'    ] = "simplified chinese";
-$langNameOfLang['spanish'        ] = "spanish";
-$langNameOfLang['swedish'        ] = "swedish";
-$langNameOfLang['thai'            ] = "thai";
-$langNameOfLang['turkish'        ] = "turkish";
-$langNameOfLang['vietnamese'    ] = "vietnamese";
+$langNameOfLang['arabic']        = "arabian";
+$langNameOfLang['brazilian']     = "brazilian";
+$langNameOfLang['bulgarian']     = "bulgarian";
+$langNameOfLang['catalan']       = "catalan";
+$langNameOfLang['croatian']      = "croatian";
+$langNameOfLang['danish']        = "danish";
+$langNameOfLang['dutch']         = "dutch";
+$langNameOfLang['english']       = "english";
+$langNameOfLang['finnish']       = "finnish";
+$langNameOfLang['french']        = "french";
+$langNameOfLang['galician']      = "galician";
+$langNameOfLang['german']        = "german";
+$langNameOfLang['greek']         = "greek";
+$langNameOfLang['italian']       = "italian";
+$langNameOfLang['indonesian']    = "indonesian";
+$langNameOfLang['japanese']      = "japanese";
+$langNameOfLang['malay']         = "malay"; 
+$langNameOfLang['polish']        = "polish";
+$langNameOfLang['portuguese']    = "portuguese";
+$langNameOfLang['russian']       = "russian";
+$langNameOfLang['simpl_chinese'] = "simplified chinese";
+$langNameOfLang['slovenian']     = "slovenian";
+$langNameOfLang['spanish']       = "spanish";
+$langNameOfLang['swedish']       = "swedish";
+$langNameOfLang['thai']          = "thai";
+$langNameOfLang['turkish']       = "turkish";
+$langNameOfLang['vietnamese']    = "vietnamese";
+
 
 $charset = 'utf-8';
 $text_dir = 'rtl';
 $left_font_family = 'verdana, helvetica, arial, geneva, sans-serif';
-$right_font_family = 'helvetica, arial, geneva, sans-serif';
-$number_thousands_separator = ' ';
-$number_decimal_separator = ',';
+$right_font_family = '"Windows UI", Tahoma, verdana, arial, helvetica, sans-serif';
+$number_thousands_separator = ',';
+$number_decimal_separator = '.';
 $byteUnits = array('Octets', 'Ko', 'Mo', 'Go');
 
 $langDay_of_weekNames['init'] = array('D', 'L', 'M', 'M', 'J', 'V', 'S');
 $langDay_of_weekNames['short'] = array('Di', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam');
-$langDay_of_weekNames['long'] = array('Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi');
+$langDay_of_weekNames['long'] = array('الأحد', 'الإثنين', 'الثلاثاء', 'الأربعاء', 'الخميس', 'الجمعة', 'السبت');
 
 $langMonthNames['init']  = array('J', 'F', 'M', 'A', 'M', 'J', 'J', 'A', 'S', 'O', 'N', 'D');
 $langMonthNames['short'] = array('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec');
-$langMonthNames['long'] = array(' جان�?ي ', ' �?ي�?ري ', ' مارس ', ' أ�?ريل ', ' ماي ', ' جوان ', ' جويلية ',' أوت ',' سبتمبر ',' أكتوبر    ',' نو�?مبر ',' ديسمبر ');
+$langMonthNames['long'] = array('يناير', 'فبراير', 'مارس', 'أبريل', 'مايو', 'يونيو', 'يوليو', 'أغسطس', 'سبتمبر', 'أكتوبر', 'نوفمبر', 'ديسمبر');
 
 
 // Voir http://www.php.net/manual/en/function.strftime.php pour la variable
@@ -61,7 +71,7 @@ $langMonthNames['long'] = array(' جان�?ي ', ' �?ي�?ري ', ' مارس ', ' 
 
 $dateFormatShort =  "%a %d %b %y";
 $dateFormatLong  = '%A %d %B %Y';
-$dateTimeFormatLong  = '%A %d %B %Y à %H:%M';
+$dateTimeFormatLong  = '%d %B %Y الساعة %H:%M';
 $timeNoSecFormat = '%H:%M';
 
 ?>
