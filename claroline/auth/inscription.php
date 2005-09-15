5f86d77be46987efe610c115fd0e5c757d645ed2
mathieu <mathieu@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-09-15 09:38:11 +0000

    * display message if registration on the platform is not allowed

--- claroline/auth/inscription.php
+++ claroline/auth/inscription.php
@@ -19,8 +19,14 @@ define('DISP_REGISTRATION_AGREEMENT',__LINE__);
 
 require '../inc/claro_init_global.inc.php';
 
-// Redirect before first output
-if ( ! isset($allowSelfReg) || $allowSelfReg == FALSE || isset($_uid) )
+// Registration not allowed
+if ( ! isset($allowSelfReg) || $allowSelfReg == FALSE )
+{
+    claro_die($langMessageRegistrationNotAllowed);
+}
+
+// Already logged
+if ( isset($_uid) )
 {
     header("Location: ".$rootWeb);
     exit;
