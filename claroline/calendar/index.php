f759fb5b224c862b000eba204046c8164c32e20a
moosh <moosh@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-09-15 07:10:10 +0000

    -redirect correctly

--- claroline/calendar/index.php
+++ claroline/calendar/index.php
@@ -1,10 +1,28 @@
 <?php // $Id$
 /**
-      +----------------------------------------------------------------------+
-      | CLAROLINE 
-      +----------------------------------------------------------------------+
-      | Copyright (c) 2001, 2004 Universite catholique de Louvain (UCL)      |
-      +----------------------------------------------------------------------+
-*/
-  header("Location:./myagenda.php");
+ * CLAROLINE 
+ *
+ * select the good agenda waiting that two scripts are merged.
+ *
+ * @version 1.7 $Revision$
+ *
+ * @copyright (c) 2001-2005 Universite catholique de Louvain (UCL)
+ *
+ * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE 
+ *
+ * @package CLCAL
+ *
+ * @author Claro Team <cvs@claroline.net>
+ */
+
+$tlabelReq = 'CLANN___';
+require '../inc/claro_init_global.inc.php';
+if (isset($_cid))
+{
+    header('Location:./agenda.php');
+}
+else
+{
+    header('Location:./myagenda.php');
+}
 ?>
