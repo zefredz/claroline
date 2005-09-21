0cff58a3e6e72a643fcdad754259c5d72eeabf9a
seb <seb@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-09-21 10:00:20 +0000

    Use claro_sql function; fix a notice; indentation

--- claroline/learnPath/navigation/scormAPI.inc.php
+++ claroline/learnPath/navigation/scormAPI.inc.php
@@ -35,70 +35,69 @@ $TABLEASSET              = $tbl_lp_asset;
 $TABLEUSERMODULEPROGRESS = $tbl_lp_user_module_progress;
 $TABLEUSERS              = $tbl_user;
 
-  $SCORMServerURL = $clarolineRepositoryWeb."learnPath/navigation/SCORMserver.php";
-  $redirectionURL = $clarolineRepositoryWeb."learnPath/learningPath.php";
-  $TOCurl = $clarolineRepositoryWeb."learnPath/navigation/tableOfContent.php";
+$SCORMServerURL = $clarolineRepositoryWeb."learnPath/navigation/SCORMserver.php";
+$redirectionURL = $clarolineRepositoryWeb."learnPath/learningPath.php";
+$TOCurl = $clarolineRepositoryWeb."learnPath/navigation/tableOfContent.php";
 /*======================================
        CLAROLINE MAIN
   ======================================*/
 
-  if($_uid)
-  {
-        // Get general information to generate the right API inmplementation
-        $sql = "SELECT *
-                  FROM `".$TABLEUSERMODULEPROGRESS."` AS UMP,
-                       `".$TABLELEARNPATHMODULE."` AS LPM,
-                       `".$TABLEUSERS."` AS U,
-                       `".$TABLEMODULE."` AS M
-                 WHERE UMP.`user_id` = ". (int)$_uid."
-                   AND UMP.`user_id` = U.`user_id`
-                   AND UMP.`learnPath_module_id` = LPM.`learnPath_module_id`
-                   AND M.`module_id` = LPM.`module_id`
-                   AND LPM.`learnPath_id` = ". (int)$_SESSION['path_id']."
-                   AND LPM.`module_id` = ". (int)$_SESSION['module_id'];
+if($_uid)
+{
+    // Get general information to generate the right API inmplementation
+    $sql = "SELECT *
+              FROM `".$TABLEUSERMODULEPROGRESS."` AS UMP,
+                   `".$TABLELEARNPATHMODULE."` AS LPM,
+                   `".$TABLEUSERS."` AS U,
+                   `".$TABLEMODULE."` AS M
+             WHERE UMP.`user_id` = ". (int)$_uid."
+               AND UMP.`user_id` = U.`user_id`
+               AND UMP.`learnPath_module_id` = LPM.`learnPath_module_id`
+               AND M.`module_id` = LPM.`module_id`
+               AND LPM.`learnPath_id` = ". (int)$_SESSION['path_id']."
+               AND LPM.`module_id` = ". (int)$_SESSION['module_id'];
 
-        $query = claro_sql_query($sql);
-        if ( ! ($userProgressionDetails = mysql_fetch_array($query) ) )
-                die ("mysql_error in ".__FILE__." [line ".__LINE__."] )");
+    $userProgressionDetails = claro_sql_query_get_single_row($sql);
+}
 
-        // set vars
-        $sco['student_id'] = "$_uid";
-        $sco['student_name'] = $userProgressionDetails['nom'].", ".$userProgressionDetails['prenom'];
-        $sco['lesson_location'] = $userProgressionDetails['lesson_location'];
-        $sco['credit'] = strtolower($userProgressionDetails['credit']);
-        $sco['lesson_status'] = strtolower($userProgressionDetails['lesson_status']);
-        $sco['entry'] = strtolower($userProgressionDetails['entry']);
-        $sco['raw'] = ($userProgressionDetails['raw'] == -1) ? "" : "".$userProgressionDetails['raw'];
-        $sco['scoreMin'] = ($userProgressionDetails['scoreMin'] == -1) ? "" : "".$userProgressionDetails['scoreMin'];
-        $sco['scoreMax'] = ($userProgressionDetails['scoreMax'] == -1) ? "" : "".$userProgressionDetails['scoreMax'];
-        $sco['total_time'] = $userProgressionDetails['total_time'];
-        $sco['suspend_data'] = $userProgressionDetails['suspend_data'];
-  }
-  else // anonymous
-  {
-        $sco['student_id'] = "-1";
-        $sco['student_name'] = "Anonymous, User";
-        $sco['lesson_location'] = "";
-        $sco['credit'] ="no-credit";
-        $sco['lesson_status'] = "not attempted";
-        $sco['entry'] = "ab-initio";
-        $sco['raw'] = "";
-        $sco['scoreMin'] = "";
-        $sco['scoreMax'] = "";
-        $sco['total_time'] = "0000:00:00.00";
-        $sco['suspend_data'] = "";
-  }
-
-
-  //common vars
-  $sco['_children'] = "student_id,student_name,lesson_location,credit,lesson_status,entry,score,total_time,exit,session_time";
-  $sco['score_children'] = "raw,min,max";
-  $sco['exit'] = "";
-  $sco['session_time'] = "0000:00:00.00";
-  // get in XML manifest
-  $sco['launch_data'] = stripslashes($userProgressionDetails['launch_data']);
+if( !$_uid || !$userProgressionDetails )
+{
+    $sco['student_id'] = "-1";
+    $sco['student_name'] = "Anonymous, User";
+    $sco['lesson_location'] = "";
+    $sco['credit'] ="no-credit";
+    $sco['lesson_status'] = "not attempted";
+    $sco['entry'] = "ab-initio";
+    $sco['raw'] = "";
+    $sco['scoreMin'] = "";
+    $sco['scoreMax'] = "";
+    $sco['total_time'] = "0000:00:00.00";
+    $sco['suspend_data'] = "";
+    $sco['launch_data'] = "";
+}
+else // authenticated user and no error in query
+{
+    // set vars
+    $sco['student_id'] = $_uid;
+    $sco['student_name'] = $userProgressionDetails['nom'].", ".$userProgressionDetails['prenom'];
+    $sco['lesson_location'] = $userProgressionDetails['lesson_location'];
+    $sco['credit'] = strtolower($userProgressionDetails['credit']);
+    $sco['lesson_status'] = strtolower($userProgressionDetails['lesson_status']);
+    $sco['entry'] = strtolower($userProgressionDetails['entry']);
+    $sco['raw'] = ($userProgressionDetails['raw'] == -1) ? "" : "".$userProgressionDetails['raw'];
+    $sco['scoreMin'] = ($userProgressionDetails['scoreMin'] == -1) ? "" : "".$userProgressionDetails['scoreMin'];
+    $sco['scoreMax'] = ($userProgressionDetails['scoreMax'] == -1) ? "" : "".$userProgressionDetails['scoreMax'];
+    $sco['total_time'] = $userProgressionDetails['total_time'];
+    $sco['suspend_data'] = $userProgressionDetails['suspend_data'];
+    $sco['launch_data'] = stripslashes($userProgressionDetails['launch_data']);
+}
 
 
+//common vars
+$sco['_children'] = "student_id,student_name,lesson_location,credit,lesson_status,entry,score,total_time,exit,session_time";
+$sco['score_children'] = "raw,min,max";
+$sco['exit'] = "";
+$sco['session_time'] = "0000:00:00.00";
 
 ?>
 
