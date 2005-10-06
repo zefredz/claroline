cfb0f3ab71f0ad0c940bad04f19a54210a78ab24
mathieu <mathieu@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-10-06 19:58:35 +0000

    Fix minor issues detected by zend : unused variable and coding standards

--- claroline/admin/upgrade/upgrade_course_16.lib.php
+++ claroline/admin/upgrade/upgrade_course_16.lib.php
@@ -159,7 +159,6 @@ function quizz_upgrade_to_16_step_2()
         }
         else
         {
-            $warning = true;
             log_message('Warning: ' . sprintf( '%1$s and %2$s exists in %3$s. '
                                              . 'It\'s due to an old upgrade error. '
                                              . 'Check if all files of %1$s ares in %1$s and delete %1$s', 'image', 'exercise', $currentcoursePathSys));
@@ -194,190 +193,192 @@ function assignment_upgrade_to_16($course_code)
     $tbl_course_tool = $tbl_mdb_names['tool'];
     
     if ( preg_match($versionRequiredToProceed,$currentCourseVersion) )
-    switch( $step = get_upgrade_status($tool,$course_code) )
-    {         
-        case 1 :  
-
-        /**
-         * STEP 1 Create new work table
-         */
-
-          $sql_step1[] = "CREATE TABLE IF NOT EXISTS `".$currentCourseDbNameGlu."wrk_assignment` (
-            `id` int(11) NOT NULL auto_increment,
-            `title` varchar(200) NOT NULL default '',
-            `description` text NOT NULL,
-            `visibility` enum('VISIBLE','INVISIBLE') NOT NULL default 'VISIBLE',
-            `def_submission_visibility` enum('VISIBLE','INVISIBLE') NOT NULL default 'VISIBLE',
-            `assignment_type` enum('INDIVIDUAL','GROUP') NOT NULL default 'INDIVIDUAL',
-            `authorized_content` enum('TEXT','FILE','TEXTFILE') NOT NULL default 'FILE',
-            `allow_late_upload` enum('YES','NO') NOT NULL default 'YES',
-            `start_date` datetime NOT NULL default '0000-00-00 00:00:00',
-            `end_date` datetime NOT NULL default '0000-00-00 00:00:00',
-            `prefill_text` text NOT NULL,
-            `prefill_doc_path` varchar(200) NOT NULL default '',
-            `prefill_submit` enum('ENDDATE','AFTERPOST') NOT NULL default 'ENDDATE',
-            PRIMARY KEY  (`id`)
-            ) TYPE=MyISAM"; 
-                  
-          $sql_step1[] = "UPDATE `".$currentCourseDbNameGlu."wrk_assignment` 
-            SET 
-            `end_date` = '".date('Y-m-d H:i:00', mktime( date('H'),date('i'),0,date('m'), date('d'), date('Y')+1 ) )."'
-            WHERE `end_date` = '0000-00-00 00:00:00'
-            ";        
-        $sql_step1[] = "CREATE TABLE IF NOT EXISTS `".$currentCourseDbNameGlu."wrk_submission` (
-            `id` int(11) NOT NULL auto_increment,
-            `assignment_id` int(11) default NULL,
-            `parent_id` int(11) default NULL,
-            `user_id`  int(11) default NULL ,
-            `group_id` int(11) default NULL,
-            `title` varchar(200) NOT NULL default '',
-            `visibility` enum('VISIBLE','INVISIBLE') default 'VISIBLE',
-            `creation_date` datetime NOT NULL default '0000-00-00 00:00:00',
-            `last_edit_date` datetime NOT NULL default '0000-00-00 00:00:00',
-            `authors` varchar(200) NOT NULL default '',
-            `submitted_text` text NOT NULL,
-            `submitted_doc_path` varchar(200) NOT NULL default '',
-            `private_feedback` text,
-            `original_id` int(11) default NULL,
-            `score` smallint(3) default NULL,
-            PRIMARY KEY  (`id`)
-            ) TYPE=MyISAM";
-            if ( ! upgrade_apply_sql($sql_step1) ) return $step;
-            $step = set_upgrade_status($tool, 2, $course_code);
-
-        case 2 :
-
+    {
+        switch( $step = get_upgrade_status($tool,$course_code) )
+        {         
+            case 1 :  
+    
             /**
-             * STEP 2 : Create a new assignment
+             * STEP 1 Create new work table
              */
-
-            // get work intro in tool 
-            $sql_work_intro = "SELECT ti.texte_intro
-                                FROM `" . $currentCourseDbNameGlu . "tool_list` tl,
-                                     `" . $currentCourseDbNameGlu . "tool_intro` ti,
-                                     `" . $tbl_course_tool . "` ct
-                                WHERE ti.id = tl.id
-                                    AND tl.tool_id =  ct.id
-                                    AND ct.claro_label = 'CLWRK___'";
+    
+            $sql_step1[] = "CREATE TABLE IF NOT EXISTS `".$currentCourseDbNameGlu."wrk_assignment` (
+                `id` int(11) NOT NULL auto_increment,
+                `title` varchar(200) NOT NULL default '',
+                `description` text NOT NULL,
+                `visibility` enum('VISIBLE','INVISIBLE') NOT NULL default 'VISIBLE',
+                `def_submission_visibility` enum('VISIBLE','INVISIBLE') NOT NULL default 'VISIBLE',
+                `assignment_type` enum('INDIVIDUAL','GROUP') NOT NULL default 'INDIVIDUAL',
+                `authorized_content` enum('TEXT','FILE','TEXTFILE') NOT NULL default 'FILE',
+                `allow_late_upload` enum('YES','NO') NOT NULL default 'YES',
+                `start_date` datetime NOT NULL default '0000-00-00 00:00:00',
+                `end_date` datetime NOT NULL default '0000-00-00 00:00:00',
+                `prefill_text` text NOT NULL,
+                `prefill_doc_path` varchar(200) NOT NULL default '',
+                `prefill_submit` enum('ENDDATE','AFTERPOST') NOT NULL default 'ENDDATE',
+                PRIMARY KEY  (`id`)
+                ) TYPE=MyISAM"; 
+                      
+            $sql_step1[] = "UPDATE `".$currentCourseDbNameGlu."wrk_assignment` 
+                SET 
+                `end_date` = '".date('Y-m-d H:i:00', mktime( date('H'),date('i'),0,date('m'), date('d'), date('Y')+1 ) )."'
+                WHERE `end_date` = '0000-00-00 00:00:00'
+                ";        
+            $sql_step1[] = "CREATE TABLE IF NOT EXISTS `".$currentCourseDbNameGlu."wrk_submission` (
+                `id` int(11) NOT NULL auto_increment,
+                `assignment_id` int(11) default NULL,
+                `parent_id` int(11) default NULL,
+                `user_id`  int(11) default NULL ,
+                `group_id` int(11) default NULL,
+                `title` varchar(200) NOT NULL default '',
+                `visibility` enum('VISIBLE','INVISIBLE') default 'VISIBLE',
+                `creation_date` datetime NOT NULL default '0000-00-00 00:00:00',
+                `last_edit_date` datetime NOT NULL default '0000-00-00 00:00:00',
+                `authors` varchar(200) NOT NULL default '',
+                `submitted_text` text NOT NULL,
+                `submitted_doc_path` varchar(200) NOT NULL default '',
+                `private_feedback` text,
+                `original_id` int(11) default NULL,
+                `score` smallint(3) default NULL,
+                PRIMARY KEY  (`id`)
+                ) TYPE=MyISAM";
+                if ( ! upgrade_apply_sql($sql_step1) ) return $step;
+                $step = set_upgrade_status($tool, 2, $course_code);
+    
+            case 2 :
+    
+                /**
+                 * STEP 2 : Create a new assignment
+                 */
+    
+                // get work intro in tool 
+                $sql_work_intro = "SELECT ti.texte_intro
+                                    FROM `" . $currentCourseDbNameGlu . "tool_list` tl,
+                                         `" . $currentCourseDbNameGlu . "tool_intro` ti,
+                                         `" . $tbl_course_tool . "` ct
+                                    WHERE ti.id = tl.id
+                                        AND tl.tool_id =  ct.id
+                                        AND ct.claro_label = 'CLWRK___'";
+                
+                $work_intro = claro_sql_query_get_single_value($sql_work_intro);
+                
+                if ( $work_intro === FALSE ) $work_intro = '';            
+                
+                $sql_step2[] = "INSERT INTO `".$currentCourseDbNameGlu."wrk_assignment` 
+                    SET `id` = 1,
+                    `title` = 'Assignments', 
+                    `description`= '" . mysql_escape_string($work_intro) . "', 
+                    `visibility` = 'VISIBLE', 
+                    `def_submission_visibility` = 'VISIBLE',
+                    `assignment_type` = 'INDIVIDUAL',
+                    `authorized_content` = 'FILE',
+                    `allow_late_upload` = 'NO',
+                    `start_date` = '" . $currentCourseCreationDate . "',
+                    `end_date` = DATE_ADD(NOW(),INTERVAL 1 YEAR),
+                    `prefill_text` = '',
+                    `prefill_doc_path` = '',
+                    `prefill_submit` = 'ENDDATE' ";
+                if ( ! upgrade_apply_sql($sql_step2) ) return $step;
+                $step = set_upgrade_status($tool, 3, $course_code);
             
-            $work_intro = claro_sql_query_get_single_value($sql_work_intro);
-            
-            if ( $work_intro === FALSE ) $work_intro = '';            
-            
-            $sql_step2[] = "INSERT INTO `".$currentCourseDbNameGlu."wrk_assignment` 
-                SET `id` = 1,
-                `title` = 'Assignments', 
-                `description`= '" . mysql_escape_string($work_intro) . "', 
-                `visibility` = 'VISIBLE', 
-                `def_submission_visibility` = 'VISIBLE',
-                `assignment_type` = 'INDIVIDUAL',
-                `authorized_content` = 'FILE',
-                `allow_late_upload` = 'NO',
-                `start_date` = '" . $currentCourseCreationDate . "',
-                `end_date` = DATE_ADD(NOW(),INTERVAL 1 YEAR),
-                `prefill_text` = '',
-                `prefill_doc_path` = '',
-                `prefill_submit` = 'ENDDATE' ";
-            if ( ! upgrade_apply_sql($sql_step2) ) return $step;
-            $step = set_upgrade_status($tool, 3, $course_code);
-        
-        case 3 :
-
-            /**
-             * STEP 3 : Add old works as submissions of new assignment
-             */
-
-            // get course manager of the course
-            $sql_get_id_of_one_teacher = "SELECT `user_id` `uid` " .
-                                         " FROM `". $tbl_rel_course_user . "` " .
-                                         " WHERE `code_cours` = '".$course_code."' LIMIT 1";
-            
-            $res_id_of_one_teacher = claro_sql_query($sql_get_id_of_one_teacher);
-            
-            $teacher = claro_sql_fetch_all($res_id_of_one_teacher);
-            
-            $teacher_uid = $teacher[0]['uid'];
-            
-            // if no course manager, you are enrolled in as            
-            if ( !is_numeric($teacher_uid) )
-            {
-                $teacher_uid = $_uid;
-                $sql_set_teacher = "INSERT INTO `". $tbl_rel_course_user . "`  
-                                    SET `user_id` = '" . $teacher_uid . "'
-                                         , `code_cours` = '" . $course_code . "'
-                                         , `role` = 'Course missing manager';";
-                if ( ! claro_sql_query($sql_set_teacher) ) return $step;            
-                log_message('Warning : Course '.$currentCourseCode.' has no teacher, you are enrolled in as course manager.');
-            }
-
-            // add old work in submission of course manager
-            $sql_step3[] = "INSERT IGNORE INTO `".$currentCourseDbNameGlu."wrk_submission`
-             (assignment_id,user_id,title,visibility,authors,submitted_text,submitted_doc_path)
-             SELECT 1, '". $teacher_uid ."', titre, IF(accepted,'VISIBLE','INVISIBLE'), auteurs, description, url 
-                FROM `".$currentCourseDbNameGlu."assignment_doc`";  
-
-            if ( ! upgrade_apply_sql($sql_step3) ) return $step;
-            $step = set_upgrade_status($tool, 4, $course_code);
-
-        case 4 :
-
-            /**
-             * STEP 4 : Update document path of submissions
-             */
-
-            $sql_step4[] = "UPDATE `".$currentCourseDbNameGlu."wrk_submission` 
-                            SET submitted_doc_path = REPLACE (`submitted_doc_path` ,'work/','')";            
-            if ( ! upgrade_apply_sql($sql_step4) ) return $step;
-            $step = set_upgrade_status($tool, 5, $course_code);
-
-        case 5 :
-
-            /**
-             * STEP 5 : Create new folder to store assig_1 and move old old work documents in it
-             */
-            
-            // create new folder
-            $work_dirname = $currentcoursePathSys.'work/';
-            $assignment_dirname = $work_dirname . 'assig_1/';
-            if ( !is_dir($assignment_dirname) )
-            {
-                if ( !@mkdir($assignment_dirname, CLARO_FILE_PERMISSIONS) )
+            case 3 :
+    
+                /**
+                 * STEP 3 : Add old works as submissions of new assignment
+                 */
+    
+                // get course manager of the course
+                $sql_get_id_of_one_teacher = "SELECT `user_id` `uid` " .
+                                             " FROM `". $tbl_rel_course_user . "` " .
+                                             " WHERE `code_cours` = '".$course_code."' LIMIT 1";
+                
+                $res_id_of_one_teacher = claro_sql_query($sql_get_id_of_one_teacher);
+                
+                $teacher = claro_sql_fetch_all($res_id_of_one_teacher);
+                
+                $teacher_uid = $teacher[0]['uid'];
+                
+                // if no course manager, you are enrolled in as            
+                if ( !is_numeric($teacher_uid) )
                 {
-                    log_message('Error: Cannot create ' . $assignment_dirname );
-                    return $step;
+                    $teacher_uid = $_uid;
+                    $sql_set_teacher = "INSERT INTO `". $tbl_rel_course_user . "`  
+                                        SET `user_id` = '" . $teacher_uid . "'
+                                             , `code_cours` = '" . $course_code . "'
+                                             , `role` = 'Course missing manager';";
+                    if ( ! claro_sql_query($sql_set_teacher) ) return $step;            
+                    log_message('Warning : Course '.$currentCourseCode.' has no teacher, you are enrolled in as course manager.');
                 }
-            }
-            
-            // move assignment from work to work/assig_1                
-            if ( is_dir($work_dirname) )
-            {
-                if ( ( $handle = opendir($work_dirname) ) )
-                {   
-                    while ( FALSE !== ($file = readdir($handle)) )
+    
+                // add old work in submission of course manager
+                $sql_step3[] = "INSERT IGNORE INTO `".$currentCourseDbNameGlu."wrk_submission`
+                 (assignment_id,user_id,title,visibility,authors,submitted_text,submitted_doc_path)
+                 SELECT 1, '". $teacher_uid ."', titre, IF(accepted,'VISIBLE','INVISIBLE'), auteurs, description, url 
+                    FROM `".$currentCourseDbNameGlu."assignment_doc`";  
+    
+                if ( ! upgrade_apply_sql($sql_step3) ) return $step;
+                $step = set_upgrade_status($tool, 4, $course_code);
+    
+            case 4 :
+    
+                /**
+                 * STEP 4 : Update document path of submissions
+                 */
+    
+                $sql_step4[] = "UPDATE `".$currentCourseDbNameGlu."wrk_submission` 
+                                SET submitted_doc_path = REPLACE (`submitted_doc_path` ,'work/','')";            
+                if ( ! upgrade_apply_sql($sql_step4) ) return $step;
+                $step = set_upgrade_status($tool, 5, $course_code);
+    
+            case 5 :
+    
+                /**
+                 * STEP 5 : Create new folder to store assig_1 and move old old work documents in it
+                 */
+                
+                // create new folder
+                $work_dirname = $currentcoursePathSys.'work/';
+                $assignment_dirname = $work_dirname . 'assig_1/';
+                if ( !is_dir($assignment_dirname) )
+                {
+                    if ( !@mkdir($assignment_dirname, CLARO_FILE_PERMISSIONS) )
                     {
-                        if ( is_dir($work_dirname.$file) ) continue;
-            
-                        if ( @rename($work_dirname.$file,$assignment_dirname.$file) === FALSE )
-                        {
-                            log_message('Error: Cannot rename ' . $work_dirname . $file . ' to ' . $assignment_dirname . $file );
-                            return $step;
-                        }
-            
+                        log_message('Error: Cannot create ' . $assignment_dirname );
+                        return $step;
                     }
-                    closedir($handle);
-                }                    
-            }            
-            $step = set_upgrade_status($tool, 6, $course_code);
-
-        case 6 : 
-            /**
-             * STEP 6 Drop deprecated assignment_doc
-             */
-            // $sql_step6[] = "DROP TABLE IF EXISTS `".$currentCourseDbNameGlu."assignment_doc`"; 
-            // if ( ! upgrade_apply_sql($sql_step6) ) return $step;
-            $step = set_upgrade_status($tool, 0, $course_code);
-        default : 
-            return $step;
+                }
+                
+                // move assignment from work to work/assig_1                
+                if ( is_dir($work_dirname) )
+                {
+                    if ( ( $handle = opendir($work_dirname) ) )
+                    {   
+                        while ( FALSE !== ($file = readdir($handle)) )
+                        {
+                            if ( is_dir($work_dirname.$file) ) continue;
+                
+                            if ( @rename($work_dirname.$file,$assignment_dirname.$file) === FALSE )
+                            {
+                                log_message('Error: Cannot rename ' . $work_dirname . $file . ' to ' . $assignment_dirname . $file );
+                                return $step;
+                            }
+                
+                        }
+                        closedir($handle);
+                    }                    
+                }            
+                $step = set_upgrade_status($tool, 6, $course_code);
+    
+            case 6 : 
+                /**
+                 * STEP 6 Drop deprecated assignment_doc
+                 */
+                // $sql_step6[] = "DROP TABLE IF EXISTS `".$currentCourseDbNameGlu."assignment_doc`"; 
+                // if ( ! upgrade_apply_sql($sql_step6) ) return $step;
+                $step = set_upgrade_status($tool, 0, $course_code);
+            default : 
+                return $step;
+        } // end switch
     }
     return false;
 }
