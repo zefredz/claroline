df007b00165c1dd1cbff09d6c0c9f10a6b31bdcb
mathieu <mathieu@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-10-13 18:03:31 +0000

    Dutch 1.7.1 100% by svenn d'hert - wmtown.com (nickname svensson)

--- claroline/lang/dutch/missing.lang.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$langConfirmDeleteThisGroup = "Are you sure to delete this group ?";
-$langEnrollmentKey = "enrolment key";
-$langGroupsNotEmptied = "Unable to empty groups";
-$langNotSaved = "Unable to save";
-?>
