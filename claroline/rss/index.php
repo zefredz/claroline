00010db7dc9c81b2e9f44d493d026662a8e81626
moosh <moosh@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-10-13 15:16:32 +0000

    - protect for  magic_quote_gpc = on

--- claroline/rss/index.php
+++ claroline/rss/index.php
@@ -1,6 +1,6 @@
 <?php // $Id$
-/** 
- * CLAROLINE 
+/**
+ * CLAROLINE
  *
  * Build the frameset for chat.
  *
@@ -8,7 +8,7 @@
  *
  * @copyright 2001-2005 Universite catholique de Louvain (UCL)
  *
- * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE 
+ * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
  *
  * @see http://www.claroline.net/wiki/index.php/CLRSS
  *
@@ -25,52 +25,60 @@ $is_courseAllowed = false;
 require '../inc/claro_init_global.inc.php';
 include_once $includePath . '/conf/rss.conf.php';
 
-// RSS enabled 
+// RSS enabled
 if ( isset($enable_rss_in_course) && $enable_rss_in_course == false )
 {
-        // Codes Status HTTP 404 for rss feeder
-        header('HTTP/1.0 404 Not Found');
-        exit;
+    // Codes Status HTTP 404 for rss feeder
+    header('HTTP/1.0 404 Not Found');
+    exit;
 }
 
-if(!$_cid) 
+if(!$_cid)
 {
     die( '<form >cidReq = <input name="cidReq" type="text" ><input type="submit"></form>');
 }
 
 if ( !$_course['visibility'] && !$is_courseAllowed )
 {
-    if (!isset($_SERVER['PHP_AUTH_USER'])) 
+    if (!isset($_SERVER['PHP_AUTH_USER']))
     {
-       header('WWW-Authenticate: Basic realm="'.sprintf($lang_p_FeedOf_s, $_course['name']).'"');
-       header('HTTP/1.0 401 Unauthorized');
-       echo '<h2>' . sprintf($lang_p_youNeedToBeAuthenticatedWithYour_s_account, $siteName) . '</h2>'
-       .    '<a href="index.php?cidReq=' . $_cid . '">' . $langRetry . '</a>'
-       ;
-       exit;
-    } 
-    else 
+        header('WWW-Authenticate: Basic realm="'.sprintf($lang_p_FeedOf_s, $_course['name']).'"');
+        header('HTTP/1.0 401 Unauthorized');
+        echo '<h2>' . sprintf($lang_p_youNeedToBeAuthenticatedWithYour_s_account, $siteName) . '</h2>'
+        .    '<a href="index.php?cidReq=' . $_cid . '">' . $langRetry . '</a>'
+        ;
+        exit;
+    }
+    else
     {
-            $login    = $_SERVER['PHP_AUTH_USER'];
-            $password = $_SERVER['PHP_AUTH_PW'];
-            require '../inc/claro_init_local.inc.php';
-            if (!$_course['visibility'] && !$is_courseAllowed)
-            {
-                   header('WWW-Authenticate: Basic realm="'.sprintf($lang_p_FeedOf_s, $_course['name']).'"');
-                   header('HTTP/1.0 401 Unauthorized');
-                   echo '<h2>' . sprintf($lang_p_youNeedToBeAuthenticatedWithYour_s_account, $siteName) . '</h2>'
-                   .    '<a href="index.php?cidReq=' . $_cid . '">' . $langRetry . '</a>'
-                   ;
-                   exit;
-            }
+        if ( get_magic_quotes_gpc() ) // claro_unquote_gpc don't wash
+        {
+            $_REQUEST['login']    = stripslashes($_SERVER['PHP_AUTH_USER']);
+            $_REQUEST['password'] = stripslashes($_SERVER['PHP_AUTH_PW']);
+        }
+        else
+        {
+            $_REQUEST['login']    = $_SERVER['PHP_AUTH_USER'];
+            $_REQUEST['password'] = $_SERVER['PHP_AUTH_PW'] ;
+        }
+        require '../inc/claro_init_local.inc.php';
+        if (!$_course['visibility'] && !$is_courseAllowed)
+        {
+            header('WWW-Authenticate: Basic realm="'.sprintf($lang_p_FeedOf_s, $_course['name']).'"');
+            header('HTTP/1.0 401 Unauthorized');
+            echo '<h2>' . sprintf($lang_p_youNeedToBeAuthenticatedWithYour_s_account, $siteName) . '</h2>'
+            .    '<a href="index.php?cidReq=' . $_cid . '">' . $langRetry . '</a>'
+            ;
+            exit;
+        }
     }
 }
 
 // OK TO SEND FEED
 
-include( $includePath . '/lib/rss/write/gencourse_rss.inc.php');
+include $includePath . '/lib/rss/write/gencourse_rss.inc.php';
 
-header('Content-type: text/xml;'); 
+header('Content-type: text/xml;');
 readfile (build_course_feed(!$use_rss_cache, $_cid));
 
 ?>
