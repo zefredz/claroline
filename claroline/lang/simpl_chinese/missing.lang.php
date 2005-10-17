15d97c3a4899325060b28a5b08a935f4be568a25
mathieu <mathieu@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-10-17 09:44:17 +0000

    rebuild missing language files

--- claroline/lang/simpl_chinese/missing.lang.php
+++ claroline/lang/simpl_chinese/missing.lang.php
@@ -1,7 +1,11 @@
 <?php 
+$langConfirmDeleteThisGroup = "Are you sure to delete this group ?";
 $langCreatingCourse = "Creating course (it may take a while) ... ";
+$langEnrollmentKey = "enrolment key";
+$langGroupsNotEmptied = "Unable to empty groups";
 $langMessageEnrollToCourseNotAllowed = "Enroll to course not allowed";
 $langMessageRegistrationNotAllowed = "Registration not allowed on the platform";
 $langNoTrackingForExerciseAttempt = "There is no tracking for this attempt.";
+$langNotSaved = "Unable to save";
 $langWikiRecentChangesPattern = "%1\$s modified on %2\$s by %3\$s";
 ?>
