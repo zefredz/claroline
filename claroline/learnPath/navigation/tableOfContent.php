f304ab58a70b3081e836b858467f62c951e4331c
seb <seb@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-11-08 09:28:11 +0000

    Fix a bug in query when using mysql 5 (Table from 'from' clause used in 'left join' must be right before the left join)

--- claroline/learnPath/navigation/tableOfContent.php
+++ claroline/learnPath/navigation/tableOfContent.php
@@ -67,8 +67,8 @@ $sql = "SELECT LPM.`learnPath_module_id` ,
             UMP.`lesson_status`, UMP.`raw`,
             UMP.`scoreMax`, UMP.`credit`,
             A.`path`
-         FROM `".$TABLELEARNPATHMODULE."` AS LPM,
-              `".$TABLEMODULE."` AS M
+         FROM (`".$TABLELEARNPATHMODULE."` AS LPM,
+              `".$TABLEMODULE."` AS M)
    LEFT JOIN `".$TABLEUSERMODULEPROGRESS."` AS UMP
            ON UMP.`learnPath_module_id` = LPM.`learnPath_module_id`
            ".$uidCheckString."
