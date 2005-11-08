a3c8a357abf354abe7cef10d44bdf1886e8f7b5f
mathieu <mathieu@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-11-08 10:15:16 +0000

    htmlarea l10n error in german ( missing js lang var ) http://www.claroline.net/forum/viewtopic.php?t=4430

--- claroline/inc/htmlarea/lang/de.js
+++ claroline/inc/htmlarea/lang/de.js
@@ -49,6 +49,29 @@ HTMLArea.I18N = {
 
 	msg: {
 		"Path":         "Pfad",
-		"TEXT_MODE":    "Du befindest dich im HTML Modus.  Benuetze die [<>] Schaltflaeche um in den WYSIWIG-Modus zu wechseln."
-	}
+		"TEXT_MODE":    "Du befindest dich im HTML Modus.  Benuetze die [<>] Schaltflaeche um in den WYSIWIG-Modus zu wechseln.",
+      "IE-sucks-full-screen" :
+      // translate here
+      "The full screen mode is known to cause problems with Internet Explorer, " +
+      "due to browser bugs that we weren't able to workaround.  You might experience garbage " +
+      "display, lack of editor functions and/or random browser crashes.  If your system is Windows 9x " +
+      "it's very likely that you'll get a 'General Protection Fault' and need to reboot.\n\n" +
+      "You have been warned.  Please press OK if you still want to try the full screen editor." 
+	},
+
+   dialogs: {
+      "Cancel"                                            : "Cancel",
+      "Insert/Modify Link"                                : "Insert/Modify Link",
+      "New window (_blank)"                               : "New window (_blank)",
+      "None (use implicit)"                               : "None (use implicit)",
+      "OK"                                                : "OK",
+      "Other"                                             : "Other",
+      "Same frame (_self)"                                : "Same frame (_self)",
+      "Target:"                                           : "Target:",
+      "Title (tooltip):"                                  : "Title (tooltip):",
+      "Top frame (_top)"                                  : "Top frame (_top)",
+      "URL:"                                              : "URL:",
+      "You must enter the URL where this link points to"  : "You must enter the URL where this link points to"
+   } 
+
 };
