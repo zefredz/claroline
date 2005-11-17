5d64c1ac44b77c3a6c0af68c67f659ba45882ea5
seb <seb@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-11-17 13:58:37 +0000

    Add confval : confval_def_sub_vis_change_only_new

--- claroline/inc/conf/def/CLWRK.def.conf.inc.php
+++ claroline/inc/conf/def/CLWRK.def.conf.inc.php
@@ -21,13 +21,29 @@
 $conf_def['config_code'] = 'CLWRK';
 $conf_def['config_file'] = 'CLWRK.conf.php';
 $conf_def['config_name'] = 'Assignments tool';
-$conf_def['config_class']='tool';
+$conf_def['config_class']= 'tool';
 
-$conf_def['section']['storage']['label']      = 'Quota';
-$conf_def['section']['storage']['properties'] = 
+$conf_def['section']['main']['label']      = ' Main settings';
+$conf_def['section']['main']['properties'] = 
+array ( 'confval_def_sub_vis_change_only_new' );
+
+$conf_def['section']['quota']['label']      = 'Quota';
+$conf_def['section']['quota']['properties'] = 
 array ( 'max_file_size_per_works' );
+
+
 //PROPERTIES
 
+$conf_def_property_list['confval_def_sub_vis_change_only_new'] =
+array ('label'     => 'Assignment property "Default works visibility" acts'
+        ,'description' => 'Sets how the assignment property "default works visibility" acts.  It will change the visibility of all the new submissions or it will change the visibility of all submissions already done in the assignment and the new one. '
+        ,'default'   => 'TRUE'
+        ,'type'      => 'boolean'
+        ,'display'       => TRUE
+        ,'readonly'      => FALSE
+        ,'acceptedValue' => array ( 'TRUE'=> 'only for new works', 'FALSE'=>'for current and new works' )
+        );
+        
 $conf_def_property_list['max_file_size_per_works'] =
 array ('label'         => 'Maximum size for an assignment'
       ,'description'   => 'Maximum size of a document that a user can upload'
@@ -37,4 +53,5 @@ array ('label'         => 'Maximum size for an assignment'
       ,'type'          => 'integer'
       ,'unit'          => 'bytes'
       );
+
 ?>
