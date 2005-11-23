49819540ab33481302c4604b3195c59af7f2f64f
Frédéric Minne <zefredz@users.noreply.github.com>
2005-11-23 14:01:58 +0000

    correct regexp in get_link_file_url to accept get arguments and urlencoded parts (bug #418)

--- claroline/inc/lib/fileManage.lib.php
+++ claroline/inc/lib/fileManage.lib.php
@@ -556,7 +556,7 @@ function get_link_file_url($file)
    $fileContent = implode("\n", file ($file));
    $matchList =  array();
    
-   if (preg_match('^<meta http-equiv="refresh" content="[0-9]+;url=([-a-zA-Z:/.0-9]+)">^',
+   if (preg_match('~<meta http-equiv="refresh" content="[0-9]+;url=([^">]+)~',
                   $fileContent,
                   $matchList))
    {
