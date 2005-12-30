6cc21ec8f7b91443a8c80a34fe1ef59fd37444c4
moosh <moosh@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2005-12-30 19:26:28 +0000

    -phpDoc

--- claroline/user/index.php
+++ claroline/user/index.php
@@ -5,18 +5,12 @@
  *
  * redirect user to the entrance script of the tool
  *
- * @version 1.7 $Revision$
- *
+ * @version 1.8 $Revision$
  * @copyright 2001-2005 Universite catholique de Louvain (UCL)
- *
  * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
- *
  * @see http://www.claroline.net/CLUSR/
- *
  * @package CLUSR
- *
  * @author Claro Team <cvs@claroline.net>
- *
  */
 header('Location:./user.php');
 exit();
