da312896777181d244d7a5411f2dd1aa644ddeb8
moosh <moosh@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2006-01-02 18:25:58 +0000

    - remove comment and a test

--- claroline/admin/maintenance/repaircats.php
+++ claroline/admin/maintenance/repaircats.php
@@ -143,7 +143,6 @@ switch ($view)
         .    ($errorCounter?claro_disp_button($_SERVER['PHP_SELF'] . '?cmd=repairTree','Repair','Run repair task on the tree ? ') : '' )
         .    claro_disp_tool_title('Course ownance')
         .    claro_disp_datagrid($courseOwnanceCheck , array('idLine' => 'numeric'
-                                                            ,'idLineShift' => 10
                                                             ,'colTitleList' => array( get_lang('Code')
                                                                                     , get_lang('Unknow faculty'))
                                                             ,))
@@ -164,98 +163,5 @@ switch ($view)
     }
 }
 
-
 include $includePath . '/claro_init_footer.inc.php';
-
-
-/**
- * display data array in a <table>
- *
- * @param array $dataGrid array of data
- * @param array $option array of options
- * @return string html stream
- *
- * $dataGrid[]=array('nom'=>'dubois', 'prenom'=>'jean');
- * $dataGrid[]=array('nom'=>'dupont', 'prenom'=>'pol');
- * $dataGrid[]=array('nom'=>'durand', 'prenom'=>'simon');
- */
-/*
-function claro_disp_datagrid($dataGrid, $option = null)
-{
-    if(is_null($option) || ! is_array($option) )  $option=array();
-
-    if (! array_key_exists('idLine',      $option)) $option['idLine'] = 'blank';
-    if (! array_key_exists('idLineShift', $option)) $option['idLineShift'] = 1;
-    if (! array_key_exists('colTitleList', $option)) $option['colTitleList'] = array_keys($dataGrid[0]);
-
-
-
-    $stream = '';
-    if (is_array($dataGrid) && count($dataGrid))
-    {
-        $stream .= '<table class="claroTable emphaseLine" width="100%" border="0" cellspacing="2">'
-        .          '<thead>' . "\n"
-        .          '<tr class="headerX" align="center" valign="top">' . "\n"
-        .          '<th>'
-        .          '</th>' . "\n"
-        ;
-        $i=0;
-        foreach ($option['colTitleList'] as $colTitle)
-            $stream .= '<th scope="col" id="c' . $i++ . '" >' . $colTitle . '</th>' . "\n";
-
-        $stream .= '</tr>' . "\n"
-        .          '</thead>' . "\n"
-        ;
-
-        if (array_key_exists('dispCounter',$option))
-        {
-            $stream .= '<tfoot>' . "\n"
-            .          '<tr class="headerX" align="center" valign="top">' . "\n"
-            .          '<td>' . "\n"
-            .          '</td>' . "\n"
-            .          '<td>' . "\n"
-            .          count($dataGrid)
-            .          '</td>' . "\n"
-            .          '</tr>' . "\n"
-            .          '</tr>' . "\n"
-            .          '</tfoot>' . "\n"
-            ;
-
-        }
-
-        $stream .= '<tbody>' . "\n"
-        ;
-        foreach ($dataGrid as $key => $dataLine)
-        {
-            switch ($option['idLine'])
-            {
-                case 'blank'   : $idLine = '';   break;
-                case 'numeric' : $idLine = $key + $option['idLineShift']; break;
-                default        : $idLine = '';   break;
-            }
-
-            $stream .= '<tr>' . "\n"
-            .          '<td>' . $idLine .'</td>' . "\n"
-            ;
-            $i=0;
-            foreach ($dataLine as $dataCell)
-            {
-                $stream .= '<td headers="c' . $i++ . '">';
-                $stream .= $dataCell;
-                $stream .= '</td>' . "\n";
-            }
-            $stream .= '</tr>' . "\n";
-
-        }
-        $stream .= '</tbody>' . "\n"
-        .          '</table>' . "\n"
-        ;
-
-    }
-
-    return $stream;
-
-}
-
-*/
 ?>
