ea58fbcbe50c5be0ec926b907dddf928811835b3
moosh <moosh@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2006-01-08 17:34:07 +0000

    - add option value 'none' to option 'idLine' to hide the counter column. - add option colAttributeList to set  attribute to all cell of each column - add lot of comment

--- claroline/inc/lib/datagrid.lib.php
+++ claroline/inc/lib/datagrid.lib.php
@@ -17,8 +17,27 @@
  * @author Christophe Gesché <moosh@claroline.net>
  *
  */
- 
- 
+
+/**
+ * datagrid is actually a function but can became an object.
+ *
+ * function claro_disp_datagrid($dataGrid, $option = null)
+ *
+ * would became a static method.
+ *
+ * but in dynamic work,
+ * new datagrid($dataGrid = null, $option_list = null)
+ * set_grid(array of array $datagrid)
+ * set_option_list(array $option_list)
+ * set_idLineType(string $line_type)
+ * set_idLineShift(integer $line_shift)
+ * set_colTitleList(array('colName'=>'colTitle'));
+ * set_colAttributeList(array('colName'=> array('attribName'=>'attribValue'))
+ * set_caption(string 'caption');
+ * set_counterLine(bool 'dispCounter')
+ *
+ */
+
 /**
  * display data array in a <table>
  *
@@ -35,25 +54,56 @@ function claro_disp_datagrid($dataGrid, $option = null)
 {
     if(is_null($option) || ! is_array($option) )  $option=array();
 
-    if (! array_key_exists('idLine',      $option)) $option['idLine'] = 'numeric';
     if (! array_key_exists('idLineShift', $option)) $option['idLineShift'] = 1;
     if (! array_key_exists('colTitleList', $option)) $option['colTitleList'] = array_keys($dataGrid[0]);
+    if (! array_key_exists('idLine',      $option)) $option['idLine'] = 'numeric';
 
+    $dispIdCol = true;
+    switch (strtolower($option['idLine']))
+    {
+        case 'blank'   : $idLine = '';       break;
+        case 'none'    : $dispIdCol = false; break;
+        case 'numeric' : $internalkey = 0;   break;
+        default        : $idLine = '';       break;
+    }
 
 
     $stream = '';
     if (is_array($dataGrid) && count($dataGrid))
     {
+
+        /**
+         * Build attributes for column
+         *
+         * In  W3C <COL> seems be the good usage but browser don't follow the tag
+         *
+         * So all attribute would be in each td of column.
+         */
+
+        foreach (array_keys($option['colTitleList']) as $col)
+        {
+            $attrCol[$col]='';
+            if (key_exists('colAttributeList',$option))
+            if (key_exists($col,$option['colAttributeList']))
+            foreach ($option['colAttributeList'][$col] as $attriName => $attriValue )
+            {
+                $attrCol[$col] .=' '.$attriName.'="'.$attriValue.'" ';
+            }
+        }
+
         $stream .= '<table class="claroTable emphaseLine" width="100%" border="0" cellspacing="2">'
+        // THEAD LINE
         .          '<thead>' . "\n"
         .          '<tr class="headerX" align="center" valign="top">' . "\n"
-        .          '<th>'
-        .          '</th>' . "\n"
         ;
+
+        if ($dispIdCol) $stream .= '<th></th>' . "\n";
+
         $i=0;
         foreach ($option['colTitleList'] as $colTitle)
+        {
             $stream .= '<th scope="col" id="c' . $i++ . '" >' . $colTitle . '</th>' . "\n";
-
+        }
         $stream .= '</tr>' . "\n"
         .          '</thead>' . "\n"
         ;
@@ -74,26 +124,23 @@ function claro_disp_datagrid($dataGrid, $option = null)
 
         }
 
-        $stream .= '<tbody>' . "\n"
-        ;
-        $internalkey = 0;
-        foreach ($dataGrid as $key => $dataLine)
+        $stream .= '<tbody>' . "\n";
+        foreach ($dataGrid as $key => $dataLine )
         {
             switch ($option['idLine'])
             {
-                case 'blank'   : $idLine = '';   break;
-                case 'key'     : $idLine = $key + $option['idLineShift']; break;
-                case 'numeric' : $idLine = $internalkey++ + $option['idLineShift']; break;
-                default        : $idLine = '';   break;
+                case 'key'     : $idLine = $option['idLineShift'] + $key ;           break;
+                case 'numeric' : $idLine = $option['idLineShift'] + $internalkey++ ; break;
             }
 
-            $stream .= '<tr>' . "\n"
-            .          '<td>' . $idLine . '</td>' . "\n"
-            ;
+            $stream .= '<tr>' . "\n";
+
+            if ($dispIdCol) $stream .= '<td>' . $idLine . '</td>' . "\n";
+
             $i=0;
-            foreach ($dataLine as $dataCell)
+            foreach ($dataLine as $colId => $dataCell)
             {
-                $stream .= '<td headers="c' . $i++ . '">';
+                $stream .= '<td headers="c' . $i++ . '" ' . $attrCol[$colId] . '>';
                 $stream .= $dataCell;
                 $stream .= '</td>' . "\n";
             }
