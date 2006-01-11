f575487e84fc4351812c7ff7159975340139624b
moosh <moosh@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2006-01-11 22:55:29 +0000

    - create  a function to build menu and use it.

--- claroline/admin/index.php
+++ claroline/admin/index.php
@@ -1,7 +1,7 @@
 <?php // $Id$
 /**
  * CLAROLINE
- * @version 1.7 $Revision$
+ * @version 1.8 $Revision$
  *
  * @copyright (c) 2001-2005 Universite catholique de Louvain (UCL)
  *
@@ -22,9 +22,9 @@ if ( ! $is_platformAdmin ) claro_die(get_lang('Not allowed'));
 
 require_once $includePath . '/lib/admin.lib.inc.php';
 
-//------------------------------------------------------------------------------------------------------------------------
+//------------------------
 //  USED SESSION VARIABLES
-//------------------------------------------------------------------------------------------------------------------------
+//------------------------
 // clean session of possible previous search information. : COURSE
 
 unset($_SESSION['admin_course_code']);
@@ -50,6 +50,52 @@ unset($_SESSION['admin_user_action']);
 unset($_SESSION['admin_order_crit']);
 
 $controlMsg = array();
+$menuUsers[] =  array('type'=>'free', 'stream'=>'<form name="searchUser" action="adminusers.php" method="GET" >' . "\n"
+                                              . '<label for="search_user">' . get_lang('User') . '</label>'
+                                              . ' : '
+                                              . '<input name="search" id="search_user" />'
+                                              . '<input type="submit" value="' . get_lang('Search') . '" />'
+                                              . '&nbsp;&nbsp;'
+                                              . '<small><a href="advancedUserSearch.php">' . get_lang('Advanced') . '</small></a>'
+                                              . '</form>');
+$menuUsers[] =  array('type'=>'link', 'url'=>'adminusers.php',       'attribute'=>'class="toollink"', 'label'=>get_lang('ListUsers'));
+$menuUsers[] =  array('type'=>'link', 'url'=>'adminaddnewuser.php',       'attribute'=>'class="toollink"', 'label'=> get_lang('CreateUser'));
+$menuUsers[] =  array('type'=>'link', 'url'=>'admin_class.php',       'attribute'=>'class="toollink"', 'label'=>get_lang('ManageClasses'));
+$menuUsers[] =  array('type'=>'link', 'url'=>'../user/AddCSVusers.php?AddType=adminTool',       'attribute'=>'class="toollink"', 'label'=> get_lang('AddCSVUsers'));
+
+
+$menuCourses[] =  array('type'=>'free', 'stream'=>'<form name="searchCourse" action="admincourses.php" method="GET" >' . "\n"
+                                              . '<label for="search_course">' . get_lang('Course') . '</label> :' . "\n"
+                                              . '<input name="search" id="search_course" />' . "\n"
+                                              . '<input type="submit" value="' . get_lang('Search'). '" />' . "\n"
+                                              . '&nbsp; &nbsp;<small><a href="advancedCourseSearch.php">' . get_lang('Advanced') . '</a></small>' . "\n"
+                                              . '</form>');
+$menuCourses[] =  array('type'=>'link', 'url'=>'adminusers.php',       'attribute'=>'class="toollink"', 'label'=>get_lang('ListUsers'));
+
+
+
+$menuCourses[] =  array('type'=>'link', 'url'=>'admincourses.php',       'attribute'=>'class="toollink"', 'label'=>get_lang('CourseList'));
+$menuCourses[] =  array('type'=>'link', 'url'=>'../create_course/add_course.php?fromAdmin=yes',       'attribute'=>'class="toollink"', 'label'=>get_lang('CreateCourse'));
+$menuCourses[] =  array('type'=>'link', 'url'=>'admincats.php',       'attribute'=>'class="toollink"', 'label'=>get_lang('ManageCourseCategories'));
+
+
+
+$menuPlatform[] =  array('type'=>'link', 'url'=>'tool/config_list.php',       'attribute'=>'class="toollink"', 'label'=>get_lang('Configuration'));
+$menuPlatform[] =  array('type'=>'link', 'url'=>'managing/editFile.php',      'attribute'=>'class="toollink"', 'label'=>get_lang('HomePageTextZone'));
+$menuPlatform[] =  array('type'=>'link', 'url'=>'campusLog.php',              'attribute'=>'class="toollink"', 'label'=>get_lang('ViewPlatFormStatistics'));
+$menuPlatform[] =  array('type'=>'link', 'url'=>'campusProblem.php',          'attribute'=>'class="toollink"', 'label'=>get_lang('ViewPlatFormError'));
+$menuPlatform[] =  array('type'=>'link', 'url'=>'maintenance/repaircats.php', 'attribute'=>'class="toollink"', 'label'=>get_lang('CategoriesRepairs'));
+$menuPlatform[] =  array('type'=>'link', 'url'=>'upgrade/index.php',          'attribute'=>'class="toollink"', 'label'=>get_lang('Upgrade'));
+
+
+$menuClaroline[] =  array('type'=>'link', 'url'=>'registerCampus.php',            'attribute'=>'class="toollink"', 'label'=>get_lang('RegisterMyCampus'));
+$menuClaroline[] =  array('type'=>'link', 'url'=>'http://www.claroline.net/forum','attribute'=>'class="extlink"','label'=>get_lang('SupportForum'));
+$menuClaroline[] =  array('type'=>'link', 'url'=>'clarolinenews.php',             'attribute'=>'class="extlink"','label'=>get_lang('ClarolineNetNews'));
+
+
+if ( defined('CLAROLANG') && CLAROLANG == 'TRANSLATION') $menuSDK[] =  array('type'=>'link', 'url'=>'xtra/sdk/translation_index.php', 'attribute'=>'class="toollink"', 'label'=>get_lang('TranslationTools'));
+if ( defined('DEVEL_MODE') && DEVEL_MODE == TRUE ) $menuSDK[] =  array('type'=>'link', 'url'=>'devTools',                             'attribute'=>'class="toollink"', 'label'=>get_lang('DevTools'));
+
 //----------------------------------
 // DISPLAY
 //----------------------------------
@@ -73,140 +119,38 @@ echo claro_disp_tool_title($nameTools)
 .    claro_disp_msg_arr( $controlMsg,1) . "\n\n"
 ;
 
-?>
-<table cellspacing="5" align="center">
+echo '<table cellspacing="5" align="center">' . "\n"
+.    '<tr valign="top">' . "\n"
+.    '<td nowrap="nowrap">' . "\n"
+.    claro_disp_tool_title('<img src="' . $imgRepositoryWeb . 'user.gif" alt="" />&nbsp;'.get_lang('Users'))
+.    claro_disp_menu_vlist($menuUsers)
+.    '</td>' . "\n"
+.    '<td nowrap="nowrap">'
+.    claro_disp_tool_title('<img src="' . $imgRepositoryWeb . 'course.gif" alt="" />&nbsp;'.get_lang('Courses'))
+.    claro_disp_menu_vlist($menuCourses) . "\n"
+.    '</td>' . "\n"
+.    '</tr>' . "\n"
+.    '<tr valign="top">' . "\n"
+.    '<td nowrap="nowrap">' . "\n"
+.    claro_disp_tool_title('<img src="' . $imgRepositoryWeb . 'settings.gif" alt="" />&nbsp;'.get_lang('Platform')) . "\n"
+.    claro_disp_menu_vlist($menuPlatform) . "\n"
+.    '</td>' . "\n"
+.    '<td nowrap="nowrap">' . "\n"
+.    claro_disp_tool_title('<img src="' . $imgRepositoryWeb . 'claroline.gif" alt="" />&nbsp;Claroline.net')
+.    claro_disp_menu_vlist($menuClaroline)
+.    '</td>' . "\n"
+.    '</tr>' . "\n"
+;
 
-<tr valign="top">
-
-<td nowrap="nowrap">
-<h4><img src="<?php echo $imgRepositoryWeb; ?>user.gif" alt="" /> <?php echo get_lang('Users')?></h4>
-<ul>
-<li>
-<form name="searchUser" action="adminusers.php" method="GET" >
-<label for="search_user"><?php echo get_lang('User'); ?></label>
-:
-<input name="search" id="search_user" />
-<input type="submit" value="<?php echo get_lang('Search'); ?>" />
-&nbsp;&nbsp;<small><a href="advancedUserSearch.php"><?php echo get_lang('Advanced')?></small></a>
-</form>
-<li>
-<a href="adminusers.php" class="toollink"><?php echo get_lang('ListUsers')?></a>
-</li>
-<li>
-<a href="adminaddnewuser.php" class="toollink"><?php echo get_lang('CreateUser')?></a>
-</li>
-<li>
-<a href="admin_class.php" class="toollink"><?php echo get_lang('ManageClasses')?></a>
-</li>
-<li>
-<a href="../user/AddCSVusers.php?AddType=adminTool" class="toollink"><?php echo get_lang('AddCSVUsers')?></a>
-</li>
-</ul>
-</td>
-
-<td nowrap="nowrap">
-<h4><img src="<?php echo $imgRepositoryWeb; ?>course.gif" alt="" /> <?php echo get_lang('Courses')?></h4>
-<ul>
-<li>
-<form name="searchCourse" action="admincourses.php" method="GET" >
-<label for="search_course"><?php echo get_lang('Course'); ?></label> :
-<input name="search" id="search_course" />
-<input type="submit" value="<?php echo get_lang('Search'); ?>" />
-&nbsp; &nbsp;<small><a href="advancedCourseSearch.php"><?php echo get_lang('Advanced')?></a></small>
-</form>
-</li>
-<li>
-<a href="admincourses.php" class="toollink"><?php echo get_lang('CourseList')?></a>
-</li>
-<li>
-<a href="../create_course/add_course.php?fromAdmin=yes" class="toollink"><?php echo get_lang('CreateCourse')?></a><br />
-</li>
-<li>
-<a href="admincats.php" class="toollink"><?php echo get_lang('ManageCourseCategories')?></a>
-</li>
-</ul>
-</td>
-
-</tr>
-<tr valign="top">
-
-<td nowrap="nowrap">
-<h4><img src="<?php echo $imgRepositoryWeb; ?>settings.gif" alt="" /> <?php echo get_lang('Platform')?></h4>
-<ul>
-<li>
-<a href="tool/config_list.php" class="toollink"><?php echo get_lang('Configuration')?></a>
-</li>
-<li>
-<a href="managing/editFile.php" class="toollink"><?php echo get_lang('HomePageTextZone') ?></a>
-</li>
-<li>
-<a href="campusLog.php" class="toollink"><?php echo get_lang('ViewPlatFormStatistics')?></a>
-</li>
-<li>
-<a href="campusProblem.php" class="toollink"><?php echo get_lang('ViewPlatFormError') ?></a>
-</li>
-<li>
-<a href="maintenance/repaircats.php" class="toollink"><?php echo get_lang('CategoriesRepairs') ?></a>
-</li>
-<li>
-<a href="upgrade/index.php" class="toollink"><?php echo get_lang('Upgrade')?></a>
-</li>
-</ul>
-</td>
-
-<td nowrap="nowrap">
-<h4><img src="<?php echo $imgRepositoryWeb; ?>claroline.gif" alt="" />&nbsp;Claroline.net</h4>
-<ul>
-<li>
-<a href="registerCampus.php"  class="toollink" ><?php echo get_lang('RegisterMyCampus'); ?></a>
-</li>
-<li>
-<a href="http://www.claroline.net/forum/" class="extlink" ><?php echo get_lang('SupportForum'); ?></a>
-</li>
-<li>
-<a href="clarolinenews.php" class="extlink" ><?php echo get_lang('ClarolineNetNews'); ?></a>
-</li>
-</ul>
-</td>
-
-</tr>
-
-<?php
 if ( ( defined('DEVEL_MODE') && DEVEL_MODE == TRUE )
 || ( defined('CLAROLANG') && CLAROLANG == 'TRANSLATION') )
 {
     echo '<tr valign="top">'
     .    '<td nowrap="nowrap">'
-    .    '<h4>'
-    .    get_lang('SDK')
-    .    '</h4>'
+    .    claro_disp_tool_title('<img src="' . $imgRepositoryWeb . 'exe.gif" alt="" />&nbsp;'.get_lang('SDK')) . "\n"
+    .    claro_disp_menu_vlist($menuSDK)
     .    '<ul>'
     ;
-}
-
-if ( defined('CLAROLANG') && CLAROLANG == 'TRANSLATION')
-{
-    echo '<li>'
-    .    '<a href="xtra/sdk/translation_index.php" class="toollink">'
-    .    get_lang('TranslationTools')
-    .    '</a>'
-    .    '</li>'
-    ;
-}
-if ( defined('DEVEL_MODE') && DEVEL_MODE == TRUE )
-{
-    echo '<li>'
-    .    '<a href="devTools/" class="toollink">'
-    .    get_lang('DevTools')
-    .    '</a>'
-    .    '</li>'
-    ;
-}
-
-if ( ( defined('DEVEL_MODE') && DEVEL_MODE == TRUE )
-|| ( defined('CLAROLANG') && CLAROLANG == 'TRANSLATION') )
-{
-    // close opened tag if needed
 ?>
 </ul>
 </td>
@@ -218,4 +162,34 @@ if ( ( defined('DEVEL_MODE') && DEVEL_MODE == TRUE )
 </table>
 <?php
 include $includePath . '/claro_init_footer.inc.php';
+
+function claro_disp_menu_vlist($itemList)
+{
+    // class="toollink"
+    $htmlStream = '<UL>' . "\n";
+    foreach ($itemList as $item )
+    {
+        $htmlStream .= '<LI>' . "\n";
+        switch($item['type'])
+        {
+            case 'link' :
+            {
+                $htmlStream .= '<a href="'. $item['url'] . '" ' . $item['attribute'] . ' >'
+                .              $item['label']
+                .              '</a>'
+                ;
+
+            } break;
+            case 'free' :
+            {
+                $htmlStream .= $item['stream'];
+            } break;
+
+        }
+        $htmlStream .= '</LI>' . "\n"
+        ;
+    }
+    $htmlStream .= '</UL>' . "\n";
+    return $htmlStream;
+}
 ?>
