f7916e87c7c673d8467cec5efca09b626ea1cc4b
moosh <moosh@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2006-01-23 12:39:56 +0000

    -coding rules : SQL with AS

--- claroline/inc/lib/announcement.lib.php
+++ claroline/inc/lib/announcement.lib.php
@@ -38,11 +38,15 @@
 
 function announcement_get_item_list($order='DESC', $course_id=NULL)
 {
-    $tbl_c_names = claro_sql_get_course_tbl(claro_get_course_db_name_glued($course_id));
-    $tbl_announcement = $tbl_c_names['announcement'];
+    $tbl = claro_sql_get_course_tbl(claro_get_course_db_name_glued($course_id));
 
-    $sql = "SELECT id, title, contenu content, temps `time`, visibility, ordre rank
-            FROM `" . $tbl_announcement . "`
+    $sql = "SELECT            id,
+                              title,
+                   contenu AS content,
+                   temps   AS `time`,
+                              visibility,
+                   ordre AS   rank
+            FROM `" . $tbl['announcement'] . "`
             ORDER BY ordre " . ($order == 'DESC' ? 'DESC' : 'ASC');
     return claro_sql_query_fetch_all($sql);
 }
@@ -57,10 +61,9 @@ function announcement_get_item_list($order='DESC', $course_id=NULL)
  */
 function announcement_delete_item($id, $course_id=NULL)
 {
-    $tbl_c_names = claro_sql_get_course_tbl(claro_get_course_db_name_glued($course_id));
-    $tbl_announcement = $tbl_c_names['announcement'];
+    $tbl = claro_sql_get_course_tbl(claro_get_course_db_name_glued($course_id));
 
-    $sql = "DELETE FROM  `" . $tbl_announcement . "`
+    $sql = "DELETE FROM  `" . $tbl['announcement'] . "`
             WHERE id='" . (int) $id . "'";
     return claro_sql_query($sql);
 }
@@ -76,10 +79,9 @@ function announcement_delete_item($id, $course_id=NULL)
  */
 function announcement_delete_all_items($course_id=NULL)
 {
-    $tbl_c_names = claro_sql_get_course_tbl(claro_get_course_db_name_glued($course_id));
-    $tbl_announcement = $tbl_c_names['announcement'];
+    $tbl = claro_sql_get_course_tbl(claro_get_course_db_name_glued($course_id));
 
-    $sql = "DELETE FROM  `" . $tbl_announcement . "`";
+    $sql = "DELETE FROM  `" . $tbl['announcement'] . "`";
     return claro_sql_query($sql);
 }
 
@@ -97,8 +99,7 @@ function announcement_delete_all_items($course_id=NULL)
 
 function announcement_add_item($title='',$content='', $visibility='SHOW', $time=NULL, $course_id=NULL)
 {
-    $tbl_c_names = claro_sql_get_course_tbl(claro_get_course_db_name_glued($course_id));
-    $tbl_announcement = $tbl_c_names['announcement'];
+    $tbl= claro_sql_get_course_tbl(claro_get_course_db_name_glued($course_id));
 
     if(is_null($time))
     {
@@ -110,13 +111,13 @@ function announcement_add_item($title='',$content='', $visibility='SHOW', $time=
     }
 
     // DETERMINE THE ORDER OF THE NEW ANNOUNCEMENT
-    $sql = "SELECT (MAX(ordre) + 1) nextRank
-            FROM  `" . $tbl_announcement . "`";
+    $sql = "SELECT (MAX(ordre) + 1) AS nextRank
+            FROM  `" . $tbl['announcement'] . "`";
 
     $nextRank = claro_sql_query_fetch_all($sql);
     // INSERT ANNOUNCEMENT
 
-    $sql = "INSERT INTO `" . $tbl_announcement . "`
+    $sql = "INSERT INTO `" . $tbl['announcement'] . "`
             SET title ='" . addslashes(trim($title)) . "',
                 contenu = '" . addslashes(trim($content)) . "',
                 visibility = '" . ($visibility=='HIDE'?'HIDE':'SHOW') . "',
@@ -139,8 +140,7 @@ function announcement_add_item($title='',$content='', $visibility='SHOW', $time=
 
 function announcement_update_item($announcement_id, $title=NULL, $content=NULL, $visibility=NULL, $time=NULL, $course_id=NULL)
 {
-    $tbl_c_names = claro_sql_get_course_tbl(claro_get_course_db_name_glued($course_id));
-    $tbl_announcement = $tbl_c_names['announcement'];
+    $tbl= claro_sql_get_course_tbl(claro_get_course_db_name_glued($course_id));
     $sqlSet = array();
     if(!is_null($title))      $sqlSet[] = " title = '" . addslashes(trim($title)) . "' ";
     if(!is_null($content))    $sqlSet[] = " contenu = '" . addslashes(trim($content)) . "' ";
@@ -149,7 +149,7 @@ function announcement_update_item($announcement_id, $title=NULL, $content=NULL, 
 
     if (count($sqlSet)>0)
     {
-        $sql = "UPDATE  `" . $tbl_announcement . "`
+        $sql = "UPDATE  `" . $tbl['announcement'] . "`
                 SET " . implode(', ',$sqlSet) . "
                 WHERE id='" . (int) $announcement_id . "'";
         return claro_sql_query($sql);
@@ -168,13 +168,16 @@ function announcement_update_item($announcement_id, $title=NULL, $content=NULL, 
 
 function announcement_get_item($announcement_id, $course_id=NULL)
 {
-    $tbl_c_names = claro_sql_get_course_tbl(claro_get_course_db_name_glued($course_id));
-    $tbl_announcement = $tbl_c_names['announcement'];
+    $tbl = claro_sql_get_course_tbl(claro_get_course_db_name_glued($course_id));
 
-
-    $sql = "SELECT id, title, contenu content, visibility, ordre rank
-            FROM  `" . $tbl_announcement . "`
+    $sql = "SELECT                id,
+                                  title,
+                   contenu     AS content,
+                                  visibility,
+                   ordre       AS rank
+            FROM  `" . $tbl['announcement'] . "`
             WHERE id=" . (int) $announcement_id ;
+
     $announcement = claro_sql_query_get_single_row($sql);
 
     if ($announcement) return $announcement;
@@ -183,11 +186,11 @@ function announcement_get_item($announcement_id, $course_id=NULL)
 
 function announcement_set_item_visibility($announcement_id, $visibility, $course_id=NULL)
 {
-    $tbl_c_names = claro_sql_get_course_tbl(claro_get_course_db_name_glued($course_id));
-    $tbl_announcement = $tbl_c_names['announcement'];
+    $tbl = claro_sql_get_course_tbl(claro_get_course_db_name_glued($course_id));
+
     if (!in_array($visibility, array ('HIDE','SHOW')))
      trigger_error('ANNOUNCEMENT_VISIBILITY_UNKNOW', E_USER_NOTICE);
-    $sql = "UPDATE `" . $tbl_announcement . "`
+    $sql = "UPDATE `" . $tbl['announcement'] . "`
             SET   visibility = '" . ($visibility=='HIDE'?'HIDE':'SHOW') . "'
                   WHERE id =  '" . (int) $announcement_id . "'";
     return  claro_sql_query($sql);
@@ -204,8 +207,7 @@ function announcement_set_item_visibility($announcement_id, $visibility, $course
  */
 function move_entry($item_id, $cmd, $course_id=NULL)
 {
-    $tbl_c_names = claro_sql_get_course_tbl(claro_get_course_db_name_glued($course_id));
-    $tbl_announcement = $tbl_c_names['announcement'];
+    $tbl = claro_sql_get_course_tbl(claro_get_course_db_name_glued($course_id));
 
     if ( $cmd == 'DOWN' )
     {
@@ -222,8 +224,9 @@ function move_entry($item_id, $cmd, $course_id=NULL)
 
     if ( $sortDirection )
     {
-        $sql = "SELECT id, ordre rank
-            FROM `" . $tbl_announcement . "`
+        $sql = "SELECT          id,
+                       ordre AS rank
+            FROM `" . $tbl['announcement'] . "`
             ORDER BY `ordre` " . $sortDirection;
 
         $result = claro_sql_query($sql);
@@ -239,13 +242,13 @@ function move_entry($item_id, $cmd, $course_id=NULL)
                 $nextAnnouncementId    = $announcementId;
                 $nextAnnouncementRank  = $announcementRank;
 
-                $sql = "UPDATE `" . $tbl_announcement . "`
+                $sql = "UPDATE `" . $tbl['announcement'] . "`
                     SET ordre = '" . (int) $nextAnnouncementRank . "'
                     WHERE id =  '" . (int) $thisAnnouncementId . "'";
 
                 claro_sql_query($sql);
 
-                $sql = "UPDATE `" . $tbl_announcement . "`
+                $sql = "UPDATE `" . $tbl['announcement'] . "`
                     SET ordre = '" . $thisAnnouncementRank . "'
                     WHERE id =  '" . $nextAnnouncementId . "'";
                 claro_sql_query($sql);
