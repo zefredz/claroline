deb146e39bd8911281652458d69acd2e95791f9a
seb <seb@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2006-01-24 14:46:59 +0000

    Add config var to know if submitted files have to be opened in another window or not

--- claroline/inc/conf/def/CLWRK.def.conf.inc.php
+++ claroline/inc/conf/def/CLWRK.def.conf.inc.php
@@ -23,11 +23,12 @@ $conf_def['config_file'] = 'CLWRK.conf.php';
 $conf_def['config_name'] = 'Assignments tool';
 $conf_def['config_class']= 'tool';
 
-$conf_def['section']['main']['label']      = ' Main settings';
+$conf_def['section']['main']['label']      = ' Main';
 $conf_def['section']['main']['properties'] = 
-array ( 'confval_def_sub_vis_change_only_new' );
+array ( 'confval_def_sub_vis_change_only_new', 'open_submitted_file_in_new_window' );
 
 $conf_def['section']['quota']['label']      = 'Quota';
+$conf_def['section']['quota']['description']= 'Disk space allowed for submitted files';
 $conf_def['section']['quota']['properties'] = 
 array ( 'max_file_size_per_works' );
 
@@ -43,7 +44,19 @@ array ('label'     => 'Assignment property "Default works visibility" acts'
         ,'readonly'      => FALSE
         ,'acceptedValue' => array ( 'TRUE'=> 'only for new works', 'FALSE'=>'for current and new works' )
         );
-        
+
+$conf_def_property_list['open_submitted_file_in_new_window'] =
+array ( 'description' => 'When users click on a submitted file, it opens a new window'
+      , 'label'       => 'New window for submitted files'
+      , 'default'     => 'FALSE'
+      , 'type'        => 'boolean'
+      , 'acceptedValue' => array ('TRUE'=>'Yes'
+                               ,'FALSE'=>'No'
+                               )
+      , 'display'     => TRUE
+      , 'readonly'    => FALSE
+      );
+              
 $conf_def_property_list['max_file_size_per_works'] =
 array ('label'         => 'Maximum size for an assignment'
       ,'description'   => 'Maximum size of a document that a user can upload'
