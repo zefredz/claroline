c05fabb398ac07cefd9d8875b429187d9db5dd59
moosh <moosh@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2006-01-28 22:28:28 +0000

    - coding rules - create function get_class_info_by_id($class_id)  (to move in a lib)

--- claroline/admin/admin_class_course_registered.php
+++ claroline/admin/admin_class_course_registered.php
@@ -4,20 +4,23 @@
  *
  * this tool manage the
  *
- * @version 1.7 $Revision$
+ * @version 1.8 $Revision$
  *
- * @copyright (c) 2001-2005 Universite catholique de Louvain (UCL)
+ * @copyright (c) 2001-2006 Universite catholique de Louvain (UCL)
  *
  * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
  *
  * @author Claro Team <cvs@claroline.net>
- * @author  Guillaume Lederer <lederer@cerdecam.be>
+ * @author Guillaume Lederer <lederer@cerdecam.be>
+ * @author Christophe Gesché <moosh@claroline.net>
  */
 
 require '../inc/claro_init_global.inc.php';
+
 require_once $includePath . '/lib/admin.lib.inc.php';
 require_once $includePath . '/lib/class.lib.php';
 require_once $includePath . '/lib/user.lib.php';
+
 include $includePath . '/conf/user_profile.conf.php'; // find this file to modify values.
 
 // Security check
@@ -28,32 +31,15 @@ if ( ! $is_platformAdmin ) claro_die(get_lang('Not allowed'));
 
 $nameTools=get_lang('Class registered');
 $interbredcrump[]= array ('url' => $rootAdminWeb, 'name' => get_lang('Class registered'));
-/**#@+
- * DB tables definition
- * @var $tbl_mdb_names array table name for the central database
- */
-$tbl_mdb_names = claro_sql_get_main_tbl();
-$tbl_user        = $tbl_mdb_names['user'];
-$tbl_course      = $tbl_mdb_names['course'];
-$tbl_course_user = $tbl_mdb_names['rel_course_user'];
-$tbl_class       = $tbl_mdb_names['user_category'];
-$tbl_class_user  = $tbl_mdb_names['user_rel_profile_category'];
-/**#@-*/
-
-//find info about the class
-
-$sqlclass = "SELECT *
-             FROM `" . $tbl_class . "`
-             WHERE `id`='". (int) $_SESSION['admin_user_class_id'] . "'";
-list($classinfo) = claro_sql_query_fetch_all($sqlclass);
-
-//------------------------------------
-// Execute COMMAND section
-//------------------------------------
 
 if ( isset($_REQUEST['cmd']) ) $cmd = $_REQUEST['cmd'];
 else                           $cmd = null;
 
+
+
+//------------------------------------
+// Execute COMMAND section
+//------------------------------------
 if (isset($cmd) && $is_platformAdmin)
 {
     if ($cmd == 'exReg')
@@ -80,35 +66,48 @@ if (isset($cmd) && $is_platformAdmin)
 
 }
 
-//------------------------------------
-// DISPLAY
-//------------------------------------
+/**
+ * PREPARE DISPLAY
+ */
 
+$classinfo =  get_class_info_by_id($_SESSION['admin_user_class_id']);
+
+if ( !empty($outputResultLog) ) $dialogBox = $outputResultLog;
+$cmd_menu[] =  '<p><a class="claroCmd" href="index.php">' . get_lang('BackToAdmin') . '</a>';
+$cmd_menu[] =  '<a class="claroCmd" href="' . 'admin_class_user.php?class=' . $classinfo['id'] . '">' . get_lang('BackToClassMembers') . '</a>';
+$cmd_menu[] =  '<a class="claroCmd" href="' . $clarolineRepositoryWeb . 'auth/courses.php?cmd=rqReg&amp;fromAdmin=class' . '">' . get_lang('Register class for course') . '</a></p>';
+
+
+/**
+ * DISPLAY
+ */
 include $includePath . '/claro_init_header.inc.php';
-// Display tool title
 
 echo claro_disp_tool_title(get_lang('Class registered') . ' : ' . $classinfo['name']);
 
-//Display Forms or dialog box(if needed)
+if ( !empty($dialogBox) ) echo claro_disp_message_box($dialogBox);
 
-// display log
-if ( !empty($outputResultLog) )
-{
-    $dialogBox = $outputResultLog;
-}
-
-if ( !empty($dialogBox) )
-{
-    echo claro_disp_message_box($dialogBox);
-}
-
-// display TOOL links :
-
-echo '<p><a class="claroCmd" href="index.php">' . get_lang('BackToAdmin') . '</a> | ';
-echo '<a class="claroCmd" href="' . 'admin_class_user.php?class=' . $classinfo['id'] . '">' . get_lang('BackToClassMembers') . '</a> | ';
-echo '<a class="claroCmd" href="' . $clarolineRepositoryWeb . 'auth/courses.php?cmd=rqReg&amp;fromAdmin=class' . '">' . get_lang('Register class for course') . '</a></p>';
-
-// display footer
+echo claro_html::menu_horizontal($cmd_menu);
 
 include $includePath . '/claro_init_footer.inc.php';
+
+/**
+ * get info about a class
+ *
+ * @param integer $class_id
+ * @return array (id, name)
+ */
+function get_class_info_by_id($class_id)
+{
+
+    $tbl_mdb_names = claro_sql_get_main_tbl();
+    $tbl_class       = $tbl_mdb_names['user_category'];
+    $sqlclass = "SELECT id,
+                        name
+                 FROM `" . $tbl_class . "`
+                 WHERE `id`='". (int) $class_id . "'";
+    return claro_sql_query_get_single_row($sqlclass);
+}
+
+
 ?>
