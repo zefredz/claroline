3ad896bf357259e7c682be2aa9c003b31905baaf
guillaume <guillaume@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2006-02-02 14:50:35 +0000

    MODULE SYSTEM - footer display uses dock and buffering system

--- claroline/inc/claro_init_footer.inc.php
+++ claroline/inc/claro_init_footer.inc.php
@@ -13,7 +13,7 @@ if (!isset($hide_body) || $hide_body == false)
 // depends on $claro_brailleViewMode
 if ( isset($claro_banner) ) 
 {
-    echo $claro_banner;
+    echo $claro_banner;    
 }
 
 // don't display the footer text if requested, only display minimal html closing tags
@@ -22,34 +22,57 @@ if (!isset($hide_footer) || $hide_footer == false)
 
 ?>
 
-<div id="footer">
-
+<div id="campusFooter">
 <hr />
+<?php 
 
-<?php
+//FOOTER LEFT DOCK declaration
+
+$footerLeftDock = new Dock('campusFooterLeft');
+$appletList = getAppletList($footerLeftDock);
+$footerLeftDock->setAppletList($appletList);
+
 if(isset($_cid))
 {
-?>
-<div id="courseManager">
-<?php printf(get_lang('_footer_p_CourseManager'), $_course['officialCode'])?> :
-<a href="<?php echo (empty($_course['email'])?$clarolineRepositoryWeb."user/user.php":"mailto:".$_course['email']."?body=".$_course['officialCode']."&amp;subject=[".rawurlencode($siteName)."]") ?>"><?php echo $_course['titular'] ?></a>
-</div>
-<?php
+$courseManagerOutput = '<div id="courseManager">
+'. sprintf(get_lang('_footer_p_CourseManager'), $_course['officialCode']).' :
+<a href="'. (empty($_course['email'])?$clarolineRepositoryWeb."user/user.php":"mailto:".$_course['email']."?body=".$_course['officialCode']."&amp;subject=[".rawurlencode($siteName)."]") .'">'. $_course['titular'].'</a>
+</div>';
+$footerLeftDock->addOutput($courseManagerOutput,true);
 }
-?>
 
-<div id="platformManager">
-<?php printf(get_lang('_p_platformManager'),$siteName); ?> : 
-<a href="mailto:<?php echo $administrator_email."?body=".$_course['officialCode']."&amp;subject=[".rawurlencode($siteName)."]" ?>"><?php echo $administrator_name ?></a>
-</div>
+echo $footerLeftDock->render();
 
-<div id="poweredBy">
-<?php echo get_lang('Powered by') ?> <a href="http://www.claroline.net" target="_blank">Claroline</a> &copy; 2001 - 2006
-</div>
+//FOOTER RIGHT DOCK declaration
 
-</div>
+$footerRightDock = new Dock('campusFooterRight');
+$appletList = getAppletList($footerRightDock);
+$footerRightDock->setAppletList($appletList);
 
-<?php
+$platformManagerOutput = '<div id="platformManager">'.sprintf(get_lang('_p_platformManager'),$siteName). ' : 
+<a href="mailto:' . $administrator_email."?body=".$_course['officialCode']."&amp;subject=[".rawurlencode($siteName)."]".'">'. $administrator_name .'</a>
+</div>';
+
+$footerRightDock->addOutput($platformManagerOutput,true);
+
+echo $footerRightDock->render();
+
+
+//FOOTER CENTER DOCK declaration
+
+$footerCenterDock = new Dock('campusFooterCenter');
+$appletList = getAppletList($footerCenterDock);
+$footerCenterDock->setAppletList($appletList);
+
+$poweredByOutput = '<div id="poweredBy">'. get_lang('Powered by') . ' <a href="http://www.claroline.net" target="_blank">Claroline</a> &copy; 2001 - 2006 </div></div>';
+
+
+$footerCenterDock->addOutput($poweredByOutput,true);
+
+echo $footerCenterDock->render();
+
+
+
 } // if (!isset($hide_footer) || $hide_footer == false)
 ?>
 </body>
