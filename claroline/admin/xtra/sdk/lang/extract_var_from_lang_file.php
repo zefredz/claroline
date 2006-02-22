c2bcffae80bc8f84ce0fce6d24a1471e4c963cef
moosh <moosh@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2006-02-22 12:56:23 +0000

    - I10n translation Tools like in upper menu

--- claroline/admin/xtra/sdk/lang/extract_var_from_lang_file.php
+++ claroline/admin/xtra/sdk/lang/extract_var_from_lang_file.php
@@ -44,7 +44,7 @@ $urlSDK = $rootAdminWeb . 'xtra/sdk/';
 $urlTranslation = $urlSDK . 'translation_index.php';
 $interbredcrump[] = array ("url"=>$rootAdminWeb, "name"=> get_lang('Administration'));
 $interbredcrump[] = array ("url"=>$urlSDK, "name"=> get_lang('SDK'));
-$interbredcrump[] = array ("url"=>$urlTranslation, "name"=> get_lang('TranslationTools'));
+$interbredcrump[] = array ("url"=>$urlTranslation, "name"=> get_lang('Translation Tools'));
 
 include($includePath."/claro_init_header.inc.php");
 
