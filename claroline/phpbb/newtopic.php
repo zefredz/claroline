6a7c32d7e210f3d2a596a23bbfb05fe75da8dfeb
moosh <moosh@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2006-03-13 13:04:59 +0000

    - cvs Tag

--- claroline/phpbb/newtopic.php
+++ claroline/phpbb/newtopic.php
@@ -1,4 +1,4 @@
-<?php   // Id: $
+<?php // $Id$
 /**
  * CLAROLINE
  *
@@ -6,7 +6,7 @@
  *
  * @version 1.8 $Revision$
  *
- * @copyright 2001-2006 Universite catholique de Louvain (UCL) 
+ * @copyright 2001-2006 Universite catholique de Louvain (UCL)
  * @copyright (C) 2001 The phpBB Group
  *
  * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
@@ -61,7 +61,7 @@ if ( isset( $_REQUEST['cancel'] ) )
     header('Location: viewforum.php?forum='.$forum);
     exit();
 }
- 
+
 if ( isset($_REQUEST['subject']) ) $subject = $_REQUEST['subject'];
 else                               $subject = '';
 
@@ -70,10 +70,10 @@ else                               $message = '';
 
 $forumSettingList = get_forum_settings($forum_id);
 
-$is_allowedToEdit = claro_is_allowed_to_edit() 
+$is_allowedToEdit = claro_is_allowed_to_edit()
                     || ( $is_groupTutor && !$is_courseAdmin);
-                    // ( $is_groupTutor 
-                    //  is added to give admin status to tutor 
+                    // ( $is_groupTutor
+                    //  is added to give admin status to tutor
                     // && !$is_courseAdmin)
                     // is added  to let course admin, tutor of current group, use student mode
 
@@ -88,19 +88,19 @@ elseif ( $forumSettingList )
     $forum_type         = $forumSettingList['forum_type'  ];
     $forum_groupId      = $forumSettingList['idGroup'     ];
     $forum_cat_id       = $forumSettingList['cat_id'      ];
-    
-    /* 
-     * Check if the topic isn't attached to a group,  or -- if it is attached --, 
+
+    /*
+     * Check if the topic isn't attached to a group,  or -- if it is attached --,
      * check the user is allowed to see the current group forum.
      */
-    
+
     if ( ! $forum_post_allowed
-        || (    ! is_null($forumSettingList['idGroup']) 
+        || (    ! is_null($forumSettingList['idGroup'])
             && ( $forumSettingList['idGroup'] != $_gid || ! $is_groupAllowed) ) )
     {
-        // NOTE : $forumSettingList['idGroup'] != $_gid is necessary to prevent any hacking 
-        // attempt like rewriting the request without $cidReq. If we are in group 
-        // forum and the group of the concerned forum isn't the same as the session 
+        // NOTE : $forumSettingList['idGroup'] != $_gid is necessary to prevent any hacking
+        // attempt like rewriting the request without $cidReq. If we are in group
+        // forum and the group of the concerned forum isn't the same as the session
         // one, something weird is happening, indeed ...
         $allowed = FALSE;
         $error_message = get_lang('Not allowed') ;
@@ -111,33 +111,33 @@ elseif ( $forumSettingList )
         {
             // Either valid user/pass, or valid session. continue with post.. but first:
             // Check that, if this is a private forum, the current user can post here.
-        
+
             /*------------------------------------------------------------------------
                                         PREPARE THE DATA
               ------------------------------------------------------------------------*/
-        
+
             // SUBJECT
             $subject = trim($subject);
-        
+
             // MESSAGE
             if ( get_conf('allow_html') == 0 || isset($html) ) $message = htmlspecialchars($message);
             $message = trim($message);
-        
+
             // USER
             $userLastname  = $_user['lastName'];
             $userFirstname = $_user['firstName'];
             $poster_ip     = $_SERVER['REMOTE_ADDR'];
-        
+
             $time = date('Y-m-d H:i');
-            
+
             // prevent to go further if the fields are actually empty
-            if ( strip_tags($message) == '' || $subject == '' ) 
+            if ( strip_tags($message) == '' || $subject == '' )
             {
                 $error_message = get_lang('You cannot post an empty message');
                 $error = TRUE;
             }
-        
-            if ( !$error ) 
+
+            if ( !$error )
             {
                 // record new topic
                 $topic_id = create_new_topic($subject, $time, $forum_id, $_uid, $userFirstname, $userLastname);
@@ -146,11 +146,11 @@ elseif ( $forumSettingList )
                     create_new_post($topic_id, $forum_id, $_uid, $time, $poster_ip, $userLastname, $userFirstname, $message);
                 }
                 // notify eventmanager that a new message has been posted
-        
-                $eventNotifier->notifyCourseEvent("forum_new_topic",$_cid, $_tid, $forum_id."-".$topic_id, $_gid, "0");
-     
+
+                $eventNotifier->notifyCourseEvent('forum_new_topic',$_cid, $_tid, $forum_id."-".$topic_id, $_gid, 0);
+
             }
-        
+
         } // end if submit
     }
 }
@@ -164,7 +164,7 @@ else
 /*=================================================================
   Display Section
  =================================================================*/
- 
+
 if (   isset($forum_cat_id) && $forum_cat_id == GROUP_FORUMS_CATEGORY
     && $is_groupAllowed )
 {
@@ -193,8 +193,8 @@ else
     {
         // Display success message
         disp_confirmation_message (get_lang('Your message has been entered'), $forum_id, $topic_id);
-    
-    } 
+
+    }
     else
     {
         if ( $error )
@@ -212,20 +212,20 @@ else
 
          . '<table border="0">' . "\n"
          . '<tr valign="top">' . "\n"
-         . '<td align="right"><label for="subject">' . get_lang('Subject') . '</label> : </td>' 
+         . '<td align="right"><label for="subject">' . get_lang('Subject') . '</label> : </td>'
          . '<td><input type="text" name="subject" id="subject" size="50" maxlength="100" value="' . htmlspecialchars($subject) . '" /></td>'
-         . '<tr  valign="top">' . "\n" 
-         . '<td align="right"><br />' . get_lang('Message body') . ' :</td>'; 
+         . '<tr  valign="top">' . "\n"
+         . '<td align="right"><br />' . get_lang('Message body') . ' :</td>';
 
         if ( !empty($message) ) $content = htmlspecialchars($message);
         else                    $content = '';
-        
+
         echo '<td>'
             .claro_html::textarea_editor('message',$content)
             .'</td>'
             . '</tr>'
             . '<tr  valign="top"><td>&nbsp;</td>'
-            . '<td><input type="submit" name="submit" value="' . get_lang('Ok') . '" />' 
+            . '<td><input type="submit" name="submit" value="' . get_lang('Ok') . '" />'
             . '&nbsp;<input type="submit" name="cancel" value="' . get_lang('Cancel') . '" />' . "\n"
             . '</td></tr>'
             . '</table>'
