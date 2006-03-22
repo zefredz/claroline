d62784870f00a66a75655aff03103d1dc100ab09
seb <seb@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2006-03-22 10:44:16 +0000

    Rollback "redirector.submit()" change, it does not handle the frame of chat or learning path correctly...

--- claroline/inc/claro_init_banner.inc.php
+++ claroline/inc/claro_init_banner.inc.php
@@ -173,7 +173,7 @@ if (is_array($_courseToolList) && $is_courseAllowed)
     $courseToolSelector = '<form action="'.$clarolineRepositoryWeb.'redirector.php" 
       name="redirector" method="POST">
     <select name="url" size="1" 
-        onchange="redirector.submit()" >';
+        onchange="top.location=redirector.url.options[selectedIndex].value" >';
 
     $courseToolSelector .= '<option value="' . $clarolineRepositoryWeb . 'course/index.php?cid=' . htmlspecialchars($_cid) .'" style="padding-left:22px;background:url('.$imgRepositoryWeb.'course.gif) no-repeat">' . get_lang('Course Home') . '</option>' . "\n";
 
