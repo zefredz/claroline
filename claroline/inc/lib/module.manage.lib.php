3e66cdad63bcec936097d696661cc2bf7a68f6aa
moosh <moosh@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2006-04-28 07:43:37 +0000

    get feture  onf module.inc.php in admin/module (before move function)

---
+++ claroline/inc/lib/module.manage.lib.php
@@ -0,0 +1,22 @@
+<?php // $Id$
+/**
+ * CLAROLINE
+ *
+ * manage module of the system
+ *
+ * @version 1.8 $Revision$
+ *
+ * @copyright 2001-2006 Universite catholique de Louvain (UCL)
+ *
+ * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
+ *
+ * @see http://www.claroline.net/wiki/index.php/Install
+ *
+ * @author Claro Team <cvs@claroline.net>
+ *
+ * @package MODULES
+ *
+ */
+ include_once(realpath(dirname(__FILE__) . '/../../admin/module') . '/module.inc.php');
+
+?>
