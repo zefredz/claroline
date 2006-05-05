ffc132310d67a681a45730be934ab88b479e4c04
moosh <moosh@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2006-05-05 13:48:09 +0000

    - remove last  global

--- claroline/work/lib/ical.write.lib.php
+++ claroline/work/lib/ical.write.lib.php
@@ -15,78 +15,67 @@
 
 function CLWRK_write_ical( & $iCal, $context)
 {
-    global $_course;
     if (is_array($context) && count($context)>0)
     {
-        if (in_array('course',$context))
+        $courseCode = (in_array('course',$context)) ? $context['course'] : $courseCode = $GLOBALS['_cid'];
+
+        $courseData = claro_get_course_data($courseCode);
+        $toolNameList = claro_get_tool_name_list();
+        $assignmentList = assignmentList();
+        $organizer = (array) array($courseData['titular'], $courseData['email']);
+        $attendees = array();
+        $categories = array( get_conf('siteName'),
+        $courseData['officialCode'],
+        trim($toolNameList[str_pad('CLWRK',8,'_')]),
+        $courseData['categoryCode']
+        );
+
+        $iCal = (object) new iCal('', 0, 'S:/cvs.claroline.net/clarolinedev/claroline.rss/'); // (ProgrammID, Method (1 = Publish | 0 = Request), Download Directory)
+        foreach ($assignmentList as $thisAssignment)
         {
-            $courseCode = $context['course'];
-        }
-        else
-        {
-            $courseCode = $GLOBALS['_cid'];
-        }
-    }
+            if('VISIBLE' == $thisAssignment['visibility'])
+            {
 
-    $toolNameList = claro_get_tool_name_list();
-    $assignmentList = assignmentList();
-    $organizer = (array) array($_course['titular'], $_course['email']);
-    $attendees = array();
-    $categories = array( get_conf('siteName'),
-    $_course['officialCode'],
-    trim($toolNameList[str_pad('CLWRK',8,'_')]),
-    $_course['categoryCode']
-    );
+                $categories[] = $thisAssignment['assignment_type'];
 
-    $iCal = (object) new iCal('', 0, 'S:/cvs.claroline.net/clarolinedev/claroline.rss/'); // (ProgrammID, Method (1 = Publish | 0 = Request), Download Directory)
-    foreach ($assignmentList as $thisAssignment)
-    {
-        if('VISIBLE' == $thisAssignment['visibility'])
-        {
-
-            $categories[] = $thisAssignment['assignment_type'];
-
-            $iCal->addToDo(
-            $thisAssignment['title'], // Title
-            $thisAssignment['description'], // Description
-            '', // Location
-            (int) $thisAssignment['start_date_unix'], // Start time
-            3600, //(($thisAssignment['end_date_unix']-$thisAssignment['start_date_unix'])/60), // Duration in minutes
-            (int) $thisAssignment['end_date_unix'], // End time
-            1, // Percentage complete
-            5, // Priority = 0-9
-            1, // Status of the event (0 = TENTATIVE, 1 = CONFIRMED, 2 = CANCELLED)
-            1, // Class (0 = PRIVATE | 1 = PUBLIC | 2 = CONFIDENTIAL)
-            $organizer, // Organizer
-            $attendees, // Array (key = attendee name, value = e-mail, second value = role of the attendee [0 = CHAIR | 1 = REQ | 2 = OPT | 3 =NON])
-            $categories, // Array with Strings
-            time(), // Last Modification
-            0, // Sets the time in minutes an alarm appears before the event in the programm. no alarm if empty string or 0
-            0, // frequency: 0 = once, secoundly - yearly = 1-7
-            0, // recurrency end: ('' = forever | integer = number of times | timestring = explicit date)
-            0, // Interval for frequency (every 2,3,4 weeks...)
-            array(), // Array with the number of the days the event accures (example: array(0,1,5) = Sunday, Monday, Friday
-            1, // Startday of the Week ( 0 = Sunday - 6 = Saturday)
-            '', // exeption dates: Array with timestamps of dates that should not be includes in the recurring event
-            get_conf('clarolineRepositoryWeb') . 'work/workList.php?cidReq=' . $courseCode.'&amp;assigId=' . $thisAssignment['id'], // optional URL for that event
-            get_conf('iso639_1_code'), // Language of the Strings
-            '' // Optional UID for this ToDo
-            );
-
-
-
+                $iCal->addToDo(
+                $thisAssignment['title'], // Title
+                $thisAssignment['description'], // Description
+                '', // Location
+                (int) $thisAssignment['start_date_unix'], // Start time
+                3600, //(($thisAssignment['end_date_unix']-$thisAssignment['start_date_unix'])/60), // Duration in minutes
+                (int) $thisAssignment['end_date_unix'], // End time
+                1, // Percentage complete
+                5, // Priority = 0-9
+                1, // Status of the event (0 = TENTATIVE, 1 = CONFIRMED, 2 = CANCELLED)
+                1, // Class (0 = PRIVATE | 1 = PUBLIC | 2 = CONFIDENTIAL)
+                $organizer, // Organizer
+                $attendees, // Array (key = attendee name, value = e-mail, second value = role of the attendee [0 = CHAIR | 1 = REQ | 2 = OPT | 3 =NON])
+                $categories, // Array with Strings
+                time(), // Last Modification
+                0, // Sets the time in minutes an alarm appears before the event in the programm. no alarm if empty string or 0
+                0, // frequency: 0 = once, secoundly - yearly = 1-7
+                0, // recurrency end: ('' = forever | integer = number of times | timestring = explicit date)
+                0, // Interval for frequency (every 2,3,4 weeks...)
+                array(), // Array with the number of the days the event accures (example: array(0,1,5) = Sunday, Monday, Friday
+                1, // Startday of the Week ( 0 = Sunday - 6 = Saturday)
+                '', // exeption dates: Array with timestamps of dates that should not be includes in the recurring event
+                get_conf('clarolineRepositoryWeb') . 'work/workList.php?cidReq=' . $courseCode.'&amp;assigId=' . $thisAssignment['id'], // optional URL for that event
+                get_conf('iso639_1_code'), // Language of the Strings
+                '' // Optional UID for this ToDo
+                );
+            }
         }
     }
-
     return $iCal;
 }
 
 function assignmentList()
 {
-        $tbl_cdb_names = claro_sql_get_course_tbl();
-        $tbl_wrk_assignment = $tbl_cdb_names['wrk_assignment'];
+    $tbl_cdb_names = claro_sql_get_course_tbl();
+    $tbl_wrk_assignment = $tbl_cdb_names['wrk_assignment'];
 
-        $sql = "SELECT `id`,
+    $sql = "SELECT `id`,
         				`title`,
         				`description`,
         				`def_submission_visibility`,
@@ -96,7 +85,7 @@ function assignmentList()
         				unix_timestamp(`end_date`) as `end_date_unix`
             	FROM `" . $tbl_wrk_assignment . "`";
 
-        return claro_sql_query_fetch_all_rows($sql);
+    return claro_sql_query_fetch_all_rows($sql);
 }
 
 ?>
