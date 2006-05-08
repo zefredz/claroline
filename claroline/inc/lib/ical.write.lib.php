08deff1b05b767bcec3bc787fb45c7022966e666
moosh <moosh@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2006-05-08 08:28:15 +0000

    - add function ical_get_tool_compatible_list / This function  use  cache lite - load fileManage.lib.php only if needed - generate also xml and rdf version of iCal

--- claroline/inc/lib/ical.write.lib.php
+++ claroline/inc/lib/ical.write.lib.php
@@ -14,43 +14,157 @@
  */
 
 
+/**
+ * This lib use
+ * * cache lite
+ * * icalendar/class.iCal.inc.php
+ *
+ */
+
 require_once dirname(__FILE__) . '/icalendar/class.iCal.inc.php';
-require_once dirname(__FILE__) . '/fileManage.lib.php';
 
-
-function buildICal($context)
+function buildICal($context, $calType='ics')
 {
-    if (is_array($context) && count($context)>0)
+    if (is_array($context) && count($context) > 0)
     {
         $iCalRepositorySys =  get_conf('rootSys') . get_conf('iCalRepository','iCal/');
-        if (file_exists($iCalRepositorySys) || claro_mkdir($iCalRepositorySys, CLARO_FILE_PERMISSIONS, true))
+
+
+        if (!file_exists($iCalRepositorySys))
         {
-            $iCal = (object) new iCal('', 0, $iCalRepositorySys ); // (ProgrammID, Method (1 = Publish | 0 = Request), Download Directory)
-
-            $toolLabelList = array('CLCAL', 'CLANN', 'CLWRK');
-            foreach ($toolLabelList as $toolLabel)
-            {
-                $icalToolLibPath = get_module_path($toolLabel) . '/lib/ical.write.lib.php';
-                $icalToolFuncName =  $toolLabel . '_write_ical';
-                if ( file_exists($icalToolLibPath)
-                )
-                {
-                    include_once $icalToolLibPath;
-                    if (function_exists($icalToolFuncName)) $iCal = call_user_func($icalToolFuncName, $iCal,$context );
-                }
-            }
-
-
-            $iCalFilePath = $iCalRepositorySys . '/' ;
-            if (array_key_exists('course',$context)) $iCalFilePath .= $context['course'] . '.';
-            if (array_key_exists('group',$context)) $iCalFilePath .= 'g'.$context['group'] . '.';
-            $iCalFilePath .= 'ics';
-            $fpICal = fopen($iCalFilePath, 'w');
-            fwrite($fpICal, $iCal->getOutput('ics'));
-            return $iCalFilePath;
-
+            require_once dirname(__FILE__) . '/fileManage.lib.php';
+            claro_mkdir($iCalRepositorySys, CLARO_FILE_PERMISSIONS, true);
         }
+
+        $iCal = (object) new iCal('', 0, $iCalRepositorySys ); // (ProgrammID, Method (1 = Publish | 0 = Request), Download Directory)
+
+        $toolLabelList = ical_get_tool_compatible_list();
+        foreach ($toolLabelList as $toolLabel)
+        {
+            $icalToolLibPath = get_module_path($toolLabel) . '/lib/ical.write.lib.php';
+            $icalToolFuncName =  $toolLabel . '_write_ical';
+            if ( file_exists($icalToolLibPath)
+            )
+            {
+                include_once $icalToolLibPath;
+                if (function_exists($icalToolFuncName)) $iCal = call_user_func($icalToolFuncName, $iCal, $context );
+            }
+        }
+
+
+        $iCalFilePath = $iCalRepositorySys . '/' ;
+        if (array_key_exists('course',$context)) $iCalFilePath .= $context['course'] . '.';
+        if (array_key_exists('group',$context)) $iCalFilePath .= 'g'.$context['group'] . '.';
+
+
+        if (get_conf('iCalGenStandard', true))
+        {
+            $stdICalFilePath = $iCalFilePath . 'ics';
+            $fpICal = fopen($stdICalFilePath, 'w');
+            fwrite($fpICal, $iCal->getOutput('ics'));
+        }
+
+        if (get_conf('iCalGenXml', true))
+        {
+            $xmlICalFilePath = $iCalFilePath . 'xml';
+            $fpICal = fopen($xmlICalFilePath, 'w');
+            fwrite($fpICal, $iCal->getOutput('xcs'));
+        }
+
+        if (get_conf('iCalGenRdf', false))
+        {
+            $rdfICalFilePath = $iCalFilePath . 'rdf';
+            $fpICal = fopen($rdfICalFilePath, 'w');
+            fwrite($fpICal, $iCal->getOutput('rdf'));
+        }
+
+
+        switch ($calType)
+        {
+            case 'xcs' :
+                $iCalFilePath .= 'xml';
+                return $iCalFilePath;
+                break;
+            case 'rdf' :
+                $iCalFilePath .= 'rss';
+                return $iCalFilePath;
+                break;
+            default :
+                $iCalFilePath .= 'ics';
+                return $iCalFilePath;
+                break;
+        }
+
+
     }
     return false;
+}
+
+
+/**
+ * Build the list of claro label of tool having a iCal creator.
+ *
+ * @return array of claro_label
+ *
+ * This function use 2 level of cache.
+ * - memory Cache to compute only one time the list by script execution
+ * - if enabled : use cache lite
+ */
+function ical_get_tool_compatible_list()
+{
+    static $iCalToolList = null;
+    if (is_null($iCalToolList))
+    {
+        if(get_conf('ical_use_cache',true))
+        {
+            include_once dirname(__FILE__) . '/pear/Lite.php';
+
+            // Cache_lite setting & init
+            $cache_options = array(
+            'cacheDir' => get_conf('rootSys') . 'cache/ical/',
+            'lifeTime' => get_conf('cache_lifeTime', get_conf('iCalCacheLifeTime'), 600000), // 600.000 little less than a week
+            'automaticCleaningFactor' => 500,
+            );
+            if (get_conf('CLARO_DEBUG_MODE',false) ) $cache_options['pearErrorMode'] = CACHE_LITE_ERROR_DIE;
+            if (! file_exists($cache_options['cacheDir']) )
+            {
+                include_once dirname(__FILE__) . '/fileManage.lib.php';
+                claro_mkdir($cache_options['cacheDir'],CLARO_FILE_PERMISSIONS,true);
+            }
+
+            $iCalToolListCache = new Cache_Lite($cache_options);
+
+            if (false === $iCalToolListSerialized = $iCalToolListCache->get('iCal'))
+            {
+                $toolList = $GLOBALS['_courseToolList'];
+                foreach ($toolList as $tool)
+                {
+                    $toolLabel = trim($tool['label'],'_');
+                    $icalToolLibPath = get_module_path($toolLabel) . '/lib/ical.write.lib.php';
+                    $icalToolFuncName =  $toolLabel . '_write_ical';
+                    if ( file_exists($icalToolLibPath)
+                    )
+                    {
+                        include_once $icalToolLibPath;
+                        if (function_exists($icalToolFuncName))
+                        {
+                            $iCalToolList[] = $toolLabel;
+                        }
+                    }
+
+                }
+                $iCalToolListSerialized = serialize($iCalToolList);
+                $iCalToolListCache->save($iCalToolListSerialized, 'iCal');
+            }
+            else
+            $iCalToolList = unserialize($iCalToolListSerialized);
+
+        }
+        else
+        $iCalToolList = array();
+
+    } // if is_null $iCalToolList -> if not use static
+
+    return $iCalToolList;
 }
 ?>
