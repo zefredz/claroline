3eef63ff7950eedb2aed88599a0b1ecfb95f9911
moosh <moosh@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2006-05-12 14:01:02 +0000

    - move group settings conf to group menu

--- claroline/inc/conf/def/CLGRP.def.conf.inc.php
+++ claroline/inc/conf/def/CLGRP.def.conf.inc.php
@@ -1,6 +1,6 @@
 <?php // $Id$
 /**
- * CLAROLINE 
+ * CLAROLINE
  *
  * This file describe the parameter for edit setting  of groups in a course
  *
@@ -22,24 +22,24 @@
 
 $conf_def['config_code']='CLGRP';
 $conf_def['config_file']='CLGRP.conf.php';
-$conf_def['config_name']='Groups tool';
-$conf_def['config_class']='tool';
+$conf_def['config_name']='Groups permissions';
+$conf_def['config_class']='Groups';
 
 
 $conf_def['section']['users']['label']='Users';
 $conf_def['section']['users']['description']='Settings for users of group';
-$conf_def['section']['users']['properties'] = 
+$conf_def['section']['users']['properties'] =
 array ( 'multiGroupAllowed'
       );
 
 
 $conf_def['section']['tutors']['label']='Tutors';
 $conf_def['section']['tutors']['description']='Settings for tutors of group';
-$conf_def['section']['tutors']['properties'] = 
+$conf_def['section']['tutors']['properties'] =
 array ( 'tutorCanBeSimpleMemberOfOthersGroupsAsStudent'
       , 'showTutorsInGroupList'
       );
-      
+
 //PROPERTIES
 $conf_def_property_list['multiGroupAllowed'] =
 array ( 'description' => 'Allow teacher to subscribe users in several groups'
