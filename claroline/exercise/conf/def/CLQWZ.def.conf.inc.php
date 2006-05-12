b9396030385652b0a7f229614c885ed04d226542
moosh <moosh@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2006-05-12 21:05:26 +0000

    - move def file to tool directory

---
+++ claroline/exercise/conf/def/CLQWZ.def.conf.inc.php
@@ -0,0 +1,46 @@
+<?php // $Id$
+/**
+ * CLAROLINE 
+ *
+ * This file describe the parameter for user tool
+ *
+ * @version 1.8 $Revision$
+ *
+ * @copyright 2001-2006 Universite catholique de Louvain (UCL)
+ *
+ * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
+ *
+ * @see http://www.claroline.net/wiki/index.php/Config
+ *
+ * @author Claro Team <cvs@claroline.net>
+ *
+ * @package CLUSR
+ *
+ */
+// TOOL
+$conf_def['config_code'] = 'CLQWZ';
+$conf_def['config_file'] = 'CLQWZ.conf.php';
+$conf_def['config_name'] = 'Exercise tool';
+$conf_def['config_class']='tool';
+
+
+//SECTION
+$conf_def['section']['main']['label']='Main settings';
+//$conf_def['section']['main']['description']='';
+$conf_def['section']['main']['properties'] = 
+array ( 'enableExerciseExportQTI'
+);
+
+//PROPERTIES
+
+$conf_def_property_list['enableExerciseExportQTI'] =
+array ('label'         => 'Enable IMS-QTI Export'
+      ,'description'   => ''
+      ,'default'       => 'TRUE'
+      ,'type'          => 'boolean'
+      ,'acceptedValue' => array ('TRUE'  => 'Yes'
+                                ,'FALSE' => 'No'
+                                )
+      );
+
+?>
---
+++ claroline/wiki/conf/def/CLWIKI.def.conf.inc.php
@@ -0,0 +1,57 @@
+<?php // $Id$
+/**
+ * CLAROLINE 
+ *
+ * This file describe the parameter for user tool
+ *
+ * @version 1.8 $Revision$
+ *
+ * @copyright 2001-2006 Universite catholique de Louvain (UCL)
+ *
+ * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
+ *
+ * @see http://www.claroline.net/wiki/index.php/Config
+ *
+ * @author Claro Team <cvs@claroline.net>
+ *
+ * @package CLUSR
+ *
+ */
+// TOOL
+$conf_def['config_code'] = 'CLWIKI';
+$conf_def['config_file'] = 'CLWIKI.conf.php';
+$conf_def['config_name'] = 'Wiki tool';
+$conf_def['config_class']='tool';
+
+
+//SECTION
+$conf_def['section']['main']['label']='Main settings';
+//$conf_def['section']['main']['description']='Settings of the tool';
+$conf_def['section']['main']['properties'] = 
+array ( 'showWikiEditorToolbar'
+    , 'forcePreviewBeforeSaving'
+      );
+
+//PROPERTIES
+
+$conf_def_property_list['showWikiEditorToolbar'] =
+array ('label'         => 'Show wiki syntax toolbar in wiki editor'
+ //     ,'description'   => '...'
+      ,'default'       => 'TRUE'
+      ,'type'          => 'boolean'
+      ,'acceptedValue' => array ('TRUE'  => 'Yes'
+                                ,'FALSE' => 'No'
+                                )
+      );
+
+$conf_def_property_list['forcePreviewBeforeSaving'] =
+array ('label'         => 'Force preview before saving in wiki editor'
+    //  ,'description'   => '...'
+      ,'default'       => 'FALSE'
+      ,'type'          => 'boolean'
+      ,'acceptedValue' => array ('TRUE'  => 'Yes'
+                                ,'FALSE' => 'No'
+                                )
+      );
+
+?>
