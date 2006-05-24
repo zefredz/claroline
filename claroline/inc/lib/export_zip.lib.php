b9d1664393d1362a2164d5b252e2cea5007b2606
moosh <moosh@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2006-05-24 22:50:17 +0000

    - remove unusefull code

--- claroline/inc/lib/export_zip.lib.php
+++ claroline/inc/lib/export_zip.lib.php
@@ -17,37 +17,37 @@
 require_once($includePath.'\lib\pclzip\pclzip.lib.php');
 require_once($includePath.'\lib\claro_main.lib.php');
 /**
- * 		
+ *
  * compress the directory in the same path with the name $directory.zip
- * 
- * @author Yannick Wautelet <yannick_wautelet@hotmail.com> 
- * @param  string  $directory       
- * @return false if a problem occured, true if not.  
- */     	
+ *
+ * @author Yannick Wautelet <yannick_wautelet@hotmail.com>
+ * @param  string  $directory
+ * @return false if a problem occured, true if not.
+ */
 function compress_directory($directory)
-{	
-	if(!file_exists($directory)) return claro_failure::set_failure("dir doesn't exist");
-	else if(!is_dir($directory)) return claro_failure::set_failure("is not a directory");		
-	$archive = new PclZip($directory."../".basename($directory).".zip");	
-	$archive->create($directory,PCLZIP_OPT_REMOVE_PATH, $directory) == 0;
-	
+{
+    if(!file_exists($directory)) return claro_failure::set_failure("dir doesn't exist");
+	else if(!is_dir($directory)) return claro_failure::set_failure("is not a directory");
+	$archive = new PclZip($directory."../".basename($directory).".zip");
+	$archive->create($directory,PCLZIP_OPT_REMOVE_PATH, $directory);
+
 	return true;
 }
 /**
- * 		
+ *
  * extract the $archiveName into the $path directory with the basename of $archiveName
- * 
- * @author Yannick Wautelet <yannick_wautelet@hotmail.com> 
+ *
+ * @author Yannick Wautelet <yannick_wautelet@hotmail.com>
  * @param  string  $archiveName     - path and file of origin
  * @param  string  $path     		- path of destination
- * @return false if a problem occured, true if not.  
- */ 
+ * @return false if a problem occured, true if not.
+ */
 function extract_archive($archiveName,$path)
 {
-	if(!file_exists($archiveName)) return claro_failure::set_failure("archive doesn't exist");   	
+	if(!file_exists($archiveName)) return claro_failure::set_failure("archive doesn't exist");
 	if(!($archive = new PclZip($archiveName))) return claro_failure::set_failure("opendir failed");
-				
-	if(false === ($archive->extract(PCLZIP_OPT_ADD_PATH,$path."/". basename($archiveName,'.zip')))) 
+
+	if(false === ($archive->extract(PCLZIP_OPT_ADD_PATH,$path."/". basename($archiveName,'.zip'))))
 			return claro_failure::set_failure("couldn't extract file");
 
 	return true;
