4553a5b25fa9d3dc344edc83b703e4e324353f73
Frédéric Minne <zefredz@users.noreply.github.com>
2006-05-28 09:30:53 +0000

    phpdoc

--- claroline/wiki/lib/class.wiki2xhtmlexport.php
+++ claroline/wiki/lib/class.wiki2xhtmlexport.php
@@ -2,6 +2,21 @@
 
     // vim: expandtab sw=4 ts=4 sts=4:
     
+    /**
+     * CLAROLINE
+     *
+     * @version 1.8 $Revision$
+     *
+     * @copyright 2001-2006 Universite catholique de Louvain (UCL)
+     *
+     * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
+     * This program is under the terms of the GENERAL PUBLIC LICENSE (GPL)
+     * as published by the FREE SOFTWARE FOUNDATION. The GPL is available
+     * through the world-wide-web at http://www.gnu.org/copyleft/gpl.html
+     *
+     * @author Frederic Minne <zefredz@gmail.com>
+     */
+    
     require_once dirname(__FILE__) . '/class.wiki2xhtmlrenderer.php';
     require_once dirname(__FILE__) . '/class.wikistore.php';
     require_once dirname(__FILE__) . '/class.wikipage.php';
