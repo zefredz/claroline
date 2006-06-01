ab9fecb98921f30d2e519bc6ed6cd84e314ed1bd
Frédéric Minne <zefredz@users.noreply.github.com>
2006-06-01 12:34:54 +0000

    missing '/' in manifest path

--- claroline/admin/module/module.inc.php
+++ claroline/admin/module/module.inc.php
@@ -1048,7 +1048,7 @@ function readModuleManifest($modulePath)
     // Find XML manifest and parse it to retrieve module informations
 
     //check if manifest is present
-    $manifestPath = $modulePath. 'manifest.xml';
+    $manifestPath = $modulePath. '/manifest.xml';
     if (! check_name_exist($manifestPath))
     {
         return claro_failure::set_failure(get_lang('Manifest missing : %filename',array('%filename' => $manifestPath)));
