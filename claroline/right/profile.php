aff38ba9f91fc355cf33f50036b1461131849fc2
mathieu <mathieu@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2006-06-16 09:23:49 +0000

    manage right, add courses scripts

---
+++ claroline/right/profile.php
@@ -0,0 +1,124 @@
+<?php // $Id$
+/**
+ * CLAROLINE
+ *
+ * This tool manage profile of the course
+ *
+ * @version 1.8 $Revision$
+ * @copyright (c) 2001-2006 Universite catholique de Louvain (UCL)
+ *
+ * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
+ *
+ * @author claroline Team <cvs@claroline.net>
+ *
+ * @package RIGHT
+ *
+ */
+
+require '../inc/claro_init_global.inc.php';
+
+$nameTools = get_lang('Course profile');
+$dialogBox = '';
+$tidReset = true;
+
+if ( ! $_cid || ! $_uid) claro_disp_auth_form(true);
+
+$is_allowedToEdit = $is_courseAdmin;
+
+if ( ! $is_allowedToEdit )
+{
+    claro_die(get_lang('Not allowed'));
+}
+
+require_once $includePath . '/lib/right/courseProfileToolAction.class.php' ;
+require_once $includePath . '/lib/right/profileToolRightHtml.class.php' ;
+require_once $includePath . '/lib/pager.lib.php';
+
+// Main section
+
+$cmd = isset($_REQUEST['cmd'])?$_REQUEST['cmd']:null;
+$display = isset($_REQUEST['display'])?$_REQUEST['display']:null;
+$profile_id = isset($_REQUEST['profile_id'])?$_REQUEST['profile_id']:null;
+$tool_id = isset($_REQUEST['tool_id'])?$_REQUEST['tool_id']:null;
+$right_value = isset($_REQUEST['right_value'])?$_REQUEST['right_value']:null;
+
+if ( !empty($profile_id) )
+{
+    // load profile
+    $profile = new RightProfile();
+
+    if ( $profile->load($profile_id) )
+    {
+        // load profile tool right    
+        $courseProfileRight = new RightCourseProfileToolRight();
+        $courseProfileRight->setCourseId($_cid);
+        $courseProfileRight->load($profile);
+
+/*
+        $class_methods = get_class_methods('RightCourseProfileToolRight');
+
+        foreach ($class_methods as $method_name) {
+            echo "$method_name\n";
+        }
+*/
+
+        if ( $cmd == 'set_right' && !empty($tool_id) )
+        {        
+            $courseProfileRight->setToolRight($tool_id,$right_value);
+            $courseProfileRight->save();
+        }
+    }
+    else
+    {
+        $profile_id = null;
+    }
+}
+
+// Display section
+
+// define bredcrumb
+$interbredcrump[] = array ('url' => 'profile_list.php', 'name' => get_lang('Course Profile List'));
+
+if ( !empty($profile_id) )
+{
+      $nameTools = get_lang('Course profile : %name',array('%name' => $profile->getName() ));
+//    $interbredcrump[] = array ('url' => 'profile.php?profile_id=' . $profile->getId(), 'name' => $profile->getName() );
+}
+
+// Display header
+
+include $includePath . '/claro_init_header.inc.php';
+
+if ( !empty($profile_id) )
+{
+    // display tool title
+    echo claro_html_tool_title(array('mainTitle'=>get_lang('Course Profile'),'subTitle'=>$profile->getName()));
+
+    echo '<p><a href="' . $_SERVER['PHP_SELF'] . '?profile_id=' . $profile->getId() . '&amp;display=view">View</a>' 
+    .    ' - <a href="' . $_SERVER['PHP_SELF'] . '?profile_id=' . $profile->getId() . '&amp;display=edit">Edit</a></p>'; 
+
+    $profileRightHtml = new RightProfileToolRightHtml($courseProfileRight);
+
+    $profileRightHtml->addUrlParam('display',$display);
+
+    if ( $display == 'edit' )
+    {
+        $profileRightHtml->setDisplayMode('edit');
+        echo $profileRightHtml->displayProfileToolRightList();
+    }
+    else
+    {
+        $profileRightHtml->setDisplayMode('view');
+        echo $profileRightHtml->displayProfileToolRightList();
+    }
+}
+else
+{
+    echo claro_html_message_box(get_lang('Profile not found'));
+}
+
+// Display footer
+
+include $includePath . '/claro_init_footer.inc.php';
+
+?>
---
+++ claroline/right/profile_list.php
@@ -0,0 +1,93 @@
+<?php // $Id$
+/**
+ * CLAROLINE
+ *
+ * This tool manage profile of the course
+ *
+ * @version 1.8 $Revision$
+ * @copyright (c) 2001-2006 Universite catholique de Louvain (UCL)
+ *
+ * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
+ *
+ * @author claroline Team <cvs@claroline.net>
+ *
+ * @package RIGHT
+ *
+ */
+
+require '../inc/claro_init_global.inc.php';
+
+$nameTools = get_lang('Course profile');
+
+$dialogBox = '';
+$tidReset = true;
+
+if ( ! $_cid || ! $_uid) claro_disp_auth_form(true);
+
+$is_allowedToEdit = $is_courseAdmin;
+
+if ( ! $is_allowedToEdit )
+{
+    claro_die(get_lang('Not allowed'));
+}
+
+require_once $includePath . '/lib/right/profile.class.php' ;
+require_once $includePath . '/lib/pager.lib.php';
+
+// Main section
+
+$cmd = isset($_REQUYEST['cmd'])?$_REQUYEST['cmd']:null;
+
+// Build profile list
+
+$itemPerPage = 10;
+
+$tbl_mdb_names = claro_sql_get_main_tbl();
+$tblProfile = $tbl_mdb_names['right_profile'];
+
+$sql = " SELECT profile_id as id, name, description, locked, required
+         FROM `" . $tblProfile . "`
+         WHERE type = 'COURSE' ";
+
+$offset = (isset($_REQUEST['offset']) && !empty($_REQUEST['offset']) ) ? $_REQUEST['offset'] : 0;
+$profilePager = new claro_sql_pager($sql,$offset, $itemPerPage);
+$profileList = $profilePager->get_result_list();
+
+// Display section
+
+include $includePath . '/claro_init_header.inc.php';
+
+echo claro_html_tool_title($nameTools);
+
+// Display table header
+
+echo '<table class="claroTable emphaseLine" >' . "\n"
+    . '<thead>' . "\n"
+    . '<tr class="headerX">' . "\n"
+    . '<th>' . get_lang('Name') . '</th>' . "\n"
+    . '<th>' . get_lang('Rights') .'</th>' . "\n"
+    . '</tr>' . "\n"
+    . '</thead>' . "\n"
+    . '<tbody>' ;
+
+foreach ( $profileList as $thisProfile )
+{
+    echo '<tr align="center">' . "\n"
+        . '<td align="left">' . $thisProfile['name'] ;
+
+    if ( $thisProfile['locked'] == '1' )
+    {
+        echo '<img src="' . $imgRepositoryWeb . 'locked.gif" alt="' . get_lang('Lock') . '" />';
+    }
+
+    echo '<br /><em>' . $thisProfile['description'] . '</em>' . "\n"
+        . '<td><a href="profile.php?cmd=rqEdit&profile_id='. $thisProfile['id'].'"><img src="' .  $imgRepositoryWeb . 'settings.gif" alt="' . get_lang('Rights') . '" /></td>' . "\n" ;
+    echo '</tr>' . "\n\n";
+}
+
+echo '</tbody></table>';
+
+
+include $includePath . '/claro_init_footer.inc.php';
+
+?>
