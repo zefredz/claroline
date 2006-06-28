eb75d110dfdc441d40014bc760b3f140ed42fbdb
Frédéric Minne <zefredz@users.noreply.github.com>
2006-06-28 15:44:44 +0000

    add lib.compat for PHP 5 to PHP 4 backward compatibility TODO move to inc/lib

--- claroline/wiki/wiki.php
+++ claroline/wiki/wiki.php
@@ -79,6 +79,10 @@
     require_once "lib/lib.wikisql.php";
     require_once "lib/lib.javascript.php";
     require_once "lib/lib.wikidisplay.php";
+    
+    // PHP 5 to PHP 4 compatibility library
+    // file_put_contents
+    require_once "lib/lib.compat.php";
 
     // filter request variables
 
