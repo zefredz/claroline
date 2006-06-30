72a6e9b50d2d9b80872a61b68c4ed0c6373c2522
mathieu <mathieu@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2006-06-30 13:26:38 +0000

    manage right, include right_profile.lib.php

--- claroline/install/index.php
+++ claroline/install/index.php
@@ -69,6 +69,7 @@ include_once $newIncludePath . 'lib/course.lib.inc.php';
 include_once $newIncludePath . 'lib/claro_main.lib.php';
 include_once $newIncludePath . 'lib/language.lib.php';
 include_once $newIncludePath . 'lib/module.manage.lib.php';
+include_once $newIncludePath . 'lib/right/right_profile.lib.php';
 
 /**
  * Unquote GET, POST AND COOKIES if magic quote gpc is enabled in php.ini
