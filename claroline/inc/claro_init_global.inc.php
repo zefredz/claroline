fc617d13e5f47f149d2eb7b9e340baf67a58f213
mathieu <mathieu@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2006-07-10 09:58:39 +0000

    try to fix bug 591 : move down init event after claro_init_local (see revision 1.62 in claro_init_global.inc.php)

--- claroline/inc/claro_init_global.inc.php
+++ claroline/inc/claro_init_global.inc.php
@@ -152,12 +152,6 @@ if ($statsDbName == '')
 }
 
 /*----------------------------------------------------------------------
-  Include the event manager declarations for the notification system
-  ----------------------------------------------------------------------*/
-
-require $includePath . '/lib/event/init_event_manager.inc.php';
-
-/*----------------------------------------------------------------------
   Include the events library for tracking
   ----------------------------------------------------------------------*/
 
@@ -168,6 +162,12 @@ require $includePath . '/lib/events.lib.inc.php';
   ----------------------------------------------------------------------*/
 
 require $includePath . '/claro_init_local.inc.php';
+
+/*----------------------------------------------------------------------
+  Include the event manager declarations for the notification system
+  ----------------------------------------------------------------------*/
+
+require $includePath . '/lib/event/init_event_manager.inc.php';
 
 /*----------------------------------------------------------------------
   Load language translation and locale settings
