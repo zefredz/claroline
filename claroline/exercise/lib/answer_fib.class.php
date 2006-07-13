099539e0b4fbf7dc499c4e5a813bc7ad83fa53a6
mathieu <mathieu@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2006-07-13 08:55:32 +0000

    missing .php extension

--- claroline/exercise/lib/answer_fib.class.php
+++ claroline/exercise/lib/answer_fib.class.php
@@ -535,7 +535,7 @@ class answerFillInBlanks
     function getFormHtml($exId = null) 
     {
     	$html = 
-    		'<form method="post" action="./edit_answers?exId='.$exId.'&amp;quId='.$this->questionId.'">' . "\n"
+    		'<form method="post" action="./edit_answers.php?exId='.$exId.'&amp;quId='.$this->questionId.'">' . "\n"
     	.	'<input type="hidden" name="cmd" value="exEdit" />' . "\n"
     	.	'<input type="hidden" name="step" value="'.$this->step.'" />' . "\n";
     	
