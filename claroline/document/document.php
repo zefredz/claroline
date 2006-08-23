430ede8779696f4943c6b4599484ee48ade46ec4
Frédéric Minne <zefredz@users.noreply.github.com>
2006-08-23 14:19:28 +0000

    allow LaTeX in html documents created by Claroline

--- claroline/document/document.php
+++ claroline/document/document.php
@@ -478,6 +478,8 @@ if ( $is_allowedToEdit ) // Document edition are reserved to certain people
             $cwd = preg_replace('~^(\.\.)$|(/\.\.)|(\.\./)~', '', $cwd);
 
             $htmlContent =  $htmlContentHeader . $_REQUEST['htmlContent'] . $htmlContentFooter;
+            
+            $htmlContent = claro_parse_user_text( $htmlContent );
 
             create_file($baseWorkDir.$cwd.'/'.$fileName,
                         $htmlContent);
