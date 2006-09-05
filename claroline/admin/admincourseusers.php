5d77c69d97391a5793afe5fa798040a98b1347fc
mathieu <mathieu@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2006-09-05 15:46:06 +0000

    lang update

--- claroline/admin/admincourseusers.php
+++ claroline/admin/admincourseusers.php
@@ -172,7 +172,7 @@ $htmlHeadXtra[] =
 $sortUrlList = $myPager->get_sort_url_list($_SERVER['PHP_SELF'] . '?cidToEdit=' . $cidToEdit);
 
 $dg_opt_list['idLineShift'] = $myPager->offset + 1;
-$dg_opt_list['colTitleList'] = array ( 'user_id'  => '<a href="' . $sortUrlList['user_id'] . '">' . get_lang('Userid') . '</a>'
+$dg_opt_list['colTitleList'] = array ( 'user_id'  => '<a href="' . $sortUrlList['user_id'] . '">' . get_lang('User Id') . '</a>'
                                      , 'name'     => '<a href="' . $sortUrlList['name'] . '">' . get_lang('Last name') . '</a>'
                                      , 'firstname'=> '<a href="' . $sortUrlList['firstname'] . '">' . get_lang('First name') . '</a>'
                                      , 'profileId'=> '<a href="' . $sortUrlList['profileId'] . '">' . get_lang('Profile') . '</a>'
