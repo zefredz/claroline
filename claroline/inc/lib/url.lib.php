b7ec977e41184ecb541c222a2685385ab7b74043
moosh <moosh@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2006-09-13 07:11:38 +0000

    - remove call to a moodle function :)

--- claroline/inc/lib/url.lib.php
+++ claroline/inc/lib/url.lib.php
@@ -1,10 +1,18 @@
-<?php  // $Id$
-    
-    // vim: expandtab sw=4 ts=4 sts=4:
-    
+<?php // $Id$
+
+// vim: expandtab sw=4 ts=4 sts=4:
+
+/**
+ * Enter description here...
+ *
+ * @param string $completePath
+ * @param string $baseFile
+ * @return string : path fragement
+ *
+ */
 function get_slashed_argument($completePath, $baseFile)
 {
-    
+
     $pahtElementList = explode($baseFile, $completePath);
 
     if ( count($pahtElementList) > 1)
@@ -12,10 +20,10 @@ function get_slashed_argument($completePath, $baseFile)
         $argument = array_pop($pahtElementList);
 
         $questionMarkPos = strpos($argument, '?');
-        
+
         if (is_int($questionMarkPos)) return substr($argument, 0, $questionMarkPos);
         else                          return $argument;
-        
+
     }
     else
     {
@@ -30,34 +38,47 @@ function get_slashed_argument($completePath, $baseFile)
  * server, and the way PHP is compiled (ie. as a CGI, module, ISAPI, etc.)
  * <b>NOTE:</b> This function returns false if the global variables needed are not set.
  *
+ * @since 1.8
  * @return string
  */
- function get_request_uri() {
-
-    if (!empty($_SERVER['REQUEST_URI'])) {
+function get_request_uri()
+{
+    if (!empty($_SERVER['REQUEST_URI']))
+    {
         return $_SERVER['REQUEST_URI'];
-
-    } else if (!empty($_SERVER['PHP_SELF'])) {
-        if (!empty($_SERVER['QUERY_STRING'])) {
+    }
+    else if (!empty($_SERVER['PHP_SELF']))
+    {
+        if (!empty($_SERVER['QUERY_STRING']))
+        {
             return $_SERVER['PHP_SELF'] .'?'. $_SERVER['QUERY_STRING'];
         }
         return $_SERVER['PHP_SELF'];
 
-    } else if (!empty($_SERVER['SCRIPT_NAME'])) {
-        if (!empty($_SERVER['QUERY_STRING'])) {
+    }
+    elseif (!empty($_SERVER['SCRIPT_NAME']))
+    {
+        if (!empty($_SERVER['QUERY_STRING']))
+        {
             return $_SERVER['SCRIPT_NAME'] .'?'. $_SERVER['QUERY_STRING'];
         }
         return $_SERVER['SCRIPT_NAME'];
 
-    } else if (!empty($_SERVER['URL'])) {     // May help IIS (not well tested)
-        if (!empty($_SERVER['QUERY_STRING'])) {
+    }
+    elseif (!empty($_SERVER['URL']))
+    {     // May help IIS (not well tested)
+        if (!empty($_SERVER['QUERY_STRING']))
+        {
             return $_SERVER['URL'] .'?'. $_SERVER['QUERY_STRING'];
         }
         return $_SERVER['URL'];
 
-    } else {
-        notify('Warning: Could not find any of these web server variables: $REQUEST_URI, $PHP_SELF, $SCRIPT_NAME or $URL');
+    }
+    else
+    {
+        pushClaroMessage('Warning: Could not find any of these web server variables: $REQUEST_URI, $PHP_SELF, $SCRIPT_NAME or $URL');
         return false;
     }
 }
+
 ?>
