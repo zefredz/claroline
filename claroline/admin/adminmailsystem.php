c6ee3a50c0de2a8311e72b3b6ed9c4e5fe47252d
moosh <moosh@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2006-09-21 12:08:16 +0000

    - hide  contact column

--- claroline/admin/adminmailsystem.php
+++ claroline/admin/adminmailsystem.php
@@ -31,9 +31,9 @@ $nameTools = get_lang('System mail : recipients list');
 $error = false;
 $messageList = array();
 
-/*=====================================================================
+/*
 Main Section
-=====================================================================*/
+*/
 
 
 $platformAdminUidList = claro_get_uid_of_platform_admin();
@@ -42,11 +42,11 @@ if ( isset($_REQUEST['cmd']) )  //for formular modification
 {
     $notifiedList = isset($_REQUEST['notifiedList']) && is_array($_REQUEST['notifiedList'])?$_REQUEST['notifiedList']:array();
     $requestList = isset($_REQUEST['requestList']) && is_array($_REQUEST['requestList'])?$_REQUEST['requestList']:array();
-    $contactList = isset($_REQUEST['contactList']) && is_array($_REQUEST['contactList'])?$_REQUEST['contactList']:array();
+//    $contactList = isset($_REQUEST['contactList']) && is_array($_REQUEST['contactList'])?$_REQUEST['contactList']:array();
 
     foreach ($platformAdminUidList as $platformAdminUid )
     {
-        claro_set_uid_of_platform_contact($platformAdminUid,in_array($platformAdminUid,$contactList));
+      //  claro_set_uid_of_platform_contact($platformAdminUid,in_array($platformAdminUid,$contactList));
         claro_set_uid_recipient_of_system_notification($platformAdminUid,in_array($platformAdminUid,$notifiedList));
         claro_set_uid_recipient_of_request_admin($platformAdminUid,in_array($platformAdminUid,$requestList));
     }
@@ -73,8 +73,7 @@ foreach ($platformAdminUidList as $k => $platformAdminUid )
     $userDataGrid[$k]['firstname'] = $userData['firstname'];
     $userDataGrid[$k]['email'] = $userData['email'];
     $userDataGrid[$k]['authsource'] = $userData['authsource'];
-    $userDataGrid[$k]['contact_switch'] = '<input name="contactList[]" type="checkbox" value="' . $platformAdminUid . '" '
-    .    ((bool) in_array($platformAdminUid,$contactUidList)  ? 'checked="checked" >' : '>');
+    //$userDataGrid[$k]['contact_switch'] = '<input name="contactList[]" type="checkbox" value="' . $platformAdminUid . '" ' . ((bool) in_array($platformAdminUid,$contactUidList)  ? 'checked="checked" >' : '>');
     $userDataGrid[$k]['request_switch'] = '<input name="requestList[]" type="checkbox" value="' . $platformAdminUid . '" '
     .    ((bool) in_array($platformAdminUid,$requestUidList)  ? 'checked="checked" > ' : '> ');
     $userDataGrid[$k]['notification_switch'] = '<input name="notifiedList[]" type="checkbox" value="' . $platformAdminUid . '" '
@@ -89,15 +88,15 @@ $adminDataGrid->set_colTitleList(array ( 'user id'              => get_lang('Use
                                         , 'firstname'           => get_lang('First name')
                                         , 'email'               => get_lang('Email')
                                         , 'authsource'          => get_lang('Authentication source')
-                                        , 'contact_switch'      => get_lang('Contact')
+//                                        , 'contact_switch'      => get_lang('Contact')
                                         , 'request_switch'      => get_lang('Request')
                                         , 'notification_switch' => get_lang('Notify')
                                         )
                                         );
 
-$adminDataGrid->set_colAttributeList( array ( 'contact_switch' => array ('align' => 'left')
-                                             , 'request_switch' => array ('align' => 'left')
+$adminDataGrid->set_colAttributeList( array (  'request_switch' => array ('align' => 'left')
                                              , 'notification_switch' => array ('align' => 'left')
+                                             //, 'contact_switch' => array ('align' => 'left')
                                              , 'authsource'  => array ('align' => 'center')
                                              ));
 /**
