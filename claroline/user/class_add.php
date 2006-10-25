ccf23f1bd25c11bfce7662f55948c09a01b07df3
moosh <moosh@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2006-10-25 04:23:15 +0000

    - refine courseadmin authorization - use claro_html_msg_list

--- claroline/user/class_add.php
+++ claroline/user/class_add.php
@@ -20,47 +20,23 @@
 
 $tlabelReq = 'CLUSR';
 $gidReset = true;
+$dialogBoxMsg = array();
 require '../inc/claro_init_global.inc.php';
 
 if ( !$_cid || !$is_courseAllowed ) claro_disp_auth_form(true);
-if ( !$is_courseAdmin ) claro_die(get_lang('Not allowed'));
+
+$can_import_user_class  = (bool) ($is_courseAdmin
+                        && get_conf('is_coursemanager_allowed_to_import_user_class') )
+                        || $is_platformAdmin;
+
+// TODO replace calro_die by best usage.
+
+if ( !$can_import_user_class ) claro_die(get_lang('Not allowed'));
 
 require_once $includePath . '/lib/admin.lib.inc.php';
 require_once $includePath . '/lib/user.lib.php';
 require_once $includePath . '/lib/class.lib.php';
 require_once $includePath . '/lib/sendmail.lib.php';
-
-// javascript confirm pop up declaration for header
-
-$htmlHeadXtra[] =
-'<script>
-    function confirmation_enrol (name)
-    {
-        if (confirm("' . clean_str_for_javascript(get_lang('Are you sure you want to enrol the whole class on the course ?')) . '"))
-            {return true;}
-        else
-            {return false;}
-    }
-    function confirmation_unenrol (name)
-    {
-        if (confirm("' . clean_str_for_javascript(get_lang('Are you sure you want to unenrol the whole class on the course ?')) . '"))
-            {return true;}
-        else
-            {return false;}
-    }
-</script>';
-
-/*
- * DB tables definition
- */
-
-$tbl_cdb_names = claro_sql_get_course_tbl();
-$tbl_mdb_names = claro_sql_get_main_tbl();
-$tbl_users      = $tbl_mdb_names['user'];
-$tbl_class      = $tbl_mdb_names['class'];
-$tbl_class_user = $tbl_mdb_names['rel_class_user'];
-$tbl_course_class = $tbl_mdb_names['rel_course_class'];
-
 
 /*---------------------------------------------------------------------*/
 /*----------------------EXECUTE COMMAND SECTION------------------------*/
@@ -91,11 +67,7 @@ switch ( $cmd )
 
         if ( register_class_to_course( $form_data['class_id'], $_cid) )
         {
-            $dialogBox  = get_lang('Class has been enroled') ;
-        }
-        else
-        {
-
+            $dialogBoxMsg[]  = get_lang('Class has been enroled') ;
         }
         break;
 
@@ -105,11 +77,7 @@ switch ( $cmd )
 
         if ( unregister_class_to_course( $form_data['class_id'], $_cid) )
         {
-            $dialogBox  = get_lang('Class has been unenroled') ;
-        }
-        else
-        {
-
+            $dialogBoxMsg[]  = get_lang('Class has been unenroled') ;
         }
         break;
 }
@@ -118,15 +86,7 @@ switch ( $cmd )
 /*----------------------FIND information SECTION-----------------------*/
 /*---------------------------------------------------------------------*/
 
-$sql = "SELECT C.id,
-               C.name,
-               C.class_parent_id,
-               CC.courseId as course_id
-        FROM `" . $tbl_class . "` C
-              LEFT JOIN `" . $tbl_course_class . "` CC ON CC.`classId` = C.`id`
-              AND CC.`courseId` = '" . addslashes($_cid) . "'
-        ORDER BY C.`name`";
-$class_list = claro_sql_query_fetch_all($sql);
+$classList = get_class_list_by_course($_cid);
 
 /*---------------------------------------------------------------------*/
 /*----------------------DISPLAY SECTION--------------------------------*/
@@ -136,6 +96,26 @@ $class_list = claro_sql_query_fetch_all($sql);
 
 $nameTools = get_lang('Enrol class');
 $interbredcrump[] = array ('url' => 'user.php', 'name' => get_lang('Users'));
+// javascript confirm pop up declaration for header
+
+$htmlHeadXtra[] =
+'<script>
+    function confirmation_enrol (name)
+    {
+        if (confirm("' . clean_str_for_javascript(get_lang('Are you sure you want to enrol the whole class on the course ?')) . '"))
+            {return true;}
+        else
+            {return false;}
+    }
+    function confirmation_unenrol (name)
+    {
+        if (confirm("' . clean_str_for_javascript(get_lang('Are you sure you want to unenrol the whole class on the course ?')) . '"))
+            {return true;}
+        else
+            {return false;}
+    }
+</script>';
+
 
 // display top banner
 
@@ -143,35 +123,31 @@ include $includePath . '/claro_init_header.inc.php';
 
 // Display tool title
 
-echo claro_html_tool_title(get_lang('Enrol class'));
+echo claro_html_tool_title(get_lang('Enrol class'))
 
 // Display Forms or dialog box (if needed)
 
-if(isset($dialogBox) && $dialogBox!='')
-{
-    echo claro_html_message_box($dialogBox);
-}
+.    claro_html_msg_list($dialogBoxMsg)
 
 // display tool links
-echo '<p><a class="claroCmd" href="user.php">' . get_lang('Back to list') . '</a></p>' ;
-
+.    '<p>'
+.    '<a class="claroCmd" href="user.php">' . get_lang('Back to list') . '</a>'
+.    '</p>'
 // display cols headers
-
-echo '<table class="claroTable" width="100%" border="0" cellspacing="2">' . "\n"
-    .    '<thead>' . "\n"
-    .    '<tr class="headerX">' . "\n"
-    .    '<th>' . get_lang('Classes') . '</th>' . "\n"
-    .    '<th>' . get_lang('Users') . '</th>' . "\n"
-    .    '<th>' . get_lang('Enrol to course') . '</th>' . "\n"
-    .    '</tr>' . "\n"
-    .    '</thead>' . "\n"
-    .    '<tbody>' . "\n" ;
-
+.    '<table class="claroTable" width="100%" border="0" cellspacing="2">' . "\n"
+.    '<thead>' . "\n"
+.    '<tr class="headerX">' . "\n"
+.    '<th>' . get_lang('Classes') . '</th>' . "\n"
+.    '<th>' . get_lang('Users') . '</th>' . "\n"
+.    '<th>' . get_lang('Enrol to course') . '</th>' . "\n"
+.    '</tr>' . "\n"
+.    '</thead>' . "\n"
+.    '<tbody>' . "\n"
 // display Class list (or tree)
-echo display_tree_class_in_user($class_list, $_cid);
-
-echo '</tbody>' . "\n"
-    . '</table>' . "\n" ;
+.    display_tree_class_in_user($classList, $_cid)
+.    '</tbody>' . "\n"
+.    '</table>' . "\n"
+;
 
 // display footer banner
 
