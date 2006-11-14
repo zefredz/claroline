c19c9ddb433d008376c3d685324b80a17a5a6c7e
Frédéric Minne <zefredz@users.noreply.github.com>
2006-11-14 08:18:02 +0000

    use backlog reporter for debug info

--- claroline/inc/claro_init_footer.inc.php
+++ claroline/inc/claro_init_footer.inc.php
@@ -88,8 +88,12 @@ echo '</div>';
     {
         $claroMsgList = getClaroMessageList();
         if (0 < count($claroMsgList))
-        echo claro_html_tool_title('Debug info');
-        echo claro_html_msg_list($claroMsgList);
+        $dbgTitle = claro_html_tool_title('Debug info');
+        $dbgContent = claro_html_msg_list($claroMsgList);
+        
+        require_once dirname( __FILE__ ) . '/lib/backlog.class.php';
+        
+        echo Backlog_Reporter::report( $dbgTitle, $dbgContent, get_lang('expand'), true );
     }
 
 echo '</body>'
