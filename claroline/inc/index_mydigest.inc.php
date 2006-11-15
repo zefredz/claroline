702afdab8d1135a8ed11094dacab62a8a64a56c5
moosh <moosh@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2006-11-15 16:07:59 +0000

    - cache the  calendar digest with cache_lite

--- claroline/inc/index_mydigest.inc.php
+++ claroline/inc/index_mydigest.inc.php
@@ -9,9 +9,27 @@ if ( count( get_included_files() ) == 1 ) die( '---' );
  * @version 1.8 $Revision$
  * @copyright (c) 2001-2006 Universite catholique de Louvain (UCL)
  * @license (GPL) GENERAL PUBLIC LICENSE - http://www.gnu.org/copyleft/gpl.html
- * @package CLINDEX
- ******************************************************************************/
+ * @package CLCALDIGEST
+ *
+ */
 if ( ! isset($_uid) ) claro_disp_auth_form();
+
+include_once $GLOBALS['includePath'] . '/lib/pear/Lite.php';
+
+// Cache_lite setting & init
+$cache_options = array(
+'cacheDir' => get_conf('rootSys') . 'tmp/cache/CLCALdigest/',
+'lifeTime' => get_conf('cache_lifeTime', 600),
+'automaticCleaningFactor' => 50,
+);
+if (get_conf('CLARO_DEBUG_MODE',false) ) $cache_options['pearErrorMode'] = CACHE_LITE_ERROR_DIE;
+if (get_conf('CLARO_DEBUG_MODE',false) ) $cache_options['lifeTime'] = 120;
+if (! file_exists($cache_options['cacheDir']) )
+{
+    include_once $GLOBALS['includePath'] . '/lib/fileManage.lib.php';
+    claro_mkdir($cache_options['cacheDir'],CLARO_FILE_PERMISSIONS,true);
+}
+$Cache_LiteCLCALDIGEST = new Cache_Lite($cache_options);
 
 $courseDigestList = array('courseSysCode'      => array(),
                           'courseOfficialCode' => array(),
@@ -19,17 +37,19 @@ $courseDigestList = array('courseSysCode'      => array(),
                           'date'               => array(),
                           'content'            => array());
 
-$personnalCourseList = get_user_course_list($_uid);
-
-foreach($personnalCourseList as $thisCourse)
+if (false === $htmlCLCALDIGEST = $Cache_LiteCLCALDIGEST->get('CALDIGEST'.$_uid))
 {
-    /*
-     * ANNOUNCEMENTS : get announcements of this course since last user loggin
-     */
+    $personnalCourseList = get_user_course_list($_uid);
 
-    $tableAnn = get_conf('courseTablePrefix') . $thisCourse['db'] . get_conf('dbGlu') . 'announcement';
+    foreach($personnalCourseList as $thisCourse)
+    {
+        /*
+        * ANNOUNCEMENTS : get announcements of this course since last user loggin
+        */
 
-    $sql = "SELECT '" . addslashes($thisCourse['sysCode']     ) ."' AS `courseSysCode`,
+        $tableAnn = get_conf('courseTablePrefix') . $thisCourse['db'] . get_conf('dbGlu') . 'announcement';
+
+        $sql = "SELECT '" . addslashes($thisCourse['sysCode']     ) ."' AS `courseSysCode`,
                    '" . addslashes($thisCourse['officialCode']) ."' AS `courseOfficialCode`,
                    'CLANN'                                          AS `toolLabel`,
                    CONCAT(`temps`, ' ', '00:00:00')                 AS `date`,
@@ -42,21 +62,21 @@ foreach($personnalCourseList as $thisCourse)
             ORDER BY `date` DESC
             LIMIT 1";
 
-    $resultList = claro_sql_query_fetch_all_cols($sql);
+        $resultList = claro_sql_query_fetch_all_cols($sql);
 
-    foreach($resultList as $colName => $colValue)
-    {
-        if (count($colValue) == 0) break;
-        $courseDigestList[$colName] = array_merge($courseDigestList[$colName], $colValue);
-    }
+        foreach($resultList as $colName => $colValue)
+        {
+            if (count($colValue) == 0) break;
+            $courseDigestList[$colName] = array_merge($courseDigestList[$colName], $colValue);
+        }
 
-    /*
-     * AGENDA : get the next agenda entries of this course from now
-     */
+        /*
+        * AGENDA : get the next agenda entries of this course from now
+        */
 
-    $tableCal = get_conf('courseTablePrefix') . $thisCourse['db'] . get_conf('dbGlu') . 'calendar_event';
+        $tableCal = get_conf('courseTablePrefix') . $thisCourse['db'] . get_conf('dbGlu') . 'calendar_event';
 
-    $sql = "SELECT '". addslashes($thisCourse['sysCode']     ) ."' AS `courseSysCode`,
+        $sql = "SELECT '". addslashes($thisCourse['sysCode']     ) ."' AS `courseSysCode`,
                    '". addslashes($thisCourse['officialCode']) ."' AS `courseOfficialCode`,
                    'CLCAL' AS `toolLabel`,
             CONCAT(`day`, ' ',`hour`) AS `date`,
@@ -68,81 +88,86 @@ foreach($personnalCourseList as $thisCourse)
             ORDER BY `date`
             LIMIT 1";
 
-    $resultList = claro_sql_query_fetch_all_cols($sql);
+        $resultList = claro_sql_query_fetch_all_cols($sql);
 
-    foreach($resultList as $colName => $colValue)
-    {
-        if (count($colValue) == 0) break;
-        $courseDigestList[$colName] = array_merge($courseDigestList[$colName], $colValue);
-    }
+        foreach($resultList as $colName => $colValue)
+        {
+            if (count($colValue) == 0) break;
+            $courseDigestList[$colName] = array_merge($courseDigestList[$colName], $colValue);
+        }
 
-} // end foreach($personnalCourseList as $thisCourse)
+    } // end foreach($personnalCourseList as $thisCourse)
 
 
 
-/*
- * Sort all these digest by date
- */
+    /*
+    * Sort all these digest by date
+    */
 
-array_multisort( $courseDigestList['toolLabel'         ],
-                 $courseDigestList['date'              ],
-                 $courseDigestList['courseOfficialCode'],
-                 $courseDigestList['courseSysCode'     ],
-                 $courseDigestList['content'           ] );
+    array_multisort( $courseDigestList['toolLabel'         ],
+    $courseDigestList['date'              ],
+    $courseDigestList['courseOfficialCode'],
+    $courseDigestList['courseSysCode'     ],
+    $courseDigestList['content'           ] );
 
-/******************************************************************************
-                                    DISPLAY
- ******************************************************************************/
+    /******************************************************************************
+    DISPLAY
+    ******************************************************************************/
 
     $title = '';
 
-for( $i=0, $itemCount = count($courseDigestList['toolLabel']); $i < $itemCount; $i++)
-{
-    switch ($courseDigestList['toolLabel'][$i])
+    for( $i=0, $itemCount = count($courseDigestList['toolLabel']); $i < $itemCount; $i++)
     {
-        case 'CLANN':
-            $itemIcon = 'announcement.gif';
-            $url = 'claroline/announcements/announcements.php?cidReq='
-                 . $courseDigestList['courseSysCode'][$i];
-            $name = get_lang('Latest announcements');
-            break;
+        switch ($courseDigestList['toolLabel'][$i])
+        {
+            case 'CLANN':
+                $itemIcon = 'announcement.gif';
+                $url = 'claroline/announcements/announcements.php?cidReq='
+                . $courseDigestList['courseSysCode'][$i];
+                $name = get_lang('Latest announcements');
+                break;
 
-        case 'CLCAL':
-            $itemIcon = 'agenda.gif';
-            $url = 'claroline/calendar/agenda.php?cidReq='
-                 . $courseDigestList['courseSysCode'][$i];
-            $name = get_lang('Agenda next events');
-            break;
-    }
+            case 'CLCAL':
+                $itemIcon = 'agenda.gif';
+                $url = 'claroline/calendar/agenda.php?cidReq='
+                . $courseDigestList['courseSysCode'][$i];
+                $name = get_lang('Agenda next events');
+                break;
+        }
 
-    if ($title != $name)
-    {
-        $title = $name;
-        echo '<h4>' . $title . '</h4>' . "\n";
-    }
+        if ($title != $name)
+        {
+            $title = $name;
+            $htmlCLCALDIGEST .= '<h4>' . $title . '</h4>' . "\n";
+        }
 
-    $courseDigestList['content'][$i] = preg_replace('/<br( \/)?>/', ' ', $courseDigestList['content'][$i]);
-    $courseDigestList['content'][$i] = strip_tags($courseDigestList['content'][$i]);
-    $courseDigestList['content'][$i] = substr($courseDigestList['content'][$i],0, get_conf('max_char_from_content') );
+        $courseDigestList['content'][$i] = preg_replace('/<br( \/)?>/', ' ', $courseDigestList['content'][$i]);
+        $courseDigestList['content'][$i] = strip_tags($courseDigestList['content'][$i]);
+        $courseDigestList['content'][$i] = substr($courseDigestList['content'][$i],0, get_conf('max_char_from_content') );
 
-    echo '<p>' . "\n"
-    .    '<small>'
-    .    '<a href="' . $url . '">'
-    .    '<img src="' . $imgRepositoryWeb . $itemIcon . '" alt="" />'
-    .    '</a>' . "\n"
+        $htmlCLCALDIGEST .= '<p>' . "\n"
+        .    '<small>'
+        .    '<a href="' . $url . '">'
+        .    '<img src="' . $imgRepositoryWeb . $itemIcon . '" alt="" />'
+        .    '</a>' . "\n"
 
-    .    claro_disp_localised_date( $dateFormatLong,
-                                 strtotime($courseDigestList['date'][$i]) )
-    .    '<br />' . "\n"
-    .    '<a href="' . $url . '">'
-    .    $courseDigestList['courseOfficialCode'][$i]
-    .    '</a> : ' . "\n"
-    .    '<small>'  . "\n"
-    .    $courseDigestList['content'][$i]  . "\n"
-    .    '</small>' . "\n"
-    .    '</small>' . "\n"
-    .    '</p>' . "\n"
-    ;
-} // end for( $i=0, ... $i < $itemCount; $i++)
+        .    claro_disp_localised_date( $dateFormatLong,
+        strtotime($courseDigestList['date'][$i]) )
+        .    '<br />' . "\n"
+        .    '<a href="' . $url . '">'
+        .    $courseDigestList['courseOfficialCode'][$i]
+        .    '</a> : ' . "\n"
+        .    '<small>'  . "\n"
+        .    $courseDigestList['content'][$i]  . "\n"
+        .    '</small>' . "\n"
+        .    '</small>' . "\n"
+        .    '</p>' . "\n"
+        ;
+    } // end for( $i=0, ... $i < $itemCount; $i++)
+    $Cache_LiteCLCALDIGEST->save($htmlCLCALDIGEST,'CALDIGEST'.$_uid);
+}
+
+unset ($Cache_LiteCLCALDIGEST);
+echo $htmlCLCALDIGEST;
 
 ?>
