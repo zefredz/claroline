728c7c0dfce970a2fe313d0bf1c797df43d80926
Frédéric Minne <zefredz@users.noreply.github.com>
2006-11-17 11:46:35 +0000

    adapt code to include in claroline pages (remove <html><head>...and move <style> to $htmlHeadXtra)

--- claroline/admin/technical/lib/PhpSecInfo.lib.php
+++ claroline/admin/technical/lib/PhpSecInfo.lib.php
@@ -239,39 +239,7 @@ class PhpSecInfo
 
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "DTD/xhtml1-transitional.dtd">
-<html><head>
-<style type="text/css">
-body {background-color: #ffffff; color: #000000;}
-body, td, th, h1, h2 {font-family: sans-serif;}
-pre {margin: 0px; font-family: monospace;}
-a:link {color: #000099; text-decoration: none; background-color: #ffffff;}
-a:hover {text-decoration: underline;}
-table {border-collapse: collapse;}
-.center {text-align: center;}
-.center table { margin-left: auto; margin-right: auto; text-align: left;}
-.center th { text-align: center !important; }
-td, th { border: 1px solid #000000; font-size: 75%; vertical-align: baseline;}
-h1 {font-size: 150%;}
-h2 {font-size: 125%;}
-.p {text-align: left;}
-.e {background-color: #ccccff; font-weight: bold; color: #000000;}
-.h {background-color: #9999cc; font-weight: bold; color: #000000;}
-.v {background-color: #cccccc; color: #000000;}
-.vr {background-color: #cccccc; text-align: right; color: #000000;}
-img {float: right; border: 0px;}
-hr {width: 600px; background-color: #cccccc; border: 0px; height: 1px; color: #000000;}
-
-.v-ok {background-color:#009900;color:#ffffff;}
-.v-notice {background-color:orange;color:#000000;}
-.v-warn {background-color:#990000;color:#ffffff;}
-.v-notrun {background-color:#cccccc;color:#000000;}
-.v-error {background-color:#F6AE15;color:#000000;font-weight:bold;}
-
-}
-</style>
-<title>phpsecinfo()</title></head>
-<body><div class="center">
+<div class="center">
 <table border="0" cellpadding="3" width="600">
 <tr class="h"><td>
 <h1 class="p">
@@ -295,7 +263,7 @@ PHP Environment Security Info
 
 			?>
 
-</div></body></html>
+</div>
 		<?php
 	}
 
--- claroline/admin/technical/phpInfo.php
+++ claroline/admin/technical/phpInfo.php
@@ -44,6 +44,31 @@ if (isset($_REQUEST['to']))
 $is_allowedToAdmin = $is_platformAdmin;
 if ($is_allowedToAdmin)
 {
+    $htmlHeadXtra[] = '<style type="text/css">
+table {border-collapse: collapse;}
+.center {text-align: center;}
+.center table { margin-left: auto; margin-right: auto; text-align: left;}
+.center th { text-align: center !important; }
+td, th { border: 1px solid #000000; font-size: 75%; vertical-align: baseline;}
+h1 {font-size: 150%;}
+h2 {font-size: 125%;}
+.p {text-align: left;}
+.e {background-color: #ccccff; font-weight: bold; color: #000000;}
+.h {background-color: #9999cc; font-weight: bold; color: #000000;}
+.v {background-color: #cccccc; color: #000000;}
+.vr {background-color: #cccccc; text-align: right; color: #000000;}
+img {float: right; border: 0px;}
+hr {width: 600px; background-color: #cccccc; border: 0px; height: 1px; color: #000000;}
+
+.v-ok {background-color:#009900;color:#ffffff;}
+.v-notice {background-color:orange;color:#000000;}
+.v-warn {background-color:#990000;color:#ffffff;}
+.v-notrun {background-color:#cccccc;color:#000000;}
+.v-error {background-color:#F6AE15;color:#000000;font-weight:bold;}
+
+}
+</style>';
+    
     include($includePath . '/claro_init_header.inc.php');
     echo claro_html_tool_title( array( 'mainTitle'=>$nameTools
     , 'subTitle'=> $siteName . ' - ' . $clarolineVersion . ' - '
