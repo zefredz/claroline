75b096afcf4b2047669de85d3efcf88e22723d2e
Frédéric Minne <zefredz@users.noreply.github.com>
2006-11-17 14:24:28 +0000

    add lib to gt phpinfo with no html, head and body tags + use it to display php info

--- claroline/admin/technical/phpInfo.php
+++ claroline/admin/technical/phpInfo.php
@@ -18,6 +18,8 @@
 
 require '../../inc/claro_init_global.inc.php';
 
+require_once dirname( __FILE__ ) . '/lib/phpinfo.lib.php';
+
 // Security check
 if ( ! $_uid ) claro_disp_auth_form();
 if ( ! $is_platformAdmin ) claro_die(get_lang('Not allowed'));
@@ -44,30 +46,7 @@ if (isset($_REQUEST['to']))
 $is_allowedToAdmin = $is_platformAdmin;
 if ($is_allowedToAdmin)
 {
-    $htmlHeadXtra[] = '<style type="text/css">
-table {border-collapse: collapse;}
-.center {text-align: center;}
-.center table { margin-left: auto; margin-right: auto; text-align: left;}
-.center th { text-align: center !important; }
-td, th { border: 1px solid #000000; font-size: 75%; vertical-align: baseline;}
-h1 {font-size: 150%;}
-h2 {font-size: 125%;}
-.p {text-align: left;}
-.e {background-color: #ccccff; font-weight: bold; color: #000000;}
-.h {background-color: #9999cc; font-weight: bold; color: #000000;}
-.v {background-color: #cccccc; color: #000000;}
-.vr {background-color: #cccccc; text-align: right; color: #000000;}
-img {float: right; border: 0px;}
-hr {width: 600px; background-color: #cccccc; border: 0px; height: 1px; color: #000000;}
-
-.v-ok {background-color:#009900;color:#ffffff;}
-.v-notice {background-color:orange;color:#000000;}
-.v-warn {background-color:#990000;color:#ffffff;}
-.v-notrun {background-color:#cccccc;color:#000000;}
-.v-error {background-color:#F6AE15;color:#000000;font-weight:bold;}
-
-}
-</style>';
+    $htmlHeadXtra[] = phpinfo_getStyle();
     
     include($includePath . '/claro_init_header.inc.php');
     echo claro_html_tool_title( array( 'mainTitle'=>$nameTools
@@ -108,11 +87,11 @@ function localtest()
 [<a href="<?php echo $_SERVER['PHP_SELF'] ?>?cmd=ext">Extentions</a>]
 </DIV>
 <DIV class="elementServeur">
-<span class="elementServeur" >Claroline</span> <?php echo $clarolineVersion ;?></strong> : [<a href="<?php echo $_SERVER['PHP_SELF'] ?>?cmd=clarconf">Config Claroline</a>]&nbsp;
+<span class="elementServeur" >Claroline</span> <?php echo $clarolineVersion ;?> : [<a href="<?php echo $_SERVER['PHP_SELF'] ?>?cmd=clarconf">Config Claroline</a>]&nbsp;
 [<a href="<?php echo $_SERVER['PHP_SELF'] ?>?cmd=clarcredit">Claroline credit</a>]&nbsp;
 </DIV>
 <DIV class="elementServeur">
-<span class="elementServeur" >WebServer</span> <?php echo $_SERVER['SERVER_SOFTWARE'] ;?></strong><br />
+<span class="elementServeur" >WebServer</span> <?php echo $_SERVER['SERVER_SOFTWARE'] ;?><br />
 
 [<?php echo get_lang('Mail to') . ' : ' ; ?><a href="mailto:<?php echo $_SERVER['SERVER_ADMIN'] ?>">Admin apache (<?php echo $_SERVER['SERVER_ADMIN'] ?>)</A>]
 <br />
@@ -150,7 +129,9 @@ if ($cmd == 'ext')
 }
 elseif ( $cmd == 'info' )
 {
-    phpinfo();
+    echo '<div class="center">';
+    echo phpinfoNoHtml();
+    echo '</div>';
 }
 elseif ($cmd == 'phpinfo')
 {
