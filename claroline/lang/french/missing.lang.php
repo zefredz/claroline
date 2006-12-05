9d28d8dbdb3d2717abf431bc31a93843af648333
mathieu <mathieu@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2006-12-05 22:21:29 +0000

    French translation progress (need review)

--- claroline/lang/french/missing.lang.php
+++ claroline/lang/french/missing.lang.php
@@ -5,121 +5,18 @@ $_lang['Attempt %attemptCount'] = 'Attempt %attemptCount';
 $_lang['Attempt %attemptCount on %attempts'] = 'Attempt %attemptCount on %attempts';
 $_lang['Available from %startDate'] = 'Available from %startDate';
 $_lang['File are always created in cache, but if this value is true feed file in cache arent rebuilt on request if exiting in cache.'] = 'File are always created in cache, but if this value is true feed file in cache arent rebuilt on request if exiting in cache.';
-$_lang['Maximum conversation lines'] = 'Maximum conversation lines';
-$_lang['Maximum conversation lines displayed to the user. '] = 'Maximum conversation lines displayed to the user.';
-$_lang['Maximum conversation lines in chat file'] = 'Maximum conversation lines in chat file';
-$_lang['Maximum lengh for a nick'] = 'Maximum lengh for a nick';
-$_lang['Maximum lines in the active chat file. For performance, it\'s interesting to not work with too big file.'] = 'Maximum lines in the active chat file. For performance, it\'s interesting to not work with too big file.';
-$_lang['Maximum size for an assignment'] = 'Maximum size for an assignment';
-$_lang['Maximum size of a document that a user can upload'] = 'Maximum size of a document that a user can upload';
 $_lang['More verbose when error occurs.'] = 'More verbose when error occurs.';
-$_lang['Multiple'] = 'Multiple';
-$_lang['MySQL database settings'] = 'MySQL database settings';
-$_lang['Name displayed in the top banner.'] = 'Name displayed in the top banner.';
-$_lang['New window for documents'] = 'New window for documents';
-$_lang['New window for submitted files'] = 'New window for submitted files';
-$_lang['No, rebuild the file on each request'] = 'No, rebuild the file on each request';
-$_lang['Not valid course code'] = 'Not valid course code';
-$_lang['Not valid user id'] = 'Not valid user id';
-$_lang['Note :  this repository should be protected with a .htaccess or
-       be placed outside the web. Because there contain data of private courses.'] = 'Note :  this repository should be protected with a .htaccess or
-       be placed outside the web. Because there contain data of private courses.';
-$_lang['Note : this repository should be protected with a .htaccess or
-       be placed outside the web. Because there contain data of private courses. Claroline Would be able to read and write in this dir'] = 'Note : this repository should be protected with a .htaccess or
-       be placed outside the web. Because there contain data of private courses. Claroline Would be able to read and write in this dir';
-$_lang['Number of assignment per page'] = 'Number of assignment per page';
-$_lang['Number of columns'] = 'Number of columns';
-$_lang['Number of columns displayed per page'] = 'Number of columns displayed per page';
-$_lang['Number of exercices per page'] = 'Number of exercices per page';
-$_lang['Number of posts per page'] = 'Number of posts per page';
-$_lang['Number of rows displayed per page'] = 'Number of rows displayed per page';
-$_lang['Number of seconds before the cookie expires'] = 'Number of seconds before the cookie expires';
-$_lang['Number of topics per page'] = 'Number of topics per page';
-$_lang['Number of user per page'] = 'Number of user per page';
-$_lang['Official Code'] = 'Official Code';
-$_lang['Official Code is'] = 'Official Code is';
-$_lang['Official code'] = 'Official code';
-$_lang['Once a user logs to the Claroline platform a cookie is sent to the user browser if the authentication process succeeds. The cookie value is also stored in a internal table of the Claroline platform for a certain time. If requested, the Claroline SSO '] = 'Once a user logs to the Claroline platform a cookie is sent to the user browser if the authentication process succeeds. The cookie value is also stored in a internal table of the Claroline platform for a certain time. If requested, the Claroline SSO';
-$_lang['Only his own submissions'] = 'Only his own submissions';
-$_lang['Order course by'] = 'Order course by';
-$_lang['Organisation Name'] = 'Organisation Name';
-$_lang['Organisation logo url'] = 'Organisation logo url';
-$_lang['Organisation website'] = 'Organisation website';
-$_lang['Password security check'] = 'Password security check';
-$_lang['Personal language selector'] = 'Personal language selector';
-$_lang['Platform & Organisation'] = 'Platform & Organisation';
-$_lang['Platform language'] = 'Platform language';
-$_lang['Platform local path '] = 'Platform local path';
-$_lang['Platform logo url'] = 'Platform logo url';
-$_lang['Platform name'] = 'Platform name';
-$_lang['Platform web URL'] = 'Platform web URL';
-$_lang['Port of CAS server'] = 'Port of CAS server';
-$_lang['Prefix for course table  / db names'] = 'Prefix for course table  / db names';
-$_lang['Prefix for main table names'] = 'Prefix for main table names';
-$_lang['Prefix for tracking table names'] = 'Prefix for tracking table names';
-$_lang['Private&nbsp+ New Registration allowed'] = 'Private&nbsp+ New Registration allowed';
-$_lang['Production'] = 'Production';
 $_lang['Profile SQL'] = 'Profile SQL';
 $_lang['Profile SQL in DEBUG MODE.
 Display for each request :duration, counter,  statement '] = 'Profile SQL in DEBUG MODE.
 Display for each request :duration, counter,  statement';
-$_lang['Profile form'] = 'Profile form';
-$_lang['Public&nbsp;&nbsp;+ New Registration allowed'] = 'Public&nbsp;&nbsp;+ New Registration allowed';
-$_lang['Public&nbsp;&nbsp;+ New Registration denied'] = 'Public&nbsp;&nbsp;+ New Registration denied';
 $_lang['Registration agreement'] = 'Registration agreement';
 $_lang['Relative to the complete platform url'] = 'Relative to the complete platform url';
-$_lang['Repository for cache files'] = 'Repository for cache files';
-$_lang['Repository for cache files and dirs'] = 'Repository for cache files and dirs';
-$_lang['Repository for temporary files and dirs'] = 'Repository for temporary files and dirs';
-$_lang['Root folder of CAS (example : \'esup-cas/\')'] = 'Root folder of CAS (example : \'esup-cas/\')';
-$_lang['Root of CAS server'] = 'Root of CAS server';
-$_lang['SSO authentication key list'] = 'SSO authentication key list';
-$_lang['SSO cookie domain'] = 'SSO cookie domain';
-$_lang['SSO cookie name'] = 'SSO cookie name';
-$_lang['SSO cookie path'] = 'SSO cookie path';
-$_lang['SSO cookie period validity'] = 'SSO cookie period validity';
-$_lang['Secure document download'] = 'Secure document download';
-$_lang['Session lost warning'] = 'Session lost warning';
-$_lang['Set the Cascading Style Sheet (CSS) layout.'] = 'Set the Cascading Style Sheet (CSS) layout.';
-$_lang['Set the default language of the platform. It doesn\'t prevent course managers to set an other language for each course they create.'] = 'Set the default language of the platform. It doesn\'t prevent course managers to set an other language for each course they create.';
-$_lang['Set the editor that will replace standard html textarea.'] = 'Set the editor that will replace standard html textarea.';
 $_lang['Sets how the assignment property "default works visibility" acts.  It will change the visibility of all the new submissions or it will change the visibility of all submissions already done in the assignment and the new one. '] = 'Sets how the assignment property "default works visibility" acts.  It will change the visibility of all the new submissions or it will change the visibility of all submissions already done in the assignment and the new one.';
-$_lang['Sets if user can see only his own submissions (or those from his groups) or if he can see every visible submission.'] = 'Sets if user can see only his own submissions (or those from his groups) or if he can see every visible submission.';
-$_lang['Settings for tutors of group'] = 'Settings for tutors of group';
-$_lang['Settings for users of group'] = 'Settings for users of group';
-$_lang['Settings of the right menu elements'] = 'Settings of the right menu elements';
-$_lang['Settings of the tool'] = 'Settings of the tool';
-$_lang['Settings of the user course list'] = 'Settings of the user course list';
-$_lang['Show groups in resource browser'] = 'Show groups in resource browser';
-$_lang['Show only author submissions'] = 'Show only author submissions';
-$_lang['Show user profile page'] = 'Show user profile page';
-$_lang['Show wiki syntax toolbar'] = 'Show wiki syntax toolbar';
-$_lang['Single'] = 'Single';
-$_lang['Single Sign On'] = 'Single Sign On';
-$_lang['Sql installation query succeeded'] = 'Sql installation query succeeded';
-$_lang['System Info'] = 'System Info';
-$_lang['Teacher can add a user in his course'] = 'Teacher can add a user in his course';
-$_lang['Teacher can add some users in his course'] = 'Teacher can add some users in his course';
-$_lang['Teacher can import an existing class course'] = 'Teacher can import an existing class course';
-$_lang['Teacher can import user list in his course'] = 'Teacher can import user list in his course';
-$_lang['The content of this panel is editable in administration '] = 'The content of this panel is editable in administration';
 $_lang['The domain that the cookie is available.  To make the cookie available on all subdomains of example.com, you\'d set it to ".example.com". The . is not required but makes it compatible with more browsers. Setting it to www.example.com  will make the co'] = 'The domain that the cookie is available.  To make the cookie available on all subdomains of example.com, you\'d set it to ".example.com". The . is not required but makes it compatible with more browsers. Setting it to www.example.com  will make the co';
-$_lang['The name of the cookie. By default the name is "clarolineSsoCookie"'] = 'The name of the cookie. By default the name is "clarolineSsoCookie"';
 $_lang['The path on the server in which the cookie will be available on.  If set to "/", the cookie will be available within the entire domain. If set to "/foo/", the cookie will only be available within the /foo/ directory and all sub-directories such as /f'] = 'The path on the server in which the cookie will be available on.  If set to "/", the cookie will be available within the entire domain. If set to "/foo/", the cookie will only be available within the /foo/ directory and all sub-directories such as /f';
-$_lang['The username is case sensitive'] = 'The username is case sensitive';
-$_lang['Theme'] = 'Theme';
-$_lang['These informations are displayed on the footer of the platform'] = 'These informations are displayed on the footer of the platform';
-$_lang['This is where tracking and statistics data are stored. This database can be the same as the main database.'] = 'This is where tracking and statistics data are stored. This database can be the same as the main database.';
-$_lang['This option insert a command in the user profile form to request a status of course creator. This request is sent by e-mail to platform administrator.'] = 'This option insert a command in the user profile form to request a status of course creator. This request is sent by e-mail to platform administrator.';
-$_lang['This option insert a command in the user profile form to request the removal of the user from the platform.  This request is sent by e-mail to platform administrator.
-This option allow only to request it, and don\'t prework the answer
-'] = 'This option insert a command in the user profile form to request the removal of the user from the platform.  This request is sent by e-mail to platform administrator.
-This option allow only to request it, and don\'t prework the answer
-';
 $_lang['This prefix could allow to order more easily the tables / DB in the user interface of your server technical back office  '] = 'This prefix could allow to order more easily the tables / DB in the user interface of your server technical back office';
 $_lang['This renderer is used for TEX/LaTEX expressions. It is available into the \'claroline/inc/lib/\' directory and has to be copied on a server location where CGI programs are expected.'] = 'This renderer is used for TEX/LaTEX expressions. It is available into the \'claroline/inc/lib/\' directory and has to be copied on a server location where CGI programs are expected.';
-$_lang['This zone is empty'] = 'This zone is empty';
-$_lang['Thumbnail width'] = 'Thumbnail width';
 $_lang['Time before really compute data. 86400 = 1 day.'] = 'Time before really compute data. 86400 = 1 day.';
 $_lang['Time to automatically refresh the user screen. Each refresh is a request to your server.
 Too low value can be hard for your server. Too high value can be hard for user.
@@ -127,14 +24,9 @@ Too low value can be hard for your server. Too high value can be hard for user.
 Too low value can be hard for your server. Too high value can be hard for user.
 ';
 $_lang['Time to keep a cache as valid'] = 'Time to keep a cache as valid';
-$_lang['Today'] = 'Today';
-$_lang['Tracking database name'] = 'Tracking database name';
-$_lang['Translation'] = 'Translation';
 $_lang['Translation: use a single language file
 Production: each script use its own language file.'] = 'Translation: use a single language file
 Production: each script use its own language file.';
-$_lang['Tutors'] = 'Tutors';
-$_lang['Tutors can subscribe to a group as a simple member'] = 'Tutors can subscribe to a group as a simple member';
 $_lang['URL trail'] = 'URL trail';
 $_lang['Unknown question format in file %file'] = 'Unknown question format in file %file';
 $_lang['Use Jpspan mode for the resource linking utility. Warning : Jpspan does not work on IIS web servers.'] = 'Use Jpspan mode for the resource linking utility. Warning : Jpspan does not work on IIS web servers.';
