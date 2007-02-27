1789dfb141a8102e47502a94efc838031b09d7c3
seb <seb@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2007-02-27 14:31:42 +0000

    Add new course description library and class

---
+++ claroline/course_description/lib/courseDescription.lib.php
@@ -0,0 +1,51 @@
+<?php // $Id$
+if ( count( get_included_files() ) == 1 ) die( '---' );
+/**
+ * CLAROLINE
+ *
+ * @version 1.8 $Revision$
+ *
+ * @copyright 2001-2006 Universite catholique de Louvain (UCL)
+ *
+ * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
+ *
+ * @see http://www.claroline.net/wiki/CLDSC/
+ *
+ * @author Claro Team <cvs@claroline.net>
+ *
+ * @package CLDSC
+ *
+ */
+
+
+/**
+ * get all the items
+ *
+ * @param $courseId string  glued dbName of the course to affect default: current course
+ *
+ * @return array of arrays with data of the item
+ *
+ * @author Christophe Gesché <moosh@claroline.net>
+ *
+ */
+
+function course_description_get_item_list($courseId = null)
+{
+    $tbl = claro_sql_get_course_tbl(claro_get_course_db_name_glued($courseId));
+    $tblCourseDescription = $tbl['course_description'];
+    
+$tblCourseDescription = 'c_IMSQTI_course_description2'; // TODO remove this after tests
+
+    $sql = "SELECT `id`,
+                `category`,
+                `title`,
+                `content`,
+                UNIX_TIMESTAMP(`lastEditDate`) AS `unix_lastEditDate`,
+                `visibility`
+        FROM `".$tblCourseDescription."`
+        ORDER BY `category` ASC";
+        
+    return  claro_sql_query_fetch_all($sql);
+}
+
+?>
