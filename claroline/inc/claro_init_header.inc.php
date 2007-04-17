e1fd4a8cdae53fb43032030cd111426e8d35dc3f
moosh <moosh@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2007-04-17 19:06:05 +0000

    use claro_html_foo_banner function

--- claroline/inc/claro_init_header.inc.php
+++ claroline/inc/claro_init_header.inc.php
@@ -8,81 +8,16 @@ if ( count( get_included_files() ) == 1 ) die( '---' );
 /*
  * HTTP HEADER
  */
-if (isset($charset)) header('Content-Type: text/html; charset='. $charset);
 
-if (!empty($httpHeadXtra) && is_array($httpHeadXtra) )
-{
-    foreach($httpHeadXtra as $thisHttpHead)
-    {
-        header($thisHttpHead);
-    }
-}
+claro_send_http_headers();
 
 /*
  * HTML HEADER
  */
 echo claro_html_doctype() . "\n"
 .    '<html>' . "\n"
-.    '<head>' . "\n"
+.    claro_html_headers() . "\n"
 ;
-
-$titlePage = '';
-
-if(!empty($nameTools))
-{
-    $titlePage .= $nameTools . ' - ';
-}
-
-if(!empty($_course['officialCode']))
-{
-    $titlePage .= $_course['officialCode'] . ' - ';
-}
-
-$titlePage .= get_conf('siteName');
-
-?>
-<title><?php echo $titlePage; ?></title>
-
-<meta http-equiv="Content-Script-Type" content="text/javascript" />
-<meta http-equiv="Content-Style-Type" content="text/css" />
-<meta http-equiv="Content-Type" content="text/HTML; charset=<?php echo $charset; ?>"  />
-
-<link rel="stylesheet" type="text/css" href="<?php echo $clarolineRepositoryWeb ?>css/<?php echo $claro_stylesheet ?>" media="screen, projection, tv" />
-<link rel="stylesheet" type="text/css" href="<?php echo $clarolineRepositoryWeb ?>css/print.css" media="print" />
-
-<link rel="top" href="<?php echo $urlAppend ?>/index.php" title="" />
-<link href="http://www.claroline.net/documentation.htm" rel="Help" />
-<link href="http://www.claroline.net/credits.htm" rel="Author" />
-<link href="http://www.claroline.net" rel="Copyright" />
-
-<script type="text/javascript">
-document.cookie="javascriptEnabled=true; path=<?php echo get_conf('urlAppend')?>";
-<?php
-if ( true === get_conf( 'warnSessionLost', true ) && $_uid )
-{
-    echo "function claro_session_loss_countdown(sessionLifeTime){
-    var chrono = setTimeout('claro_warn_of_session_loss()', sessionLifeTime * 1000);
-}
-
-function claro_warn_of_session_loss() {
-    alert('" . clean_str_for_javascript (get_lang('WARNING ! You have just lost your session on the server.') . "\n" . get_lang('Copy any text you are currently writing and paste it outside the browser')) . "');
-}
-";
-    $claroBodyOnload[] = 'claro_session_loss_countdown(' . ini_get('session.gc_maxlifetime') . ');';
-}
-?>
-</script>
-
-<?php
-if ( isset($htmlHeadXtra) && is_array($htmlHeadXtra) )
-{
-    foreach($htmlHeadXtra as $thisHtmlHead)
-    {
-        echo($thisHtmlHead);
-    }
-}
-
-echo '</head>';
 
 if ( isset( $claroBodyOnload ) )
 {
