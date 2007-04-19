9da0904773e6e36381168f528d5c7d43b0e268f2
moosh <moosh@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2007-04-19 04:32:27 +0000

    - phpdoc indent

--- claroline/inc/claro_init_header.inc.php
+++ claroline/inc/claro_init_header.inc.php
@@ -1,32 +1,34 @@
 <?php // $Id$
-if ( count( get_included_files() ) == 1 ) die( '---' );
-
-/* --------------------------------------
- * HEADERS SECTION
- * --------------------------------------*/
-
-/*
- * HTTP HEADER
+/**
+ * CLAROLINE
+ *
+ * @version 1.9 $Revision$
+ *
+ * @copyright 2001-2007 Universite catholique de Louvain (UCL)
+ *
+ * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
+ *
+ * @package CLKERNEL
+ *
+ * @author Claro Team <cvs@claroline.net>
+ *
  */
 
+if ( count( get_included_files() ) == 1 ) die( '---' );
+// Start the output, no outpout would be done before call of this file
 claro_send_http_headers();
-
 /*
 * HTML HEADER
 */
 echo claro_html_doctype() . "\n"
 .    '<html>' . "\n"
 .    claro_html_headers() . "\n"
-;
 
-if ( isset( $claroBodyOnload ) )
-{
-    echo '<body dir="' . $text_dir . '" onload="' . implode('', $claroBodyOnload ) . '">';
-}
-else
-{
-    echo '<body dir="' . $text_dir . '">';
-}
+.    '<body dir="' . $text_dir . '" '
+.    ( isset( $claroBodyOnload ) ? ' onload="' . implode('', $claroBodyOnload ) . '" ':'')
+.    '>'
+
+;
 
 //  Banner
 
