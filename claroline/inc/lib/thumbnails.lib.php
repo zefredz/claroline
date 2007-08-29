8fafb659f6fb0e51102124d5351f3a9225748648
Frédéric Minne <zefredz@users.noreply.github.com>
2007-08-29 13:27:36 +0000

    protect lib for direct call

--- claroline/inc/lib/thumbnails.lib.php
+++ claroline/inc/lib/thumbnails.lib.php
@@ -2,6 +2,11 @@
 
     // vim: expandtab sw=4 ts=4 sts=4:
     
+    if ( count( get_included_files() ) == 1 )
+    {
+        die( 'The file ' . basename(__FILE__) . ' cannot be accessed directly, use include instead' );
+    }
+    
     function img_get_extension( $imgPath )
     {
         $pathInfo = pathinfo( $imgPath );
