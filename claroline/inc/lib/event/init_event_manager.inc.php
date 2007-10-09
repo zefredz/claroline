7753ea44712a0492d5b2fc2fe01debb7918fd5ee
Frédéric Minne <zefredz@users.noreply.github.com>
2007-10-09 19:33:06 +0000

    file protection against direct inclusion

--- claroline/inc/lib/event/init_event_manager.inc.php
+++ claroline/inc/lib/event/init_event_manager.inc.php
@@ -1,5 +1,11 @@
 <?php // $Id$
-if ( count( get_included_files() ) == 1 ) die( '---' );
+
+if ( count( get_included_files() ) == 1 )
+{
+    die( 'The file ' . basename(__FILE__) . ' cannot be accessed directly, use include instead' );
+}
+
+
 /**
  * Declaration of needed CLASSES for the EventManager pattern
  *
