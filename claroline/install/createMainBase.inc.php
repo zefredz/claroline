f3ab0e21c43467ce8e8ca3ebf8c4c6ca6b9272e5
seb <seb@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2007-11-13 15:24:47 +0000

    Add logging table

--- claroline/install/createMainBase.inc.php
+++ claroline/install/createMainBase.inc.php
@@ -192,6 +192,19 @@ $creationStatementList[] = "CREATE TABLE `".$mainTblPrefixForm."tracking_event` 
   KEY `course_id` (`course_code`)
 ) TYPE=MyISAM";
 
+$creationStatementList[] = "CREATE TABLE `".$mainTblPrefixForm."log` (
+  `id` int(11) NOT NULL auto_increment,
+  `course_code` varchar(40) NULL DEFAULT NULL,
+  `tool_id` int(11) NULL DEFAULT NULL,
+  `user_id` int(11) NULL DEFAULT NULL,
+  `ip` varchar(15) NULL DEFAULT NULL,
+  `date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `type` varchar(60) NOT NULL DEFAULT '',
+  `data` text NOT NULL DEFAULT '',
+  PRIMARY KEY  (`id`),
+  KEY `course_id` (`course_code`)
+) TYPE=MyISAM";
+
 // table used for upgrading tools
 
 $creationStatementList[] = "CREATE TABLE `".$mainTblPrefixForm."upgrade_status` (
