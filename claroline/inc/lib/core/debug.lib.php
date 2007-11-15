2f058a2e3751a4e5b3ca8aa14f76d27379707add
Frédéric Minne <zefredz@users.noreply.github.com>
2007-11-15 12:25:39 +0000

    add profiler class

--- claroline/inc/lib/core/debug.lib.php
+++ claroline/inc/lib/core/debug.lib.php
@@ -8,7 +8,7 @@
     }
     
     /**
-     * Debug functions
+     * Debugging functions and classes
      *
      * @version     1.9 $Revision$
      * @copyright   2001-2007 Universite catholique de Louvain (UCL)
@@ -22,5 +22,136 @@
     function dbg_html_var( $var )
     {
         return htmlspecialchars(var_export( $var, true ));
+    }
+
+    class Profiler
+    {
+    	const PROFILER_STATUS_STARTED = 'started';
+    	const PROFILER_STATUS_NOT_STARTED = 'not_started';
+    	const PROFILER_STATUS_STOPPED = 'stopped';
+    	
+        private $startTime;
+        private $status;
+        private $endTime;
+        private $log;
+
+        public function __construct()
+        {
+            $this->log = array();
+            $this->status = self::PROFILER_STATUS_NOT_STARTED;
+        }
+
+        public function start( $restart = false )
+        {
+            if ( $this->status == self::PROFILER_STATUS_STARTED
+                && ! $restart )
+            {
+                return;
+            }
+            
+            $this->startTime = $this->_getCurrentTime();
+            $this->status = self::PROFILER_STATUS_STARTED;
+            
+            pushClaroMessage(sprintf("&gt;&gt; Profiler (re)started at %f", $this->startTime), 'profile');
+        }
+
+        public function restart()
+        {
+            $this->start( true );
+        }
+
+        public function stop()
+        {
+            if ( $this->status != self::PROFILER_STATUS_STARTED )
+            {
+                $this->restart();
+            }
+
+            $this->endTime = $this->_getCurrentTime();
+            $this->status = self::PROFILER_STATUS_STOPPED;
+            
+            pushClaroMessage(sprintf("&gt;&gt; Profiler stoped at %f", $this->endTime), 'profile');
+            pushClaroMessage(
+                sprintf("** Elapsed time : %f seconds **", $this->getElapsedTime())
+                , 'profile');
+        }
+
+        public function mark( $file, $line, $msg = '##MARK##' )
+        {
+            if ( $this->status != self::PROFILER_STATUS_STARTED )
+            {
+                $this->restart();
+            }
+
+            $now = $this->_getCurrentTime();
+
+            $elapsed = $now - $this->startTime;
+            $elapsed = sprintf( '%f seconds', $elapsed );
+            $timestamp = sprintf( '[@%f]', $now );
+
+            $mark = "$timestamp $msg <br />in $file at line $line after $elapsed";
+
+            $this->log[] = $mark;
+            pushClaroMessage($mark, 'profile');
+        }
+
+        public function report( $htmlReport = true )
+        {
+            if ( $this->status != self::PROFILER_STATUS_STOPPED )
+            {
+                $this->stop();
+            }
+
+            if ( $htmlReport )
+            {
+                return $this->_htmlReport();
+            }
+            else
+            {
+                return $this->_plainReport();
+            }
+        }
+        
+        public function getElapsedTime()
+        {
+            return $this->endTime - $this->startTime;
+        }
+
+        private function _htmlReport()
+        {
+            $report = '<pre>' . "\n";
+
+            $report .= $this->_plainReport();
+
+            $report .= '</pre>' . "\n";
+
+            return $report;
+        }
+
+        private function _plainReport()
+        {
+            $report = "--- Start of Profiler Report ---\n";
+
+            $report .= "\nProfiler started at " . $this->startTime . "\n";
+
+            $report .= implode( "\n", $this->log );
+
+            $report .= "\nProfiler stoped at " . $this->endTime . "\n";
+
+            $strTime = "Elapsed time : %f seconds\n";
+            $elapsedTime = $this->getElapsedTime();
+
+            $report .= sprintf( $strTime, $elapsedTime );
+
+            $report .= "--- End of Profiler Report ---\n\n";
+
+            return $report;
+        }
+
+        private function _getCurrentTime()
+        {
+            list($usec, $sec) = explode(" ", microtime());
+            return ((float)$usec + (float)$sec);
+        }
     }
 ?>
