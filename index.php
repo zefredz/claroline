2f81f0c3845b56d27697a4a56c4b05059c0a3831
moosh_fr <moosh_fr@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2008-01-01 10:32:40 +0000

    - happy new year 2008

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
  ******************************************************************************
  * Campus Home Page
  *
- * @version 1.8 $Revision$
+ * @version 1.9 $Revision$
  *
- * @copyright (c) 2001-2007 Universite catholique de Louvain (UCL)
+ * @copyright (c) 2001-2008 Universite catholique de Louvain (UCL)
  *
  * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
  *
