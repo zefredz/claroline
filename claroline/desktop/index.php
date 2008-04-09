1093fc6eaf0a9d48f33d2392ee003cd9aee1ab40
gregk84 <gregk84@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2008-04-09 06:14:16 +0000

    add index.php

--- claroline/desktop/index.php
+++ claroline/desktop/index.php
@@ -26,8 +26,8 @@
     require_once get_path( 'includePath' ) . '/lib/user.lib.php';
     require_once dirname(__FILE__) . '/lib/portlet.lib.php';
     require_once dirname(__FILE__) . '/lib/portletRightMenu.lib.php';
-    require_once dirname(__FILE__) . '/lib/filefinder.lib.php';
     require_once dirname(__FILE__) . '/lib/porletInsertConfigDB.lib.php';
+    uses('utils/filefinder.lib.php');
 
     // users authentified 
     if( ! claro_is_user_authenticated() ) claro_disp_auth_form();
