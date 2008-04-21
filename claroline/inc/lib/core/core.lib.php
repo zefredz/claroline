855b53d69d9c34c7f00d1994640adf722484a2fb
Frédéric Minne <zefredz@users.noreply.github.com>
2008-04-21 07:56:03 +0000

    Allow loading all libraries in a directory (WARNING not recursively !!!!) by using uses( 'directory_name/*' );

--- claroline/inc/lib/core/core.lib.php
+++ claroline/inc/lib/core/core.lib.php
@@ -114,30 +114,75 @@
         
         foreach ( $args as $lib )
         {
-            if ( substr($lib, -4) !== '.php' ) $lib .= '.php';
-            
-            $lib = protect_against_file_inclusion( $lib );
-            
-            $kernelPath = INCLUDES . '/' . $lib;
-            $localPath = get_module_path(get_current_module_label()) . '/lib/' . $lib;
-            
-            if ( file_exists( $localPath ) )
+            if ( basename( $lib ) == '*' )
             {
-                require_once $localPath;
-            }
-            elseif ( file_exists( $kernelPath ) )
-            {
-                require_once $kernelPath;
+                uses( 'utils/finder.lib' );
+                
+                $dir = dirname( $lib );
+                
+                $kernelPath = INCLUDES . '/' . $dir;
+                $localPath = get_module_path(get_current_module_label()) . '/lib/' . $dir;
+                
+                if ( file_exists( $kernelPath )
+                    && is_dir( $kernelPath )
+                    && is_readable( $kernelPath )
+                )
+                {
+                    $path = $kernelPath;
+                }
+                elseif ( file_exists( $localPath )
+                    && is_dir( $localPath )
+                    && is_readable( $localPath )
+                )
+                {
+                    $path = $localPath;
+                }
+                else
+                {
+                    if ( claro_debug_mode() )
+                    {
+                        throw new Exception( "Cannot load libraries from {$dir}" );
+                    }
+                    
+                    $notFound[] = $lib;
+                    
+                    continue;
+                }
+                
+                $finder = new Claro_FileFinder_Extension( $path, '.php', false );
+                
+                foreach ( $finder as $file )
+                {
+                    require_once $file->getRealPath();
+                }
             }
             else
             {
-                // error not found
-                if ( claro_debug_mode() ) 
-                {
-                    throw new Exception( "Lib not found $lib" );
-                }
+                if ( substr($lib, -4) !== '.php' ) $lib .= '.php';
                 
-                $notFound[] = $lib;
+                $lib = protect_against_file_inclusion( $lib );
+                
+                $kernelPath = INCLUDES . '/' . $lib;
+                $localPath = get_module_path(get_current_module_label()) . '/lib/' . $lib;
+                
+                if ( file_exists( $localPath ) )
+                {
+                    require_once $localPath;
+                }
+                elseif ( file_exists( $kernelPath ) )
+                {
+                    require_once $kernelPath;
+                }
+                else
+                {
+                    // error not found
+                    if ( claro_debug_mode() ) 
+                    {
+                        throw new Exception( "Lib not found $lib" );
+                    }
+                    
+                    $notFound[] = $lib;
+                }
             }
         }
         
