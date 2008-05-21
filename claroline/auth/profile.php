3c429035bd027b93cb0c03717f3bdfc1f6b3994b
fragile_be <fragile_be@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2008-05-21 15:14:53 +0000

    Fix url to user tracking

--- claroline/auth/profile.php
+++ claroline/auth/profile.php
@@ -308,7 +308,7 @@ switch ( $display )
         if( get_conf('is_trackingEnabled') )
         {
             // display user tracking link
-            $profileMenu[] = '<a class="claroCmd" href="' . get_conf('urlAppend') . '/claroline/tracking/userLog.php?userId='.claro_get_current_user_id() . claro_url_relay_context('&amp;') . '">'
+            $profileMenu[] = '<a class="claroCmd" href="' . get_conf('urlAppend') . '/claroline/tracking/userReport.php?userId='.claro_get_current_user_id() . claro_url_relay_context('&amp;') . '">'
             .                 '<img src="' . get_icon_url('statistics') . '" alt="" />' . get_lang('View my statistics')
             .                 '</a>'
             ;
