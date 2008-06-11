405624b32e9b48065da0d1ac1909d04ef49f902f
Frédéric Minne <zefredz@users.noreply.github.com>
2008-06-11 07:33:14 +0000

    version 1.9

--- claroline/right/profile.php
+++ claroline/right/profile.php
@@ -4,7 +4,7 @@
  *
  * This tool manage profile of the course
  *
- * @version 1.8 $Revision$
+ * @version 1.9 $Revision$
  * @copyright (c) 2001-2008 Universite catholique de Louvain (UCL)
  *
  * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
