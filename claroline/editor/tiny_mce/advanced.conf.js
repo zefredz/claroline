27f04d57c4e2cd1f319fe69ac80afd407b36c5e4
fragile_be <fragile_be@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2008-06-13 09:24:44 +0000

    Use template module

--- claroline/editor/tiny_mce/advanced.conf.js
+++ claroline/editor/tiny_mce/advanced.conf.js
@@ -1,10 +1,12 @@
+var baseURI = tinyMCE.baseURI.path;
+
 tinyMCE.init({
 
 	//-- general
     mode : "textareas",
     editor_selector : "advancedMCE",
     // plugins must be the same as in tinyMCE_GZ.init
-    plugins : "media,paste,table,safari",
+    plugins : "template,media,paste,table,safari",
     theme : "advanced",
     browsers : "safari,msie,gecko,opera",
 	directionality : text_dir,
@@ -16,7 +18,7 @@ tinyMCE.init({
     
     //-- advanced theme
     theme_advanced_buttons1 : "fontselect,fontsizeselect,formatselect,bold,italic,underline,strikethrough,separator,sub,sup,separator,undo,redo",
-    theme_advanced_buttons2 : "cut,copy,paste,pasteword,separator,justifyleft,justifycenter,justifyright,justifyfull,separator,bullist,numlist,separator,outdent,indent,separator,forecolor,backcolor,separator,hr,link,unlink,image,media,code",
+    theme_advanced_buttons2 : "cut,copy,paste,pasteword,separator,justifyleft,justifycenter,justifyright,justifyfull,separator,bullist,numlist,separator,outdent,indent,separator,forecolor,backcolor,separator,hr,link,unlink,image,media,template,code",
     theme_advanced_buttons3 : "tablecontrols,separator,help",
     theme_advanced_toolbar_location : "top",
     theme_advanced_toolbar_align : "left",
@@ -30,5 +32,8 @@ tinyMCE.init({
     apply_source_formatting : true,
 	cleanup_on_startup : true,
     entity_encoding : "raw",
-    extended_valid_elements : "a[name|href|target|title|onclick],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name],hr[class|width|size|noshade],font[face|size|color|style],span[class|align|style]"
+    extended_valid_elements : "a[name|href|target|title|onclick],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|onmouseover|onmouseout|name],hr[class|width|size|noshade],font[face|size|color|style],span[class|align|style]",
+    
+    //-- Other functionnalities
+    template_external_list_url : baseURI + "../../backends/template_list.php"
 });
--- claroline/editor/tiny_mce/advanced_gzip.conf.js
+++ claroline/editor/tiny_mce/advanced_gzip.conf.js
@@ -1,6 +1,6 @@
 tinyMCE_GZ.init({
 	// plugins must be the same as in tinyMCE.init
-	plugins : "media,paste,table,safari",
+	plugins : "template,media,paste,table,safari",
 	themes : "advanced",
 	languages : "en",
 	disk_cache : true,
