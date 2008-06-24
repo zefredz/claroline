33b1de47f019d8a284099136a90555c5c88806c6
fragile_be <fragile_be@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2008-06-24 12:12:57 +0000

    Show an image on ajax calls (use show and hide functions directly in setFileList function as ajaxStart and ajaxStop function doesn't seems to work; trigger change event on src field to force automatic update on others fields and preview

--- claroline/editor/tiny_mce/tiny_mce/plugins/claroimage/js/ajax.js
+++ claroline/editor/tiny_mce/tiny_mce/plugins/claroimage/js/ajax.js
@@ -2,11 +2,14 @@
     {
         // init file list.  After an upload we have to display the correct list so pass it directly 
         setFileList(relativePath);
+	    
+	    $("#processing").hide();
     });
     
     
     function setFileList(relPath)
     {
+        $("#processing").show();
         $.ajax({
             url: "./backend.php",
             data: "cmd=getFileList&relPath=" + relPath,
@@ -17,15 +20,24 @@
                         $("#image_list").append(response);
                         $("#relativePath").val(relPath);
                     }
+                    // hide processing icon after receiving ajax response
+                    $("#processing").hide();
                 },
             error: function(response){
+                $("#processing").hide();
             },
             dataType: 'html'
         });
+
     }
     
     function selectImage(imageUrl, elem)
     {
+        // set src 
         $('#src').val(imageUrl);
-        ImageDialog.showPreviewImage(imageUrl, 1);
+        // force change event on src field to force claculation of size etc...
+        $('#src').trigger('onchange');
+        // and force previewing if required
+        // normally this shouldn't be needed as the change event on src request a showPreviewImage
+        //ImageDialog.showPreviewImage(imageUrl, 1);
     }
