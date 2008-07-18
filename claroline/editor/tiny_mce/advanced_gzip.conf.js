1fa4d01c551c020fbbfc88405fb380adae7cd950
fragile_be <fragile_be@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2008-07-18 13:25:55 +0000

    Add new plugin to allow easy inclusion of youtube and dailymotion videos

--- claroline/editor/tiny_mce/advanced_gzip.conf.js
+++ claroline/editor/tiny_mce/advanced_gzip.conf.js
@@ -1,6 +1,6 @@
 tinyMCE_GZ.init({
 	// plugins must be the same as in tinyMCE.init
-	plugins : "template,media,paste,table,safari,claroimage",
+	plugins : "template,media,paste,table,safari,claroimage,dailytube",
 	themes : "advanced",
 	languages : "en",
 	disk_cache : true,
