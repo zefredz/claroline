9063bcc4438f4a9acd423657bb87d14bba669853
Frédéric Minne <zefredz@users.noreply.github.com>
2008-08-08 12:19:41 +0000

    load required lib

--- claroline/desktop/lib/userprofilebox.lib.php
+++ claroline/desktop/lib/userprofilebox.lib.php
@@ -2,10 +2,7 @@
 
 // vim: expandtab sw=4 ts=4 sts=4:
 
-if ( count( get_included_files() ) == 1 )
-{
-    die( 'The file ' . basename(__FILE__) . ' cannot be accessed directly, use include instead' );
-}
+FromKernel::uses('user.lib');
 
 /**
 * CLAROLINE
