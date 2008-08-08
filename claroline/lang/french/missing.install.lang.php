68dcff6ff4c606294b211b67bc503a90d40e59fa
fragile_be <fragile_be@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2008-08-08 13:13:41 +0000

    update translation

---
+++ claroline/lang/french/missing.install.lang.php
@@ -0,0 +1,5 @@
+<?php 
+$_lang['Database <em>%dbName</em> already exists'] = 'Database <em>%dbName</em> already exists';
+$_lang['However, you can record tracking data into a separate database or set a specific prefix for tracking tables.'] = 'However, you can record tracking data into a separate database or set a specific prefix for tracking tables.';
+$_lang['Powered by %clarolineLink'] = 'Powered by %clarolineLink';
+?>
