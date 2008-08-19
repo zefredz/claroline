6c2dcac03f8c569db804d63fe215e44dde763b31
fragile_be <fragile_be@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2008-08-19 06:25:17 +0000

    Missing is empty as translation is complete

--- claroline/lang/italian/missing.install.lang.php
+++
@@ -1,5 +0,0 @@
-<?php 
-$_lang['For help ask on %clarolineForumLink'] = 'For help ask on %clarolineForumLink';
-$_lang['Is root folder readable ?'] = 'Is root folder readable ?';
-$_lang['Is root folder writable ?'] = 'Is root folder writable ?';
-?>
