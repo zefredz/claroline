486d927815eff719c145ca1406efdc8f089daccf
fragile_be <fragile_be@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2008-08-27 15:04:37 +0000

    Remove deprecated link to cvs view

--- claroline/admin/xtra/sdk/lang/progression_translation.php
+++ claroline/admin/xtra/sdk/lang/progression_translation.php
@@ -140,7 +140,7 @@ if ( isset($_REQUEST['exCmd']) && $_REQUEST['exCmd'] == 'ToTranslate' )
         }
 
         echo "<td>". $varName ."</td>\n"
-            . "<td><a href=\"http://cvs.claroline.net/cgi-bin/viewcvs.cgi/claroline/" . $sourceFile . "\">". $sourceFile ."</a></td>\n"
+            . "<td>". $sourceFile ."</td>\n"
             . "</tr>\n";
     }
 
