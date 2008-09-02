e7761555181fd8c4d83d4d783777b216f3b48bf2
fragile_be <fragile_be@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2008-09-02 08:01:17 +0000

    Tracking tables should also go to the paradise of mysql tables

--- claroline/exercise/setup/course_uninstall.sql
+++ claroline/exercise/setup/course_uninstall.sql
@@ -12,8 +12,8 @@ DROP TABLE IF EXISTS `__CL_COURSE__qwz_answer_fib`;
 
 DROP TABLE IF EXISTS `__CL_COURSE__qwz_answer_matching`;
 
-DROP TABLE IF EXISTS `__CL_COURSE__qwz_question`;
+DROP TABLE IF EXISTS `__CL_COURSE__qwz_tracking`;
 
-DROP TABLE IF EXISTS `__CL_COURSE__qwz_rel_exercise_question`;
+DROP TABLE IF EXISTS `__CL_COURSE__qwz_tracking_questions`;
 
-DROP TABLE IF EXISTS `__CL_COURSE__qwz_answer_truefalse`;
+DROP TABLE IF EXISTS `__CL_COURSE__qwz_tracking_answers`;
