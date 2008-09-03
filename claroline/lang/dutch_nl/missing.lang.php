fc805dac5be7689987ad016b5fd0d4bae272d7d6
fragile_be <fragile_be@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2008-09-03 15:01:10 +0000

    Move dutch to dutch_BE and add new Dutch_NL by Dominicus de Vries

---
+++ claroline/lang/dutch_nl/missing.lang.php
@@ -0,0 +1,39 @@
+<?php 
+$_lang['Access allowed only to course members (people on the <a href="%url">user list</a>)'] = 'Access allowed only to course members (people on the <a href="%url">user list</a>)';
+$_lang['Access allowed only to course members (people on the user list)'] = 'Access allowed only to course members (people on the user list)';
+$_lang['Access allowed only to platform members (user registered to the platform)'] = 'Access allowed only to platform members (user registered to the platform)';
+$_lang['Access allowed to anybody (even without login)'] = 'Access allowed to anybody (even without login)';
+$_lang['Access to platform'] = 'Access to platform';
+$_lang['Allowed with enrolment key'] = 'Allowed with enrolment key';
+$_lang['An error occurred.  Learning Path import failed.'] = 'An error occurred.  Learning Path import failed.';
+$_lang['Are you sure to delete selected message(s) ?'] = 'Are you sure to delete selected message(s) ?';
+$_lang['Configure the way files are downloaded from the platform'] = 'Configure the way files are downloaded from the platform';
+$_lang['Courses : %disk_usage (perhaps with other directories)'] = 'Courses : %disk_usage (perhaps with other directories)';
+$_lang['Data checking'] = 'Data checking';
+$_lang['Delete messages'] = 'Delete messages';
+$_lang['Display all feedbacks'] = 'Display all feedbacks';
+$_lang['Display of detailed answers is not authorized.'] = 'Display of detailed answers is not authorized.';
+$_lang['Enable cache'] = 'Enable cache';
+$_lang['Enabling the cache may increase performance'] = 'Enabling the cache may increase performance';
+$_lang['If activated, all the feedbacks will be shown to users; if not, only those corresponding to the selected or correct answers will be displayed'] = 'If activated, all the feedbacks will be shown to users; if not, only those corresponding to the selected and correct answers will be displayed.';
+$_lang['If false, other fields are optional'] = 'If false, other fields are optional';
+$_lang['If the user is existing in the platform, he will be added to the class only if his firstname, 
+                                        lastname and username are similar.'] = 'If the user is existing in the platform, he will be added to the class only if his firstname, 
+                                        lastname and username are similar.';
+$_lang['If the user is existing in the platform, he will be added to the course only if his firstname, 
+                                        lastname and username are similar.'] = 'If the user is existing in the platform, he will be added to the course only if his firstname, 
+                                        lastname and username are similar.';
+$_lang['Impossible to promote as a group tutor a student that is already register in the group'] = 'Impossible to promote as a group tutor a student that is already register in the group';
+$_lang['In iCal an event has a duration but not in claroline. 3600 seconds = 1 Hour.'] = 'In iCal an event has a duration but not in claroline. 3600 seconds = 1 Hour.';
+$_lang['Manage administrator email notifications'] = 'Manage administrator email notifications';
+$_lang['Number of exercises per page'] = 'Number of exercises per page';
+$_lang['Reserved to course members'] = 'Reserved to course members';
+$_lang['Reserved to platform members'] = 'Reserved to platform members';
+$_lang['Select date'] = 'Select date';
+$_lang['Select interval'] = 'Select interval';
+$_lang['Time before really compute data. 86400 seconds = 1 day.'] = 'Time before really compute data. 86400 seconds = 1 day.';
+$_lang['Unnamed module'] = 'Unnamed module';
+$_lang['Unnamed path'] = 'Unnamed path';
+$_lang['User registered in a course having an unexisting (deprecated) status'] = 'User registered in a course having an unexisting (deprecated) status';
+$_lang['You can choose a title and a description for the wiki :'] = 'You can choose a title and a description for the wiki : ';
+?>
