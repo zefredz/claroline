4419717cfdd4867e1aee98285704b5e8e265f488
fragile_be <fragile_be@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2008-10-03 13:16:52 +0000

    Use same display as in main user course list

--- claroline/desktop/lib/portlet/mycourselist.class.php
+++ claroline/desktop/lib/portlet/mycourselist.class.php
@@ -21,6 +21,8 @@ if ( count( get_included_files() ) == 1 )
 */
 
 uses('courselist.lib');
+// we need CLHOME conf file for render_user_course_list function
+include claro_get_conf_repository() . 'CLHOME.conf.php'; // conf file
 
 class MyCourseList extends UserDesktopPortlet
 {
@@ -28,91 +30,18 @@ class MyCourseList extends UserDesktopPortlet
     {
         global $platformLanguage;
 
-        $output = '<a class="claroCmd" href="'.get_path('url')
+        $out = '<a class="claroCmd" href="'.get_path('url')
             . '/index.php#myCourseList">'
             . '<img src="' . get_icon_url('edit') . '" alt="" /> '
             . get_lang('Edit')
             . '</a>'
             ;
 
-        $personnalCourseList = get_user_course_list(claro_get_current_user_id());
+        $out .= '<div id="portletMyCourseList">'
+        . render_user_course_list()
+        . '</div>' . "\n";
 
-        // get the list of personnal courses marked as contening new events
-        if ( count($personnalCourseList) )
-        {
-            $output .= '<dl id="portletMyCourseList">'."\n";
-
-            foreach($personnalCourseList as $thisCourse)
-            {
-                if ($thisCourse['isCourseManager'] == 1)
-                {
-                    $userStatusImg = '&nbsp;&nbsp;<img src="' . get_icon_url('manager') . '" alt="'.get_lang('Course manager').'" />';
-                }
-                else
-                {
-                    $userStatusImg = '';
-                }
-
-                // show course language if not the same of the platform
-                if ( $platformLanguage != $thisCourse['language'] )
-                {
-                    if ( !empty($langNameOfLang[$thisCourse['language']]) )
-                    {
-                        $course_language_txt = ' - ' . ucfirst($langNameOfLang[$thisCourse['language']]);
-                    }
-                    else
-                    {
-                        $course_language_txt = ' - ' . ucfirst($thisCourse['language']);
-                    }
-                }
-                else
-                {
-                    $course_language_txt = '';
-                }
-
-                $course_order_by = $thisCourse['title'];
-
-                $url = get_path('url') . '/claroline/course/index.php?cid='
-                .    htmlspecialchars($thisCourse['sysCode'])
-                ;
-
-                $output .= '<dt>' . "\n"
-                .    '<img class="iconDefinitionList" src="' . get_icon_url('course') . '" alt="" />'
-                .    '<small>'
-                .    '<a href="' . htmlspecialchars( $url ) . '">'
-                .    $course_order_by
-                .    $userStatusImg
-                .    '</a>' . "\n"
-                .    '</small>' . "\n"
-                .    '</dt>' . "\n"
-                .    '<dd>'
-                .    '<small>'
-                .    '<a href="' . $url . '">'
-                .    htmlspecialchars( $thisCourse['officialCode'] )
-                .    '</a>' . "\n"
-                .    '<small>' . "\n"
-                .    ' : ' . htmlspecialchars( $thisCourse['titular'] . $course_language_txt )
-                .    '</small>' . "\n"
-                .    '</small>' . "\n"
-                .    '</dd>' . "\n"
-                ;
-            }
-
-            $output .= '</dl>' . "\n";
-        }
-        else
-        {
-            $output .= "\n"
-            .    '<dl>' . "\n"
-            .    '<dt>' . "\n"
-            .    '<img class="iconDefinitionList" src="' . get_icon_url('course') . '" alt="" />'
-            .    get_lang('No courses') . "\n"
-            .    '</dt>' . "\n"
-            .    '</dl>' . "\n"
-            ;
-        }
-
-        $this->content = $output;
+        $this->content = $out;
 
         return $this->content;
     }
