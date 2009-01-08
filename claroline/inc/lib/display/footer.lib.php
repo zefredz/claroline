30a618e1c8cd47b3f9e16354295d5b0d730856d4
fragile_be <fragile_be@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2009-01-08 12:00:31 +0000

    Happy new Claroline Year !

--- claroline/inc/lib/display/footer.lib.php
+++ claroline/inc/lib/display/footer.lib.php
@@ -113,7 +113,7 @@
             $poweredByOutput = '<div id="poweredBy">'
                 . get_lang('Powered by')
                 . ' <a href="http://www.claroline.net" target="_blank">Claroline</a> '
-                . '&copy; 2001 - 2008'
+                . '&copy; 2001 - 2009'
                 . '</div>'
                 ;
             
