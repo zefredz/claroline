a907c1829cc770c47841f4e1a4112e81c54b3f50
fragile_be <fragile_be@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2009-01-13 08:56:54 +0000

    Update missing lang files to last changes

--- claroline/lang/french/missing.lang.php
+++ claroline/lang/french/missing.lang.php
@@ -1,4 +1,21 @@
 <?php 
+$_lang['Access allowed only to course members (people on the <a href="%url">course user list</a>)'] = 'Access allowed only to course members (people on the <a href="%url">course user list</a>)';
+$_lang['Access allowed only to course members (people on the user list)'] = 'Access allowed only to course members (people on the user list)';
+$_lang['Administration tools'] = 'Administration tools';
 $_lang['Any'] = 'Any';
-$_lang['User profile box'] = 'User profile box';
+$_lang['Attach an existing resource'] = 'Attach an existing resource';
+$_lang['Available from %startDate until %endDate'] = 'Available from %startDate until %endDate';
+$_lang['Cannot delete : missing file or directory name'] = 'Cannot delete : missing file or directory name';
+$_lang['Cannot give manager rights to guest or anonymous users.'] = 'Cannot give manager rights to guest or anonymous users.';
+$_lang['Group user courses by categories'] = 'Group user courses by categories';
+$_lang['Impossible to promote as a group tutor a student who is already registered in the group'] = 'Impossible to promote as a group tutor a student who is already registered in the group';
+$_lang['Module activation in courses failed'] = 'Module activation in courses failed';
+$_lang['Module activation in courses succeeded'] = 'Module activation in courses succeeded';
+$_lang['Simple'] = 'Simple';
+$_lang['There is already an account with this username.'] = 'There is already an account with this username.';
+$_lang['This account has been disabled, please contact the platform administrator'] = 'This account has been disabled, please contact the platform administrator';
+$_lang['Time exceeded'] = 'Time exceeded';
+$_lang['Trigger debug mode in url'] = 'Trigger debug mode in url';
+$_lang['View platform statistics'] = 'View platform statistics';
+$_lang['You have reached the maximum of %allowedAttempts allowed attempts.'] = 'You have reached the maximum of %allowedAttempts allowed attempts.';
 ?>
--- claroline/lang/simpl_chinese/missing.lang.php
+++ claroline/lang/simpl_chinese/missing.lang.php
@@ -1,36 +1,21 @@
 <?php 
+$_lang['Access allowed only to course members (people on the <a href="%url">course user list</a>)'] = 'Access allowed only to course members (people on the <a href="%url">course user list</a>)';
 $_lang['Access allowed only to course members (people on the course user list)'] = 'Access allowed only to course members (people on the user list)';
+$_lang['Attach an existing resource'] = 'Attach an existing resource';
+$_lang['Available from %startDate until %endDate'] = 'Available from %startDate until %endDate';
+$_lang['Cannot delete : missing file or directory name'] = 'Cannot delete : missing file or directory name';
+$_lang['Cannot give manager rights to guest or anonymous users.'] = 'Cannot give manager rights to guest or anonymous users.';
 $_lang['Fill in the text area to motivate your request and then submit the form to send it to platform administrators'] = 'Fill the area to explain your motivation and submit your request. An e-mail will be sent to platform adminisrator(s).';
-$_lang['If the name and the firstname are longer than this value, the script reduce it.
-For revelance, it\'s interesting to not work with to little value'] = 'If the name and the firstname are longer than this value, the script reduce it.
-For revelance, it\'s interesting to not work with to little value';
-$_lang['If the user is existing in the platform, he will be added to the class only if his firstname, 
-                                        lastname and username are similar.'] = 'If the user is existing in the platform, he will be added to the class only if his firstname, 
-                                        lastname and username are similar.';
-$_lang['If the user is existing in the platform, he will be added to the course only if his firstname, 
-                                        lastname and username are similar.'] = 'If the user is existing in the platform, he will be added to the course only if his firstname, 
-                                        lastname and username are similar.';
-$_lang['Note :  this repository should be protected with a .htaccess or
-       be placed outside the web. Because there contain data of private courses.'] = 'Note :  this repository should be protected with a .htaccess or
-       be placed outside the web. Because there contain data of private courses.';
-$_lang['Note : this repository should be protected with a .htaccess or
-       be placed outside the web. Because there contain data of private courses. Claroline Would be able to read and write in this dir'] = 'Note : this repository should be protected with a .htaccess or
-       be placed outside the web. Because there contain data of private courses. Claroline Would be able to read and write in this dir';
-$_lang['Profile SQL in DEBUG MODE.
-Display for each request :duration, counter,  statement '] = 'Profile SQL in DEBUG MODE.
-Display for each request :duration, counter,  statement';
+$_lang['Group user courses by categories'] = 'Group user courses by categories';
+$_lang['Impossible to promote as a group tutor a student who is already registered in the group'] = 'Impossible to promote as a group tutor a student who is already registered in the group';
+$_lang['Module activation in courses failed'] = 'Module activation in courses failed';
+$_lang['Module activation in courses succeeded'] = 'Module activation in courses succeeded';
+$_lang['Simple'] = 'Simple';
 $_lang['Teachers are allowed to register whole classes to their courses'] = 'Teacher can import an existing class course';
-$_lang['This option insert a command in the user profile form to request the removal of the user from the platform.  This request is sent by e-mail to platform administrator.
-This option allow only to request it, and don\'t prework the answer
-'] = 'This option insert a command in the user profile form to request the removal of the user from the platform.  This request is sent by e-mail to platform administrator.
-This option allow only to request it, and don\'t prework the answer
-';
-$_lang['Time to automatically refresh the user screen. Each refresh is a request to your server.
-Too low value can be hard for your server. Too high value can be hard for user.
-'] = 'Time to automatically refresh the user screen. Each refresh is a request to your server.
-Too low value can be hard for your server. Too high value can be hard for user.
-';
-$_lang['Warning: When you delete a message keep in mind that it will be deleted for every user.
-        <br /><br />You cannot retrieve deleted messages!'] = 'Warning: When you delete a message keep in mind that it will be deleted for every user.
-        <br /><br />You cannot retrieve deleted messages!';
+$_lang['There is already an account with this username.'] = 'There is already an account with this username.';
+$_lang['This account has been disabled, please contact the platform administrator'] = 'This account has been disabled, please contact the platform administrator';
+$_lang['Time exceeded'] = 'Time exceeded';
+$_lang['Trigger debug mode in url'] = 'Trigger debug mode in url';
+$_lang['View platform statistics'] = 'View platform statistics';
+$_lang['You have reached the maximum of %allowedAttempts allowed attempts.'] = 'You have reached the maximum of %allowedAttempts allowed attempts.';
 ?>
