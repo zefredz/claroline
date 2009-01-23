f65f447b784e630cc7db929a1adf80ecea73e929
dimitrirambout <dimitrirambout@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2009-01-23 10:55:22 +0000

    Add merge for wiki_submission

--- claroline/wiki/connector/mergeuser.cnr.php
+++ claroline/wiki/connector/mergeuser.cnr.php
@@ -4,8 +4,9 @@ class CLWIKI_MergeUser implements Module_MergeUser
 {
     public function mergeCourseUsers( $uidToRemove, $uidToKeep, $courseId )
     {
-        $moduleCourseTbl = get_module_course_tbl( array('wiki_pages', 'wiki_pages_content'), $courseId );
+        $moduleCourseTbl = get_module_course_tbl( array('wiki_pages', 'wiki_pages_content', 'wiki_submission'), $courseId );
         
+        // Update wiki_pages
         $sql = "UPDATE `{$moduleCourseTbl['wiki_pages']}`
                 SET   owner_id = ".(int)$uidToKeep."
                 WHERE owner_id = ".(int)$uidToRemove;
@@ -15,6 +16,7 @@ class CLWIKI_MergeUser implements Module_MergeUser
             throw new Exception("Cannot update wiki_pages in {$thisCourseCode}");
         }
         
+        // Update wiki_pages_content
         $sql = "UPDATE `{$moduleCourseTbl['wiki_pages_content']}`
                 SET   editor_id = ".(int)$uidToKeep."
                 WHERE editor_id = ".(int)$uidToRemove;
@@ -22,6 +24,16 @@ class CLWIKI_MergeUser implements Module_MergeUser
         if ( ! claro_sql_query($sql) )
         {
             throw new Exception("Cannot update wiki_pages_content in {$thisCourseCode}");
+        }
+        
+        // Update wiki_submission
+        $sql = "UPDATE `{$moduleCourseTbl['wiki_submission']}`
+                SET     user_id = ".(int)$uidToKeep."
+                WHERE   user_id = ".(int)$uidToRemove;
+        
+        if( ! claro_sql_query($sql) )
+        {
+            throw new Exception("Cannot update wiki_submission in {$thisCourseCode}");
         }
     }
     
