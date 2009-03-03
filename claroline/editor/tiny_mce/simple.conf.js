8bad9f23bc1f8f9eeb9cbab6e2a78167ce791888
dimitrirambout <dimitrirambout@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2009-03-03 09:34:02 +0000

    Add spoiler & tex plugin

--- claroline/editor/tiny_mce/simple.conf.js
+++ claroline/editor/tiny_mce/simple.conf.js
@@ -4,7 +4,7 @@ tinyMCE.init({
     mode : "textareas",
     editor_selector : "simpleMCE",
     // plugins must be the same as in tinyMCE_GZ.init
-    plugins : "paste,safari",
+    plugins : "paste,safari,texformula,spoiler",
     theme : "advanced",
     browsers : "safari,msie,gecko,opera",
 	directionality : text_dir,
@@ -15,7 +15,7 @@ tinyMCE.init({
     relative_urls : false,
     
     //-- advanced theme
-    theme_advanced_buttons1 : "cut,copy,paste,pasteword,bold,italic,underline,strikethrough,separator,bullist,numlist,undo,redo,link,unlink",
+    theme_advanced_buttons1 : "cut,copy,paste,pasteword,bold,italic,underline,strikethrough,separator,bullist,numlist,undo,redo,link,unlink,texformula,spoiler",
     theme_advanced_buttons2 : "",
     theme_advanced_buttons3 : "",
     theme_advanced_toolbar_location : "top",
