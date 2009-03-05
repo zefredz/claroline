c0ef3b3ef63ccef330c7bc80a33c4f959887fbb0
Frédéric Minne <zefredz@users.noreply.github.com>
2009-03-05 13:28:37 +0000

    RC4

--- claroline/inc/installedVersion.inc.php
+++ claroline/inc/installedVersion.inc.php
@@ -30,7 +30,7 @@ $is_upgrade_available = false;
 
 // var version_db  max. 10 chars
 
-$new_version = '1.9-rc3';
+$new_version = '1.9rc4';
 $new_version_branch = '1.9';
 
 if (!$is_upgrade_available)
