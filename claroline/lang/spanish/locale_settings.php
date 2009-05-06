daf9d1ce746d19901d4bfac58903d677a09e0d5d
dimitrirambout <dimitrirambout@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2009-05-06 07:05:54 +0000

    Update spanish lang

--- claroline/lang/spanish/locale_settings.php
+++ claroline/lang/spanish/locale_settings.php
@@ -2,7 +2,7 @@
 /**
  * CLAROLINE
  * Spanish Translation
- * @version 1.8 $Revision$
+ * @version 1.9 $Revision$
  *
  * @copyright (c) 2001-2006 Universite catholique de Louvain (UCL)
  *
@@ -20,9 +20,9 @@ $iso639_2_code = "spa";
 
 $langNameOfLang['arabic'		]="árabe";
 $langNameOfLang['brazilian'		]="portugués";
-$langNameOfLang['bulgarian'		]="bulgarian";
-$langNameOfLang['croatian'		]="croato";
-$langNameOfLang['dutch'			]="dutch";
+$langNameOfLang['bulgarian'		]="búlgaro";
+$langNameOfLang['croatian'		]="croata";
+$langNameOfLang['dutch'			]="holandés";
 $langNameOfLang['english'		]="inglés";
 $langNameOfLang['finnish'		]="finlandés";
 $langNameOfLang['french'		]="francés";
@@ -33,9 +33,9 @@ $langNameOfLang['japanese'		]="japonés";
 $langNameOfLang['polish'		]="polaco";
 $langNameOfLang['simpl_chinese'		]="chino";
 $langNameOfLang['spanish'		]="español";
-$langNameOfLang['spanish_latin'		]="español latin";
+$langNameOfLang['spanish_latin'		]="español latino";
 $langNameOfLang['swedish'		]="sueco";
-$langNameOfLang['thai'			]="thailandés";
+$langNameOfLang['thai'			]="tailandés";
 $langNameOfLang['turkish'		]="turco";
 
 $charset = 'iso-8859-1';
@@ -46,21 +46,21 @@ $number_thousands_separator = ',';
 $number_decimal_separator = '.';
 $byteUnits = array('Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB');
 
-$langDay_of_weekNames['init'] = array('S', 'M', 'T', 'W', 'T', 'F', 'S');
-$langDay_of_weekNames['short'] = array('Dom', 'Lun', 'Mar', 'Mier', 'Jue', 'Vie', 'Sab');
+$langDay_of_weekNames['init'] = array('D', 'L', 'M', 'X', 'J', 'V', 'S');
+$langDay_of_weekNames['short'] = array('Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sab');
 $langDay_of_weekNames['long'] = array('Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado');
 
 $langMonthNames['init']  = array('E', 'F', 'M', 'A', 'M', 'J', 'J', 'A', 'S', 'O', 'N', 'D');
 $langMonthNames['short'] = array('Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic');
-$langMonthNames['long'] = array('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Dicembre');
+$langMonthNames['long'] = array('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre');
 
 // Voir http://www.php.net/manual/en/function.strftime.php pour la variable
 // ci-dessous
 
-$dateFormatShort =  "%b %d, %y";
-$dateFormatLong  = '%A %B %d, %Y';
-$dateTimeFormatLong  = '%B %d, %Y at %I:%M %p';
-$timeNoSecFormat = '%I:%M %p';
+$dateFormatShort =  "%d/%m/%Y";
+$dateFormatLong  = '%A, %e de %B de %Y';
+$dateTimeFormatLong  = '%e de %B de %Y a las %H:%M';
+$timeNoSecFormat = '%H:%M';
 
 
 ?>
