a72d479bc212c27ede832803b1962a5f38254f36
fragile_be <fragile_be@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2009-08-20 09:28:08 +0000

    Update dutch_nl translation.  Credits go to Dominicus de Vries

--- claroline/lang/dutch_nl/missing.lang.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$_lang['A list of keys allowing requests to the Claroline SSO server. The SSO client has to provide one of the keys contained into this list to receive any answer from the Claroline SSO server.'] = 'A list of keys allowing requests to the Claroline SSO server. The SSO client has to provide one of the keys contained into this list to receive any answer from the Claroline SSO server.';
-$_lang['The list of questions has been saved'] = 'The list of questions has been saved';
-$_lang['Unable to add user %username in this course, option is disabled in configuration'] = 'Unable to add user %username in this course, option is disabled in configuration';
-$_lang['Unable to create user %username, option is disabled in configuration'] = 'Unable to create user %username, option is disabled in configuration';
-$_lang['Username seems to be duplicate at line %key'] = 'Username seems to be duplicate at line %key';
-?>
