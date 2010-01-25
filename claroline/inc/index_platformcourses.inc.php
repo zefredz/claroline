de64c6a95bdfff345c2705ec261b2266282ebbfe
jrm_ <jrm_@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2010-01-25 11:42:50 +0000

    display access type icon in course lists and use same rendering method for user and platform course lists

--- claroline/inc/index_platformcourses.inc.php
+++ claroline/inc/index_platformcourses.inc.php
@@ -1,127 +1,116 @@
 <?php // $Id$
 if ( count( get_included_files() ) == 1 ) die( '---' );
 
-
-if ( !empty ($_REQUEST['category']) ) $category = $_REQUEST['category'];
+if ( !empty( $_REQUEST['category']) ) $category = $_REQUEST['category'];
 else                                  $category = null;
 
+$out = '';
 
-
-if ( isset($_REQUEST['cmd']) && $_REQUEST['cmd'] == 'search')
+if ( isset($_REQUEST['cmd'] ) && $_REQUEST['cmd'] == 'search' )
 {
     $categoryList = array();
-    $courseList = search_course($_REQUEST['keyword']);
+    $courseList = search_course( $_REQUEST['keyword'] );
 }
 else
 {
-    $categoryBrowser = new category_browser($category);
-    $parentCategory = $categoryBrowser->get_current_category_settings();
-    $categoryList   = $categoryBrowser->get_sub_category_list();
-    $courseList     = $categoryBrowser->get_course_list();
+    $categoryBrowser = new category_browser( $category );
+    $parentCategory  = $categoryBrowser->get_current_category_settings();
+    $categoryList    = $categoryBrowser->get_sub_category_list();
+    $courseList      = $categoryBrowser->get_course_list();
 }
 
-
-if ( trim($category) != '' ) // means that we are not on the root level of the category tree
+if ( trim( $category ) != '' ) // means that we are not on the root level of the category tree
 {
     $backCommandLine = '<p>'
                       .'<small>'
-                      .'<a href="'.$_SERVER['PHP_SELF']."?category=". urlencode($parentCategory['code_P']) .'">'
-                      .'&lt;&lt; '.get_lang('previous level')
+                      .'<a href="' . $_SERVER['PHP_SELF'] . "?category=" . urlencode( $parentCategory['code_P'] ) . '">'
+                      .'&lt;&lt; ' . get_lang( 'previous level' )
                       .'</a>'
                       .'</small>'
                       .'</p>'. "\n";
 
-    $pageTitle      = $parentCategory['name'];
+    $pageTitle = $parentCategory['name'];
 }
 else
 {
     $backCommandLine = '';
-    $pageTitle       = get_lang('Platform Courses');
+    $pageTitle = get_lang( 'Platform Courses' );
 }
 
-echo $backCommandLine;
+$out .= $backCommandLine;
 
-echo claro_html_tool_title($pageTitle);
+$out .= claro_html_tool_title($pageTitle);
 
-if ( ( count($categoryList) - 1 )  >= 0 )
+if ( ( count( $categoryList ) - 1 )  >= 0 )
 {
 
-    echo claro_html_title(get_lang('Categories'),4)
-    .    '<ul>'  . "\n"
+    $out .= claro_html_title( get_lang( 'Categories' ), 4 )
+         .  '<ul>'  . "\n"
     ;
 
-    foreach($categoryList as $thisCategory)
+    foreach( $categoryList as $thisCategory )
     {
-        echo '<li>'                                           ."\n";
+        $out .= '<li>' . "\n";
 
         if ( $thisCategory['nbCourse'] + $thisCategory['nb_childs'] > 0 )
         {
-            echo '<a href="'.$_SERVER['PHP_SELF'].'?category='. urlencode($thisCategory['code']) .'">'
-            .    $thisCategory['name']
-            .    '</a>'
+            $out .= '<a href="' . $_SERVER['PHP_SELF'] . '?category=' . urlencode( $thisCategory['code'] ) . '">'
+                 .  $thisCategory['name']
+                 .  '</a>'
             ;
         }
         else
         {
-            echo $thisCategory['name'];
+            $out .= $thisCategory['name'];
         }
 
-        echo ' <small>(' . $thisCategory['nbCourse'] . ')</small>'."\n"
-        .    '</li>'                                              . "\n"
+        $out .= ' <small>(' . $thisCategory['nbCourse'] . ')</small>' . "\n"
+             .  '</li>' . "\n"
         ;
     }
 
-    echo '</ul>' . "\n";
+    $out .= '</ul>' . "\n";
 }
 
-if ( count($courseList) > 0 )
+if ( count( $courseList ) > 0 )
 {
-    if ( ( count($categoryList) - 1 )  > 0 )
+    if ( ( count( $categoryList ) - 1 )  > 0 )
     {
-        echo '<hr size="1" noshade="noshade" />' . "\n";
+        $out .= '<hr size="1" noshade="noshade" />' . "\n";
     }
 
-    echo '<h4>'.get_lang('Course list').'</h4>' . "\n"
-    .    '<ul style="list-style-image:url(' .get_icon_url('course') . ');">' . "\n";
+    $out .= '<h4>' . get_lang( 'Course list' ) . '</h4>' . "\n"
+         .  '<dl class="userCourseList">' . "\n";
 
-    foreach($courseList as $thisCourse)
-    {
-        echo '<li>' . "\n"
-        .    '<a href="' .   get_path('url') . '/claroline/course/index.php?cid=' . htmlspecialchars($thisCourse['sysCode']) . '">'
-        .    $thisCourse['officialCode'] . ' - '
-        .    $thisCourse['title']
-        .    '</a>'
-        .    '<br />';
-        if (claro_is_user_authenticated())
+    foreach( $courseList as $thisCourse )
         {
-            echo '<small><a href="mailto:'.$thisCourse['email'].'">' . $thisCourse['titular'] . '</a></small>' . "\n";
-        }
-        else
-        {
-            echo '<small>' . $thisCourse['titular'] . '</small>' . "\n";
-        }
 
-        echo '</li>' . "\n";
+
+        // Otherwise it will name normally be displayed
+        //$hot = (bool) in_array ($thisCourse['sysCode'], $modified_course);
+        $out .= render_course_dt_in_dd_list( $thisCourse, false );
+
     }
-
-    echo '</ul>' . "\n";
+    $out .= '</dl>' . "\n";
 
 }
 else
 {
     if ( isset($_REQUEST['cmd']) && $_REQUEST['cmd'] = 'search')
     {
-        echo '<blockquote>' . get_lang('Your search did not match any courses') . '</blockquote>' . "\n";
+        $out .= '<blockquote>' . get_lang( 'Your search did not match any courses' ) . '</blockquote>' . "\n";
     }
 }
 
-echo "\n" . '<blockquote>' . "\n"
-.    '<p><label for="keyword">' . get_lang('Search from keyword') . '</label> : </p>' . "\n"
-.    '<form method="post" action="' . $_SERVER['PHP_SELF'] . '">' . "\n"
-.    '<input type="hidden" name="cmd" value="search" />' . "\n"
-.    '<input type="text" name="keyword" id="keyword" />' . "\n"
-.    '&nbsp;<input type="submit" value="' . get_lang('Search') . '" />' . "\n"
-.    '</form>' . "\n"
-.    '</blockquote>' . "\n";
+$out .= "\n" . '<blockquote>' . "\n"
+     .  '<p><label for="keyword">' . get_lang( 'Search from keyword' ) . '</label> : </p>' . "\n"
+     .  '<form method="post" action="' . $_SERVER['PHP_SELF'] . '">' . "\n"
+     .  '<input type="hidden" name="cmd" value="search" />' . "\n"
+     .  '<input type="text" name="keyword" id="keyword" />' . "\n"
+     .  '&nbsp;<input type="submit" value="' . get_lang( 'Search' ) . '" />' . "\n"
+     .  '</form>' . "\n"
+     .  '</blockquote>' . "\n";
 
-echo $backCommandLine;
+$out .= $backCommandLine;
+
+echo $out;
