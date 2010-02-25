41c6f288a50f66e64e0ae0e4cc3b33907d3e4faf
dkp1060 <dkp1060@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2010-02-25 11:14:39 +0000

    2951946  	 Add image button in the simple mode of tiny

--- claroline/editor/tiny_mce/simple.conf.js
+++ claroline/editor/tiny_mce/simple.conf.js
@@ -4,7 +4,7 @@ tinyMCE.init({
     mode : "textareas",
     editor_selector : "simpleMCE",
     // plugins must be the same as in tinyMCE_GZ.init
-    plugins : "paste,safari,texformula,spoiler,resources",
+    plugins : "claroimage,paste,safari,texformula,spoiler,resources",
     theme : "advanced",
     browsers : "safari,msie,gecko,opera",
     directionality : text_dir,
@@ -15,7 +15,7 @@ tinyMCE.init({
     relative_urls : false,
     
     //-- advanced theme
-    theme_advanced_buttons1 : "cut,copy,paste,pasteword,bold,italic,underline,strikethrough,separator,bullist,numlist,undo,redo,link,unlink,texformula,spoiler,forecolor,resources",
+    theme_advanced_buttons1 : "cut,copy,paste,pasteword,claroimage,bold,italic,underline,strikethrough,separator,bullist,numlist,link,unlink,texformula,spoiler,forecolor,resources",
     theme_advanced_buttons2 : "",
     theme_advanced_buttons3 : "",
     theme_advanced_toolbar_location : "top",
