6775c8597759fcd16b7b39ab1ae81efa05942b12
Frédéric Minne <zefredz@users.noreply.github.com>
2010-02-26 09:54:04 +0000

    set copyright to 2010 in footer

--- claroline/inc/lib/display/footer.lib.php
+++ claroline/inc/lib/display/footer.lib.php
@@ -113,7 +113,7 @@
             $poweredByOutput = '<div id="poweredBy">'
                 . get_lang('Powered by')
                 . ' <a href="http://www.claroline.net" target="_blank">Claroline</a> '
-                . '&copy; 2001 - 2009'
+                . '&copy; 2001 - 2010'
                 . '</div>'
                 ;
             
