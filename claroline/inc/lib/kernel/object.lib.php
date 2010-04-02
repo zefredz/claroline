4483565e1b931c0dfb844fde432a4e4e93d6e988
Frédéric Minne <zefredz@users.noreply.github.com>
2010-04-02 12:11:14 +0000

    Add phpdoc

--- claroline/inc/lib/kernel/object.lib.php
+++ claroline/inc/lib/kernel/object.lib.php
@@ -3,7 +3,8 @@
 // vim: expandtab sw=4 ts=4 sts=4:
 
 /**
- * Description
+ * "Magic" class to represent kernel objects. Defines __get, __set, __isset and
+ * __unset magic methods.
  *
  * @version     1.10 $Revision$
  * @copyright   2001-2010 Universite catholique de Louvain (UCL)
@@ -17,7 +18,14 @@
 class KernelObject
 {
     protected $_rawData = array();
-    
+
+    /**
+     * Get the value of a property of the object. Magic method called by
+     * $var = $obj->propertyName;
+     * @param string $nm property name
+     * @return mixed value of the property if the property exists, if not the
+     * method returns null
+     */
     public function __get( $nm )
     {
         if ( isset ( $this->_rawData[$nm] ) )
@@ -29,7 +37,14 @@ class KernelObject
             return null;
         }
     }
-    
+
+    /**
+     * Prevent from changing the value of one of the object public property.
+     * Magic method called by $obj->propertyName = $value;
+     * @param string $nm
+     * @param mixed $value
+     * @throws Exception automaticaly ! (this object is read only)
+     */
     public function __set( $nm, $value )
     {
         if ( $nm === '_rawData' )
@@ -41,7 +56,12 @@ class KernelObject
             throw new Exception("Cannot change variable {$nm} : ".__CLASS__." is readonly !");
         }
     }
-    
+
+    /**
+     * Magic method called by isset($obj->propertyName);
+     * @param string $nm property name
+     * @return boolean true if the property is set for the object
+     */
     public function __isset( $nm )
     {
         if ( isset ( $this->_rawData[$nm] ) )
@@ -53,12 +73,23 @@ class KernelObject
             return false;
         }
     }
-    
+
+    /**
+     * Prevent from unsetting read only properties. Magic method called by
+     * unset($obj->propertyName);
+     * @param string $nm property name
+     * @throws Exception automaticaly ! (this object is read only)
+     */
     public function __unset( $nm )
     {
         throw new Exception("Cannot unset variable {$nm} : ".__CLASS__." is readonly !");
     }
-    
+
+    /**
+     * Get the raw data of the object
+     * @todo rewrite the kernel so thi method can be made 'protected'
+     * @return array raw data contained in the object
+     */
     public function getRawData()
     {
         return $this->_rawData;
