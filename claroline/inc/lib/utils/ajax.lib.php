0cd4545a887502e1c9239182454953afb9ae474c
Frédéric Minne <zefredz@users.noreply.github.com>
2010-04-09 13:24:28 +0000

    Add server classes to easily implement remote services using AJAX. @todo enhance ajax request factory to support JSON based RPC-like requests.

--- claroline/inc/lib/utils/ajax.lib.php
+++ claroline/inc/lib/utils/ajax.lib.php
@@ -14,6 +14,8 @@
  * @package     core.linker
  */
 
+require_once dirname(__FILE__) . '/input.lib.php';
+
 class Json_Response
 {
     const SUCCESS = 'success';
@@ -65,5 +67,123 @@ class Json_Exception extends Json_Error
         }
         
         parent::__construct( $errorArr );
+    }
+}
+
+class Ajax_Request
+{
+    protected $klass, $method, $params;
+    
+    public function __construct( $class, $method, $params = array() )
+    {
+        $this->klass = $class; 
+        $this->method = $method;
+        $this->params = $params;
+    }
+    
+    public function getClass()
+    {
+        return $this->klass;
+    }
+    
+    public function getMethod()
+    {
+        return $this->method;
+    }
+    
+    public function getParameters()
+    {
+        return $this->params;
+    }
+
+    public function  __toString()
+    {
+        return $this->getClass().'::'.$this->getMethod().'('.implode(',',$this->getParameters()).')';
+    }
+
+    public static function getRequest( Claro_Input $userInput )
+    {
+        $request = new self(
+            $userInput->getMandatory('class'),
+            $userInput->getMandatory('method'),
+            $userInput->get('parameters', array())
+        );
+
+        return $request;
+    }
+}
+
+interface Ajax_Remote_Service
+{
+
+}
+
+class Ajax_Remote_Service_Broker
+{
+    protected $register = array();
+
+    public function register( $className, Ajax_Remote_Service $object, $methods = null, $overwrite = false )
+    {
+        if ( ! isset($this->register[$className]) || $overwrite === true )
+        {
+            $this->register[$className] = array(
+                'object' => $object,
+                'methods' => $methods
+            );
+        }
+        else
+        {
+            throw new Exception ("Service Error : try to overwrite class {$className}");
+        }
+    }
+
+    public function handle( Ajax_Request $request )
+    {
+        try
+        {
+            if ( isset ($this->register[$request->getClass()]) )
+            {
+                if (
+                    in_array( $request->getMethod(), $this->register[$request->getClass()]['methods'] )
+                    || is_null($this->register[$request->getClass()]['methods'])
+                )
+                {
+                    if ( is_callable( array(
+                            $this->register[$request->getClass()]['object'],
+                            $request->getMethod() )
+                        )
+                    )
+                    {
+                        $response = call_user_method_array(
+                            $request->getMethod(),
+                            $this->register[$request->getClass()]['object'],
+                            $request->getParameters() );
+
+                        return new Json_Response(array(
+                            'class' => $request->getClass(),
+                            'method' => $request->getMethod(),
+                            'params' => $request->getParameters(),
+                            'reponse' => $response
+                        ));
+                    }
+                    else
+                    {
+                        throw new Exception( "Method not callable {$request->getMethod()} in class {$request->getClass()}" );
+                    }
+                }
+                else
+                {
+                    throw new Exception( "Method not found {$request->getMethod()} in class {$request->getClass()}" );
+                }
+            }
+            else
+            {
+                throw new Exception( "Class not found {$request->getClass()}" );
+            }
+        }
+        catch ( Exception $e )
+        {
+            return new Json_Exception($e);
+        }
     }
 }
