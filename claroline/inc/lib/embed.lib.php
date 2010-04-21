1f33c039262131a7989edb42a22bd6af2d388a3f
Frédéric Minne <zefredz@users.noreply.github.com>
2010-04-21 09:37:09 +0000

    @deprecated

--- claroline/inc/lib/embed.lib.php
+++ claroline/inc/lib/embed.lib.php
@@ -2,22 +2,16 @@
 
 // vim: expandtab sw=4 ts=4 sts=4:
 
-if ( count( get_included_files() ) == 1 )
-{
-    die( 'The file ' . basename(__FILE__) . ' cannot be accessed directly, use include instead' );
-}
-
 /**
  * Embed script output into Claroline layout
  *
  * @version     1.9 $Revision$
  * @deprecated since Claroline 1.9
- * @copyright   2001-2008 Universite catholique de Louvain (UCL)
+ * @copyright   2001-2010 Universite catholique de Louvain (UCL)
  * @author      Frederic Minne <zefredz@claroline.net>
  * @license     http://www.gnu.org/copyleft/gpl.html
  *              GNU GENERAL PUBLIC LICENSE
  * @package     EMBED
- * @deprecated  since 1.9, use display lib instead
  */
 
 /**
@@ -28,6 +22,7 @@ if ( count( get_included_files() ) == 1 )
  * @param   bool    $hide_footer hide Claroline banner (opt)
  * @param   bool    $hide_body hide Claroline banner (opt)
  * @todo    TODO return string instead of echoing it
+ * @deprecated  since Claroline 1.9
  */
 function claro_embed( $output
     , $inPopup = false
