49336220279cd37fad04e12bdf324a43778bc190
Frédéric Minne <zefredz@users.noreply.github.com>
2010-04-21 11:36:27 +0000

    phpdoc + code cleaning

--- claroline/inc/lib/file.lib.php
+++ claroline/inc/lib/file.lib.php
@@ -5,8 +5,8 @@
 /**
  * File handling functions
  *
- * @version     1.9 $Revision$
- * @copyright   2001-2008 Universite catholique de Louvain (UCL)
+ * @version     1.10 $Revision$
+ * @copyright   2001-2010 Universite catholique de Louvain (UCL)
  * @author      Claroline Team <info@claroline.net>
  * @license     http://www.gnu.org/copyleft/gpl.html
  *              GNU GENERAL PUBLIC LICENSE version 2 or later
