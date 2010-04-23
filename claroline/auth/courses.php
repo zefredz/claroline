838c45c62aeef0855506905bde7a4ccd4ef7a172
dkp1060 <dkp1060@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2010-04-23 12:22:30 +0000

    remove error in annoucement and subscription classes

--- claroline/auth/courses.php
+++ claroline/auth/courses.php
@@ -569,7 +569,7 @@ switch ( $displayMode )
                 }
                 elseif ( $fromAdmin == 'class')
                 {
-                   if (!get_class_list_of_course($thisCourse['sysCode'],$classId))
+                   if (in_array(get_class_list_of_course($thisCourse['sysCode']),$class_id))
                    {
                     $out .= '<td valign="top"  align="center">' . "\n"
                     .    '<a href="' . get_path('clarolineRepositoryWeb') . 'admin/admin_class_course_registered.php'
