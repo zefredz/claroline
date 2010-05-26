5d7bab314135915dd358498e869844d65924642a
abourguignon <abourguignon@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2010-05-26 15:29:14 +0000

    Add a few javascript in the announcement's form.

--- claroline/announcements/announcements.php
+++ claroline/announcements/announcements.php
@@ -540,7 +540,30 @@ if ( $displayForm )
     . '<input type="submit" class="claroButton" name="submitEvent" value="' . get_lang('Ok') . '" />'
     . claro_html_button(htmlspecialchars(Url::Contextualize($_SERVER['PHP_SELF'])), get_lang('Cancel'))
     
-    . '</form>';
+    . '</form>'
+    .'<script type="text/javascript">
+    $("#visible").click(function(){
+        $("#enable_visible_from").attr("disabled", false);
+        $("#enable_visible_until").attr("disabled", false);
+        $("#visible_from_day").attr("disabled", false);
+        $("#visible_from_month").attr("disabled", false);
+        $("#visible_from_year").attr("disabled", false);
+        $("#visible_until_day").attr("disabled", false);
+        $("#visible_until_month").attr("disabled", false);
+        $("#visible_until_year").attr("disabled", false);
+    });
+    
+    $("#invisible").click(function(){
+        $("#enable_visible_from").attr("disabled", true);
+        $("#enable_visible_until").attr("disabled", true);
+        $("#visible_from_day").attr("disabled", true);
+        $("#visible_from_month").attr("disabled", true);
+        $("#visible_from_year").attr("disabled", true);
+        $("#visible_until_day").attr("disabled", true);
+        $("#visible_until_month").attr("disabled", true);
+        $("#visible_until_year").attr("disabled", true);
+    });'
+    . '</script>';
 }
 
 
