e9f0e8fc49df5e43a65345e290f464ccb16eef7f
Frédéric Minne <zefredz@users.noreply.github.com>
2010-06-07 10:37:16 +0000

    add svn keywords

--- claroline/document/connector/tracking.cnr.php
+++ claroline/document/connector/tracking.cnr.php
@@ -3,7 +3,7 @@ if ( count( get_included_files() ) == 1 ) die( '---' );
 /**
  * CLAROLINE
  *
- * @version 1.8 $Revision: 415 $
+ * @version 1.8 $Revision$
  *
  * @copyright (c) 2001-2006 Universite catholique de Louvain (UCL)
  *
