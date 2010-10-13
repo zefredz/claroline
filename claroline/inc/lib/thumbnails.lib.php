54a7dac452abdbdf1cf648fd6541bb5efb744219
Frédéric Minne <zefredz@users.noreply.github.com>
2010-10-13 13:26:37 +0000

    fail gracefully if gd image conversion fails

--- claroline/inc/lib/thumbnails.lib.php
+++ claroline/inc/lib/thumbnails.lib.php
@@ -68,20 +68,20 @@ class Thumbnailer
         {
             case 'png':
             {
-                $image = imagecreatefrompng( $srcPath );
+                $image = @imagecreatefrompng( $srcPath );
             } break;
             case 'jpg':
             case 'jpeg':
             {
-                $image = imagecreatefromjpeg( $srcPath );
+                $image = @imagecreatefromjpeg( $srcPath );
             } break;
             case 'gif':
             {
-                $image = imagecreatefromgif( $srcPath );
+                $image = @imagecreatefromgif( $srcPath );
             } break;
             case 'bmp':
             {
-                $image = imagecreatefromwbmp( $srcPath );
+                $image = @imagecreatefromwbmp( $srcPath );
             } break;
             default:
             {
@@ -89,6 +89,13 @@ class Thumbnailer
             }
         }
         
+        // image loading failed use srcPath instead
+        if ( ! $image )
+        {
+            Console::warning("Failed to create GD image from {$srcPath}");
+            return $srcPath;
+        }
+
         $oldWidth = imageSX( $image );
         $oldHeight = imageSY( $image );
         
