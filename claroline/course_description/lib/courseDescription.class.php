ccee5d2204d8379e5a297356bdf02508c0325470
abourguignon <abourguignon@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2010-12-01 17:02:02 +0000

    Fix identation.

--- claroline/course_description/lib/courseDescription.class.php
+++ claroline/course_description/lib/courseDescription.class.php
@@ -4,17 +4,15 @@ if ( count( get_included_files() ) == 1 ) die( '---' );
 /**
  * CLAROLINE
  *
- * @version 1.9 $Revision$
- *
- * @copyright (c) 2001-2010, Universite catholique de Louvain (UCL)
- *
- * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
- *
- * @author Claro Team <cvs@claroline.net>
- *
+ * @version     $Revision$
+ * @copyright   (c) 2001-2010, Universite catholique de Louvain (UCL)
+ * @license     http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
+ * @author      Claro Team <cvs@claroline.net>
+ * @since       1.9
  */
+
 /*
-    From 1.8 to 1.9 : 
+    From 1.8 to 1.9 :
     -   'id' becomes int(11) auto_increment
     -   add 1 field 'category' that takes the 'id' value of 1.8 table for values lower that size of $titreBloc
     -   'category' is -1 when id was upper than size of $titreBloc
@@ -31,8 +29,8 @@ CREATE TABLE `__CL_COURSE__course_description` (
   PRIMARY KEY `id` (`id`)
 );
 
-
 */
+
 
 class CourseDescription
 {
@@ -43,7 +41,7 @@ class CourseDescription
 
     /**
      * @protected $category id of predefined 'type' of description, > 0 for predefined categories, -1 for "others"
-     */ 
+     */
     protected $category;
      
     /**
@@ -64,23 +62,22 @@ class CourseDescription
     /**
      * @protected $visibility
      */
-    protected $visibility;    
+    protected $visibility;
     
     
-    /** 
+    /**
      * constructor
      *
      * @param $course_id
      */
-     
     public function __construct($course_id = null)
     {
-        $this->id = (int) -1;
-        $this->category = -1;
-        $this->title = '';
-        $this->content = '';
+        $this->id           = (int) -1;
+        $this->category     = -1;
+        $this->title        = '';
+        $this->content      = '';
         $this->lastEditDate = time();
-           $this->visibility = 'VISIBLE';
+        $this->visibility   = 'VISIBLE';
 
         $tbl_cdb_names = claro_sql_get_course_tbl(claro_get_course_db_name_glued($course_id));
         $this->tblCourseDescription = $tbl_cdb_names['course_description'];
@@ -93,18 +90,17 @@ class CourseDescription
      * @param integer $id id of description
      * @return boolean true if load is successfull false otherwise
      * @author Sebastien Piraux <pir@cerdecam.be>
-     */   
-      
+     */
     public function load($id)
     {
-           $sql = "SELECT `id`,
+        $sql = "SELECT `id`,
                        `category`,
-                    `title`,
-                    `content`,
-                    UNIX_TIMESTAMP(`lastEditDate`) AS `unix_lastEditDate`,
-                    `visibility`
-            FROM `".$this->tblCourseDescription."`
-            WHERE `id` = ".(int) $id;
+                       `title`,
+                       `content`,
+                       UNIX_TIMESTAMP(`lastEditDate`) AS `unix_lastEditDate`,
+                       `visibility`
+                FROM `".$this->tblCourseDescription."`
+                WHERE `id` = ".(int) $id;
 
         $data = claro_sql_query_get_single_row($sql);
 
@@ -114,7 +110,7 @@ class CourseDescription
             $this->setCategory($data['category']);
             $this->setTitle($data['title']);
             $this->setContent($data['content']);
-               $this->setLastEditDate($data['unix_lastEditDate']);            
+            $this->setLastEditDate($data['unix_lastEditDate']);
             $this->setVisibility($data['visibility']);
 
             return true;
@@ -132,7 +128,6 @@ class CourseDescription
      * @return mixed false or id of the record
      * @author Sebastien Piraux <pir@cerdecam.be>
      */
-     
     public function save()
     {
         if( $this->id == -1 )
@@ -152,12 +147,11 @@ class CourseDescription
      * @return mixed false or id of the record
      * @author Sebastien Piraux <pir@cerdecam.be>
      */
-     
     public function insert()
     {
            // insert
         $sql = "INSERT INTO `".$this->tblCourseDescription."`
-                SET `category` = ".$this->getCategory().", 
+                SET `category` = ".$this->getCategory().",
                     `title` = '".claro_sql_escape($this->getTitle())."',
                     `content` = '".claro_sql_escape($this->getContent())."',
                     `lastEditDate` = NOW(),
@@ -183,14 +177,13 @@ class CourseDescription
      * update description to DB
      *
      * @return mixed false or id of the record
-     * @author Sebastien Piraux <pir@cerdecam.be>     
+     * @author Sebastien Piraux <pir@cerdecam.be>
      */
-     
     public function update()
     {
         // update, main query
         $sql = "UPDATE `".$this->tblCourseDescription."`
-                SET `category` = ".$this->getCategory().", 
+                SET `category` = ".$this->getCategory().",
                     `title` = '".claro_sql_escape($this->getTitle())."',
                     `content` = '".claro_sql_escape($this->getContent())."',
                     `lastEditDate` = NOW(),
@@ -205,7 +198,7 @@ class CourseDescription
         else
         {
             return false;
-        }    
+        }
     }
     
     
@@ -213,32 +206,31 @@ class CourseDescription
      * delete description
      *
      * @return boolean true if delete is successfull false otherwise
-     * @author Sebastien Piraux <pir@cerdecam.be>     
-     */ 
-        
+     * @author Sebastien Piraux <pir@cerdecam.be>
+     */
     public function delete()
     {
         $sql = "DELETE FROM `".$this->tblCourseDescription."`
                 WHERE `id` = ".$this->getId();
 
-        if( claro_sql_query($sql) ) 
+        if( claro_sql_query($sql) )
         {
             $this->setId(-1);
             return true;
-        }  
+        }
         else
         {
             return true;
         }
     }
     
+    
     /**
      * validate object content
      *
      * @return boolean true if delete is successfull false otherwise
-     * @author Sebastien Piraux <pir@cerdecam.be>     
-     */   
-     
+     * @author Sebastien Piraux <pir@cerdecam.be>
+     */
     public function validate()
     {
         // there is nothing to validate at this time as a course description
@@ -248,7 +240,7 @@ class CourseDescription
         
         return true;
     }
- 
+    
     
     // get and set
     
@@ -256,9 +248,8 @@ class CourseDescription
      * get id of description
      *
      * @return integer id of description
-     * @author Sebastien Piraux <pir@cerdecam.be>  
+     * @author Sebastien Piraux <pir@cerdecam.be>
      */
-     
     public function getId()
     {
         return (int) $this->id;
@@ -269,9 +260,8 @@ class CourseDescription
      * set id of description
      *
      * @param integer id of description
-     * @author Sebastien Piraux <pir@cerdecam.be>  
+     * @author Sebastien Piraux <pir@cerdecam.be>
      */
-     
     public function setId($id)
     {
         $this->id = (int) $id;
@@ -282,9 +272,8 @@ class CourseDescription
      * get category of description
      *
      * @return integer category of description
-     * @author Sebastien Piraux <pir@cerdecam.be>  
+     * @author Sebastien Piraux <pir@cerdecam.be>
      */
-
     public function getCategory()
     {
         return (int) $this->category;
@@ -295,9 +284,8 @@ class CourseDescription
      * set category of description
      *
      * @param integer category of description, < 0 for others, > 0 for predefined categories
-     * @author Sebastien Piraux <pir@cerdecam.be>  
+     * @author Sebastien Piraux <pir@cerdecam.be>
      */
-     
     public function setCategory($category)
     {
         if( $category < 0 ) $this->category = (int) -1;
@@ -309,21 +297,20 @@ class CourseDescription
      * get title of description
      *
      * @return integer title of description
-     * @author Sebastien Piraux <pir@cerdecam.be>  
+     * @author Sebastien Piraux <pir@cerdecam.be>
      */
-
     public function getTitle()
     {
         return $this->title;
     }
     
+    
     /**
      * set title of description
      *
      * @param string title of description
-     * @author Sebastien Piraux <pir@cerdecam.be>  
+     * @author Sebastien Piraux <pir@cerdecam.be>
      */
-     
     public function setTitle($title)
     {
         $this->title = trim($title);
@@ -334,21 +321,20 @@ class CourseDescription
      * get content of description
      *
      * @return integer content of description
-     * @author Sebastien Piraux <pir@cerdecam.be>  
+     * @author Sebastien Piraux <pir@cerdecam.be>
      */
-
     public function getContent()
     {
         return $this->content;
     }
-
+    
+    
     /**
      * set content of description
      *
      * @param string content of description
-     * @author Sebastien Piraux <pir@cerdecam.be>  
+     * @author Sebastien Piraux <pir@cerdecam.be>
      */
-         
     public function setContent($content)
     {
         $this->content = trim($content);
@@ -359,22 +345,20 @@ class CourseDescription
      * get last edition date
      *
      * @return integer last edition date timestamp
-     * @author Sebastien Piraux <pir@cerdecam.be>  
+     * @author Sebastien Piraux <pir@cerdecam.be>
      */
-
     public function getLastEditDate()
     {
         return $this->lastEditDate;
     }
-
-
+    
+    
     /**
      * set last edition date timestamp
      *
      * @param integer last edition date timestamp
-     * @author Sebastien Piraux <pir@cerdecam.be>  
+     * @author Sebastien Piraux <pir@cerdecam.be>
      */
-     
     public function setLastEditDate($date)
     {
         $this->lastEditDate = $date;
@@ -385,21 +369,20 @@ class CourseDescription
      * get visibility of description
      *
      * @return string visibility of description 'VISIBLE' or 'INVISIBLE'
-     * @author Sebastien Piraux <pir@cerdecam.be>  
+     * @author Sebastien Piraux <pir@cerdecam.be>
      */
-    
     public function getVisibility()
     {
         return $this->visibility;
     }
-
+    
+    
     /**
      * set visibility
      *
      * @param string visibility of description 'VISIBLE' or 'INVISIBLE'
-     * @author Sebastien Piraux <pir@cerdecam.be>  
+     * @author Sebastien Piraux <pir@cerdecam.be>
      */
-     
     public function setVisibility($visibility)
     {
         $acceptedValues = array('VISIBLE', 'INVISIBLE');
