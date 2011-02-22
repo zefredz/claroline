7521d6beb9fcc374938da1fa2b886b944995da7c
dkp1060 <dkp1060@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2011-02-22 16:17:45 +0000

    'set_magic_quotes_runtime()' obsolete in php 5.3 (http://forum.claroline.net/viewtopic.php?f=47&t=17744)

--- claroline/inc/lib/thirdparty/pear/Lite.php
+++ claroline/inc/lib/thirdparty/pear/Lite.php
@@ -677,7 +677,7 @@ class Cache_Lite
             clearstatcache(); // because the filesize can be cached by PHP itself...
             $length = @filesize($this->_file);
             $mqr = get_magic_quotes_runtime();
-            set_magic_quotes_runtime(0);
+            ini_set ("magic_quotes_runtime", 0);
             if ($this->_readControl) {
                 $hashControl = @fread($fp, 32);
                 $length = $length - 32;
@@ -687,7 +687,7 @@ class Cache_Lite
             } else {
                 $data = '';
             }
-            set_magic_quotes_runtime($mqr);
+            ini_set ("magic_quotes_runtime", $mqr);
             if ($this->_fileLocking) @flock($fp, LOCK_UN);
             @fclose($fp);
             if ($this->_readControl) {
