7180879d59df6d800ee4341a8821706b0a0a2b14
Frédéric Minne <zefredz@users.noreply.github.com>
2011-03-15 14:12:54 +0000

    empty groups are not displayed so I've commented the group members count fix out until a solution is found

--- claroline/group/group.php
+++ claroline/group/group.php
@@ -19,15 +19,10 @@
  * complete listing of  groups member is not aivailable. the  unsorted info is in user tool
  *
  * @version 1.9 $Revision$
- *
- * @copyright   (c) 2001-2011, Universite catholique de Louvain (UCL)
- *
+ * @copyright 2001-2011 Universite catholique de Louvain (UCL)
  * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
- *
  * @see http://www.claroline.net/wiki/index.php/CLGRP
- *
  * @package CLGRP
- *
  * @author Claro Team <cvs@claroline.net>
  *
  */
@@ -101,6 +96,11 @@ $tools['chat'    ] = $_groupProperties['tools']['CLCHT' ];*/
 
 $dialogBox = new DialogBox();
 //// **************** ACTIONS ***********************
+
+if ( isset($_REQUEST['regDone']) )
+{
+    $dialogBox->success( get_lang("You have been removed of the group.") );
+}
 
 $display_groupadmin_manager = (bool) $is_allowedToManage;
 
@@ -303,6 +303,10 @@ if ( $is_allowedToManage )
                                               ? (int) $_REQUEST['self_registration']
                                               : 0;
 
+        $newPropertyList['self_unregistration'] = isset($_REQUEST['self_unregistration'])
+                                              ? (int) $_REQUEST['self_unregistration']
+                                              : 0;
+
         $newPropertyList['private'          ] = isset($_REQUEST['private'] )
                                               ? (int) $_REQUEST['private']
                                               : $private = 0;
@@ -374,11 +378,7 @@ if ( $is_allowedToManage )
         )
         );
 
-        $groupPrivate    = $_groupProperties['private'        ];
-        /* $groupHaveForum  = $_groupProperties['tools']['CLFRM' ];
-        $groupHaveDocs   = $_groupProperties['tools']['CLDOC' ];
-        $groupHaveWiki   = $_groupProperties['tools']['CLWIKI'];
-        $groupHaveChat   = $_groupProperties['tools']['CLCHT' ];*/
+        $groupPrivate    = $_groupProperties['private'];
 
     }    // end if $submit
 
@@ -453,10 +453,10 @@ if (DISP_GROUP_LIST == $display )
           ON `ug2`.`team` = `g`.`id`
           
           # Limit to regsitered users
-          INNER JOIN `" . $tbl_CoursUsers . "` AS `cu` ON `cu`.user_id = `ug2`.`user`
-          AND `cu`.`code_cours` = '" . $currentCourseId ."'
+          #INNER JOIN `" . $tbl_CoursUsers . "` AS `cu` ON `cu`.user_id = `ug2`.`user`
+          #AND `cu`.`code_cours` = '" . $currentCourseId ."'
 
-          WHERE `cu`.`code_cours` = '" . $currentCourseId ."'
+          #WHERE `cu`.`code_cours` = '" . $currentCourseId ."'
 
           GROUP BY `g`.`id`";
 
@@ -810,5 +810,3 @@ $out .= '</tbody>' . "\n"
 $claroline->display->body->appendContent($out);
 
 echo $claroline->display->render();
-
-?>
