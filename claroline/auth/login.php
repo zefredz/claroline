ffdd913c5e24bcd2fef106a2435b2a47f8b5ad98
abourguignon <abourguignon@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2011-03-28 10:21:57 +0000

    Move the login form and the course's redirection form in templates.

--- claroline/auth/login.php
+++ claroline/auth/login.php
@@ -20,10 +20,7 @@ if (isset($_REQUEST['login']))
     $_SESSION['lastUserName'] = $_REQUEST['login'];
 }
 
-/* Capture the source of the authentication trigger to get back to it
- * if the authentication succeeds
- */
-
+// Capture the source of the authentication's trigger to get back to it
 if ( isset($_REQUEST['fromPortal']) && $_REQUEST['fromPortal'] == 'true' && !isset($_REQUEST['sourceUrl']) )
 {
     $sourceUrl = null;
@@ -91,35 +88,28 @@ else
     $sourceGidFormField = '';
 }
 
-$cidRequired = (isset($_REQUEST['cidRequired']) ? $_REQUEST['cidRequired'] : false );
+$cidRequired = (isset($_REQUEST['cidRequired']) ? $_REQUEST['cidRequired'] : false);
 $cidRequiredFormField = ($cidRequired ? '<input type="hidden" name="cidRequired" value="true" />' : '');
 
-$uidRequired = true; // todo : possibility to continue in anonymous
+//TODO: possibility to continue in anonymous
+$uidRequired = true;
 
-if ( ! claro_is_user_authenticated() && $uidRequired )
+// The script needs the user to be authentificated
+if (!claro_is_user_authenticated() && $uidRequired)
 {
-    $out = '';
-
     $defaultLoginValue= '';
-
+    $dialogBox = new DialogBox;
+    
     if (isset($_SESSION['lastUserName']))
     {
-        $defaultLoginValue= $_SESSION['lastUserName'];
+        $defaultLoginValue = $_SESSION['lastUserName'];
         unset($_SESSION['lastUserName']);
     }
-
-    if( get_conf('claro_displayLocalAuthForm',true) == true )
+    
+    if (get_conf('claro_displayLocalAuthForm',true) == true)
     {
-        // Display login form
-        $out .= '<table align="center">'                                     ."\n"
-        .    '<tr>'                                                       ."\n"
-        .    '<td>'                                                       ."\n"
-        .    claro_html_tool_title(get_lang('Authentication Required'));
-
         if ( $claro_loginRequested && ! $claro_loginSucceeded ) // var comming from claro_init_local.inc.php
         {
-            $dialogBox = new DialogBox;
-            
             if ( AuthManager::getFailureMessage() )
             {
                 // need to use get_lang two times...
@@ -129,7 +119,7 @@ if ( ! claro_is_user_authenticated() && $uidRequired )
             {
                 $dialogBox->error( get_lang('Login failed.') . ' ' . get_lang('Please try again.') );
             }
-
+            
             if ( get_conf('allowSelfReg',false))
             {
                 $dialogBox->warning( get_lang('If you haven\'t a user account yet, use the <a href="%url">the account creation form</a>.',array('%url'=> get_path('url') . '/claroline/auth/inscription.php')) );
@@ -139,67 +129,37 @@ if ( ! claro_is_user_authenticated() && $uidRequired )
                 $dialogBox->error( get_lang('Contact your administrator.') );
             }
             
-            $dialogBox->warning( '<small>' . get_lang('Warning the system distinguishes uppercase (capital) and lowercase (small) letters') . '</small>' );
-
-            $out .= $dialogBox->render();
-
+            $dialogBox->warning( get_lang('Warning the system distinguishes uppercase (capital) and lowercase (small) letters') );
         }
-
-        if( get_conf('claro_secureLogin', false) )
+        
+        if (get_conf('claro_secureLogin', false))
         {
-            $target = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
+            $formAction = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
             
         }
         else
         {
-            $target = $_SERVER['PHP_SELF'];
+            $formAction = $_SERVER['PHP_SELF'];
         }
-        $out .= '<form class="claroLoginForm" action ="' . $target . '" method="post">' . "\n"
-        .    '<fieldset style="padding: 7px;">'                                                 ."\n"
-        .    $sourceUrlFormField                                          ."\n"
-        .    $cidRequiredFormField
-        .    $sourceCidFormField                                          ."\n"
-        .    $sourceGidFormField                                          ."\n"
-        .    '<legend>' . get_lang('Authentication') . '</legend>'               ."\n"
-
-        .    '<label for="login">'.get_lang('Username').'</label><br />'   ."\n"
-        .    '<input type="text" name="login" id="login" size="15" tabindex="1" value="' . $defaultLoginValue . '"/><br />' ."\n"
-        .    '<br />'
-        .    '<label for="password">'.get_lang('Password').'</label><br />'   ."\n"
-        .    '<input type="password" name="password" id="password" size="15" tabindex="2" autocomplete="off"/><br />'."\n"
-        .    '<br />'
-        .    '<input type="submit" value="'.get_lang('Ok').'" />&nbsp; '                 ."\n"
-        .    claro_html_button(get_path('clarolineRepositoryWeb'), get_lang('Cancel'))
-        .    '</fieldset>'                                                ."\n"
-        .    '</form>'                                                    ."\n";
-
-        $out .= '</td>'                                                    ."\n"
-        .    '</tr>'                                                    ."\n"
-        .    '</table>'                                                 ."\n";
-        
     } // end if claro_dispLocalAuthForm
-
-    if (get_conf('claro_CasEnabled',false))
-    {
-        $out .= '<div align="center">'
-        .    '<a href="login.php?'. ($sourceUrl ? 'sourceUrl='.urlencode($sourceUrl) : '').'&authModeReq=CAS">'
-        .    ( '' != trim(get_conf('claro_CasLoginString','')) ? get_conf('claro_CasLoginString') : get_lang('Login'))
-        .    '</a>'
-        .    '</div>';
-    } // end if claro_CASEnabled
-
-    $claroline->display->body->appendContent($out);
-
+    
+    $template = new CoreTemplate('auth_form.tpl.php');
+    $template->assign('dialogBox', $dialogBox);
+    $template->assign('formAction', $formAction);
+    $template->assign('sourceUrlFormField', $sourceUrlFormField);
+    $template->assign('cidRequiredFormField', $cidRequiredFormField);
+    $template->assign('sourceCidFormField', $sourceCidFormField);
+    $template->assign('sourceGidFormField', $sourceGidFormField);
+    $template->assign('defaultLoginValue', $defaultLoginValue);
+    $template->assign('sourceUrl', $sourceUrl);
+    
+    $claroline->display->body->appendContent($template->render());
+    
     echo $claroline->display->render();
 }
-elseif ( ! claro_is_in_a_course() && $cidRequired )
+// The script needs a course id, but no course is set
+elseif (!claro_is_in_a_course() && $cidRequired)
 {
-    /*
-     * The script the user is trying to access
-     * is only able to work inside a course
-     * and no course are set.
-     */
-
     $tbl                = claro_sql_get_main_tbl();
     $sql = "
             SELECT c.code                                             AS `value`,
@@ -207,130 +167,88 @@ elseif ( ! claro_is_in_a_course() && $cidRequired )
             FROM `" . $tbl['course'] . "`          AS c ,
                  `" . $tbl['rel_course_user'] . "` AS cu
             WHERE c.code = cu.code_cours
-              AND cu.user_id = " . (int) claro_get_current_user_id();
-
+            AND cu.user_id = " . (int) claro_get_current_user_id();
+    
     $courseList = claro_sql_query_fetch_all($sql);
-
-    $out = '';
-
-    if ( $courseList !== false && count($courseList) > 0 )
-    {
-        // Display select course form
-        $out .= '<form action="' . $_SERVER['PHP_SELF'] . '" method="post">' ."\n"
-        .    '<table align="center">'                                ."\n"
-        .    '<tr>'                                                  ."\n"
-        .    '<td colspan="2">'                                      ."\n"
-        .    claro_html_tool_title(get_lang('Choose a course to access this page.'))
-        .    $sourceUrlFormField                                     ."\n"
-        .    $cidRequiredFormField                                   ."\n"
-        .    $sourceCidFormField                                     ."\n"
-        .    $sourceGidFormField                                     ."\n"
-        .    '<tr>'                                                  ."\n"
-        .    '<td>'                                                  ."\n"
-        .    '<label for="selectCourse">'
-        .    get_lang('Course')
-        .    '</label> : '                                           ."\n"
-        .    '</td>'                                                 ."\n"
-        .    '<td>'                                                  ."\n"
-        .    '<select name="cidReq" id="selectCourse">'              ."\n"
-        .    implode("\n", prepare_option_tags($courseList) )        ."\n"
-        .    '</select>'                                             ."\n"
-        .    '</td>'                                                 ."\n"
-        .    '</tr>'                                                 ."\n"
-        .    '<tr>'                                                  ."\n"
-        .    '<td>'                                                  ."\n"
-        .    '</td>'                                                 ."\n"
-        .    '<td>'                                                  ."\n"
-        .    '<input type="submit" value="' . get_lang('Ok') . '" />&nbsp; '."\n"
-        .    claro_html_button(get_path('url') . '/index.php', get_lang('Cancel'))
-        .    '</td>'                                                 ."\n"
-        .    '</tr>'                                                 ."\n"
-        .    '</table>'                                              ."\n"
-        .    '</form>'                                               ."\n"
-        ;
-    }
-    else
-    {
-        // Display link to student to enrol to this course
-        $out .= '<p align="center">'           ."\n"
-        .    get_lang('If you wish to enrol on this course') . ' : '
-        .    ' <a href="' . get_path('clarolineRepositoryWeb') . 'auth/courses.php?cmd=rqReg">'
-        .    get_lang('Enrolment').'</a>' ."\n"
-        .    '</p>'          ."\n";
-    }
-
-    $claroline->display->body->appendContent($out);
-
+    
+    $template = new CoreTemplate('select_course_form.tpl.php');
+    $template->assign('formAction', $_SERVER['PHP_SELF']);
+    $template->assign('sourceUrlFormField', $sourceUrlFormField);
+    $template->assign('cidRequiredFormField', $cidRequiredFormField);
+    $template->assign('sourceCidFormField', $sourceCidFormField);
+    $template->assign('sourceGidFormField', $sourceGidFormField);
+    $template->assign('courseList', $courseList);
+    
+    $claroline->display->body->appendContent($template->render());
+    
     echo $claroline->display->render();
 }
-else // LOGIN SUCCEEDED
+// Login succeeded
+else
 {
     if(!isset($userLoggedOnCas))
         $userLoggedOnCas = false;
-
+    
     $claroline->notifier->event( 'user_login', array('data' => array('ip' => $_SERVER['REMOTE_ADDR']) ) );
-
+    
     if ( claro_is_in_a_course() && ! claro_is_course_allowed() )
     {
         $out = '';
-
+        
         if ( $_course['registrationAllowed'] )
         {
             if ( claro_is_user_authenticated() )
             {
                 // Display link to student to enrol to this course
                 $out .= '<p align="center">'           ."\n"
-                .    get_lang('Your user profile doesn\'t seem to be enrolled on this course').'<br />'
-                .    get_lang('If you wish to enrol on this course') . ' : '
-                .    ' <a href="' . get_path('clarolineRepositoryWeb') . 'auth/courses.php?cmd=rqReg&amp;keyword=' . urlencode($_course['officialCode']) . '">'
-                .    get_lang('Enrolment').'</a>' ."\n"
-                .    '</p>'          ."\n";
-
+                      . get_lang('Your user profile doesn\'t seem to be enrolled on this course').'<br />'
+                      . get_lang('If you wish to enrol on this course') . ' : '
+                      . ' <a href="' . get_path('clarolineRepositoryWeb') . 'auth/courses.php?cmd=rqReg&amp;keyword=' . urlencode($_course['officialCode']) . '">'
+                      . get_lang('Enrolment').'</a>' ."\n"
+                      . '</p>'          ."\n";
             }
             elseif ( get_conf('allowSelfReg') )
             {
                 // Display a link to anonymous to register on the platform
                 $out .= '<p align="center">' . "\n"
-                .    get_lang('Create first a user account on this platform') . ' : '
-                .    '<a href="' . get_path('clarolineRepositoryWeb') . 'auth/inscription.php">'
-                .    get_lang('Go to the account creation page')
-                .    '</a>'                                         ."\n"
-                .    '</p>'                                         ."\n"
-                ;
+                      . get_lang('Create first a user account on this platform') . ' : '
+                      . '<a href="' . get_path('clarolineRepositoryWeb') . 'auth/inscription.php">'
+                      . get_lang('Go to the account creation page')
+                      . '</a>'."\n"
+                      . '</p>'."\n";
             }
             else
             {
                 // Anonymous cannot register on the platform
-                $out .= '<p align="center">'                           ."\n"
-                . get_lang('Registration not allowed on the platform')
-                .    '</p>'                                         ."\n";
+                $out .= '<p align="center">'."\n"
+                      . get_lang('Registration not allowed on the platform')
+                      . '</p>'."\n";
             }
         }
         else
         {
         // Enrolment is not allowed for this course
-            $out .= '<p align="center">'                           ."\n"
-                . get_lang('Enrol to course not allowed');
+            $out .= '<p align="center">'."\n"
+                  . get_lang('Enrol to course not allowed');
+            
             if ($_course['email'] && $_course['titular'])
             {
                 $out .= '<br />' . get_lang('Please contact course titular(s)') . ' : ' . $_course['titular']
-                .    '<br /><small>' . get_lang('Email') . ' : <a href="mailto:' . $_course['email'] .'">' . $_course['email']. '</a>';
-
+                      . '<br /><small>' . get_lang('Email') . ' : <a href="mailto:' . $_course['email'] .'">' . $_course['email']. '</a>';
             }
-
             $out .= '</p>' . "\n";
         }
-
+        
         $claroline->display->body->appendContent($out);
-
+        
         echo $claroline->display->render();
-
     }
     elseif($userLoggedOnCas && isset($_SESSION['casCallBackUrl']))
     {
         claro_redirect($_SESSION['casCallBackUrl']);
     }
-    elseif( isset($sourceUrl) ) // send back the user to the script authentication trigger
+    // Send back the user to the script authentication trigger
+    elseif( isset($sourceUrl) )
     {
         $sourceUrl = base64_decode($sourceUrl);
         
@@ -339,7 +257,7 @@ else // LOGIN SUCCEEDED
             $sourceUrl .= ( strstr( $sourceUrl, '?' ) ? '&' : '?')
                        .  'cidReq=' . $_REQUEST['sourceCid'];
         }
-
+        
         if (isset($_REQUEST['sourceGid']))
         {
             $sourceUrl .= ( strstr( $sourceUrl, '?' ) ? '&' : '?')
@@ -350,7 +268,7 @@ else // LOGIN SUCCEEDED
         {
             $sourceUrl = 'http://'.$_SERVER['HTTP_HOST'].$sourceUrl;
         }
-
+        
         claro_redirect($sourceUrl);
     }
     elseif ( claro_is_in_a_course() )
---
+++ claroline/inc/templates/auth_form.tpl.php
@@ -0,0 +1,33 @@
+<!-- $Id$ -->
+
+<table align="center">
+  <tr>
+    <td>
+        <?php echo claro_html_tool_title(get_lang('Authentication Required')); ?>
+        <?php echo $this->dialogBox->render(); ?>
+        <form class="claroLoginForm" action ="<?php echo $this->formAction; ?>" method="post">
+            <fieldset>
+                <?php echo $this->sourceUrlFormField; ?>
+                <?php echo $this->cidRequiredFormField; ?>
+                <?php echo $this->sourceCidFormField; ?>
+                <?php echo $this->sourceGidFormField; ?>
+                
+                <label for="login"><?php echo get_lang('Username'); ?></label><br />
+                <input type="text" name="login" id="login" size="15" tabindex="1" value="<?php echo $this->defaultLoginValue; ?>"/><br />
+                <br />
+                <label for="password"><?php echo get_lang('Password'); ?></label><br />
+                <input type="password" name="password" id="password" size="15" tabindex="2" autocomplete="off"/><br />
+                <br />
+                <input type="submit" value="<?php echo get_lang('Ok'); ?>" />&nbsp;
+                <?php echo claro_html_button(get_path('clarolineRepositoryWeb'), get_lang('Cancel')); ?>
+            </fieldset>
+        </form>
+        
+        <?php if (get_conf('claro_CasEnabled', false)) : ?>
+        <a href="login.php?<?php echo ($this->sourceUrl ? 'sourceUrl='.urlencode($this->sourceUrl) : ''); ?>&authModeReq=CAS">
+            <?php echo ('' != trim(get_conf('claro_CasLoginString', '')) ? get_conf('claro_CasLoginString') : get_lang('Login')); ?>
+        </a>
+        <?php endif; ?>
+    </td>
+  </tr>
+</table>
