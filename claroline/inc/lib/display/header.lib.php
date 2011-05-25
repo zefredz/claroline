79de817d45f8f65b386445fe2874f282d7692545
ffervaille <ffervaille@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2011-05-25 15:27:27 +0000

    merging of rev13209

--- claroline/inc/lib/display/header.lib.php
+++ claroline/inc/lib/display/header.lib.php
@@ -38,6 +38,11 @@
             $this->_htmlXtraHeaders = array();
             $this->_httpXtraHeaders = array();
             $this->_toolName = '';
+            
+            if ( claro_debug_mode() )
+            {
+                $this->addInlineJavascript("var claro_debug_mode = true;");
+            }
         }
 
         public static function getInstance()
