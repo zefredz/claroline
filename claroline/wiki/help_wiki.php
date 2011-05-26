f94d188cb9a5bb3160ef71ff9adf2f42a1d12d8e
abourguignon <abourguignon@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2011-05-26 16:57:26 +0000

    New tool title for WIKI module.

--- claroline/wiki/help_wiki.php
+++ claroline/wiki/help_wiki.php
@@ -1,61 +1,59 @@
 <?php // $Id$
-    // vim: expandtab sw=4 ts=4 sts=4:
-    
-    /**
-     * CLAROLINE
-     *
-     * @version 1.8 $Revision$
-     *
+
+// vim: expandtab sw=4 ts=4 sts=4:
+
+/**
+ * CLAROLINE
+ *
+ * @version     1.8 $Revision$
  * @copyright   (c) 2001-2011, Universite catholique de Louvain (UCL)
-     *
-     * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
-     *
-     * @author Frederic Minne <zefredz@gmail.com>
-     *
-     * @package Wiki
-     */
-    
-    require '../inc/claro_init_global.inc.php';
+ * @license     http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
+ *              This program is under the terms of the GENERAL PUBLIC LICENSE (GPL)
+ *              as published by the FREE SOFTWARE FOUNDATION. The GPL is available
+ *              through the world-wide-web at http://www.gnu.org/copyleft/gpl.html
+ * @author      Frederic Minne <zefredz@gmail.com>
+ * @package     Wiki
+ */
 
-    $nameTools = get_lang("Wiki");
-    $hide_banner=TRUE;
-    
-    $htmlHeadXtra[] =
-        '<style type="text/css">
-            dt{font-weight:bold;margin-top:5px;}
-        </style>';
-    
-    $out = '';
-    
-    $help = ( isset( $_REQUEST['help'] ) ) ? $_REQUEST['help'] : 'syntax';
-    
-    //$out .= '<center><a href="#" onclick="window.close()">'.get_lang("Close window").'</a></center>' . "\n";
-    
-    switch( $help )
+require '../inc/claro_init_global.inc.php';
+
+$nameTools = get_lang("Wiki");
+$hide_banner=TRUE;
+
+$htmlHeadXtra[] =
+    '<style type="text/css">
+        dt{font-weight:bold;margin-top:5px;}
+    </style>';
+
+$out = '';
+
+$help = ( isset( $_REQUEST['help'] ) ) ? $_REQUEST['help'] : 'syntax';
+
+//$out .= '<center><a href="#" onclick="window.close()">'.get_lang("Close window").'</a></center>' . "\n";
+
+switch( $help )
+{
+    case 'syntax':
     {
-        case 'syntax':
-        {
-            $out .= get_block('blockWikiHelpSyntaxContent');
-            break;
-        }
-        case 'admin':
-        {
-            $out .= get_block('blockWikiHelpAdminContent');
-            break;
-        }
-        default:
-        {
-            $out .= '<center><h1>'.get_lang('Wrong parameters').'</h1></center>';
-        }
+        $out .= get_block('blockWikiHelpSyntaxContent');
+        break;
     }
-    
-    //$out .= '<center><a href="#" onclick="window.close()">'.get_lang("Close window").'</a></center>' . "\n";
-    
-    $hide_footer = true;
-    
-    $claroline->setDisplayType(Claroline::POPUP);
-    $claroline->display->body->appendContent($out);
+    case 'admin':
+    {
+        $out .= get_block('blockWikiHelpAdminContent');
+        break;
+    }
+    default:
+    {
+        $out .= '<center><h1>'.get_lang('Wrong parameters').'</h1></center>';
+    }
+}
 
-    echo $claroline->display->render();
-    
-?>
+//$out .= '<center><a href="#" onclick="window.close()">'.get_lang("Close window").'</a></center>' . "\n";
+
+$hide_footer = true;
+
+$claroline->setDisplayType(Claroline::POPUP);
+$claroline->display->body->appendContent($out);
+
+echo $claroline->display->render();
