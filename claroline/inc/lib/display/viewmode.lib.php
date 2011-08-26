18d8a4d867ed4cc8104cfd69221619209ad7dc9e
abourguignon <abourguignon@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2011-08-26 07:56:47 +0000

    libraries must be included with require_once instead of require

--- claroline/inc/lib/display/viewmode.lib.php
+++ claroline/inc/lib/display/viewmode.lib.php
@@ -15,7 +15,7 @@
  * @since       1.9
  */
 
-require get_path('incRepositorySys').'/lib/course_user.lib.php';
+require_once get_path('incRepositorySys').'/lib/course_user.lib.php';
 
 class ClaroViewMode implements Display
 {
