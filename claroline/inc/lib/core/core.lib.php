a42aaab8dfd70253b9102861e1a86924ac949e98
Frédéric Minne <zefredz@users.noreply.github.com>
2011-09-01 11:27:28 +0000

    remove uses() function (deprecated since Claroline 1.9.0)

--- claroline/inc/lib/core/core.lib.php
+++ claroline/inc/lib/core/core.lib.php
@@ -36,98 +36,6 @@ function protect_against_file_inclusion( $path )
 }
 
 /**
- * Imports the PHP libraries given in argument with path relative to
- * includePath or module lib/ directory. .php extension added automaticaly
- * @param   list of libraries
- * @return  array of not found libraries + generate an error in debug mode
- * @deprecated since 1.9.0, use FromKernel::uses() and From::module()->uses() instead
- */
-function uses()
-{
-    $args = func_get_args();
-    $notFound = array();
-    
-    defined('INCLUDES') || define ( 'INCLUDES', dirname(__FILE__) . '/..');
-    
-    foreach ( $args as $lib )
-    {
-        if ( basename( $lib ) == '*' )
-        {
-            uses( 'utils/finder.lib' );
-            
-            $dir = dirname( $lib );
-            
-            $kernelPath = INCLUDES . '/' . $dir;
-            $localPath = get_module_path(get_current_module_label()) . '/lib/' . $dir;
-            
-            if ( file_exists( $kernelPath )
-                && is_dir( $kernelPath )
-                && is_readable( $kernelPath )
-                && $dir != '.'  // do not allow loading all files in inc/lib !!!
-            )
-            {
-                $path = $kernelPath;
-            }
-            elseif ( file_exists( $localPath )
-                && is_dir( $localPath )
-                && is_readable( $localPath )
-            )
-            {
-                $path = $localPath;
-            }
-            else
-            {
-                if ( claro_debug_mode() )
-                {
-                    throw new Exception( "Cannot load libraries from {$dir}" );
-                }
-                
-                $notFound[] = $lib;
-                
-                continue;
-            }
-            
-            $finder = new Claro_FileFinder_Extension( $path, '.php', false );
-            
-            foreach ( $finder as $file )
-            {
-                require_once $file->getPathname();
-            }
-        }
-        else
-        {
-            if ( substr($lib, -4) !== '.php' ) $lib .= '.php';
-            
-            $lib = protect_against_file_inclusion( $lib );
-            
-            $kernelPath = INCLUDES . '/' . $lib;
-            $localPath = get_module_path(Claroline::getInstance()->currentModuleLabel()) . '/lib/' . $lib;
-            
-            if ( file_exists( $localPath ) )
-            {
-                require_once $localPath;
-            }
-            elseif ( file_exists( $kernelPath ) )
-            {
-                require_once $kernelPath;
-            }
-            else
-            {
-                // error not found
-                if ( claro_debug_mode() )
-                {
-                    throw new Exception( "Lib not found $lib" );
-                }
-                
-                $notFound[] = $lib;
-            }
-        }
-    }
-    
-    return $notFound;
-}
-
-/**
  * Kernel library loader
  */
 class FromKernel
@@ -194,7 +102,7 @@ class From
         {
             if ( basename( $lib ) == '*' )
             {
-                uses( 'utils/finder.lib' );
+                require_once dirname(__FILE__) . '/../utils/finder.lib.php';
                 
                 $localPath = get_module_path( $this->moduleLabel ) . '/lib/' . dirname( $lib );
                 
