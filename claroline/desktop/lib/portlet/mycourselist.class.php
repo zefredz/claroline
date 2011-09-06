c6b7aa7cee87fc83394610a2f7998e7539b964a2
abourguignon <abourguignon@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2011-09-06 15:21:53 +0000

    Remove call to course_list.js (deleted several revisions ago).

--- claroline/desktop/lib/portlet/mycourselist.class.php
+++ claroline/desktop/lib/portlet/mycourselist.class.php
@@ -27,8 +27,6 @@ class MyCourseList extends UserDesktopPortlet
     {
         global $platformLanguage;
         
-        JavascriptLoader::getInstance()->load('course_list');
-        
         $out = '';
         
         // Last user action
