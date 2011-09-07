d014dd11851b8094a6d80dc26881d4fe2b0bac88
abourguignon <abourguignon@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2011-09-07 13:26:56 +0000

    Put a part of the wiki form in a <dl> markup.

--- claroline/wiki/wiki.php
+++ claroline/wiki/wiki.php
@@ -5,7 +5,7 @@
 /**
  * CLAROLINE
  *
- * @version     1.8 $Revision$
+ * @version     $Revision$
  * @copyright   (c) 2001-2011, Universite catholique de Louvain (UCL)
  * @license     http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
  *              This program is under the terms of the GENERAL PUBLIC LICENSE (GPL)
