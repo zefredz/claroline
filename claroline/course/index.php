f58d7c16774a0fb832a9dbcca7c7753cb59225be
abourguignon <abourguignon@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2011-09-09 10:27:22 +0000

    Don't use get_path to require libraries.

--- claroline/course/index.php
+++ claroline/course/index.php
@@ -28,8 +28,8 @@ $portletLabel   = (isset($_REQUEST['portletLabel']) ? $_REQUEST['portletLabel'] 
 $portletClass   = (isset($portletLabel) ? ($portletLabel.'_portlet') : null);
 
 require '../inc/claro_init_global.inc.php';
-require_once get_path('incRepositorySys') . '/lib/claroCourse.class.php';
 
+require_once dirname(__FILE__) . '/../inc/lib/claroCourse.class.php';
 require_once dirname(__FILE__) . '/coursehomepage/lib/coursehomepageportlet.class.php';
 require_once dirname(__FILE__) . '/coursehomepage/lib/coursehomepageportletiterator.class.php';
 
