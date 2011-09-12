cce14c765ba527ae0ff04d4450a9f48f6dc879f3
ffervaille <ffervaille@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2011-09-12 09:11:57 +0000

    displays course session creation command for platform admin

--- claroline/inc/lib/display/course.lib.php
+++ claroline/inc/lib/display/course.lib.php
@@ -164,9 +164,10 @@ class CurrentCourseToolListBlock implements Display
                                     . get_lang('Course settings')
                                     . '</a>';
 
-        if ( !ClaroCourse::isSessionCourse($this->courseId) 
+        if ( !ClaroCourse::isSessionCourse($this->courseId)
             && claro_is_allowed_to_create_course()
-            && get_conf( 'courseSessionAllowed' ) )
+            && get_conf( 'courseSessionAllowed' )
+            || claro_is_platform_admin() )
         {
             $courseManageToolLinkList[] = '<a class="claroCmd" href="' . htmlspecialchars(Url::Contextualize( get_path('clarolineRepositoryWeb')
                                         . 'course/create.php', array('course_sourceCourseId'=>$this->courseId) )) . '">'
