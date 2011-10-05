b8587bceb7ff12381f449b44a62ef7604caa4b3d
Frédéric Minne <zefredz@users.noreply.github.com>
2011-10-05 09:10:47 +0000

    need to add nl2br in order to get the right display

--- claroline/inc/lib/user.lib.php
+++ claroline/inc/lib/user.lib.php
@@ -527,15 +527,15 @@ function profile_send_request_course_creator_status($explanation)
     '%lastname' => $_user['lastName'] ) );
 
     $requestMessage_Content =
-    get_block('blockRequestCourseManagerStatusMail',
-    array( '%time'      => claro_html_localised_date(get_locale('dateFormatLong')),
-    '%user_id'   => claro_get_current_user_id(),
-    '%firstname' => $_user['firstName'],
-    '%lastname'  => $_user['lastName'],
-    '%email'     => $_user['mail'],
-    '%comment'   => $explanation,
-    '%url'       => rtrim( get_path('rootWeb'), '/' ) . '/claroline/admin/adminprofile.php?uidToEdit=' . claro_get_current_user_id()
-    )
+        nl2br(get_block('blockRequestCourseManagerStatusMail',
+            array( '%time'      => claro_html_localised_date(get_locale('dateFormatLong')),
+            '%user_id'   => claro_get_current_user_id(),
+            '%firstname' => $_user['firstName'],
+            '%lastname'  => $_user['lastName'],
+            '%email'     => $_user['mail'],
+            '%comment'   => $explanation,
+            '%url'       => rtrim( get_path('rootWeb'), '/' ) . '/claroline/admin/adminprofile.php?uidToEdit=' . claro_get_current_user_id()
+        ))
     );
 
     $message = new MessageToSend(claro_get_current_user_id(),$requestMessage_Title,$requestMessage_Content);
@@ -572,17 +572,17 @@ function profile_send_request_revoquation($explanation,$login,$password)
     '%lastname' => $_user['lastName'] ) );
 
     $requestMessage_Content =
-    get_block('blockRequestUserRevoquationMail',
-    array('%time'      => claro_html_localised_date(get_locale('dateFormatLong')),
-    '%user_id'   => claro_get_current_user_id(),
-    '%firstname' => $_user['firstName'],
-    '%lastname'  => $_user['lastName'],
-    '%email'     => $_user['mail'],
-    '%login'     => $login,
-    '%password'  => '**********',
-    '%comment'   => nl2br($explanation),
-    '%url'       => rtrim( get_path('rootWeb'), '/' ) . '/claroline/admin/adminprofile.php?uidToEdit=' . claro_get_current_user_id()
-    )
+        nl2br(get_block('blockRequestUserRevoquationMail',
+            array('%time'      => claro_html_localised_date(get_locale('dateFormatLong')),
+            '%user_id'   => claro_get_current_user_id(),
+            '%firstname' => $_user['firstName'],
+            '%lastname'  => $_user['lastName'],
+            '%email'     => $_user['mail'],
+            '%login'     => $login,
+            '%password'  => '**********',
+            '%comment'   => nl2br($explanation),
+            '%url'       => rtrim( get_path('rootWeb'), '/' ) . '/claroline/admin/adminprofile.php?uidToEdit=' . claro_get_current_user_id()
+        ))
     );
 
     $message = new MessageToSend(claro_get_current_user_id(),$requestMessage_Title,$requestMessage_Content);
