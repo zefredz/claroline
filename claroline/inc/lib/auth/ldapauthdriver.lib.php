cf3bd5f49cc38ab8b0fd7b003a4e1cff75336c81
jrm_ <jrm_@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2011-10-20 14:12:52 +0000

    replace references to unset variables userAttr and userFilter with references to corresponding object properties

--- claroline/inc/lib/auth/ldapauthdriver.lib.php
+++ claroline/inc/lib/auth/ldapauthdriver.lib.php
@@ -111,7 +111,7 @@ class ClaroLdapAuthDriver extends AbstractAuthDriver
             
             // search user
             
-            $user = $auth->getUser($this->username, $userFilter, $userAttr);
+            $user = $auth->getUser($this->username, $this->userFilter, $this->userAttr);
             
             if ( $user )
             {
