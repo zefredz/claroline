34404e926b69e1faabe9205b89c17331978db840
ffervaille <ffervaille@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2011-11-03 13:47:35 +0000

    allowed post with only images, videos, etc... in posts

--- claroline/phpbb/viewtopic.php
+++ claroline/phpbb/viewtopic.php
@@ -274,7 +274,7 @@ else
     {
         $error = false;
         //this test should be handled by a "html not empty" validator
-        if ( trim( strip_tags( $message ) ) == '' )
+        if ( trim( strip_tags( $message , '<img><audio><video><embed><object><canvas>' ) ) == '' )
         {
             $dialogBox->error( get_lang( 'You cannot post an empty message' ) );
             $error = true;
