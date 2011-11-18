e095953f7cc85cb89e09d3203a6705beca03c9d6
Frédéric Minne <zefredz@users.noreply.github.com>
2011-11-18 08:57:02 +0000

    fix broken claroimage plugin : merge modifications from trunk

--- claroline/editor/tiny_mce/tiny_mce/plugins/claroimage/backend.php
+++ claroline/editor/tiny_mce/tiny_mce/plugins/claroimage/backend.php
@@ -4,7 +4,7 @@
  *
  * $Revision$
  *
- * @copyright (c) 2001-2007 Universite catholique de Louvain (UCL)
+ * @copyright   (c) 2001-2011, Universite catholique de Louvain (UCL)
  *
  * @license http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
  *
