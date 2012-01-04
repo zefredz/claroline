afae8af5e80d6d2356be2bbc96dbb7983bc1003e
abourguignon <abourguignon@1bf556a1-2563-40ed-b4eb-046cfb1bdec1>
2012-01-04 18:38:16 +0000

    Convention: remove capital letter form file's name (and adapt links).

--- claroline/admin/index.php
+++ claroline/admin/index.php
@@ -100,7 +100,7 @@ if (file_exists(dirname(__FILE__) . '/maintenance/checkmails.php'))
 }
 
 // Claroline's administration menu
-$menu['AdminClaroline'][] = '<a href="registerCampus.php">'.get_lang('Register my campus').'</a>';
+$menu['AdminClaroline'][] = '<a href="registercampus.php">'.get_lang('Register my campus').'</a>';
 $menu['AdminClaroline'][] = '<a href="http://forum.claroline.net/">'.get_lang('Support forum').'</a>';
 $menu['AdminClaroline'][] = '<a href="clarolinenews.php">'.get_lang('Claroline.net news').'</a>';
 
