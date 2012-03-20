db60170374241b61542f46ad179d8ff4930f31be
Frédéric Minne <zefredz@users.noreply.github.com>
2012-03-20 14:59:38 +0000

    Contextualize URLs

--- claroline/document/rqmkhtml.php
+++ claroline/document/rqmkhtml.php
@@ -66,15 +66,16 @@ CREATE DOCUMENT
 
 if ($cmd ==  'rqMkHtml' )
 {
-    $out .= '<form action="' . htmlspecialchars(Url::Contextualize(get_module_entry_url('CLDOC'))) .'" method="post">' . "\n"
-    .   '<input type="hidden" name="cmd" value="exMkHtml" />' . "\n"
-    .   '<input type="hidden" name="cwd" value="' . htmlspecialchars(strip_tags($cwd)) . '" />' . "\n"
-    .   '<p>' . "\n"
-    .   '<b>' . get_lang('Document name') . '&nbsp;: </b><br />' . "\n"
-    .   '<input type="text" name="fileName" size="80" />' . "\n"
-    .   '</p>' . "\n"
-    .   '<p>' . "\n"
-    .   '<b>' . get_lang('Document content') . '&nbsp;: </b>' . "\n"
+    $out .= '<form action="' . htmlspecialchars(get_module_url('CLDOC')) .'" method="post">' . "\n"
+    . claro_form_relay_context() . "\n"
+    . '<input type="hidden" name="cmd" value="exMkHtml" />' . "\n"
+    . '<input type="hidden" name="cwd" value="' . htmlspecialchars(strip_tags($cwd)) . '" />' . "\n"
+    . '<p>' . "\n"
+    . '<b>' . get_lang('Document name') . '&nbsp;: </b><br />' . "\n"
+    . '<input type="text" name="fileName" size="80" />' . "\n"
+    . '</p>' . "\n"
+    . '<p>' . "\n"
+    . '<b>' . get_lang('Document content') . '&nbsp;: </b>' . "\n"
     ;
     if (!empty($_REQUEST['htmlContent'])) $content = $_REQUEST['htmlContent']; else $content = "";
 
@@ -84,11 +85,11 @@ if ($cmd ==  'rqMkHtml' )
     // get to the editor because of an error at creation
     // (eg forgot to give a file name)
     $out .= '</p>'  . "\n"
-    .   '<p>' . "\n"
-    .   '<input type="submit" value="'. get_lang('Ok') .'" />&nbsp;'
-    .   claro_html_button(htmlspecialchars(Url::Contextualize('./document.php?cmd=exChDir&amp;file='.strip_tags($cwd))), get_lang('Cancel'))
-    .   '</p>' . "\n"
-    .   '</form>' . "\n"
+    . '<p>' . "\n"
+    . '<input type="submit" value="'. get_lang('Ok') .'" />&nbsp;'
+    . claro_html_button(htmlspecialchars(Url::Contextualize('./document.php?cmd=exChDir&amp;file='.strip_tags($cwd))), get_lang('Cancel'))
+    . '</p>' . "\n"
+    . '</form>' . "\n"
     ;
 }
 elseif($cmd == "rqEditHtml" && !empty($file) )
@@ -104,27 +105,28 @@ elseif($cmd == "rqEditHtml" && !empty($file) )
 
 
     $fileContent = get_html_body_content($fileContent);
-    $out .= '<form action="' . htmlspecialchars(Url::Contextualize(get_module_entry_url('CLDOC'))) .'" method="post">' . "\n"
-    .   '<input type="hidden" name="cmd" value="exEditHtml" />' . "\n"
-    .   '<input type="hidden" name="file" value="' . htmlspecialchars(base64_encode($file)) .'" />' . "\n"
-    .   '<b>'. get_lang('Document name') .' : </b><br />' . "\n"
+    $out .= '<form action="' . htmlspecialchars(get_module_entry_url('CLDOC')) .'" method="post">' . "\n"
+    . claro_form_relay_context() . "\n"
+    . '<input type="hidden" name="cmd" value="exEditHtml" />' . "\n"
+    . '<input type="hidden" name="file" value="' . htmlspecialchars(base64_encode($file)) .'" />' . "\n"
+    . '<b>'. get_lang('Document name') .' : </b><br />' . "\n"
     . $file . "\n"
-    .   '</p>' . "\n"
-    .   '<p>' . "\n"
-    .   '<b>'. get_lang('Document content') .' : </b>' . "\n"
-    .   claro_html_textarea_editor('htmlContent', $fileContent ) . "\n"
-    .   '</p>'
-    .   '<p>'
-    .   '<input type="submit" value="' . get_lang('Ok') .'" />&nbsp;' . "\n"
-    .   claro_html_button(htmlspecialchars(Url::Contextualize('./document.php?cmd=rqEdit&file='.base64_encode($file))), get_lang('Cancel')) . "\n"
-    .   '</p>' . "\n"
-    .   '</form>' . "\n"
+    . '</p>' . "\n"
+    . '<p>' . "\n"
+    . '<b>'. get_lang('Document content') .' : </b>' . "\n"
+    . claro_html_textarea_editor('htmlContent', $fileContent ) . "\n"
+    . '</p>'
+    . '<p>'
+    . '<input type="submit" value="' . get_lang('Ok') .'" />&nbsp;' . "\n"
+    . claro_html_button(htmlspecialchars(Url::Contextualize('./document.php?cmd=rqEdit&file='.base64_encode($file))), get_lang('Cancel')) . "\n"
+    . '</p>' . "\n"
+    . '</form>' . "\n"
     ;
     
 }
 
 $out .= '<br />' . "\n"
-.   '<br />' . "\n"
+. '<br />' . "\n"
 ;
 
 $claroline->display->body->appendContent($out);
