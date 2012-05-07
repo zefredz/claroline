9be19079d02a47720745d819bbea172319535781
Frédéric Minne <zefredz@users.noreply.github.com>
2012-05-07 07:41:25 +0000

    fix E_STRICT error in claro_text_zone : all methods are now static

--- claroline/inc/lib/textzone.lib.php
+++ claroline/inc/lib/textzone.lib.php
@@ -1,21 +1,16 @@
-<?php // $Id$
+<?php
 
-if ( count( get_included_files() ) == 1 )
-{
-    die( 'The file ' . basename(__FILE__) . ' cannot be accessed directly, use include instead' );
-}
+// $Id$
 
 /**
  *
- * @version     1.9 $Revision$
- * @copyright   (c) 2001-2011, Universite catholique de Louvain (UCL)
+ * @version     1.11 $Revision$
+ * @copyright   (c) 2001-2012, Universite catholique de Louvain (UCL)
  * @license     http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
- * @author      Christophe GeschÈ <moosh@claroline.net>
+ * @author      Christophe Gesch√© <moosh@claroline.net>
  * @since       1.8.1
  * @package     KERNEL
  */
-
-
 class claro_text_zone
 {
 
@@ -27,47 +22,61 @@ class claro_text_zone
      * @param array $right specify an array of right to specify the file
      * @return file path
      */
-    function get_textzone_file_path($key, $context = null, $right= null)
+    public static function get_textzone_file_path ( $key, $context = null, $right = null )
     {
         $textZoneFile = null;
         $key .= '.';
-        if(!is_null($right) && is_array($right))
+
+        if ( !is_null ( $right ) && is_array ( $right ) )
         {
-            foreach ($right as $context => $rightInContext)
+            foreach ( $right as $context => $rightInContext )
             {
-                if(is_array($rightInContext))
+                if ( is_array ( $rightInContext ) )
                 {
-                    $key .= $context.'_';
-                    foreach ($rightInContext as $rightName => $rightValue)
-        {
-                        if(is_bool($rightValue))
+                    $key .= $context . '_';
+                    
+                    foreach ( $rightInContext as $rightName => $rightValue )
+                    {
+                        if ( is_bool ( $rightValue ) )
                         {
-                            $key .= ($rightValue) ? $rightName :'not_' .$rightName;
+                            $key .= ($rightValue) ? $rightName : 'not_' . $rightName;
                         }
                         else
                         {
-                            $key .= $rightName.'_'. $rightValue;
+                            $key .= $rightName . '_' . $rightValue;
                         }
+                        
                         $key .= '.';
-
                     }
-
                 }
             }
         }
-        if (is_array($context) && array_key_exists(CLARO_CONTEXT_COURSE,$context))
+        
+        if ( is_array ( $context ) && array_key_exists ( CLARO_CONTEXT_COURSE, $context ) )
         {
-            if (is_array($context) && array_key_exists(CLARO_CONTEXT_GROUP,$context))
+            if ( is_array ( $context ) && array_key_exists ( CLARO_CONTEXT_GROUP, $context ) )
             {
                 // TODO  use : claro_get_data_path
-                $textZoneFile =  get_conf('coursesRepositorySys') . claro_get_course_group_path($context) . '/textzone/' . $key . 'inc.html';
+                $textZoneFile = get_conf ( 'coursesRepositorySys' ) 
+                    . claro_get_course_group_path ( $context ) 
+                    . '/textzone/' . $key . 'inc.html'
+                    ;
             }
             else
             {
-                $textZoneFile =  get_conf('coursesRepositorySys') . claro_get_course_path($context[CLARO_CONTEXT_COURSE]) . '/textzone/' . $key . 'inc.html';
+                $textZoneFile = get_conf ( 'coursesRepositorySys' ) 
+                    . claro_get_course_path ( $context[ CLARO_CONTEXT_COURSE ] ) 
+                    . '/textzone/' . $key . 'inc.html'
+                    ;
             }
         }
-        if(is_null($textZoneFile)) $textZoneFile = get_path('rootSys') . 'platform/textzone/' . $key . 'inc.html';
+        
+        if ( is_null ( $textZoneFile ) )
+        {
+            $textZoneFile = get_path ( 'rootSys' ) 
+                . 'platform/textzone/' . $key . 'inc.html'
+                ;
+        }
 
         return $textZoneFile;
     }
@@ -79,14 +88,20 @@ class claro_text_zone
      * @param array $context
      * @return string : html content
      */
-
-    function get_content($key, $context=null, $right=null)
+    public static function get_content ( $key, $context = null, $right = null )
     {
-        $textZoneFile = claro_text_zone::get_textzone_file_path($key, $context,$right);
+        $textZoneFile = claro_text_zone::get_textzone_file_path ( $key, $context, $right );
 
-        if(file_exists($textZoneFile)) $content = file_get_contents($textZoneFile);
-        else                           $content = '' ;
-        ;
+        if ( file_exists ( $textZoneFile ) )
+        {
+            $content = file_get_contents ( $textZoneFile );
+        }
+        else
+        {
+            $content = '';
+        }
+        
         return $content;
     }
+
 }
