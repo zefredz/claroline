733d9d24d80ccf78c002cfbc9fbb3037d4e7e306
Frédéric Minne <zefredz@users.noreply.github.com>
2012-05-10 10:50:19 +0000

    update missing translations

--- claroline/lang/french/missing.lang.php
+++ claroline/lang/french/missing.lang.php
@@ -1,9 +1,27 @@
 <?php 
 
+$_lang['Allow students to reset path'] = 'Allow students to reset path';
+$_lang['An error occured while resetting learning path'] = 'An error occured while resetting learning path';
+$_lang['An error occured while resetting module'] = 'An error occured while resetting module';
+$_lang['An error occurred. Learning Path import failed.'] = 'An error occurred. Learning Path import failed.';
 $_lang['Assignment end date delay'] = 'Assignment end date delay';
 $_lang['Choose "No" if you don\'t use any deprecated external auth driver. (If you are using the old PEAR-based LDAP authentication, you should replace it with the new ldap.conf.php driver found in inc/conf/extauth and set this option to "No" afterwards).'] = 'Choose "No" if you don\'t use any deprecated external auth driver. (If you are using the old PEAR-based LDAP authentication, you should replace it with the new ldap.conf.php driver found in inc/conf/extauth and set this option to "No" afterwards).';
 $_lang['Choose "No" if you don\'t use any external authentication drivers or if you prefer to set the list of authentication drivers to load in platform/extauth/drivers.list (one config file name by line).'] = 'Choose "No" if you don\'t use any external authentication drivers or if you prefer to set the list of authentication drivers to load in platform/extauth/drivers.list (one config file name by line).';
+$_lang['Consult'] = 'Consult';
+$_lang['Currently viewing module of'] = 'Currently viewing module of';
+$_lang['Display grouped by class'] = 'Display grouped by class';
+$_lang['Display ungrouped'] = 'Display ungrouped';
+$_lang['Do you really want to reset module'] = 'Do you really want to reset module';
+$_lang['Do you really want to reset the learning path of'] = 'Do you really want to reset the learning path of';
+$_lang['Enroll class'] = 'Enroll class';
+$_lang['No class'] = 'No class';
+$_lang['No question to display'] = 'No question to display';
 $_lang['Once a user logs to the Claroline platform a cookie is sent to the user browser if the authentication process succeeds. The cookie value is also stored in a internal table of the Claroline platform for a certain time. If requested, the Claroline SSO server provides a way to retrieve the user parameters from another server on the internet on the base of this cookie value.'] = 'Once a user logs to the Claroline platform a cookie is sent to the user browser if the authentication process succeeds. The cookie value is also stored in a internal table of the Claroline platform for a certain time. If requested, the Claroline SSO server provides a way to retrieve the user parameters from another server on the internet on the base of this cookie value.';
+$_lang['Path to the temporary folder used to generate the zip archive'] = 'Path to the temporary folder used to generate the zip archive';
+$_lang['Set server timezone'] = 'Set server timezone';
+$_lang['Set this value to use another timezone than the one defined by PHP. See http://www.php.net/manual/en/timezones.php for a list of accepted timezones (for instance Europe/Brussels)'] = 'Set this value to use another timezone than the one defined by PHP. See http://www.php.net/manual/en/timezones.php for a list of accepted timezones (for instance Europe/Brussels)';
+$_lang['Set to Yes to allow students to reset their progression in learning pathes'] = 'Set to Yes to allow students to reset their progression in learning pathes';
+$_lang['Show/hide %nbr more commands'] = 'Show/hide %nbr more commands';
 $_lang['The domain that the cookie is available.  To make the cookie available on all subdomains of example.com, you\'d set it to ".example.com". The . is not required but makes it compatible with more browsers. Setting it to www.example.com  will make the cookie only available in the www  subdomain.'] = 'The domain that the cookie is available.  To make the cookie available on all subdomains of example.com, you\'d set it to ".example.com". The . is not required but makes it compatible with more browsers. Setting it to www.example.com  will make the cookie only available in the www  subdomain.';
 $_lang['The path on the server in which the cookie will be available on.  If set to "/", the cookie will be available within the entire domain. If set to "/foo/", the cookie will only be available within the /foo/ directory and all sub-directories such as /foo/bar/ of domain. The default value is the current directory that the cookie is being set in.'] = 'The path on the server in which the cookie will be available on.  If set to "/", the cookie will be available within the entire domain. If set to "/foo/", the cookie will only be available within the /foo/ directory and all sub-directories such as /foo/bar/ of domain. The default value is the current directory that the cookie is being set in.';
 $_lang['This  prefix is added to each course table name. It\'s usefull in single database mode as it groups all course tables together.'] = 'This  prefix is added to each course table name. It\'s usefull in single database mode as it groups all course tables together.';
@@ -23,5 +41,9 @@ db glu is use between these two name.
 In multi db mode, IT MUST be a dot.
 In single db mode, IT CAN\'T be a dot.
 ';
+$_lang['Used when downloading the assignements. Leave empty to use the default one (which is located in courses/<COURSEID>/work/)'] = 'Used when downloading the assignements. Leave empty to use the default one (which is located in courses/<COURSEID>/work/)';
+$_lang['User tracking'] = 'User tracking';
+$_lang['class'] = 'class';
 $_lang['filename for one file module inclusion'] = 'filename for one file module inclusion';
-
+$_lang['warning : Installation cannot find the description of the learning path and has set a default comment. You should change it'] = 'warning : Installation cannot find the description of the learning path and has set a default comment. You should change it';
+$_lang['warning : Installation cannot find the name of the learning path and has set a default name. You should change it.'] = 'warning : Installation cannot find the name of the learning path and has set a default name. You should change it.';
