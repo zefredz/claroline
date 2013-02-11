dfe8a61f85404c72004397330acd0e34dab512cf
Frédéric Minne <zefredz@users.noreply.github.com>
2013-02-11 14:09:40 +0000

    CLDOC : fix issue when downloading folder as a zip : invisible files were not ignored

--- claroline/inc/lib/fileManage.lib.php
+++ claroline/inc/lib/fileManage.lib.php
@@ -557,7 +557,7 @@ function claro_search_file($searchPattern             , $baseDirPath,
             $filePath = $baseDirPath.'/'.$fileName;
 
             if (   $fileName == '.' || $fileName == '..'
-                || in_array($filePath, $excludedPathList ) )
+                || in_array('/'.ltrim($filePath,'/'), $excludedPathList ) )
             {
                 continue;
             }
@@ -648,7 +648,7 @@ function getInvisibleDocumentList ( $baseWorkDir, $cidReq = null )
 
     for( $i=0; $i < count($documentList); $i++ )
     {
-        $documentList[$i] = $baseWorkDir.$documentList[$i];
+        $documentList[$i] = '/' . ltrim($baseWorkDir.$documentList[$i],'/');
     }
 
     return $documentList ;
