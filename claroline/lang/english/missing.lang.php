5c85ccc8d1b6c5f9b5016f883f036c149ecea1d5
Frédéric Minne <zefredz@users.noreply.github.com>
2013-04-29 07:37:07 +0000

    L10N : add Czech translation by honzakrivohlavy  (89% complete) and regenerate missing language files

--- claroline/lang/english/missing.lang.php
+++ claroline/lang/english/missing.lang.php
@@ -1,4 +1,15 @@
 <?php 
 
+$_lang['Confirm your subscription as tutor of the group &quot;<b>%group_name</b>&quot;'] = 'Confirm your subscription as tutor of the group &quot;<b>%group_name</b>&quot;';
+$_lang['Confirm your unsubscription as tutor from the group &quot;<b>%group_name</b>&quot;'] = 'Confirm your unsubscription as tutor from the group &quot;<b>%group_name</b>&quot;';
+$_lang['Message displayed in the internal messaging'] = 'Message displayed in the internal messaging';
 $_lang['Missing ressource'] = 'Missing ressource';
 $_lang['Page opened in new window or tab. <a target="_blank" href="%url">Click here if it\'s not the case.</a>'] = 'Page opened in new window or tab. <a target="_blank" href="%url">Click here if it\'s not the case.</a>';
+$_lang['Register me as tutor of this group'] = 'Register me as tutor of this group';
+$_lang['Registration as tutor'] = 'Registration as tutor';
+$_lang['Unregister me as the tutor of this group'] = 'Unregister me as the tutor of this group';
+$_lang['You are not tutor of the group anymore.'] = 'You are not tutor of the group anymore.';
+$_lang['You are now the tutor of this group.'] = 'You are now the tutor of this group.';
+$_lang['cancel'] = 'cancel';
+$_lang['textzone_messaging_top'] = 'textzone_messaging_top';
+$_lang['unregister'] = 'unregister';
