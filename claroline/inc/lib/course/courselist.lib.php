7d96940e6eb83e4a4d4646ada6b3c4fc746c577e
Frédéric Minne <zefredz@users.noreply.github.com>
2014-03-26 15:07:07 +0000

    kernel: phpdoc

--- claroline/inc/lib/course/courselist.lib.php
+++ claroline/inc/lib/course/courselist.lib.php
@@ -3,7 +3,7 @@
 /**
  * CLAROLINE
  *
- * @version     $Revision$
+ * @version     Claroline 1.12 $Revision$
  * @copyright   (c) 2001-2014, Universite catholique de Louvain (UCL)
  * @license     http://www.gnu.org/copyleft/gpl.html (GPL) GENERAL PUBLIC LICENSE
  * @package     CLCOURSELIST
