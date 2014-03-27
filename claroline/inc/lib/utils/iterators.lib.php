ae7274fa0e12208758f45c40100de462b94bbbe4
Frédéric Minne <zefredz@users.noreply.github.com>
2014-03-27 10:08:24 +0000

    kernel: phpdoc + add valuelist_to_set utility function

--- claroline/inc/lib/utils/iterators.lib.php
+++ claroline/inc/lib/utils/iterators.lib.php
@@ -5,7 +5,7 @@
 /**
  * Iterator classes
  *
- * @version     Claroline 1.11 $Revision$
+ * @version     Claroline 1.12 $Revision$
  * @copyright   (c) 2001-2014, Universite catholique de Louvain (UCL)
  * @author      Claroline Team <info@claroline.net>
  * @author      Frederic Minne <zefredz@claroline.net>
@@ -15,8 +15,34 @@
  * @since       Claroline 1.11
  */
 
+/**
+ * Convert an array or iterator of [ value1, value2, ... ] to a set 
+ * [ value1 => something, value2 => something, ... ]
+ * @param mixed $iterator array or Iterator of string or int values
+ * @param bool $useRowAsValue use the row itself as both the key and the value in the set
+ * @param mixed $setValue value to use if $useRowAsValue is set to false
+ * @return type
+ */
+function valuelist_to_set( $iterator, $useRowAsValue = true, $setValue = true )
+{
+    $set = array();
+    
+    foreach ( $iterator as $row )
+    {
+        $set[$row] = $useRowAsValue ? $rows : $setValue;
+    }
+    
+    return $set;
+}
+
+/**
+ * Define a countable iterator interface
+ */
 interface CountableIterator extends Countable, Iterator{};
 
+/**
+ * Define a countable and seekable iterator interface
+ */
 interface CountableSeekableIterator extends CountableIterator, SeekableIterator{};
 
 
@@ -43,27 +69,42 @@ abstract class RowToObjectArrayIterator implements CountableIterator
     $this->maxIndex = count( $array );
     $this->keys = array_keys( $array );
   }
-
+  
+  /**
+   * @see Iterator
+   */
   public function key()
   {
     return $this->keys[$this->currentIndex];
   }
-
+  
+  /**
+   * @see Iterator
+   */
   public function next()
   {
     ++$this->currentIndex;
   }
-
+  
+  /**
+   * @see Iterator
+   */
   public function rewind()
   {
     $this->currentIndex = 0;
   }
-
+  
+  /**
+   * @see Iterator
+   */
   public function valid()
   {
     return ( isset($this->keys[$this->currentIndex]) );
   }
   
+  /**
+   * @see Countable
+   */
   public function count()
   {
       return count($this->collection);
@@ -88,26 +129,41 @@ abstract class RowToObjectIteratorIterator implements CountableIterator
         $this->internalIterator = $internalIterator;
     }
     
+    /**
+     * @see Iterator
+     */
     public function next ()
     {
         return $this->internalIterator->next();
     }
     
+    /**
+     * @see Iterator
+     */
     public function key ()
     {
         return $this->internalIterator->key();
     }
     
+    /**
+     * @see Iterator
+     */
     public function valid ()
     {
         return $this->internalIterator->valid();
     }
     
+    /**
+     * @see Iterator
+     */
     public function rewind ()
     {
         return $this->internalIterator->rewind();
     }
     
+    /**
+     * @see Countable
+     */
     public function count ()
     {
         return count( $this->internalIterator );
